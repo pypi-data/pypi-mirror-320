7bcd57ea51a6958e7cb886e92099e3741876cbdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:50 +0000

    Add package cortecs-py/0.1.0/cortecs_py-0.1.0.tar.gz

---
+++ packages/cortecs-py/cortecs_py-0.1.0.tar.gz/cortecs_py-0.1.0/cortecs_py/integrations/crewai/__init__.py
@@ -0,0 +1,10 @@
+from cortecs_py.integrations.crewai.dedicated_crew import DedicatedCrew
+from cortecs_py.integrations.crewai.dedicated_crew_base import DedicatedCrewBase
+
+__all__ = [
+    "DedicatedCrew",
+    "DedicatedCrewBase"
+]
+
+
+
