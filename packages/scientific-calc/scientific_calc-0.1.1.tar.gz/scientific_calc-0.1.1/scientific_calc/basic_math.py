7b6ed2dcab282d1a98e8151e738bb29cafce594a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:02 +0000

    Add package scientific-calc/0.1.1/scientific_calc-0.1.1.tar.gz

---
+++ packages/scientific-calc/scientific_calc-0.1.1.tar.gz/scientific_calc-0.1.1/scientific_calc/basic_math.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    result = a*b
+    return result
+
+def mul(a, b):
+    result = a * b
+    return result
+
+def sub(a, b):
+    result = a - b
+    return result
+
+def div(a, b):
+    result = a / b
+    return result
