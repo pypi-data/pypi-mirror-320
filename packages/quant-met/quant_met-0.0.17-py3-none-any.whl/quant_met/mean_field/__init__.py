feff4f598049280c1b165b47e08ce03b666cfbd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:03:52 +0000

    Add package quant-met/0.0.17/quant_met-0.0.17-py3-none-any.whl

---
+++ packages/quant-met/quant_met-0.0.17-py3-none-any.whl/quant_met/mean_field/__init__.py
@@ -0,0 +1,43 @@
+# SPDX-FileCopyrightText: 2024 Tjark Sievers
+#
+# SPDX-License-Identifier: MIT
+
+"""
+Mean field
+==========
+
+Submodules
+----------
+
+.. autosummary::
+    :toctree: generated/
+
+    hamiltonians
+
+
+Functions
+---------
+
+.. autosummary::
+   :toctree: generated/
+
+   superfluid_weight
+   quantum_metric
+   self_consistency_loop
+   search_crit_temp
+"""  # noqa: D205, D400
+
+from quant_met.mean_field import hamiltonians
+
+from .quantum_metric import quantum_metric
+from .search_crit_temp import search_crit_temp
+from .self_consistency import self_consistency_loop
+from .superfluid_weight import superfluid_weight
+
+__all__ = [
+    "hamiltonians",
+    "quantum_metric",
+    "search_crit_temp",
+    "self_consistency_loop",
+    "superfluid_weight",
+]
