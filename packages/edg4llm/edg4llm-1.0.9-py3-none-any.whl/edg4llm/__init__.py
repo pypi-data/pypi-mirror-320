ca613897c7512ac4b41b78817e1db04c889fbb42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:08 +0000

    Add package edg4llm/1.0.9/edg4llm-1.0.9-py3-none-any.whl

---
+++ packages/edg4llm/edg4llm-1.0.9-py3-none-any.whl/edg4llm/__init__.py
@@ -0,0 +1,8 @@
+from edg4llm.core.interface import EDG4LLM
+from edg4llm.post_install import display_ascii_art
+
+__all__ = ["EDG4LLM", "display_ascii_art"]
+
+__version__ = "1.0.9"
+__author__ = "Alannikos"
+__license__ = "MIT"
