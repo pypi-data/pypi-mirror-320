6fe6d554abef7118a2398a452d574e67d466f661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:01 +0000

    Add package djc-heroicons/1.0.3/djc_heroicons-1.0.3.tar.gz

---
+++ packages/djc-heroicons/djc_heroicons-1.0.3.tar.gz/djc_heroicons-1.0.3/src/djc_heroicons/apps.py
@@ -0,0 +1,14 @@
+from django.apps import AppConfig
+
+
+class HeroIconsConfig(AppConfig):
+    name = "djc_heroicons"
+
+    # This is the code that gets run when user adds djc_heroicons
+    # to Django's INSTALLED_APPS
+    def ready(self) -> None:
+        from djc_heroicons.app_settings import app_settings
+        from djc_heroicons.components.icon import Icon
+
+        # Register the component with the specified name and to the specified registry
+        app_settings.REGISTRY.register(app_settings.COMPONENT_NAME, Icon)
