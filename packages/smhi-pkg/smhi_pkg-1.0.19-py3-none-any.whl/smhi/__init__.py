f288cef53767d29b0647896619265ce6246613b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:53 +0000

    Add package smhi-pkg/1.0.19/smhi_pkg-1.0.19-py3-none-any.whl

---
+++ packages/smhi-pkg/smhi_pkg-1.0.19-py3-none-any.whl/smhi/__init__.py
@@ -0,0 +1,10 @@
+"""Initialize the SMHI package."""
+
+from smhi.smhi_lib import Smhi, SmhiAPIBase, SmhiForecast
+
+__title__ = "SMHI"
+__version__ = "1.0.19"
+__author__ = "helto4real"
+__license__ = "MIT"
+
+__all__ = ["Smhi", "SmhiAPIBase", "SmhiForecast"]
