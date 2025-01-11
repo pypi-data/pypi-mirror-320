50eafe60908b784c22e19c88f687b60c091b72d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:42 +0000

    Add package vreg/0.0.2/vreg-0.0.2.tar.gz

---
+++ packages/vreg/vreg-0.0.2.tar.gz/vreg-0.0.2/src/vreg/__init__.py
@@ -0,0 +1,37 @@
+from vreg import optimize
+from vreg.optimize import *
+
+from vreg import utils
+from vreg.utils import *
+
+from vreg import mod_affine
+from vreg.mod_affine import *
+
+from vreg import metrics
+from vreg.metrics import *
+
+from vreg import mod_freeform
+from vreg.mod_freeform import *
+
+from vreg import obsolete_transforms
+from vreg.obsolete_transforms import *
+
+from vreg import obsolete_mod_align
+from vreg.obsolete_mod_align import *
+
+from vreg import fake
+from vreg.fake import *
+
+from vreg import vol
+from vreg.vol import *
+
+# from vreg import plot
+# from vreg.plot import *
+
+from vreg import data
+from vreg.data import *
+
+
+
+# from vreg import scipy_least_squares
+# from vreg.scipy_least_squares import *
---
+++ packages/vreg/vreg-0.0.2.tar.gz/vreg-0.0.2/tests/test_dummy.py
@@ -0,0 +1,8 @@
+
+def test_dummy():
+    assert True
+
+
+if __name__ == '__main__':
+
+    test_dummy()
