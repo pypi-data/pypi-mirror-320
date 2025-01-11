24fe4a9f394893355f342442b683eac16769cfeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:56 +0000

    Add package operationunitaire/0.1.0/operationunitaire-0.1.0.tar.gz

---
+++ packages/operationunitaire/operationunitaire-0.1.0.tar.gz/operationunitaire-0.1.0/operationunitaire/__main__.py
@@ -0,0 +1,9 @@
+from .core.data_loader import DataLoader
+from .core.interpolation import Interpolator
+
+def main():
+    # Example execution logic
+    print("Welcome to Operation Unitaire!")
+
+if __name__ == "__main__":
+    main()
---
+++ packages/operationunitaire/operationunitaire-0.1.0.tar.gz/operationunitaire-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="operationunitaire",  
+    version="0.1.1",  
+    description="A Python package for rigorous distillation simulations.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="TASSAOUI Anas",
+    author_email="anass.tassaoui@gmail.com",
+    url="https://github.com/anastassaoui/operationunitaire",  # Updated URL
+    license="MIT",
+    packages=find_packages(),  
+    install_requires=["numpy", "pandas", "tabulate"],  
+    python_requires=">=3.7",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
