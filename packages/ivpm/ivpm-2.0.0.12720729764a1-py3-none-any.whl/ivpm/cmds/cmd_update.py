6edc93210e2e512185c0790a4e075084db8443a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:52 +0000

    Add package ivpm/2.0.0.12720729764a1/ivpm-2.0.0.12720729764a1-py3-none-any.whl

---
+++ packages/ivpm/ivpm-2.0.0.12720729764a1-py3-none-any.whl/ivpm/cmds/cmd_update.py
@@ -0,0 +1,41 @@
+'''
+Created on Jun 8, 2021
+
+@author: mballance
+'''
+import os
+
+from ivpm.packages_info import PackagesInfo
+from ivpm.project_ops import ProjectOps
+from ivpm.out_wrapper import OutWrapper
+from ivpm.package_updater import PackageUpdater
+from ivpm.package import Package, PackageType, SourceType
+from typing import List
+
+
+class CmdUpdate(object):
+    
+    def __init__(self):
+        self.debug = False
+        pass
+    
+    def __call__(self, args):
+        if args.project_dir is None:
+            # If a default is not provided, use the current directory
+            print("Note: project_dir not specified ; using working directory")
+            args.project_dir = os.getcwd()
+
+        ds_name = None
+        if hasattr(args, "dep_set") and args.dep_set is not None:
+            ds_name = args.dep_set
+
+        ProjectOps(args.project_dir).update(
+            dep_set=ds_name,
+            anonymous=args.anonymous,
+            force_py_install=args.force_py_install,
+            args=args)
+
+
+
+   
+    
