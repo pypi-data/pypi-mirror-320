0e8dbe7b747a20441878c29054e9d46d097666d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:04:00 +0000

    Add package catscript/1.0.1/catscript-1.0.1.tar.gz

---
+++ packages/catscript/catscript-1.0.1.tar.gz/catscript-1.0.1/src/catscript/sys.py
@@ -0,0 +1,14 @@
+def alert(text: str) -> str:
+    return ""
+
+
+def execute(command: str) -> str:
+    return ""
+
+
+def paste(text: str) -> None:
+    pass
+
+
+def toast(text: str, ms: int) -> None:
+    pass
