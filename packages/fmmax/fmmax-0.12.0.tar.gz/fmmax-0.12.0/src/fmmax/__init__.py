fd083167b8962b02d211ee898d9ba8e02808f394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:33 +0000

    Add package fmmax/0.12.0/fmmax-0.12.0.tar.gz

---
+++ packages/fmmax/fmmax-0.12.0.tar.gz/fmmax-0.12.0/src/fmmax/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+__version__ = "v0.12.0"
+
+from fmmax import basis as basis
+from fmmax import beams as beams
+from fmmax import farfield as farfield
+from fmmax import fft as fft
+from fmmax import fields as fields
+from fmmax import fmm as fmm
+from fmmax import fmm_matrices as fmm_matrices
+from fmmax import scattering as scattering
+from fmmax import sources as sources
+from fmmax import translate as translate
+from fmmax import utils as utils
+from fmmax import vector as vector
