53bce61bb6a40ea5d50c6d7c1781d58d6f796a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:12 +0000

    Add package ensfc-stemm/0.0.0/ensfc_stemm-0.0.0-py3-none-any.whl

---
+++ packages/ensfc-stemm/ensfc_stemm-0.0.0-py3-none-any.whl/ensfc_stemm/__init__.py
@@ -0,0 +1,9 @@
+from .calculations import calculate_mean
+from .equations import solve_quadratic, nth_term
+from .models import Student, Experiment
+
+__version__ = '0.1.1'
+
+'''
+This is a package called ensfc-stemm. It contains two modules: calculations and equations.
+'''
