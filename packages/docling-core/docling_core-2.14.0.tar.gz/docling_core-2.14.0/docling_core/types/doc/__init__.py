23ee58d0d9792f1ed6ec17a2825c8fc5ef74fbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:05 +0000

    Add package docling-core/2.14.0/docling_core-2.14.0.tar.gz

---
+++ packages/docling-core/docling_core-2.14.0.tar.gz/docling_core-2.14.0/docling_core/types/doc/__init__.py
@@ -0,0 +1,31 @@
+#
+# Copyright IBM Corp. 2024 - 2024
+# SPDX-License-Identifier: MIT
+#
+
+"""Package for models defined by the Document type."""
+
+from .base import BoundingBox, CoordOrigin, ImageRefMode, Size
+from .document import (
+    DocItem,
+    DoclingDocument,
+    DocumentOrigin,
+    FloatingItem,
+    GroupItem,
+    ImageRef,
+    KeyValueItem,
+    NodeItem,
+    PageItem,
+    PictureClassificationClass,
+    PictureClassificationData,
+    PictureDataType,
+    PictureItem,
+    ProvenanceItem,
+    RefItem,
+    SectionHeaderItem,
+    TableCell,
+    TableData,
+    TableItem,
+    TextItem,
+)
+from .labels import DocItemLabel, GroupLabel, TableCellLabel
