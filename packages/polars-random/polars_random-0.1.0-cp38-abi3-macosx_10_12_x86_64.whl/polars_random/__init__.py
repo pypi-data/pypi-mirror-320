f3867de20593daa33226c87f1c7ebbd64436da3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:44 +0000

    Add package polars-random/0.1.0/polars_random-0.1.0-cp38-abi3-macosx_10_12_x86_64.whl

---
+++ packages/polars-random/polars_random-0.1.0-cp38-abi3-macosx_10_12_x86_64.whl/polars_random/__init__.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import polars as pl
+from polars.plugins import register_plugin_function
+
+from polars_random._internal import __version__ as VERSION
+
+__version__ = VERSION
+
+LIB = Path(__file__).parent
+
+__all__ = ["sum_as_string"]
+
+
+@pl.api.register_expr_namespace("random")
+class Random:
+    def __init__(self, expr: pl.Expr) -> None:
+        self._expr = expr
+
+    def rand(self) -> pl.Expr:
+        return register_plugin_function(
+            args=[self._expr],
+            plugin_path=LIB,
+            function_name="rand",
+            is_elementwise=True,
+        )
+
+    def normal(self) -> pl.Expr:
+        return register_plugin_function(
+            args=[self._expr],
+            plugin_path=LIB,
+            function_name="normal",
+            is_elementwise=True,
+        )
