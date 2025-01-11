d84a8a6686914d5bcf91c7200caf01cf7ff2570b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:07:56 +0000

    Add package cotdantic/2.1.3/cotdantic-2.1.3-py3-none-any.whl

---
+++ packages/cotdantic/cotdantic-2.1.3-py3-none-any.whl/cotdantic/__init__.py
@@ -0,0 +1,24 @@
+__version__ = '2.1.3'
+
+from .models import *
+
+from . import converters
+from .cot_types import atom
+import uuid
+
+UID = f'cotdantic-{uuid.getnode()}'
+CALLSIGN = 'cotdantic'
+
+
+def __event_to_bytes(self: EventBase) -> bytes:
+	return converters.model2proto(self)
+
+
+@classmethod
+def __event_from_bytes(cls: EventBase, proto: bytes) -> EventBase:
+	return converters.proto2model(cls, proto)
+
+
+EventBase.__bytes__ = __event_to_bytes
+EventBase.to_bytes = __event_to_bytes
+EventBase.from_bytes = __event_from_bytes
