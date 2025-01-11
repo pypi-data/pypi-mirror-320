3dec13a43566e0d9182ce3558bd9a2bd658385c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:42 +0000

    Add package git-authorship/0.2.0/git_authorship-0.2.0-py3-none-any.whl

---
+++ packages/git-authorship/git_authorship-0.2.0-py3-none-any.whl/git_authorship/cli.py
@@ -0,0 +1,103 @@
+# Copyright (c) 2024 Joseph Hale
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import argparse
+import logging
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+from git import Repo
+
+from git_authorship import authorship
+from git_authorship import export
+
+log = logging.getLogger(__name__)
+
+
+@dataclass
+class Args:
+    location: str
+    clone_to: str
+    branch: str
+    author_licenses_path: Optional[Path]
+    use_authorship_cache: bool = True
+
+
+def parse_args(argv=None) -> Args:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "location", nargs="?", default=".", help="The URL/path to repo to analyze"
+    )
+    parser.add_argument(
+        "--clone-to", nargs="?", default="./build/repo", help="The path to clone to"
+    )
+    parser.add_argument(
+        "--branch", nargs="?", default=None, help="The branch/revision to checkout"
+    )
+    parser.add_argument(
+        "--author-licenses",
+        nargs="?",
+        default=None,
+        help="The path to a CSV file containing author licenses",
+    )
+    parser.add_argument(
+        "--no-authorship-cache", action="store_true", help="Ignore authorship cache"
+    )
+
+    args = parser.parse_args(argv)
+
+    return Args(
+        args.location,
+        args.clone_to,
+        args.branch,
+        _parse_author_licenses_path(args.author_licenses),
+        not args.no_authorship_cache,
+    )
+
+
+def _parse_author_licenses_path(arg: Optional[str] = None) -> Optional[Path]:
+    if not arg:
+        return None
+    else:
+        if not Path(arg).exists():
+            raise FileNotFoundError(arg)
+        else:
+            return Path(arg)
+
+
+def clone_and_checkout(args: Args):
+    log.info(
+        f"Cloning {args.location} @ {args.branch or '<default>'} to {args.clone_to}"
+    )
+
+    if not Path(args.clone_to).exists():
+        Repo.clone_from(args.location, args.clone_to)
+
+    repo = Repo(args.clone_to)
+
+    if args.branch:
+        repo.git.checkout(args.branch)
+
+    return repo
+
+
+def run(args: Args):
+    repo = clone_and_checkout(args)
+    repo_authorship = authorship.for_repo(
+        repo,
+        license_file=args.author_licenses_path,
+        use_cache=args.use_authorship_cache,
+    )
+    export.as_treemap(repo_authorship)
+    export.as_json(repo_authorship)
+    export.as_csv(repo_authorship)
+
+
+def main(argv=None):
+    logging.getLogger("git_authorship").addHandler(logging.StreamHandler())
+    logging.getLogger("git_authorship").setLevel(logging.INFO)
+
+    run(parse_args(argv))
