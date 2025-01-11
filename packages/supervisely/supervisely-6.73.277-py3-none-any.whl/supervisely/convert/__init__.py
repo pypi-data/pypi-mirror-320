a3ef6fca6d505f4e8f2e75b09ca752b2d14f40f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:11 +0000

    Add package supervisely/6.73.277/supervisely-6.73.277-py3-none-any.whl

---
+++ packages/supervisely/supervisely-6.73.277-py3-none-any.whl/supervisely/convert/__init__.py
@@ -0,0 +1,60 @@
+# isort: skip_file
+from supervisely.convert.base_converter import (
+    AvailableImageConverters,
+    AvailablePointcloudConverters,
+    AvailableVideoConverters,
+    AvailableVolumeConverters,
+    BaseConverter,
+)
+from supervisely.convert.converter import ImportManager
+
+# Image
+from supervisely.convert.image.cityscapes.cityscapes_converter import (
+    CityscapesConverter,
+)
+from supervisely.convert.image.coco.coco_converter import COCOConverter
+from supervisely.convert.image.coco.coco_anntotation_converter import FastCOCOConverter
+from supervisely.convert.image.csv.csv_converter import CSVConverter
+from supervisely.convert.image.multispectral.multispectral_converter import (
+    MultiSpectralImageConverter,
+)
+from supervisely.convert.image.medical2d.medical2d_converter import Medical2DImageConverter
+from supervisely.convert.image.masks.images_with_masks_converter import ImagesWithMasksConverter
+
+from supervisely.convert.image.pascal_voc.pascal_voc_converter import PascalVOCConverter
+from supervisely.convert.image.pdf.pdf_converter import PDFConverter
+from supervisely.convert.image.sly.sly_image_converter import SLYImageConverter
+from supervisely.convert.image.sly.fast_sly_image_converter import FastSlyImageConverter
+from supervisely.convert.image.yolo.yolo_converter import YOLOConverter
+from supervisely.convert.image.multi_view.multi_view import MultiViewImageConverter
+from supervisely.convert.image.label_me.label_me_converter import LabelmeConverter
+from supervisely.convert.image.label_studio.label_studio_converter import LabelStudioConverter
+from supervisely.convert.image.high_color.high_color_depth import HighColorDepthImageConverter
+
+
+# Pointcloud
+from supervisely.convert.pointcloud.sly.sly_pointcloud_converter import SLYPointcloudConverter
+from supervisely.convert.pointcloud.las.las_converter import LasConverter
+from supervisely.convert.pointcloud.ply.ply_converter import PlyConverter
+from supervisely.convert.pointcloud.bag.bag_converter import BagConverter
+from supervisely.convert.pointcloud.lyft.lyft_converter import LyftConverter
+from supervisely.convert.pointcloud.nuscenes_conv.nuscenes_converter import NuscenesConverter
+from supervisely.convert.pointcloud.kitti_3d.kitti_3d_converter import KITTI3DConverter
+
+# Pointcloud Episodes
+from supervisely.convert.pointcloud_episodes.sly.sly_pointcloud_episodes_converter import (
+    SLYPointcloudEpisodesConverter,
+)
+from supervisely.convert.pointcloud_episodes.bag.bag_converter import BagEpisodesConverter
+from supervisely.convert.pointcloud_episodes.lyft.lyft_converter import LyftEpisodesConverter
+from supervisely.convert.pointcloud_episodes.nuscenes_conv.nuscenes_converter import (
+    NuscenesEpisodesConverter,
+)
+
+# Video
+from supervisely.convert.video.mot.mot_converter import MOTConverter
+from supervisely.convert.video.sly.sly_video_converter import SLYVideoConverter
+
+# Volume
+from supervisely.convert.volume.sly.sly_volume_converter import SLYVolumeConverter
+from supervisely.convert.volume.dicom.dicom_converter import DICOMConverter
