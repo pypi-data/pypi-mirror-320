3d88b7dc73f43781edf4a5524878366f6a0fec3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:39 +0000

    Add package anycrc/1.3.0/anycrc-1.3.0.tar.gz

---
+++ packages/anycrc/anycrc-1.3.0.tar.gz/anycrc-1.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, Extension
+import sys
+
+if sys.platform == 'win32':
+    compile_args = ['/O2']
+    link_args = ['/O2']
+else:
+    compile_args = ['-O2']
+    link_args = ['-O2']
+
+setup(
+    name = 'anycrc',
+    version = '1.3.0',
+    package_dir = {'': 'src'},
+    ext_modules = [
+        Extension(
+            name='anycrc.crc',
+            extra_compile_args=compile_args,
+            extra_link_args=link_args,
+            sources=['src/anycrc/crc.pyx',
+                     'lib/crcany/model.c',
+                     'lib/crcany/crc.c']
+        )
+    ]
+)
