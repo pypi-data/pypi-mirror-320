cc9b21b023ba84a072d15e9ef16b2062c0cbdddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:08:38 +0000

    Add package dkist-processing-vbi/1.18.8/dkist_processing_vbi-1.18.8.tar.gz

---
+++ packages/dkist-processing-vbi/dkist_processing_vbi-1.18.8.tar.gz/dkist_processing_vbi-1.18.8/check_changelog_updated.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURRENT_VERSION=`git describe --tags --match v* --exact-match`
+ERROR_STR="CHANGELOG has not been updated for ${CURRENT_VERSION}\\nSee the README for instructions"
+ERROR_PAD="***********"
+
+if [[ $CURRENT_VERSION =~ "rc" ]]; then
+  echo "release candidate detected"
+  exit 0;
+fi
+
+ls changelog/*rst 2>/dev/null && {
+  echo -e "\\n${ERROR_PAD}"
+  echo -e "Unmerged changelog fragments found! You must run towncrier!\\nSee the README for instruction"
+  echo -e "${ERROR_PAD}"
+  exit 1;
+}
+
+head -n 1 CHANGELOG.rst | grep -q "^$CURRENT_VERSION (" - || {
+  echo -e "\\n${ERROR_PAD}"
+  echo -e $ERROR_STR;
+  echo -e "${ERROR_PAD}"
+  exit 1; }
