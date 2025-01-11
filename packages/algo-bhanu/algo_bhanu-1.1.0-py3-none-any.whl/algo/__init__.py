8f4f5bf6ef5e6173eedde782a3e5fee578613ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:23 +0000

    Add package algo-bhanu/1.1.0/algo_bhanu-1.1.0-py3-none-any.whl

---
+++ packages/algo-bhanu/algo_bhanu-1.1.0-py3-none-any.whl/algo/__init__.py
@@ -0,0 +1,5 @@
+from .Decisiontree import DecisionTree
+from .KNN import KNN
+from .Linearregression import LinearRegression
+from .Logisticregression import LogisticRegression
+from Randomforest import RandomForest
