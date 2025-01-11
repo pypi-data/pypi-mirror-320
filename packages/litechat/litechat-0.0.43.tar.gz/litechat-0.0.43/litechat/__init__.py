b2e6d1ae75a77f860f454a8dda8bfc0e8d3ebe8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:03:56 +0000

    Add package litechat/0.0.43/litechat-0.0.43.tar.gz

---
+++ packages/litechat/litechat-0.0.43.tar.gz/litechat-0.0.43/litechat/__init__.py
@@ -0,0 +1,3 @@
+from ._core import completion,pp_completion,genai,LiteAI,OpenAI,litechat_server,litechat_model,litellm_model,OPENAI_COMPATIBLE_BASE_URL,HFChatModels
+from ._codegpt import litechat_codegpt_server
+from ._gmail_auto import GmailAutomation,gmail,automail
