cfedf46b8215e2c14c905b1a776d159a913115e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:27 +0000

    Add package intellibricks/0.2.29/intellibricks-0.2.29-py3-none-any.whl

---
+++ packages/intellibricks/intellibricks-0.2.29-py3-none-any.whl/intellibricks/__init__.py
@@ -0,0 +1,32 @@
+from .llms import (
+    Synapse,
+    ChatCompletion,
+    AssistantMessage,
+    DeveloperMessage,
+    UserMessage,
+    MessageChoice,
+    MessageRole,
+    TraceParams,
+    Usage,
+    Prompt,
+    SynapticFallbackChain,
+)
+from .files import RawFile
+
+from .agents.agents import Agent
+
+__all__: list[str] = [
+    "Synapse",
+    "ChatCompletion",
+    "Usage",
+    "AssistantMessage",
+    "DeveloperMessage",
+    "UserMessage",
+    "MessageChoice",
+    "MessageRole",
+    "TraceParams",
+    "Prompt",
+    "Agent",
+    "RawFile",
+    "SynapticFallbackChain",
+]
---
+++ packages/intellibricks/intellibricks-0.2.29-py3-none-any.whl/intellibricks/llms/__init__.py
@@ -0,0 +1,30 @@
+from .constants import MessageRole
+from .schema import (
+    AssistantMessage,
+    CacheConfig,
+    ChatCompletion,
+    MessageChoice,
+    Prompt,
+    DeveloperMessage,
+    Tag,
+    Usage,
+    UserMessage,
+    TraceParams,
+)
+from .synapses import Synapse, SynapticFallbackChain
+
+__all__ = [
+    "Synapse",
+    "ChatCompletion",
+    "AssistantMessage",
+    "DeveloperMessage",
+    "UserMessage",
+    "MessageRole",
+    "Usage",
+    "Tag",
+    "MessageChoice",
+    "TraceParams",
+    "Prompt",
+    "CacheConfig",
+    "SynapticFallbackChain",
+]
