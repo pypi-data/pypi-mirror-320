f26c48dd732b6d39943830cc739b6f0c16a1d801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:04:29 +0000

    Add package django-mongodb-backend/5.0.0a2/django_mongodb_backend-5.0.0a2-py3-none-any.whl

---
+++ packages/django-mongodb-backend/django_mongodb_backend-5.0.0a2-py3-none-any.whl/django_mongodb_backend/forms/fields/__init__.py
@@ -0,0 +1,9 @@
+from .array import SimpleArrayField, SplitArrayField, SplitArrayWidget
+from .objectid import ObjectIdField
+
+__all__ = [
+    "SimpleArrayField",
+    "SplitArrayField",
+    "SplitArrayWidget",
+    "ObjectIdField",
+]
