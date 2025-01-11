63a49a145b7bec22bf627a2202ac96481b7c4c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:08:38 +0000

    Add package AIGuard/0.1.0/AIGuard-0.1.0-py3-none-any.whl

---
+++ packages/AIGuard/AIGuard-0.1.0-py3-none-any.whl/AIGuard/__init__.py
@@ -0,0 +1,5 @@
+from ..utils.sanitizer import sanitize_input
+from ..utils.classifier import classify_prompt
+from ..utils.validator import validate_prompt, load_manipulation_rules
+
+__all__ = ["sanitize_input", "classify_prompt", "validate_prompt", "load_manipulation_rules"]
