8dfaf34e87cf7bf0d0033ff9ff29229bab99912d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:39 +0000

    Add package weauth/1.5.0/weauth-1.5.0.tar.gz

---
+++ packages/weauth/weauth-1.5.0.tar.gz/weauth-1.5.0/weauth/constants/core_constant.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
+# author： NearlyHeadlessJack
+# email: wang@rjack.cn
+# datetime： 2025/1/5 20:29 
+# ide： PyCharm
+# file: core_constant.py
+import os
+
+NAME_SHORT = 'WeAuth'
+NAME = 'WeAuth'
+PACKAGE_NAME = 'weauth'
+CLI_COMMAND = PACKAGE_NAME
+
+# WeAuth Version Storage
+VERSION_PYPI: str = '1.5.0'  #
+VERSION: str = '1.5.0'  # 版本号
+
+
+
+# URLs
+GITHUB_URL = r'https://github.com/TomatoCraftMC/WeAuth'
+GITEE_VERSION_URL = r'https://gitee.com/NHJ2001/WeAuth/raw/main/VERSION'
+DOCUMENTATION_URL = r'https://github.com/TomatoCraftMC/WeAuth/blob/main/README.md'
+
+# CDKey
+CDKEY_LENGTH_ONE_PIECE = 4
