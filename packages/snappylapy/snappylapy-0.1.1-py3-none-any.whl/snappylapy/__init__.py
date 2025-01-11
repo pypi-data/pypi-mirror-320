dbd7e72c97cd5b128b6d3ff84f077102b448a5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:09 +0000

    Add package snappylapy/0.1.1/snappylapy-0.1.1-py3-none-any.whl

---
+++ packages/snappylapy/snappylapy-0.1.1-py3-none-any.whl/snappylapy/__init__.py
@@ -0,0 +1,4 @@
+"""Define the public api for the snappylapy package."""
+from .snappylapy import Expect, LoadSnapshot
+
+__all__ = ["Expect", "LoadSnapshot"]
---
+++ packages/snappylapy/snappylapy-0.1.1-py3-none-any.whl/snappylapy/constants.py
@@ -0,0 +1,4 @@
+"""Read-only constants."""
+
+SNAPSHOT_DIR_NAME = "__snapshots__"
+TEST_RESULTS_DIR_NAME = "__test_results__"
