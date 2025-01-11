ca26ae5778941dae9bb8a9e9314365ac8ecfbf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:35 +0000

    Add package OlxPyApi/0.0.7/olxpyapi-0.0.7.tar.gz

---
+++ packages/OlxPyApi/olxpyapi-0.0.7.tar.gz/olxpyapi-0.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import pathlib
+
+__version__ = "0.0.7"
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text(encoding='utf-8')
+
+setup(
+    name='OlxPyApi',
+    version=__version__,
+
+    url='https://github.com/Vladislavus1/OlxPyApi',
+    author='Vladislavus1',
+    author_email='vlydgames@gmail.com',
+
+    description='This package simplifies the extraction of data from OLX websites.',
+    long_description=README,
+    long_description_content_type='text/markdown',
+
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'beautifulsoup4==4.12.3',
+        'requests==2.31.0',
+        'lxml==5.2.2'
+    ],
+)
