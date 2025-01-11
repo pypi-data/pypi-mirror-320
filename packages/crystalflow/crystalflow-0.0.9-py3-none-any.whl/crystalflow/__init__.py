54c7b69ad7317751af22c629382ffd6d2dca3ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:52 +0000

    Add package crystalflow/0.0.9/crystalflow-0.0.9-py3-none-any.whl

---
+++ packages/crystalflow/crystalflow-0.0.9-py3-none-any.whl/crystalflow/__init__.py
@@ -0,0 +1,16 @@
+
+__description__ = 'crystalflow, XRD Enhanced Crystal Graph Dataloader'
+__author__ = 'Bin Cao, Advanced Materials Thrust, Hong Kong University of Science and Technology (Guangzhou)'
+__author_email__ = 'binjacobcao@gmail.com'
+__url__ = 'https://github.com/Bin-Cao/CrystalFlow'
+
+import datetime
+now = datetime.datetime.now()
+formatted_date_time = now.strftime('%Y-%m-%d %H:%M:%S')
+   
+
+print('crystalflow, XRD Enhanced Crystal Graph Dataloader')
+print('Bin Cao, Advanced Materials Thrust, HKUST(GZ)')
+print('If you need any help, please contact me at github.com/Bin-Cao/CrystalFlow')
+print('Executed on :',formatted_date_time, ' | Have a great day.')  
+print('='*80)
