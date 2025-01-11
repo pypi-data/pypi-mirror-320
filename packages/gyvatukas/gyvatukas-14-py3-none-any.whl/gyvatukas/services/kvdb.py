abf645b3c9c1da82afc8761cdfe3ed9e79d8f8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:07:54 +0000

    Add package gyvatukas/14/gyvatukas-14-py3-none-any.whl

---
+++ packages/gyvatukas/gyvatukas-14-py3-none-any.whl/gyvatukas/services/kvdb.py
@@ -0,0 +1,1 @@
+# todo: jsonkvdb and sqlitekvdb based on diskcache which are simple key value stores for quick prototyping. or maybe base them on diskcache protocol.
---
+++ packages/gyvatukas/gyvatukas-14-py3-none-any.whl/gyvatukas/utils/decorators.py
@@ -0,0 +1,51 @@
+import asyncio
+import functools
+import time
+import logging
+
+
+def timer(logger_name: str = "timer", min_duration_ms: int = 0):
+    """Log function duration in ms. Specify own logger or use default `timer` logger. Can set minimum threshold for logging."""
+
+    def decorator(func):
+        # Handle both async and sync functions
+        if asyncio.iscoroutinefunction(func):
+
+            @functools.wraps(func)
+            async def wrapper(*args, **kwargs):
+                logger = logging.getLogger(logger_name)
+                t0 = time.perf_counter()
+                result = await func(*args, **kwargs)
+                duration_ms = (time.perf_counter() - t0) * 1000
+                if duration_ms >= min_duration_ms:
+                    logger.info(
+                        f"`{func.__name__}` took {duration_ms:.2f}ms",
+                        extra={
+                            "ms": duration_ms,
+                            "fn": func.__name__,
+                            "metric": "timer",
+                        },
+                    )
+                return result
+        else:
+
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                logger = logging.getLogger(logger_name)
+                t0 = time.perf_counter()
+                result = func(*args, **kwargs)
+                duration_ms = (time.perf_counter() - t0) * 1000
+                if duration_ms >= min_duration_ms:
+                    logger.info(
+                        f"`{func.__name__}` took {duration_ms:.2f}ms",
+                        extra={
+                            "ms": duration_ms,
+                            "fn": func.__name__,
+                            "metric": "timer",
+                        },
+                    )
+                return result
+
+        return wrapper
+
+    return decorator
