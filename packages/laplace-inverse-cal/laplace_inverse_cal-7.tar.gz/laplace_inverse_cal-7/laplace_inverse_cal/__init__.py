7cafe95d2d529de810cdfcf75f6d29c15459c1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:04:26 +0000

    Add package laplace-inverse-cal/7/laplace_inverse_cal-7.tar.gz

---
+++ packages/laplace-inverse-cal/laplace_inverse_cal-7.tar.gz/laplace_inverse_cal-7/laplace_inverse_cal/__init__.py
@@ -0,0 +1,25 @@
+import numpy as np
+import sympy as sp
+import mpmath
+from functools import lru_cache
+from multiprocessing import Pool, cpu_count
+t, s = sp.symbols('t s')
+
+# Cache the lambdified function
+@lru_cache(maxsize=None)
+def get_lambdified_func(F):
+    return sp.lambdify(s, F, 'mpmath')
+
+# Cache the lambdified function
+@lru_cache(maxsize=None)
+def function(F):
+    return sp.lambdify(s, F, 'mpmath')
+
+# Optimized inverse Laplace transform function
+def inverse_laplace(F, t_val):
+    if t_val <= 0:
+        return 0 
+    F_func = get_lambdified_func(F)
+    return float(mpmath.invertlaplace(F_func, t_val, method='talbot'))
+
+__all__ = ['inverse_laplace', 'function', 'samplified']
