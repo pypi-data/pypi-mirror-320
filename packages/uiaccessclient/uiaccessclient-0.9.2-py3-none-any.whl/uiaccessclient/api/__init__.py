daf120986d601d88a3e86644fd2df32ef441f24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:03:59 +0000

    Add package uiaccessclient/0.9.2/uiaccessclient-0.9.2-py3-none-any.whl

---
+++ packages/uiaccessclient/uiaccessclient-0.9.2-py3-none-any.whl/uiaccessclient/api/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+
+# import apis into api package
+from uiaccessclient.api.access_policy_api import AccessPolicyApi
+from uiaccessclient.api.credential_api import CredentialApi
+from uiaccessclient.api.device_api import DeviceApi
+from uiaccessclient.api.space_api import SpaceApi
+from uiaccessclient.api.system_log_api import SystemLogApi
+from uiaccessclient.api.uni_fi_identity_api import UniFiIdentityApi
+from uiaccessclient.api.user_api import UserApi
+from uiaccessclient.api.visitor_api import VisitorApi
+
