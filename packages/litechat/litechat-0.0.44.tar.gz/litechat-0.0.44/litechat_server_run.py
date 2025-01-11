40e93a60353d533b2c9c268670338dd77190d6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:03:47 +0000

    Add package litechat/0.0.44/litechat-0.0.44.tar.gz

---
+++ packages/litechat/litechat-0.0.44.tar.gz/litechat-0.0.44/litechat_server_run.py
@@ -0,0 +1,11 @@
+from litechat import litechat_server
+from litechat import litechat_codegpt_server
+
+def run_server():
+    litechat_server()
+
+def run_server_animation():
+    litechat_server(animation=True)
+
+def codegpt_server():
+    litechat_codegpt_server()
