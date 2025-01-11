0ad4dc591272220a9d245ed29656a25ffc27f834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:26 +0000

    Add package h3spark/0.0.5/h3spark-0.0.5-py3-none-any.whl

---
+++ packages/h3spark/h3spark-0.0.5-py3-none-any.whl/h3spark/models.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+from pyspark.sql import types as T
+
+
+@dataclass
+class Coordinates:
+    lat: float
+    lon: float
+
+    def from_tuple(self, tuple):
+        return Coordinates(lat=tuple[0], lon=tuple[1])
+
+
+COORDINATE_TYPE = T.StructType(
+    [
+        T.StructField("lat", T.FloatType(), False),
+        T.StructField("lon", T.FloatType(), False),
+    ]
+)
+
+
+BOUNDARY_TYPE = T.ArrayType(COORDINATE_TYPE)
+
+
+H3_CELL_DECIMAL_TYPE = T.DecimalType(20, 0)
