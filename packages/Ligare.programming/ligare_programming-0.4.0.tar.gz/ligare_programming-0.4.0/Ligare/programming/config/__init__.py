94a828b9e6d75c135240d9ef6cf6fe9ba98ab0cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:08:55 +0000

    Add package Ligare.programming/0.4.0/ligare_programming-0.4.0.tar.gz

---
+++ packages/Ligare.programming/ligare_programming-0.4.0.tar.gz/ligare_programming-0.4.0/Ligare/programming/config/__init__.py
@@ -0,0 +1,106 @@
+import abc
+from pathlib import Path
+from typing import Any, Generic, TypeVar, cast
+
+import toml
+from Ligare.programming.collections.dict import AnyDict, merge
+from Ligare.programming.config.exceptions import (
+    ConfigBuilderStateError,
+    NotEndsWithConfigError,
+)
+from typing_extensions import Self
+
+
+class AbstractConfig(abc.ABC):
+    @abc.abstractmethod
+    def post_load(self) -> None:
+        pass
+
+
+TConfig = TypeVar("TConfig", bound=AbstractConfig)
+
+from collections import deque
+
+
+class ConfigBuilder(Generic[TConfig]):
+    _root_config: type[TConfig] | None = None
+    _configs: deque[type[AbstractConfig]] | None = None
+
+    def with_root_config(self, config_type: type[TConfig]) -> Self:
+        self._root_config = config_type
+        return self
+
+    def with_configs(self, configs: list[type[AbstractConfig]] | None) -> Self:
+        if configs is None:
+            return self
+
+        if self._configs is None:
+            self._configs = deque(configs)
+        else:
+            self._configs.extend(configs)
+
+        return self
+
+    def with_config(self, config_type: type[AbstractConfig]) -> Self:
+        if self._configs is None:
+            self._configs = deque()
+
+        self._configs.append(config_type)
+        return self
+
+    def build(self) -> type[TConfig]:
+        if self._root_config and not self._configs:
+            return self._root_config
+
+        if not self._configs:
+            raise ConfigBuilderStateError(
+                "Cannot build a config without any base config types specified."
+            )
+
+        def test_type_name(config_type: type[AbstractConfig]):
+            if not config_type.__name__.endswith("Config"):
+                raise NotEndsWithConfigError(
+                    f"Class name '{config_type.__name__}' is not a valid config class. The name must end with 'Config'"
+                )
+
+        _new_type_base = (
+            self._root_config if self._root_config else self._configs.popleft()
+        )
+
+        test_type_name(_new_type_base)
+
+        attrs: dict[Any, Any] = {}
+        annotations: dict[str, Any] = {}
+
+        for config in self._configs:
+            test_type_name(config)
+
+            config_name = config.__name__[: config.__name__.rindex("Config")].lower()
+            annotations[config_name] = config
+            attrs[config_name] = None
+
+        attrs["__annotations__"] = annotations
+        # make one type that has the names of the config objects
+        # as attributes, and the class as their type
+        _new_type = cast(
+            "type[TConfig]", type("GeneratedConfig", (_new_type_base,), attrs)
+        )
+
+        return _new_type
+
+
+def load_config(
+    config_type: type[TConfig],
+    toml_file_path: str | Path,
+    config_overrides: AnyDict | None = None,
+) -> TConfig:
+    config_dict: dict[str, Any] = toml.load(toml_file_path)
+
+    if config_overrides is not None:
+        config_dict = merge(config_dict, config_overrides)
+
+    config = config_type(**config_dict)
+
+    config.post_load()
+
+    return config
---
+++ packages/Ligare.programming/ligare_programming-0.4.0.tar.gz/ligare_programming-0.4.0/Ligare/programming/patterns/dependency_injection.py
@@ -0,0 +1,52 @@
+import logging
+import sys
+from typing import Callable, TypeVar
+
+from injector import Binder, Module, Provider
+from Ligare.programming.config import AbstractConfig
+from typing_extensions import override
+
+
+class LoggerModule(Module):
+    def __init__(
+        self,
+        name: str | None = None,
+        log_level: int = logging.INFO,
+        log_to_stdout: bool = False,
+    ) -> None:
+        super().__init__()
+        self._logger = logging.getLogger(name)
+        self._logger.setLevel(log_level)
+        if log_to_stdout:
+            handler = logging.StreamHandler(sys.stdout)
+            handler.setLevel(log_level)
+            self._logger.addHandler(handler)
+
+    @override
+    def configure(self, binder: Binder) -> None:
+        binder.bind(logging.Logger, to=self._logger)
+
+
+T = TypeVar("T")
+
+
+class BatchModule(Module):
+    def __init__(
+        self, registrations: dict[type[T], None | T | Callable[..., T] | Provider[T]]
+    ) -> None:
+        super().__init__()
+        self._registrations = registrations
+
+    @override
+    def configure(self, binder: Binder) -> None:
+        for interface, to in self._registrations.items():
+            binder.bind(interface, to)
+
+
+from abc import ABC, abstractmethod
+
+
+class ConfigurableModule(Module, ABC):
+    @staticmethod
+    @abstractmethod
+    def get_config_type() -> type[AbstractConfig]: ...
