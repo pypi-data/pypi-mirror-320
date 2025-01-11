67720ba7b87cbbc02a6c2093d9ec941bb6df9d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:08:15 +0000

    Add package gink/0.20250109.1736460167/gink-0.20250109.1736460167.tar.gz

---
+++ packages/gink/gink-0.20250109.1736460167.tar.gz/gink-0.20250109.1736460167/gink/impl/typedefs.py
@@ -0,0 +1,34 @@
+""" Various types classes for use throughout the codebase. """
+from typing import NewType, Union, TypeVar, Callable
+from datetime import datetime, timedelta, date
+from pathlib import Path
+
+
+Medallion = int
+MuTimestamp = int
+Offset = NewType('Offset', int)
+GenericTimestamp = Union[datetime, timedelta, date, int, float, str, None]
+Destination = GenericTimestamp
+UserKey = Union[str, int, bytes]
+UserValue = Union[str, int, float, datetime, bytes, bool, None, dict, tuple, list]
+EPOCH = 0
+Limit = Union[int, float]
+T = TypeVar('T')
+inf = float("inf")
+
+
+class Deletion:  # pylint: disable=too-few-public-methods
+    """ Used internally to indicate that a key/value assocation has been removed. """
+
+
+class Inclusion:
+    """ Used to indicate adding something to a set or group. """
+
+
+AuthFunc = Callable[[str, Path], int]
+
+AUTH_NONE = 0
+AUTH_READ = 1
+AUTH_RITE = 2
+AUTH_MAKE = 4
+AUTH_FULL = 7
