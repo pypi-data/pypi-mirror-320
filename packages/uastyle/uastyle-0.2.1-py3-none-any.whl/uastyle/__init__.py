33d185b790bfe150a4058fda599aac1c86fabc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:06 +0000

    Add package uastyle/0.2.1/uastyle-0.2.1-py3-none-any.whl

---
+++ packages/uastyle/uastyle-0.2.1-py3-none-any.whl/uastyle/__init__.py
@@ -0,0 +1,5 @@
+# flake8: noqa: F401
+
+from .__config__ import __version__
+from .constants import Color, L_width, M_width, S_width, XL_width, colors
+from .utils import make_default_colors, make_default_font_size
