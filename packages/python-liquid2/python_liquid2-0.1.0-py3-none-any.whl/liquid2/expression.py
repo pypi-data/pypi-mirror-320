8992425bc4bd0a029d5cf3fc975303360a930918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:58 +0000

    Add package python-liquid2/0.1.0/python_liquid2-0.1.0-py3-none-any.whl

---
+++ packages/python-liquid2/python_liquid2-0.1.0-py3-none-any.whl/liquid2/expression.py
@@ -0,0 +1,34 @@
+"""Base class for all Liquid expressions."""
+
+from __future__ import annotations
+
+from abc import ABC
+from abc import abstractmethod
+from typing import TYPE_CHECKING
+from typing import Iterable
+
+if TYPE_CHECKING:
+    from liquid2 import TokenT
+
+    from .context import RenderContext
+
+
+class Expression(ABC):
+    """Base class for all Liquid expressions."""
+
+    __slots__ = ("token",)
+
+    def __init__(self, token: TokenT) -> None:
+        self.token = token
+
+    @abstractmethod
+    def evaluate(self, context: RenderContext) -> object:
+        """Evaluate the expression in the given render context."""
+
+    async def evaluate_async(self, context: RenderContext) -> object:
+        """An async version of `liquid.expression.Expression.evaluate`."""
+        return self.evaluate(context)
+
+    @abstractmethod
+    def children(self) -> Iterable[Expression]:
+        """Return this expression's child expressions."""
