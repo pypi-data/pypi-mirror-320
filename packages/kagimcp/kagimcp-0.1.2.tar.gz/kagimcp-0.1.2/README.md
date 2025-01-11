c6e9417adba847e98dce15e1667fc55ad899db80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:51 +0000

    Add package kagimcp/0.1.2/kagimcp-0.1.2.tar.gz

---
+++ packages/kagimcp/kagimcp-0.1.2.tar.gz/kagimcp-0.1.2/README.md
@@ -0,0 +1,99 @@
+# Kagi MCP server
+
+## Setup Intructions
+Install uv first.
+
+MacOS/Linux:
+```bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+```
+
+Windows:
+```
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+```
+
+### Setup with Claude Desktop
+```json
+# claude_desktop_config.json
+# Can find location through:
+# Hamburger Menu -> File -> Settings -> Developer -> Edit Config
+{
+  "mcpServers": {
+    "kagi": {
+      "command": "uvx",
+      "args": ["kagimcp"],
+      "env": {
+        "KAGI_API_KEY": "YOUR_API_KEY_HERE"
+      }
+    }
+  }
+}
+```
+### Ask Claude a question requiring search
+e.g. "Who was time's 2024 person of the year?"
+
+## Local/Dev Setup Instructions
+### Clone repo
+`git clone https://github.com/kagisearch/kagimcp.git`
+### Install dependencies
+Install uv first.
+
+MacOS/Linux:
+```bash
+curl -LsSf https://astral.sh/uv/install.sh | sh
+```
+
+Windows:
+```
+powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+```
+
+Then install MCP server dependencies:
+```bash
+cd kagimcp
+
+# Create virtual environment and activate it
+uv venv
+
+source .venv/bin/activate # MacOS/Linux
+# OR
+.venv/Scripts/activate # Windows
+
+# Install dependencies
+uv sync
+```
+### Setup with Claude Desktop
+```json
+# claude_desktop_config.json
+# Can find location through:
+# Hamburger Menu -> File -> Settings -> Developer -> Edit Config
+{
+  "mcpServers": {
+    "kagi": {
+      "command": "uv",
+      "args": [
+        "--directory",
+        "/ABSOLUTE/PATH/TO/PARENT/FOLDER/kagimcp",
+        "run",
+        "kagimcp"
+      ],
+      "env": {
+        "KAGI_API_KEY": "YOUR_API_KEY_HERE"
+      }
+    }
+  }
+}
+```
+### Ask Claude a question requiring search
+e.g. "Who was time's 2024 person of the year?"
+### Debugging
+Run:
+```bash
+npx @modelcontextprotocol/inspector \
+      uv \
+      --directory /ABSOLUTE/PATH/TO/PARENT/FOLDER/kagimcp \
+      run \
+      kagimcp
+```
+Then access MCP Inspector at `http://localhost:5173`. You may need to add your Kagi API key in the environment variables in the inspector under `KAGI_API_KEY`.
---
+++ packages/kagimcp/kagimcp-0.1.2.tar.gz/kagimcp-0.1.2/src/kagimcp/server.py
@@ -0,0 +1,85 @@
+import textwrap
+from kagiapi import KagiClient
+from concurrent.futures import ThreadPoolExecutor
+
+from mcp.server.fastmcp import FastMCP
+from pydantic import Field
+
+
+kagi_client = KagiClient()
+mcp = FastMCP("kagimcp", dependencies=["kagiapi", "mcp[cli]"])
+
+
+@mcp.tool()
+def search(
+    queries: list[str] = Field(
+        description="One or more concise, keyword-focused search queries. Include essential context within each query for standalone use."
+    ),
+) -> str:
+    """Perform web search based on one or more queries. Results are from all queries given. They are numbered continuously, so that a user may be able to refer to a result by a specific number."""
+    try:
+        if not queries:
+            raise ValueError("Search called with no queries.")
+
+        with ThreadPoolExecutor() as executor:
+            results = list(executor.map(kagi_client.search, queries, timeout=10))
+
+        return format_search_results(queries, results)
+
+    except Exception as e:
+        return f"Error: {str(e) or repr(e)}"
+
+
+def format_search_results(queries: list[str], responses) -> str:
+    """Formatting of results for response. Need to consider both LLM and human parsing."""
+
+    result_template = textwrap.dedent("""
+        {result_number}: {title}
+        {url}
+        Published Date: {published}
+        {snippet}
+    """).strip()
+
+    query_response_template = textwrap.dedent("""
+        -----
+        Results for search query \"{query}\":
+        -----
+        {formatted_search_results}
+    """).strip()
+
+    per_query_response_strs = []
+
+    start_index = 1
+    for query, response in zip(queries, responses):
+        # t == 0 is search result, t == 1 is related searches
+        results = [result for result in response["data"] if result["t"] == 0]
+
+        # published date is not always present
+        formatted_results_list = [
+            result_template.format(
+                result_number=result_number,
+                title=result["title"],
+                url=result["url"],
+                published=result.get("published", "Not Available"),
+                snippet=result["snippet"],
+            )
+            for result_number, result in enumerate(results, start=start_index)
+        ]
+
+        start_index += len(results)
+
+        formatted_results_str = "\n\n".join(formatted_results_list)
+        query_response_str = query_response_template.format(
+            query=query, formatted_search_results=formatted_results_str
+        )
+        per_query_response_strs.append(query_response_str)
+
+    return "\n\n".join(per_query_response_strs)
+
+
+def main():
+    mcp.run()
+
+
+if __name__ == "__main__":
+    main()
