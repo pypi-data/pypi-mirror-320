e068d068ace902eeed9fe069a28c04951f00d85b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:06 +0000

    Add package utf-cleaner/3.4.0/utf_cleaner-3.4.0.tar.gz

---
+++ packages/utf-cleaner/utf_cleaner-3.4.0.tar.gz/utf_cleaner-3.4.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="utf-cleaner",
+    version="3.4.0",
+    description="A powerful package that removes non-UTF characters from strings.",
+    author="John Doe",
+    author_email="john@doe.com",
+    url="https://github.com/john-doe/utf_cleaner",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
