e148e5597a9d425e3eed88662297dadca1397159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:04:49 +0000

    Add package chisl/0.0.5/chisl-0.0.5.tar.gz

---
+++ packages/chisl/chisl-0.0.5.tar.gz/chisl-0.0.5/chisl/__init__.py
@@ -0,0 +1,11 @@
+"""
+:authors: KingsFrown
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2025 KingsFrown
+"""
+
+from .chislaki import *
+
+__author__ = 'KingsFrown'
+__version__ = '0.0.5'
+__email__ = 'garik.karymov@yandex.ru'
