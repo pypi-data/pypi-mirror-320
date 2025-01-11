ada56eaa76eba7e754bfffa5526cf5996dfb7bb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:55 +0000

    Add package fudstop2/7.1.9/fudstop2-7.1.9.tar.gz

---
+++ packages/fudstop2/fudstop2-7.1.9.tar.gz/fudstop2-7.1.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt and remove any comments
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    requirements = [r.strip() for r in requirements if not r.startswith('#')]
+
+setup(
+    name='fudstop2',
+    version='7.1.9',
+    packages=find_packages(),
+    install_requires=requirements,
+    license="MIT",  # Specify a valid license here
+)
