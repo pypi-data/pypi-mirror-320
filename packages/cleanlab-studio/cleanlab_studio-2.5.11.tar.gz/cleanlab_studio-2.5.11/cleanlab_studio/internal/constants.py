5c5b5bf2d76753ad33b5f912435faf5a14e6a645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:07:53 +0000

    Add package cleanlab-studio/2.5.11/cleanlab_studio-2.5.11.tar.gz

---
+++ packages/cleanlab-studio/cleanlab_studio-2.5.11.tar.gz/cleanlab_studio-2.5.11/cleanlab_studio/internal/constants.py
@@ -0,0 +1,32 @@
+from typing import Dict, List, Set, Tuple
+
+# TLM constants
+# prepend constants with _ so that they don't show up in help.cleanlab.ai docs
+_VALID_TLM_QUALITY_PRESETS: List[str] = ["best", "high", "medium", "low", "base"]
+_VALID_TLM_MODELS: List[str] = [
+    "gpt-3.5-turbo-16k",
+    "gpt-4",
+    "gpt-4o",
+    "gpt-4o-mini",
+    "o1-preview",
+    "claude-3-haiku",
+    "claude-3-sonnet",
+    "claude-3.5-sonnet",
+]
+_TLM_DEFAULT_MODEL: str = "gpt-4o-mini"
+_TLM_MAX_RETRIES: int = 3  # TODO: finalize this number
+_TLM_MAX_TOKEN_RANGE: Dict[str, Tuple[int, int]] = {  # model: (min, max)
+    "gpt-3.5-turbo-16k": (64, 4096),
+    "gpt-4": (64, 4096),
+    "gpt-4o": (64, 4096),
+    "gpt-4o-mini": (64, 4096),
+    "o1-preview": (64, 4096),
+    "claude-3-haiku": (64, 512),
+    "claude-3-sonnet": (64, 512),
+    "claude-3.5-sonnet": (64, 512),
+}
+TLM_NUM_CANDIDATE_RESPONSES_RANGE: Tuple[int, int] = (1, 20)  # (min, max)
+TLM_NUM_CONSISTENCY_SAMPLES_RANGE: Tuple[int, int] = (0, 20)  # (min, max)
+TLM_VALID_LOG_OPTIONS: Set[str] = {"perplexity", "explanation"}
+TLM_VALID_GET_TRUSTWORTHINESS_SCORE_KWARGS: Set[str] = {"perplexity"}
+TLM_VALID_KWARGS: Set[str] = {"constrain_outputs"}
