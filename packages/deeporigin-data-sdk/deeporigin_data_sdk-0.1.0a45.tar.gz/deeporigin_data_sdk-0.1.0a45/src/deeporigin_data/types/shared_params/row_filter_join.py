346643cf1fe680c594e5494828e0b842a0fec768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:05:02 +0000

    Add package deeporigin-data-sdk/0.1.0a45/deeporigin_data_sdk-0.1.0a45.tar.gz

---
+++ packages/deeporigin-data-sdk/deeporigin_data_sdk-0.1.0a45.tar.gz/deeporigin_data_sdk-0.1.0a45/src/deeporigin_data/types/shared_params/row_filter_join.py
@@ -0,0 +1,19 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Literal, Required, Annotated, TypedDict
+
+from ..._utils import PropertyInfo
+from .condition import Condition
+
+__all__ = ["RowFilterJoin"]
+
+
+class RowFilterJoin(TypedDict, total=False):
+    conditions: Required[Iterable[Condition]]
+
+    filter_type: Required[Annotated[Literal["join"], PropertyInfo(alias="filterType")]]
+
+    join_type: Required[Annotated[Literal["and", "or"], PropertyInfo(alias="joinType")]]
