c4758d6c4739c82d77fe03360c339cdc7317f05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:07:53 +0000

    Add package titiler-extensions/0.19.3/titiler_extensions-0.19.3.tar.gz

---
+++ packages/titiler-extensions/titiler_extensions-0.19.3.tar.gz/titiler_extensions-0.19.3/titiler/extensions/__init__.py
@@ -0,0 +1,8 @@
+"""titiler.extensions"""
+
+__version__ = "0.19.3"
+
+from .cogeo import cogValidateExtension  # noqa
+from .stac import stacExtension  # noqa
+from .viewer import cogViewerExtension, stacViewerExtension  # noqa
+from .wms import wmsExtension  # noqa
