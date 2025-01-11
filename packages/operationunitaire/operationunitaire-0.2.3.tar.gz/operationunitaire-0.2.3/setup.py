96ea07456bebb19391003878497d32906bc9cf97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:31 +0000

    Add package operationunitaire/0.2.3/operationunitaire-0.2.3.tar.gz

---
+++ packages/operationunitaire/operationunitaire-0.2.3.tar.gz/operationunitaire-0.2.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="operationunitaire",  
+    version="0.2.3",  # Incremented version
+    description="A Python package for rigorous distillation simulations.",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="TASSAOUI Anas",
+    author_email="anass.tassaoui@gmail.com",
+    url="https://github.com/anastassaoui/operationunitaire",  
+    license="MIT",
+    packages=find_packages(),  
+    include_package_data=True,  # Include data files in the package
+    package_data={
+        "operationunitaire": ["data/*.csv"]  # Specify data files to include
+    },
+    install_requires=["numpy", "pandas", "tabulate"],  
+    python_requires=">=3.7",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
