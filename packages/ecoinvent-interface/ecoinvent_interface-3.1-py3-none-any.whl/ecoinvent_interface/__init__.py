ff5abcf4605ecb7ab5bfeb37b6474d550a682626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:13 +0000

    Add package ecoinvent-interface/3.1/ecoinvent_interface-3.1-py3-none-any.whl

---
+++ packages/ecoinvent-interface/ecoinvent_interface-3.1-py3-none-any.whl/ecoinvent_interface/__init__.py
@@ -0,0 +1,20 @@
+__all__ = [
+    "__version__",
+    "CachedStorage",
+    "EcoinventRelease",
+    "EcoinventProcess",
+    "permanent_setting",
+    "ProcessFileType",
+    "ProcessMapping",
+    "ReleaseType",
+    "Settings",
+    "get_excel_lcia_file_for_version",
+]
+
+__version__ = "3.1"
+
+from .storage import CachedStorage
+from .settings import Settings, permanent_setting
+from .release import EcoinventRelease, ReleaseType, get_excel_lcia_file_for_version
+from .process_interface import EcoinventProcess, ProcessFileType
+from .mapping import ProcessMapping
