61f1fb32070f754f9c91f93ef8a59f718a26a6d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:28 +0000

    Add package kuzu/0.7.2.dev23/kuzu-0.7.2.dev23.tar.gz

---
+++ packages/kuzu/kuzu-0.7.2.dev23.tar.gz/sdist/kuzu-source/src/include/catalog/catalog_set.h
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <functional>
+#include <shared_mutex>
+
+#include "catalog_entry/catalog_entry.h"
+#include "common/case_insensitive_map.h"
+
+namespace kuzu {
+namespace binder {
+struct BoundAlterInfo;
+} // namespace binder
+
+namespace storage {
+class UndoBuffer;
+} // namespace storage
+
+namespace transaction {
+class Transaction;
+} // namespace transaction
+
+using CatalogEntrySet = common::case_insensitive_map_t<catalog::CatalogEntry*>;
+
+namespace catalog {
+class KUZU_API CatalogSet {
+    friend class storage::UndoBuffer;
+
+public:
+    bool containsEntry(const transaction::Transaction* transaction, const std::string& name);
+    CatalogEntry* getEntry(const transaction::Transaction* transaction, const std::string& name);
+    common::oid_t createEntry(transaction::Transaction* transaction,
+        std::unique_ptr<CatalogEntry> entry);
+    void dropEntry(transaction::Transaction* transaction, const std::string& name,
+        common::oid_t oid);
+    void alterEntry(transaction::Transaction* transaction, const binder::BoundAlterInfo& alterInfo);
+
+    CatalogEntrySet getEntries(const transaction::Transaction* transaction);
+    void iterateEntriesOfType(const transaction::Transaction* transaction, CatalogEntryType type,
+        const std::function<void(const CatalogEntry*)>& func);
+    CatalogEntry* getEntryOfOID(const transaction::Transaction* transaction, common::oid_t oid);
+
+    void serialize(common::Serializer serializer) const;
+    static std::unique_ptr<CatalogSet> deserialize(common::Deserializer& deserializer);
+
+private:
+    bool containsEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    CatalogEntry* getEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    CatalogEntry* createEntryNoLock(const transaction::Transaction* transaction,
+        std::unique_ptr<CatalogEntry> entry);
+    CatalogEntry* dropEntryNoLock(const transaction::Transaction* transaction,
+        const std::string& name, common::oid_t oid);
+
+    void validateExistNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+    void validateNotExistNoLock(const transaction::Transaction* transaction,
+        const std::string& name) const;
+
+    void emplaceNoLock(std::unique_ptr<CatalogEntry> entry);
+    void eraseNoLock(const std::string& name);
+
+    static std::unique_ptr<CatalogEntry> createDummyEntryNoLock(std::string name,
+        common::oid_t oid);
+
+    static CatalogEntry* traverseVersionChainsForTransactionNoLock(
+        const transaction::Transaction* transaction, CatalogEntry* currentEntry);
+    static CatalogEntry* getCommittedEntryNoLock(CatalogEntry* entry);
+
+private:
+    std::shared_mutex mtx;
+    common::oid_t nextOID = 0;
+    common::case_insensitive_map_t<std::unique_ptr<CatalogEntry>> entries;
+};
+
+} // namespace catalog
+} // namespace kuzu
---
+++ packages/kuzu/kuzu-0.7.2.dev23.tar.gz/sdist/kuzu-source/src/include/function/gds/gds_utils.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <optional>
+
+#include "common/enums/extend_direction.h"
+#include "common/types/types.h"
+
+namespace kuzu {
+namespace processor {
+struct ExecutionContext;
+class NodeOffsetMaskMap;
+} // namespace processor
+
+namespace graph {
+class Graph;
+}
+
+namespace function {
+struct FrontierTaskSharedState;
+struct RJCompState;
+class VertexCompute;
+class EdgeCompute;
+class FrontierPair;
+class SparseFrontier;
+struct VertexComputeTaskSharedState;
+struct VertexComputeTaskInfo;
+
+struct KUZU_API GDSComputeState {
+    std::unique_ptr<function::FrontierPair> frontierPair;
+    std::unique_ptr<function::EdgeCompute> edgeCompute;
+
+    processor::NodeOffsetMaskMap* outputNodeMask = nullptr;
+
+    GDSComputeState(std::unique_ptr<function::FrontierPair> frontierPair,
+        std::unique_ptr<function::EdgeCompute> edgeCompute,
+        processor::NodeOffsetMaskMap* outputNodeMask);
+    virtual ~GDSComputeState();
+
+    virtual void beginFrontierComputeBetweenTables(common::table_id_t currTableID,
+        common::table_id_t nextTableID);
+};
+
+class KUZU_API GDSUtils {
+public:
+    static void scheduleFrontierTask(common::table_id_t boundTableID, common::table_id_t nbrTableID,
+        common::table_id_t relTableID, graph::Graph* graph, common::ExtendDirection extendDirection,
+        GDSComputeState& rjCompState, processor::ExecutionContext* context,
+        std::optional<uint64_t> numThreads = std::nullopt,
+        std::optional<common::idx_t> edgePropertyIdx = std::nullopt);
+    static void runFrontiersUntilConvergence(processor::ExecutionContext* context,
+        GDSComputeState& rjCompState, graph::Graph* graph, common::ExtendDirection extendDirection,
+        uint64_t maxIters);
+    // Run vertex compute without property scan
+    static void runVertexCompute(processor::ExecutionContext* context, graph::Graph* graph,
+        VertexCompute& vc);
+    // Run vertex compute with property scan
+    static void runVertexCompute(processor::ExecutionContext* context, graph::Graph* graph,
+        VertexCompute& vc, std::vector<std::string> propertiesToScan);
+    // Run vertex compute on specific table with property scan
+    static void runVertexCompute(processor::ExecutionContext* context, graph::Graph* graph,
+        VertexCompute& vc, common::table_id_t tableID, std::vector<std::string> propertiesToScan);
+    static void runVertexComputeSparse(SparseFrontier& sparseFrontier, graph::Graph* graph,
+        VertexCompute& vc);
+};
+
+} // namespace function
+} // namespace kuzu
