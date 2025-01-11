20378a2be3ab6c02126247fa07dab02cbe7fce62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:33 +0000

    Add package crackle-codec/0.17.3/crackle_codec-0.17.3.tar.gz

---
+++ packages/crackle-codec/crackle_codec-0.17.3.tar.gz/crackle_codec-0.17.3/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+import sys
+
+extra_compile_args = []
+if sys.platform == 'win32':
+  extra_compile_args += [
+    '/std:c++20', '/O2'
+  ]
+else:
+  extra_compile_args += [
+    '-std=c++2a', '-O3'
+  ]
+
+setuptools.setup(
+  setup_requires=['pbr','pybind11','numpy'],
+  cmdclass={"build_ext": build_ext},
+  extras_require={
+    "remote": [
+      "cloud-files",
+    ],
+  },
+  ext_modules=[
+    Pybind11Extension(
+        "fastcrackle",
+        ["src/fastcrackle.cpp"],
+        extra_compile_args=extra_compile_args,
+        language="c++",
+    ),
+  ],
+  entry_points={
+    "console_scripts": [
+      "crackle=crackle_cli:main"
+    ],
+  },
+  pbr=True
+)
