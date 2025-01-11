b2efbe7897e68d73571f7197ef097eec5ad0b861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:30 +0000

    Add package femupy/0.1.3/femupy-0.1.3.tar.gz

---
+++ packages/femupy/femupy-0.1.3.tar.gz/femupy-0.1.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='femupy',
+    version='0.1.3',
+    author='Blaž Kurent',
+    author_email='blaz.kurent@fgg.uni-lj.si',
+    description='Finite element model updating using natural frequencies and mode shapes',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/blazkurent/femupy',
+    packages=find_packages(),
+    py_modules=['femupy'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'seaborn',
+        'matplotlib',
+        'tensorflow',
+        'scikit-learn',
+        'scipy',
+        'SALib',
+        'pymoo'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+    ],
+)
