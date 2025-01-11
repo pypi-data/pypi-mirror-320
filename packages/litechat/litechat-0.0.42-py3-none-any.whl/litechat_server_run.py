029ebfba2e19c42181e4ad1e825c285e37eee274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:03:56 +0000

    Add package litechat/0.0.42/litechat-0.0.42-py3-none-any.whl

---
+++ packages/litechat/litechat-0.0.42-py3-none-any.whl/litechat_server_run.py
@@ -0,0 +1,8 @@
+from litechat import litechat_server
+from litechat import litechat_codegpt_server
+
+def run_server():
+    litechat_server()
+
+def codegpt_server():
+    litechat_codegpt_server()
