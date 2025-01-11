dbf68e6732274408957206e6b1564535086a0d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:08:31 +0000

    Add package canvas/0.13.1/canvas-0.13.1-py3-none-any.whl

---
+++ packages/canvas/canvas-0.13.1-py3-none-any.whl/settings.py
@@ -0,0 +1,71 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from env_tools import env_to_bool, get_enforcement_context
+
+from canvas_sdk.utils.db import get_database_dict_from_url
+
+require_env, enforce_required_envs = get_enforcement_context()
+
+load_dotenv()
+
+ENV = os.getenv("ENV", "development")
+IS_PRODUCTION = ENV == "production"
+IS_TESTING = env_to_bool("IS_TESTING", "pytest" in sys.argv[0] or sys.argv[0] == "-c")
+CUSTOMER_IDENTIFIER = require_env("CUSTOMER_IDENTIFIER")
+
+INTEGRATION_TEST_URL = os.getenv("INTEGRATION_TEST_URL")
+INTEGRATION_TEST_CLIENT_ID = os.getenv("INTEGRATION_TEST_CLIENT_ID")
+INTEGRATION_TEST_CLIENT_SECRET = os.getenv("INTEGRATION_TEST_CLIENT_SECRET")
+
+GRAPHQL_ENDPOINT = os.getenv("GRAPHQL_ENDPOINT", "http://localhost:8000/plugins-graphql")
+
+INSTALLED_APPS = ["canvas_sdk"]
+
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    "This value is the key to securing signed data – it is vital you keep this secure, or attackers could use it to generate their own signed values.",
+)
+
+# Use BigAutoField for Default Primary Key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CANVAS_SDK_DB_NAME = os.getenv("CANVAS_SDK_DB_NAME", "home-app")
+CANVAS_SDK_DB_USERNAME = os.getenv("CANVAS_SDK_DB_USERNAME", "app")
+CANVAS_SDK_DB_PASSWORD = os.getenv("CANVAS_SDK_DB_PASSWORD", "app")
+CANVAS_SDK_DB_HOST = os.getenv("CANVAS_SDK_DB_HOST", "home-app-db")
+CANVAS_SDK_DB_PORT = os.getenv("CANVAS_SDK_DB_PORT", "5432")
+
+if os.getenv("DATABASE_URL"):
+    database_dict = get_database_dict_from_url()
+else:
+    database_dict = {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": CANVAS_SDK_DB_NAME,
+        "USER": CANVAS_SDK_DB_USERNAME,
+        "PASSWORD": CANVAS_SDK_DB_PASSWORD,
+        "HOST": CANVAS_SDK_DB_HOST,
+        "PORT": CANVAS_SDK_DB_PORT,
+    }
+
+DATABASES = {"default": database_dict}
+
+
+PLUGIN_RUNNER_SIGNING_KEY = os.getenv("PLUGIN_RUNNER_SIGNING_KEY", "")
+
+PLUGIN_DIRECTORY = os.getenv(
+    "PLUGIN_DIRECTORY",
+    (
+        "/plugin-runner/custom-plugins"
+        if IS_PRODUCTION
+        else "./plugin_runner/tests/data/plugins"
+        if IS_TESTING
+        else "./custom-plugins"
+    ),
+)
+
+MANIFEST_FILE_NAME = "CANVAS_MANIFEST.json"
+
+SECRETS_FILE_NAME = "SECRETS.json"
+enforce_required_envs()
