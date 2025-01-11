a1a6c42fdadd712fb99fd4ae050abe647b96007e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:05 +0000

    Add package xbase-util/0.9.0/xbase_util-0.9.0.tar.gz

---
+++ packages/xbase-util/xbase_util-0.9.0.tar.gz/xbase_util-0.9.0/xbase_util/test.py
@@ -0,0 +1,9 @@
+from scapy.utils import rdpcap
+
+from xbase_util.pcap_util import reassemble_tcp_pcap, reassemble_session_pcap
+
+if __name__ == '__main__':
+    packets_scapy = reassemble_tcp_pcap(rdpcap("test.pcap"))
+    skey = '10.28.7.13:52631'
+    all_packets = reassemble_session_pcap(packets_scapy, skey=skey,session_id='enn')
+    print(all_packets)
