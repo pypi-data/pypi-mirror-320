005f560c9ba5414e6d2c4a0f05b39c2ddc9cfbc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:04:49 +0000

    Add package nifti-overlay/0.0.1/nifti_overlay-0.0.1-py3-none-any.whl

---
+++ packages/nifti-overlay/nifti_overlay-0.0.1-py3-none-any.whl/nifti_overlay/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 14:58:52 2024
+
+@author: earnestt1234
+"""
+
+from nifti_overlay.core import NiftiOverlay
+
+__all__ = ['NiftiOverlay']
