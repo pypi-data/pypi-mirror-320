d1d3d6ad5df8e783fb4e0905395d7d19e3b6ff92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:04 +0000

    Add package undouble-gui/0.1.0/undouble_gui-0.1.0-py3-none-any.whl

---
+++ packages/undouble-gui/undouble_gui-0.1.0-py3-none-any.whl/undouble_gui/examples.py
@@ -0,0 +1,13 @@
+# # %%
+# from tkinter import Tk
+# from undouble.gui import Gui
+
+# image_groups = model.results['select_pathnames']
+
+# # Create the root Tkinter window
+# root = Tk()
+# # Initialize the ImageMoverApp
+# app = Gui(root, image_groups)
+# # Run the Tkinter mainloop
+# root.mainloop()
+
