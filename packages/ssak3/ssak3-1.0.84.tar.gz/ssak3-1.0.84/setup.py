bb36f6e7f661646723b93b3578efbbe805436e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:03:28 +0000

    Add package ssak3/1.0.84/ssak3-1.0.84.tar.gz

---
+++ packages/ssak3/ssak3-1.0.84.tar.gz/ssak3-1.0.84/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="ssak3",
+    version="1.0.84",
+    packages=find_namespace_packages(),       # 패키지 포함
+    include_package_data=True,
+    install_requires=[
+        'apify==2.0.0',
+        'loguru==0.7.2',
+        'tzlocal==5.2',
+    ],
+    author="bigzzodev",
+    author_email="bigzzodev@gmail.com",
+    description="ssak3 package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/bigzzodev",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
