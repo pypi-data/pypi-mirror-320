72830e6ed7cc8c3d1258d61d72c7c46450cf82c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:12 +0000

    Add package sqlmodel-fastapi-crud/0.1.0/sqlmodel_fastapi_crud-0.1.0.tar.gz

---
+++ packages/sqlmodel-fastapi-crud/sqlmodel_fastapi_crud-0.1.0.tar.gz/sqlmodel_fastapi_crud-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name="sqlmodel_fastapi_crud",
+    version="0.1.0",
+    description="A reusable CRUD base with filter class for FastAPI applications using SQLModel.",
+    author="shareef",
+    author_email="your.email@example.com",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "sqlmodel>=0.0.22",
+        "fastapi>=0.115.6",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
