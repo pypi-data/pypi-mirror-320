655f90890b311790edd9de236634eb49cfcadaf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:30 +0000

    Add package melodi/0.1.14/melodi-0.1.14-py3-none-any.whl

---
+++ packages/melodi/melodi-0.1.14-py3-none-any.whl/melodi/users/data_models.py
@@ -0,0 +1,28 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class User(BaseModel):
+    externalId: str
+    email: Optional[EmailStr] = None
+    name: Optional[str] = None
+    username: Optional[str] = None
+    segments: Optional[dict[str, List[str]]] = {}
+
+class UserSegmentTypeResponse(BaseModel):
+    id: int
+    name: str
+
+class UserSegmentRespone(BaseModel):
+    id: int
+    type: UserSegmentTypeResponse
+    name: str
+
+class UserResponse(BaseModel):
+    id: int
+    externalId: str
+    email: Optional[str] = None
+    name: Optional[str] = None
+    username: Optional[str] = None
+    segments: List[UserSegmentRespone]
