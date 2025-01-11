4560126a9300ccd6eb7f251154cf978e2bb4dbb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 14:04:35 +0000

    Add package lillie-start/0.1.1/lillie_start-0.1.1.tar.gz

---
+++ packages/lillie-start/lillie_start-0.1.1.tar.gz/lillie_start-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lillie-start",  
+    version="0.1.1",  #update
+    packages=find_packages(),  
+    py_modules=["start"],  
+    install_requires=[
+        "typer[all]",  
+        "colorama",    
+    ],
+    entry_points={
+        "console_scripts": [
+            "start-lillie=start:start",  
+        ],
+    },
+    author="Sarthak Ghoshal",
+    description="A script to run lillie.config.py with a terminal command.",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
