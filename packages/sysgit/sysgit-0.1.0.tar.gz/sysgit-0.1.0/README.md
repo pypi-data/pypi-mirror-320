96b4f065bee14eab9d54ebbdfbc899fdaf826732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:05:13 +0000

    Add package sysgit/0.1.0/sysgit-0.1.0.tar.gz

---
+++ packages/sysgit/sysgit-0.1.0.tar.gz/sysgit-0.1.0/README.md
@@ -0,0 +1,3 @@
+# sysgit
+
+When the internal logic for this package is implemented, more detail will be added.
---
+++ packages/sysgit/sysgit-0.1.0.tar.gz/sysgit-0.1.0/sysgit/__meta__.py
@@ -0,0 +1,4 @@
+from importlib import metadata
+
+__name__ = __package__ = __project__ = "sysgit"
+__version__ = metadata.version(__project__)
