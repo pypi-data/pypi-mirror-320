117da5e4fcccb16fcd08f852d2a4b7d062f267f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:05 +0000

    Add package nonebot-plugin-suggarchat/0.1.6.5/nonebot_plugin_suggarchat-0.1.6.5.tar.gz

---
+++ packages/nonebot-plugin-suggarchat/nonebot_plugin_suggarchat-0.1.6.5.tar.gz/nonebot_plugin_suggarchat-0.1.6.5/src/nonebot_plugin_suggarchat/conf.py
@@ -0,0 +1,16 @@
+import os
+from pathlib import Path
+__KERNEL_VERSION__:str = "V0.1.6.5-Public-Dev"
+# 获取当前工作目录  
+current_directory:str = os.getcwd()  
+config_dir = Path.cwd()/"config"
+if not config_dir.exists():
+    config_dir.mkdir()
+group_memory = Path.cwd()/"group"
+if not group_memory.exists():
+    group_memory.mkdir()
+private_memory = Path.cwd()/"private"
+if not private_memory.exists():
+    private_memory.mkdir()
+main_config = config_dir/"config.json"
+custom_models_dir = config_dir/"models"
