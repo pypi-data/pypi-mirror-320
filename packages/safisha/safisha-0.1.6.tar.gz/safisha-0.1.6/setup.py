7b9f1cd67d098ef945e227d9a83f2b457767b6d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:25 +0000

    Add package safisha/0.1.6/safisha-0.1.6.tar.gz

---
+++ packages/safisha/safisha-0.1.6.tar.gz/safisha-0.1.6/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="safisha",
+    version="0.1.6",
+    description="Programu ya kusafisha Mac yako.",
+    author="Mark Francis",
+    author_email="safisha@ngao.pro",
+    packages=find_packages(),
+    install_requires=[],  # Add dependencies here if needed
+    entry_points={
+        "console_scripts": [
+            "safisha=safisha.cli:main",  # Ensure this is correct
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+)
