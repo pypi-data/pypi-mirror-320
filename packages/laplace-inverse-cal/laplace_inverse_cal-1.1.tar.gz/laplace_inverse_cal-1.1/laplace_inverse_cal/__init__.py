70b7a994f7d541600622f92ea4336e6d4991b5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:03:42 +0000

    Add package laplace-inverse-cal/1.1/laplace_inverse_cal-1.1.tar.gz

---
+++ packages/laplace-inverse-cal/laplace_inverse_cal-1.1.tar.gz/laplace_inverse_cal-1.1/laplace_inverse_cal/__init__.py
@@ -0,0 +1,10 @@
+import networkx as nx
+import numpy as np
+import mpmath
+def inverse_laplace(F, t_val):
+    if t_val <= 0:
+        return 0 
+    F_func = sp.lambdify(s, F, 'mpmath')
+    return float(mpmath.invertlaplace(F_func, t_val, method='talbot'))
+
+__all__ = ['inverse_laplace']
