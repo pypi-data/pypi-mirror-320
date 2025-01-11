94edda679236ceb00a55ae72edfdf5d00812d3e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:17 +0000

    Add package metaflow-diff/0.1.2/metaflow-diff-0.1.2.tar.gz

---
+++ packages/metaflow-diff/metaflow-diff-0.1.2.tar.gz/metaflow-diff-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="metaflow-diff",
+    version="0.1.2",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "metaflow-diff=metaflow_diff.metaflow_diff:cli",
+        ],
+    },
+    install_requires=[
+        "metaflow",
+        "click"
+    ],
+    author="Ville Tuulos",
+    author_email="ville@outerbounds.co",
+    description="See and apply diffs between the current working directory and a Metaflow run",
+    python_requires=">=3.9"
+)
