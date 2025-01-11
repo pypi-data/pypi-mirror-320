1780f2303b841708ddc4e680c545e3f3f95638c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:24 +0000

    Add package bb-projects-calc/0.2.0/bb_projects_calc-0.2.0-py3-none-any.whl

---
+++ packages/bb-projects-calc/bb_projects_calc-0.2.0-py3-none-any.whl/bb_projects_calc/action/add_action.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    """
+    Multiply two numbers
+    :param a: first number
+    :param b: second number
+    :return: multiplication of two numbers
+    """
+    return a + b
---
+++ packages/bb-projects-calc/bb_projects_calc-0.2.0-py3-none-any.whl/bb_projects_calc/action/mul_action.py
@@ -0,0 +1,8 @@
+def mul(a: int, b: int) -> int:
+    """
+    Multiply two numbers
+    :param a: first number
+    :param b: second number
+    :return: multiplication of two numbers
+    """
+    return a * b
