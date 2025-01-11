78e9c718bec770d86d3e9b3293b01e9d8895da37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:29 +0000

    Add package rayuwu/1.1.3/rayuwu-1.1.3.tar.gz

---
+++ packages/rayuwu/rayuwu-1.1.3.tar.gz/rayuwu-1.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rayuwu', 
+    version='1.1.3',
+    packages=find_packages(),
+    install_requires=['requests'],
+    description='A package to interact with Discord API',
+    author='ray',
+    author_email='me.harshu.yt.04@gmail.com',
+    url='https://github.com/theabsoluteray/ray-wrapper',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
