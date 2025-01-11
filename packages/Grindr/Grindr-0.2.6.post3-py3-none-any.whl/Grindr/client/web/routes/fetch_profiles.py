0a4232b40ee58cbaa9e81cd7ad4688b0bf445539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:32 +0000

    Add package Grindr/0.2.6.post3/Grindr-0.2.6.post3-py3-none-any.whl

---
+++ packages/Grindr/Grindr-0.2.6.post3-py3-none-any.whl/Grindr/client/web/routes/fetch_profiles.py
@@ -0,0 +1,44 @@
+from typing import Optional, List
+
+from pydantic import BaseModel
+
+from Grindr.client.web.web_base import ClientRoute, URLTemplate, BodyParams
+from Grindr.client.web.web_settings import GRINDR_V3
+
+
+class ProfileData(BaseModel):
+    profileId: int | None = None
+    displayName: str | None = None
+    profileImageMediaHash: str | None = None
+    distance: float | None = None
+    isFavorite: bool | None = None
+    timestamp: int | None = None
+    tapType: int | None = None
+    lastOnline: int | None = None
+    isBoosting: bool | None = None
+    isMutual: bool | None = None
+    rightNowType: str | None = None
+    isViewable: bool | None = None
+
+
+class FetchProfilesRouteResponse(BaseModel):
+    profiles: list[ProfileData]
+
+
+class FetchProfilesRouteBody(BodyParams):
+    targetProfileIds: list[str]
+
+
+class FetchProfilesRoute(
+    ClientRoute[
+        "POST",
+        URLTemplate(GRINDR_V3, "/profiles"),
+        None,
+        FetchProfilesRouteBody,
+        FetchProfilesRouteResponse
+    ]
+):
+    """
+    Retrieve a session from the API
+
+    """
