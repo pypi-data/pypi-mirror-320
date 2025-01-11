80b65b63284b89d325695eba46f167c46e461665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:08:31 +0000

    Add package lytools/0.0.121/lytools-0.0.121.tar.gz

---
+++ packages/lytools/lytools-0.0.121.tar.gz/lytools-0.0.121/setup.py
@@ -0,0 +1,29 @@
+# coding='utf-8'
+
+from setuptools import setup
+long_description = open('README.md').read()
+setup(
+    name='lytools',
+    version='0.0.121',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Yang Li',
+    author_email='leeyang1991@gmail.com',
+    packages=['lytools'],
+    url='https://github.com/leeyang1991/lytools',
+    python_requires='>=3',
+    install_requires=[
+    'matplotlib',
+    'numpy',
+    'scipy',
+    'gdal',
+    'tqdm',
+    'pandas',
+    'seaborn',
+    'scikit-learn',
+    'outdated',
+    'basemap',
+    'netCDF4',
+    'psutil',
+    ],
+)
