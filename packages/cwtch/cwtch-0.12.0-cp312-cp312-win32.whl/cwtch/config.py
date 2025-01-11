72efc6adf6423298f35b96422d43a39f4e7b977c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:04:58 +0000

    Add package cwtch/0.12.0/cwtch-0.12.0-cp312-cp312-win32.whl

---
+++ packages/cwtch/cwtch-0.12.0-cp312-cp312-win32.whl/cwtch/config.py
@@ -0,0 +1,14 @@
+from typing import Literal
+
+
+SLOTS: bool = False
+KW_ONLY: bool = False
+VALIDATE: bool = True
+ADD_DISABLE_VALIDATION_TO_INIT: bool = False
+SHOW_INPUT_VALUE_ON_ERROR: bool = True
+EXTRA: Literal["ignore", "forbid"] = "ignore"
+REPR: bool = True
+EQ: bool = True
+ATTACH: bool = True
+RECURSIVE: bool = True
+HANDLE_CIRCULAR_REFS: bool = False
