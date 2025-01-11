6b25f1649dfe80d057f4578a13d147c1fd3f159a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:29 +0000

    Add package xbox-switch-bridge/0.3.1/xbox_switch_bridge-0.3.1.tar.gz

---
+++ packages/xbox-switch-bridge/xbox_switch_bridge-0.3.1.tar.gz/xbox_switch_bridge-0.3.1/src/xboxswitchbridge/__init__.py
@@ -0,0 +1,10 @@
+"""Xbox Switch Bridge - Use Xbox controller with Nintendo Switch via Raspberry Pi."""
+
+from .bridge import XboxSwitchBridge
+from .service import install_service, uninstall_service, check_service_status
+
+__version__ = "0.3.1"
+__author__ = "Leonardo Carta"
+__email__ = "leonardo@carta.vision"
+
+__all__ = ['XboxSwitchBridge', 'install_service', 'uninstall_service', 'check_service_status']
