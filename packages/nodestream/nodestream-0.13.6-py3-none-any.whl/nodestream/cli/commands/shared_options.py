d7bd8fdf151277a351d8f760b6dc63c0b8e98f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:47 +0000

    Add package nodestream/0.13.6/nodestream-0.13.6-py3-none-any.whl

---
+++ packages/nodestream/nodestream-0.13.6-py3-none-any.whl/nodestream/cli/commands/shared_options.py
@@ -0,0 +1,29 @@
+from cleo.helpers import argument, option
+
+PROJECT_FILE_OPTION = option(
+    "project", "p", "The project file (nodestream.yaml) to load.", flag=False
+)
+
+DATABASE_NAME_OPTION = option(
+    "database", "d", "The Database to Configure", flag=False, default="neo4j"
+)
+
+SCOPE_NAME_OPTION = option(
+    "scope", "s", "Use the provided scope for the operation", flag=False
+)
+
+JSON_OPTION = option("json", "j", "Log output in JSON", flag=True)
+
+
+PIPELINE_ARGUMENT = argument("pipeline", "the name of the pipeline")
+MANY_PIPELINES_ARGUMENT = argument(
+    "pipelines", "the names of the pipelines", multiple=True, optional=True
+)
+
+TARGETS_OPTION = option(
+    "target",
+    "t",
+    "Specify a database to target at run time.",
+    multiple=True,
+    flag=False,
+)
---
+++ packages/nodestream/nodestream-0.13.6-py3-none-any.whl/nodestream/pipeline/__init__.py
@@ -0,0 +1,39 @@
+from .class_loader import ClassLoader
+from .extractors import Extractor, IterableExtractor
+from .filters import (
+    ExcludeWhenValuesMatchPossibilities,
+    Filter,
+    ValueMatchesRegexFilter,
+    ValuesMatchPossibilitiesFilter,
+)
+from .flush import Flush
+from .meta import UNKNOWN_PIPELINE_NAME
+from .pipeline import Pipeline
+from .pipeline_file_loader import PipelineFile, PipelineInitializationArguments
+from .progress_reporter import PipelineProgressReporter
+from .scope_config import ScopeConfig
+from .step import PassStep, Step
+from .transformers import Transformer
+from .writers import LoggerWriter, Writer
+
+__all__ = (
+    "ClassLoader",
+    "Extractor",
+    "IterableExtractor",
+    "Filter",
+    "ValuesMatchPossibilitiesFilter",
+    "ValueMatchesRegexFilter",
+    "ExcludeWhenValuesMatchPossibilities",
+    "PipelineFile",
+    "Pipeline",
+    "ScopeConfig",
+    "Step",
+    "Transformer",
+    "Writer",
+    "LoggerWriter",
+    "PipelineInitializationArguments",
+    "PassStep",
+    "Flush",
+    "UNKNOWN_PIPELINE_NAME",
+    "PipelineProgressReporter",
+)
