f71d6160505035486a17a123665969cd8d76c929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 15:04:51 +0000

    Add package testcontainers-iris/1.2.0/testcontainers_iris-1.2.0.tar.gz

---
+++ packages/testcontainers-iris/testcontainers_iris-1.2.0.tar.gz/testcontainers_iris-1.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_namespace_packages
+
+description = "InterSystems IRIS component of testcontainers-python."
+
+setup(
+    packages=find_namespace_packages(),
+    description=description,
+    install_requires=[
+        "testcontainers",
+        "sqlalchemy",
+        "sqlalchemy-iris",
+        "requests<2.32.0"
+    ],
+    python_requires=">=3.7",
+)
