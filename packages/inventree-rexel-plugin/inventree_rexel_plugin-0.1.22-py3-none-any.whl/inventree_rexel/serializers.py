98d4e1ce8c2e36b3b5319ae9840ed21d4a0a8022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 03:08:06 +0000

    Add package inventree-rexel-plugin/0.1.22/inventree_rexel_plugin-0.1.22-py3-none-any.whl

---
+++ packages/inventree-rexel-plugin/inventree_rexel_plugin-0.1.22-py3-none-any.whl/inventree_rexel/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+
+
+class RexelRequestSerializer(serializers.Serializer):
+    """Serializer voor validatie van Rexel data."""
+    product_number = serializers.CharField(max_length=255, required=True)
+    part_number = serializers.CharField(max_length=255, required=True)
