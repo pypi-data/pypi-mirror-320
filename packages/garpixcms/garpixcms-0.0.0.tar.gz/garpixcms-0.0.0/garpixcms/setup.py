fbecdd630ae60f70816fe7593f203ca7936b2fd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:05 +0000

    Add package garpixcms/0.0.0/garpixcms-0.0.0.tar.gz

---
+++ packages/garpixcms/garpixcms-0.0.0.tar.gz/garpixcms-0.0.0/garpixcms/setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+from os import path
+
+__name__ in ['__main__', 'builtins'] and __import__('setuptools').setup()
+
+here = path.join(path.abspath(path.dirname(__file__)), 'garpixcms')
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='garpixcms',
+    version='4.6.0-rc37',
+    description='',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/garpixcms/garpixcms',
+    project_urls={
+        'Documentation': 'https://docs.garpixcms.ru/',
+        'GitHub': 'https://github.com/garpixcms/garpixcms/',
+        'Changelog': 'https://github.com/garpixcms/garpixcms/blob/master/CHANGELOG.md/',
+    },
+    author='Garpix LTD',
+    author_email='info@garpix.com',
+    license='MIT',
+    packages=find_packages(exclude=['testproject', 'testproject.*']),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3.8',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Django >= 3.1, < 5',
+        'garpix_utils >= 1.10, < 2',
+        'garpix_page >= 2.49.0-rc6, < 3',
+        'garpix_menu == 1.17.1',
+        'eqator >= 2.7, < 3',
+        'garpix_auth == 2.3.0',
+        'garpix_notify >= 5.17.0-rc1 < 6',
+        'garpix_user >= 3.10.0-rc25, < 4',
+        'garpix_package == 2.0.1',
+        'psycopg2-binary >= 2.8.6',
+        'uwsgi >= 2.0.19.1',
+        'environs >= 9.3.2',
+        'django-ckeditor == 6.3.0',
+        'djangorestframework >= 3.12.4',
+        'django-cors-headers >= 3.7.0, <= 3.13.0',
+        'drf-spectacular >= 0.18.2',
+        'sentry-sdk >= 1.5.2',
+        'pre-commit >= 2.17.0',
+    ],
+)
---
+++ packages/garpixcms/garpixcms-0.0.0.tar.gz/garpixcms-0.0.0/garpixcms/urls.py
@@ -0,0 +1,48 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.conf.urls.i18n import i18n_patterns
+from multiurl.multiurl import ContinueResolving, multiurl
+from django.http import Http404
+from django.conf import settings
+from django.conf.urls.static import static
+from garpix_user.views import LogoutView, LoginView, obtain_auth_token
+from garpix_page.views.page import PageView
+from garpix_page.views.index import IndexView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('page_lock/', include('garpix_admin_lock.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path(f'{settings.API_URL}/social-auth/', include('rest_framework_social_oauth2.urls'),
+         name=settings.DRFSO2_URL_NAMESPACE),
+    path('', include(('garpix_user.urls', 'garpix_user'), namespace='garpix_user')),
+    path(f'{settings.API_URL}/garpix_user/token/', obtain_auth_token, name='token'),
+    path('', include(('garpix_page.urls', 'garpix_page'), namespace='garpix_page')),
+]
+
+urlpatterns += [
+    path('logout/', LogoutView.as_view(url='/'), name="logout"),
+    path('login/', LoginView.as_view(), name="authorize")
+]
+
+
+if settings.DEBUG or settings.ENABLE_SWAGGER:
+    urlpatterns += [
+        path(f'{settings.API_URL}/schema/', SpectacularAPIView.as_view(), name='schema'),
+        path(f'{settings.API_URL}/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += i18n_patterns(
+    multiurl(
+        path('', PageView.as_view()),
+        re_path(r'^(?P<url>.*?)$', PageView.as_view(), name='page'),
+        path('', IndexView.as_view()),
+        catch=(Http404, ContinueResolving),
+    ),
+    prefix_default_language=settings.USE_DEFAULT_LANGUAGE_PREFIX,
+)
