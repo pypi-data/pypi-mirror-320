b460861e03d4f236c0792fdc67a109bca91f249c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:34 +0000

    Add package qLDPC/0.0.22/qldpc-0.0.22.tar.gz

---
+++ packages/qLDPC/qldpc-0.0.22.tar.gz/qldpc-0.0.22/qldpc/codes/__init__.py
@@ -0,0 +1,51 @@
+from .classical import (
+    BCHCode,
+    HammingCode,
+    ReedMullerCode,
+    ReedSolomonCode,
+    RepetitionCode,
+    RingCode,
+    TannerCode,
+)
+from .common import (
+    ClassicalCode,
+    CSSCode,
+    QuditCode,
+)
+from .quantum import (
+    BBCode,
+    FiveQubitCode,
+    GeneralizedSurfaceCode,
+    HGPCode,
+    LPCode,
+    QCCode,
+    QTCode,
+    SteaneCode,
+    SurfaceCode,
+    TBCode,
+    ToricCode,
+)
+
+__all__ = [
+    "BCHCode",
+    "HammingCode",
+    "ReedMullerCode",
+    "ReedSolomonCode",
+    "RepetitionCode",
+    "RingCode",
+    "TannerCode",
+    "ClassicalCode",
+    "CSSCode",
+    "QuditCode",
+    "BBCode",
+    "FiveQubitCode",
+    "GeneralizedSurfaceCode",
+    "HGPCode",
+    "LPCode",
+    "QCCode",
+    "QTCode",
+    "SteaneCode",
+    "SurfaceCode",
+    "TBCode",
+    "ToricCode",
+]
