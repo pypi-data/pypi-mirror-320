efd0f599f8d074ca67e7890bf7adb49313bcba03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 14:05:39 +0000

    Add package hf-xet/0.1.0/hf_xet-0.1.0.tar.gz

---
+++ packages/hf-xet/hf_xet-0.1.0.tar.gz/hf_xet-0.1.0/hf_xet/src/log.rs
@@ -0,0 +1,49 @@
+use std::env;
+use std::sync::Arc;
+
+use pyo3::Python;
+use tracing_subscriber::filter::FilterFn;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{EnvFilter, Layer};
+use utils::ThreadPool;
+
+use crate::log_buffer::{get_telemetry_task, LogBufferLayer, TELEMETRY_PRE_ALLOC_BYTES};
+
+/// Default log level for the library to use. Override using `RUST_LOG` env variable.
+#[cfg(not(debug_assertions))]
+const DEFAULT_LOG_LEVEL: &str = "warn";
+
+#[cfg(debug_assertions)]
+const DEFAULT_LOG_LEVEL: &str = "info";
+
+pub fn initialize_logging(py: Python, runtime: Arc<ThreadPool>) {
+    let fmt_layer = tracing_subscriber::fmt::layer()
+        .with_line_number(true)
+        .with_file(true)
+        .with_target(false)
+        .json();
+
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new(DEFAULT_LOG_LEVEL))
+        .unwrap_or_default();
+
+    if env::var("HF_HUB_DISABLE_TELEMETRY").as_deref() == Ok("1") {
+        tracing_subscriber::registry().with(fmt_layer).with(filter_layer).init();
+    } else {
+        let telemetry_buffer_layer = LogBufferLayer::new(py, TELEMETRY_PRE_ALLOC_BYTES);
+        let telemetry_task =
+            get_telemetry_task(telemetry_buffer_layer.buffer.clone(), telemetry_buffer_layer.stats.clone());
+
+        let telemetry_filter_layer =
+            telemetry_buffer_layer.with_filter(FilterFn::new(|meta| meta.target() == "client_telemetry"));
+
+        tracing_subscriber::registry()
+            .with(fmt_layer)
+            .with(filter_layer)
+            .with(telemetry_filter_layer)
+            .init();
+
+        let _telemetry_task = runtime.spawn(telemetry_task);
+    }
+}
