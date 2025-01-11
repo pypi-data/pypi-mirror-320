124972c32602082d9a0f2372badb3f94b4cd75ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:04:01 +0000

    Add package jsonfeed-util/1.1.4/jsonfeed_util-1.1.4.tar.gz

---
+++ packages/jsonfeed-util/jsonfeed_util-1.1.4.tar.gz/jsonfeed_util-1.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version = "1.1.4"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="jsonfeed-util",
+    version=version,
+    packages=find_packages(),
+    # metadata for upload to PyPI
+    author="Lukas Schwab",
+    author_email="lukas.schwab@gmail.com",
+    description="Python package for parsing and generating JSON feeds.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="json feed jsonfeed",
+    url="https://github.com/lukasschwab/jsonfeed",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
