8145cf859e41cb44c199240bde0a6cb2447c8515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:03:56 +0000

    Add package hdfset/0.2.5/hdfset-0.2.5.tar.gz

---
+++ packages/hdfset/hdfset-0.2.5.tar.gz/hdfset-0.2.5/.devcontainer/postCreate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+mkdir -p ~/.config
+cp .devcontainer/starship.toml ~/.config
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.cargo/env
+echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
