87c2fd358f187d28dc5a781e09661923be7b4954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:22 +0000

    Add package rabbitmq-client-async/0.1.0/rabbitmq_client_async-0.1.0-py3-none-any.whl

---
+++ packages/rabbitmq-client-async/rabbitmq_client_async-0.1.0-py3-none-any.whl/tests/push.py
@@ -0,0 +1,40 @@
+import asyncio
+import logging
+import os
+import sys
+from random import randint
+
+
+DIR_PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(DIR_PATH)
+
+from rabbitmq_async_client.config_logs import configure_logging  # noqa: E402
+from rabbitmq_async_client.rabbitmq_client import RabbitMQClient  # noqa: E402
+
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+    configure_logging(level=logging.INFO)
+
+    client = RabbitMQClient(host='localhost', port=5672, user='guest', password='guest', durable=True)
+
+    try:
+        # for i in range(1, 11):
+        stop = randint(1, 100)
+        while stop != 55:
+            queue, key, value = randint(1, 2), randint(1, 10), randint(1, 10)
+            data = {f'example_key_{key:<2}': f'example_value_{value:<2}'}
+            await client.publisher(f'queue_{queue}', data)
+            stop = randint(1, 100)
+
+    except KeyboardInterrupt:
+        await client.stop()
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info('Publisher interrupt')
