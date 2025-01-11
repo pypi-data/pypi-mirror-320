fcde21c9d9602fbbd1880d51458ea11dfdec735c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:10 +0000

    Add package xander-ai/0.1.0/xander_ai-0.1.0.tar.gz

---
+++ packages/xander-ai/xander_ai-0.1.0.tar.gz/xander_ai-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='xander-ai',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas', 
+        'numpy', 
+        'chardet', 
+        'tensorflow', 
+        'scikit-learn', 
+        'openpyxl', 
+        'channels', 
+        'asgiref', 
+        'keras', 
+        'keras-tuner', 
+        'joblib', 
+        'requests', 
+        'nltk',
+    ],
+    author='Atulit gaur',
+    author_email='atulit23@gmail.com',
+    description='Xander - A package to train Classification, Regression, and Image Classification models.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
+)
