ce0d25120d9b5c826e9fcfd6e0bba5c0605fc70e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:47 +0000

    Add package nodestream/0.13.6/nodestream-0.13.6.tar.gz

---
+++ packages/nodestream/nodestream-0.13.6.tar.gz/nodestream-0.13.6/nodestream/cli/operations/initialize_logger.py
@@ -0,0 +1,39 @@
+import logging
+import os
+from typing import Any
+
+from pythonjsonlogger.jsonlogger import JsonFormatter
+
+from ...pipeline.meta import get_context
+from ..commands.nodestream_command import NodestreamCommand
+from .operation import Operation
+
+
+def _get_logger_level():
+    return logging.getLevelName(os.environ.get("NODESTREAM_LOG_LEVEL", "INFO").upper())
+
+
+def configure_logging_with_json_defaults():
+    logging.basicConfig(level=_get_logger_level(), force=True)
+    old_record_factory = logging.getLogRecordFactory()
+
+    def record_factory(*args, **kwargs):
+        record = old_record_factory(*args, **kwargs)
+        record.pipeline_name = get_context().name
+        record.pipeline_scope = get_context().scope
+        return record
+
+    logging.setLogRecordFactory(record_factory)
+
+    formatter = JsonFormatter(
+        "%(name)s %(levelname)s %(pipeline_name)s %(pipeline_scope)s %(message)s",
+        timestamp=True,
+    )
+    logger = logging.getLogger()  # Configure the root logger.
+    logger.handlers[0].setFormatter(formatter)
+
+
+class InitializeLogger(Operation):
+    async def perform(self, command: NodestreamCommand) -> Any:
+        if command.has_json_logging_set:
+            configure_logging_with_json_defaults()
---
+++ packages/nodestream/nodestream-0.13.6.tar.gz/nodestream-0.13.6/nodestream/databases/query_executor_with_statistics.py
@@ -0,0 +1,48 @@
+from typing import Iterable
+
+from ..model import IngestionHook, Node, RelationshipWithNodes, TimeToLiveConfiguration
+from ..pipeline.meta import get_context
+from .query_executor import (
+    OperationOnNodeIdentity,
+    OperationOnRelationshipIdentity,
+    QueryExecutor,
+)
+
+NODE_STAT = "Node Upsert Operations"
+RELATIONSHIP_STAT = "Relationship Upsert Operations"
+TTL_STAT = "Time to Live Operations"
+HOOK_STAT = "Ingest Hooks Executed"
+
+
+class QueryExecutorWithStatistics(QueryExecutor):
+    __slots__ = ("inner",)
+
+    def __init__(self, inner: QueryExecutor) -> None:
+        self.inner = inner
+
+    async def upsert_nodes_in_bulk_with_same_operation(
+        self, operation: OperationOnNodeIdentity, nodes: Iterable[Node]
+    ):
+        await self.inner.upsert_nodes_in_bulk_with_same_operation(operation, nodes)
+        get_context().increment_stat(NODE_STAT, len(nodes))
+
+    async def upsert_relationships_in_bulk_of_same_operation(
+        self,
+        shape: OperationOnRelationshipIdentity,
+        relationships: Iterable[RelationshipWithNodes],
+    ):
+        await self.inner.upsert_relationships_in_bulk_of_same_operation(
+            shape, relationships
+        )
+        get_context().increment_stat(RELATIONSHIP_STAT, len(relationships))
+
+    async def perform_ttl_op(self, config: TimeToLiveConfiguration):
+        await self.inner.perform_ttl_op(config)
+        get_context().increment_stat(TTL_STAT)
+
+    async def execute_hook(self, hook: IngestionHook):
+        await self.inner.execute_hook(hook)
+        get_context().increment_stat(HOOK_STAT)
+
+    async def finish(self):
+        await self.inner.finish()
