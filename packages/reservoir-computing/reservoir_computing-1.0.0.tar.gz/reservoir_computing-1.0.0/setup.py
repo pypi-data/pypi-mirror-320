e738c5d81463ab0477affdbb35e1642d27798749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:24 +0000

    Add package reservoir-computing/1.0.0/reservoir_computing-1.0.0.tar.gz

---
+++ packages/reservoir-computing/reservoir_computing-1.0.0.tar.gz/reservoir_computing-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reservoir_computing",
+    version="1.0.0",
+    packages=find_packages(),
+    python_requires='>=3.9',
+    install_requires=[
+        'numpy>1.19.5',
+        'scikit_learn>=1.4',
+        'scipy',
+        'requests',
+        'matplotlib',
+    ],
+    author="Filippo Maria Bianchi",
+    author_email="filippombianchi@gmail.com",
+    description="Library for implementing reservoir computing models (Echo State Networks) for multivariate time series classification, clustering, and forecasting.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    #url="https://github.com/FilippoMB/Time-series-classification-and-clustering-with-Reservoir-Computing",
+    project_urls={
+        "Documentation": "https://reservoir-computing.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/FilippoMB/Time-series-classification-and-clustering-with-Reservoir-Computing",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
