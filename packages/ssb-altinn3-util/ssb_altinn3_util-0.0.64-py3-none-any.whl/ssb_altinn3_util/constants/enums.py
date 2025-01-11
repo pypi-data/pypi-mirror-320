681725ba0de10868c845959a21b0ea7164449291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 06:04:32 +0000

    Add package ssb-altinn3-util/0.0.64/ssb_altinn3_util-0.0.64-py3-none-any.whl

---
+++ packages/ssb-altinn3-util/ssb_altinn3_util-0.0.64-py3-none-any.whl/ssb_altinn3_util/constants/enums.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class UtsendingEnhetType(str, Enum):
+    Foretak = "FRTK"
+    Bedrift = "BEDR"
+    Person = "PERS"
+    SkjemaEnhet = "SKJEMA_ENHET"
+
+
+class MottakerType(str, Enum):
+    Alle = "ALLE"
+    Besvart = "BESVART"
+    IkkeBesvart = "IKKE_BESVART"
+
+
+class UtsendingFormat(str, Enum):
+    pdf = "PDF"
+    text = "TXT"
+    html = "HTML"
+    xml = "XML"
