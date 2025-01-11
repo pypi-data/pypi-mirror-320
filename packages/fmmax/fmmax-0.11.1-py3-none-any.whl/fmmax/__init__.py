5fd5dfd2ecf3aa59009233c871cd59916919cdba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 09:04:17 +0000

    Add package fmmax/0.11.1/fmmax-0.11.1-py3-none-any.whl

---
+++ packages/fmmax/fmmax-0.11.1-py3-none-any.whl/fmmax/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+__version__ = "v0.11.1"
+
+from fmmax import basis as basis
+from fmmax import beams as beams
+from fmmax import farfield as farfield
+from fmmax import fft as fft
+from fmmax import fields as fields
+from fmmax import fmm as fmm
+from fmmax import fmm_matrices as fmm_matrices
+from fmmax import scattering as scattering
+from fmmax import sources as sources
+from fmmax import utils as utils
+from fmmax import vector as vector
