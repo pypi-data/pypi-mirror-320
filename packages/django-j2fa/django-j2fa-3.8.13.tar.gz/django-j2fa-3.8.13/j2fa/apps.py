139cc5f9161206809a97256a6c3fe72f471aa4f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:03:44 +0000

    Add package django-j2fa/3.8.13/django-j2fa-3.8.13.tar.gz

---
+++ packages/django-j2fa/django-j2fa-3.8.13.tar.gz/django-j2fa-3.8.13/j2fa/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class A2faConfig(AppConfig):
+    name = "j2fa"
+    verbose_name = _("2-Factor Authentication")
+    default_auto_field = "django.db.models.AutoField"
