2160d9f14b431cbade144cba6780bba83ef4699c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 07:04:10 +0000

    Add package biobb-mem/5.0.5/biobb_mem-5.0.5.tar.gz

---
+++ packages/biobb-mem/biobb_mem-5.0.5.tar.gz/biobb_mem-5.0.5/biobb_mem/lipyphilic_biobb/__init__.py
@@ -0,0 +1,2 @@
+name = "lipyphilic_biobb"
+__all__ = ["lpp_assign_leaflets", "lpp_zpositions"]
---
+++ packages/biobb-mem/biobb_mem-5.0.5.tar.gz/biobb_mem-5.0.5/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="biobb_mem",
+    version="5.0.5",
+    author="Biobb developers",
+    author_email="ruben.chaves@irbbarcelona.org",
+    description="Biobb_mem is a complete code template to promote and facilitate the creation of new Biobbs by the community.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="Bioinformatics Workflows BioExcel Compatibility",
+    url="https://github.com/bioexcel/biobb_mem",
+    project_urls={
+        "Documentation": "http://biobb-mem.readthedocs.io/en/latest/",
+        "Bioexcel": "https://bioexcel.eu/"
+    },
+    packages=setuptools.find_packages(exclude=['adapters', 'docs', 'test']),
+    package_data={'biobb_mem': ['py.typed']},
+    install_requires=['biobb_common==5.0.0',
+                      'fatslim==0.2.2'],
+    python_requires='>=3.9',
+    entry_points={
+        "console_scripts": [
+            "cpptraj_density = biobb_mem.ambertools.cpptraj_density:main",
+            "fatslim_membranes = biobb_mem.fatslim.fatslim_membranes:main",
+            "lpp_assign_leaflets = biobb_mem.lipyphilic_biobb.lpp_assign_leaflets:main",
+            "lpp_lpp_zpositions = biobb_mem.lipyphilic_biobb.lpp_zpositions:main",
+            "mda_hole = biobb_mem.mdanalysis_biobb.mda_hole:main",
+        ]
+    },
+    classifiers=(
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Unix"
+    ),
+)
