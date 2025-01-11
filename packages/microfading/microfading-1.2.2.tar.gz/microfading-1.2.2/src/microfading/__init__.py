36d1f014da810be5cd058d5c22b1ea3c8349aed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 08:05:36 +0000

    Add package microfading/1.2.2/microfading-1.2.2.tar.gz

---
+++ packages/microfading/microfading-1.2.2.tar.gz/microfading-1.2.2/src/microfading/__init__.py
@@ -0,0 +1,3 @@
+from .microfading import MFT, DB, get_datasets, get_creators, get_DB, get_institutions, get_objects, get_persons, process_rawdata, create_DB, get_path_DB, update_DB_objects, update_DB_projects, add_new_institution, add_new_project, add_new_object, add_new_person, register_devices, register_references, get_devices, get_white_references
+
+__version__ = "1.2.2"
