478961932bb8bac79fef861995cdae42eb0b21dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 14:06:15 +0000

    Add package edc-consent/0.3.87/edc_consent-0.3.87-py3-none-any.whl

---
+++ packages/edc-consent/edc_consent-0.3.87-py3-none-any.whl/edc_consent/modelform_mixins/__init__.py
@@ -0,0 +1,4 @@
+from .consent_modelform_mixin import ConsentModelFormMixin
+from .requires_consent_modelform_mixin import RequiresConsentModelFormMixin
+
+__all__ = ["ConsentModelFormMixin", "RequiresConsentModelFormMixin"]
