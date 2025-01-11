bccc271c7a0180fb244ed371be0c9e069eef0b5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 04:04:14 +0000

    Add package tach/0.19.7/tach-0.19.7-cp37-abi3-win32.whl

---
+++ packages/tach/tach-0.19.7-cp37-abi3-win32.whl/tach/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+__version__: str = "0.19.7"
+
+__all__ = ["__version__"]
---
+++ packages/tach/tach-0.19.7-cp37-abi3-win32.whl/tach/cache/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from tach.cache.access import get_latest_version, get_uid, update_latest_version
+
+__all__ = ["get_uid", "update_latest_version", "get_latest_version"]
