48aaefc5f6118c68ecd5ef3744f4c8b290b39a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-11 05:04:50 +0000

    Add package manim-chess/0.0.1/manim_chess-0.0.1.tar.gz

---
+++ packages/manim-chess/manim_chess-0.0.1.tar.gz/manim_chess-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("manim_chess/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="manim_chess",
+    version='0.0.1',
+    descriptiopn='A plugin for Manim CE that allows the user to generate a chess board and other chess related functions.',
+    package_dir={"": "manim_chess"},
+    packages=find_packages(where="manim_chess"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/swoyer2/manim_chess",
+    author="Swoyer2",
+    author_email="swoyer.logan@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License"
+    ],
+    extras_require={
+        "dev": ["twine>=4.0.2"]
+    },
+)
