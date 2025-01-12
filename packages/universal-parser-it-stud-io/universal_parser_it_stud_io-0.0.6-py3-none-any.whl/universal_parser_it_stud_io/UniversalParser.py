8269dd385b75817858913814bbd55610eaa64e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 04:05:30 +0000

    Add package universal-parser-it-stud-io/0.0.6/universal_parser_it_stud_io-0.0.6-py3-none-any.whl

---
+++ packages/universal-parser-it-stud-io/universal_parser_it_stud_io-0.0.6-py3-none-any.whl/universal_parser_it_stud_io/UniversalParser.py
@@ -0,0 +1,54 @@
+import json
+import csv
+import yaml
+import xml.etree.ElementTree as et
+from ParserEngines import JsonParserEngine
+from ParserEngines import XmlParserEngine
+from ParserEngines import CsvParserEngine
+from ParserEngines import YamlParserEngine
+class UniversalParser:
+    def __init__(self, data=None):
+        self._data = data
+        self._data_format = self._detect_format()
+        self._parser_engine = self._get_parser_engine()
+
+    def parse(self):
+        return self._parser_engine.parse(self._data)
+
+    def validate(self, schema):
+        return self._parser_engine.validate(self._data, schema)
+        
+    def _detect_format(self):
+        try:
+            json.loads(self._data)
+            return 'json'
+        except json.JSONDecodeError:
+            pass
+        try:
+            et.fromstring(self._data)
+            return 'xml'
+        except et.ParseError:
+            pass
+        try:
+            csv.Sniffer().sniff(self._data, delimiters=',')
+            return 'csv'
+        except csv.Error:
+            pass
+        try:
+            yaml.safe_load(self._data)
+            return 'yaml'
+        except yaml.YAMLError:
+            pass
+        raise ValueError("Unable to detect data format")
+    
+    def _get_parser_engine(self):
+        if self._data_format == 'json':
+            return JsonParserEngine()
+        elif self._data_format == 'xml':
+            return XmlParserEngine()
+        elif self._data_format == 'csv':
+            return CsvParserEngine()
+        elif self._data_format == 'yaml':
+            return YamlParserEngine()
+        else:
+            raise ValueError("Unsupported data format")
