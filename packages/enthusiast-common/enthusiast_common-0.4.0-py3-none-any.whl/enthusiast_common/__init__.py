d4c0f5c38d75132a1e9d6ab5c03939df280d8498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:13 +0000

    Add package enthusiast-common/0.4.0/enthusiast_common-0.4.0-py3-none-any.whl

---
+++ packages/enthusiast-common/enthusiast_common-0.4.0-py3-none-any.whl/enthusiast_common/__init__.py
@@ -0,0 +1,5 @@
+from .interfaces import DocumentSourcePlugin as DocumentSourcePlugin
+from .interfaces import ProductSourcePlugin as ProductSourcePlugin
+from .interfaces import EmbeddingProvider as EmbeddingProvider
+from .structures import DocumentDetails as DocumentDetails
+from .structures import ProductDetails as ProductDetails
