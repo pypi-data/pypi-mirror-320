3614af168a815a721625ffe91be306afaecf70e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:06:27 +0000

    Add package mlr-gd/0.1.0/mlr_gd-0.1.0.tar.gz

---
+++ packages/mlr-gd/mlr_gd-0.1.0.tar.gz/mlr_gd-0.1.0/README.md
@@ -0,0 +1,11 @@
+<h1 align="center"> 
+  <img src="https://raw.githubusercontent.com/DrSolidDevil/mlr-gd/main/logo.png" width="300">
+  <br>
+</h1>
+
+Multiple linear regression by gradient descent.
+<br><br>
+<h4>Disclaimer:</h4>
+This code is very early on and my first proper attempt to create a package so things may be a bit weird/not up to standard.
+
+
---
+++ packages/mlr-gd/mlr_gd-0.1.0.tar.gz/mlr_gd-0.1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="mlr-gd",
+    version="0.1.0",
+    description="A package for multiple linear regression by gradient descent.",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DrSolidDevil/mlr-gd/",
+    author="DrSolidDevil",
+    license="BSD 3-Clause",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy >= 2.2.1"],
+    extras_require={
+        "dev": ["twine>=6.0.1"],
+    },
+    python_requires=">=3.11",
+)
