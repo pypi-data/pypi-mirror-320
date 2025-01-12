39cb54006eccfb318f2609671e3d5ba9ff2eb34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 05:04:42 +0000

    Add package scipystat/0.1.11/scipystat-0.1.11.tar.gz

---
+++ packages/scipystat/scipystat-0.1.11.tar.gz/scipystat-0.1.11/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', encoding='utf-8') as f:
+    return f.read()
+
+
+setup(
+  name='scipystat',
+  version='0.1.11',
+  description='This is the simplest module for quick work with files.',
+  license='MIT',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com',
+  packages=find_packages(),
+  keywords='files terver '
+)
