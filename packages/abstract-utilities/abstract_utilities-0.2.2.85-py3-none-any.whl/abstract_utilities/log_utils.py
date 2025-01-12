cf90025248fa6954a3a779df65b8885cf4ff1cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:05:53 +0000

    Add package abstract-utilities/0.2.2.85/abstract_utilities-0.2.2.85-py3-none-any.whl

---
+++ packages/abstract-utilities/abstract_utilities-0.2.2.85-py3-none-any.whl/abstract_utilities/log_utils.py
@@ -0,0 +1,12 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+from .path_utils import mkdirs
+def get_logFile(bpName,maxBytes=100000, backupCount=3):
+    log_dir = mkdirs('logs')
+    log_path = os.path.join(log_dir,f'{bpName}.log')
+    log_handler = RotatingFileHandler(log_path, maxBytes=100000, backupCount=backupCount)
+    log_handler.setLevel(logging.INFO)
+    logger = logging.getLogger(bpName)
+    logger.addHandler(log_handler)
+    return logger
