3865ce436b52fee5a35cc79310165dbf16c7e5dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 05:04:52 +0000

    Add package bazario/0.2.3/bazario-0.2.3-py3-none-any.whl

---
+++ packages/bazario/bazario-0.2.3-py3-none-any.whl/bazario/pipeline/wrap_pipeline_behaviours.py
@@ -0,0 +1,32 @@
+from functools import partial
+from typing import TypeAlias
+
+from bazario.protocols.handle_next import HandleNext
+from bazario.protocols.handler import NotificationHandler, RequestHandler
+from bazario.protocols.pipeline_behaviour import PipelineBehaviour
+from bazario.protocols.resolver import Resolver
+from bazario.typing.type_vars import TRes_co, TTarget_contra
+
+_HandlerType: TypeAlias = (
+    RequestHandler[TTarget_contra, TRes_co]
+    | NotificationHandler[TTarget_contra]
+)
+
+
+def wrap_pipeline_behaviours(
+    behaviours: list[PipelineBehaviour[TTarget_contra, TRes_co]],
+    handler: _HandlerType,
+) -> HandleNext[TTarget_contra, TRes_co]:
+    def process_pipeline(
+        resolver: Resolver,
+        target: TTarget_contra,
+    ) -> TRes_co:
+        def handle_next(resolver: Resolver, target: TTarget_contra) -> TRes_co:
+            return handler.handle(target)
+
+        for behaviour in reversed(behaviours):
+            handle_next = partial(behaviour.handle, handle_next=handle_next)
+
+        return handle_next(resolver, target)
+
+    return process_pipeline
