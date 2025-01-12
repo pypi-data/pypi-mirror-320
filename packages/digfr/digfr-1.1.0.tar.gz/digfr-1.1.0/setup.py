bfd4cd0e2fea56aaed0bdf3dc4653ac240e1eb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:15 +0000

    Add package digfr/1.1.0/digfr-1.1.0.tar.gz

---
+++ packages/digfr/digfr-1.1.0.tar.gz/digfr-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "digfr",
+    version = "1.1.0",
+    packages = find_packages(),
+    description = "专注财务报表数据分析",
+    author = "Tommy",
+    author_email = "",
+    url = "https://digfr.info"
+)
