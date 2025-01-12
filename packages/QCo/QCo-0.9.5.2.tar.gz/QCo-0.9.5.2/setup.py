4b119d74129db132603a4b2d7c93ca7ddcb03b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 15:04:31 +0000

    Add package QCo/0.9.5.2/QCo-0.9.5.2.tar.gz

---
+++ packages/QCo/QCo-0.9.5.2.tar.gz/QCo-0.9.5.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+packages = setuptools.find_packages(where='.', exclude=['tests', 'tests.*'])
+print("Found packages:", packages)
+
+setuptools.setup(
+    name='QCo',
+    version='0.9.5.2',
+    url='https://pypi.org/manage/account/token/',
+    packages=setuptools.find_packages(where='.', exclude=['tests', 'tests.*']),
+    license='',
+    author='FF',
+    author_email='',
+    description='',
+    install_requires=[
+        'protobuf',
+        'cryptography',
+        'bs4',
+        'urllib3==1.26.16',
+        'pydantic',
+        'requests',
+        'python-box',
+        'PySocks',
+        'python-box',
+        'loguru'
+    ]
+
+)
