9f09dac175f16ebf07ec9eb34ae40ef0e69737c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 15:04:18 +0000

    Add package tglib/0.0.1/tglib-0.0.1.tar.gz

---
+++ packages/tglib/tglib-0.0.1.tar.gz/tglib-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="tglib",
+    version="0.0.1",
+    author="Ankit Chaubey",
+    author_email="m.ankitchaubey@gmail.com",
+    description="A placeholder package for tglib, ready for initial development.",
+    long_description="This is the initial release of the tglib package, serving as a foundation for further development.",
+    long_description_content_type="text/plain",
+    url="https://github.com/ankit-chaubey/tglib",
+    packages=["tglib"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
