e2265811cf7ff3876be1fd628d888efdcac7e22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:23 +0000

    Add package distancia/0.0.75/distancia-0.0.75.tar.gz

---
+++ packages/distancia/distancia-0.0.75.tar.gz/distancia-0.0.75/setup_ym.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="distancia",
+    version="0.0.71",
+    packages=find_packages(),
+    install_requires=[
+        # listez vos dépendances ici
+    ],
+)
