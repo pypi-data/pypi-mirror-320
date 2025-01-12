41515dd0d2e0ffb7b35d57cf1f43f7cae40979ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 04:05:10 +0000

    Add package SimpSave/0.1.0/simpsave-0.1.0.tar.gz

---
+++ packages/SimpSave/simpsave-0.1.0.tar.gz/simpsave-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SimpSave',
+    version='0.1.0',
+    packages=find_packages(where='source'),
+    include_package_data=True,
+    install_requires=[],
+    author='WaterRun',
+    author_email='2263633954@qq.com',
+    description='SimpSave: Easy Python Basic Variable Persistence Library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Water-Run/SimpSave',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
