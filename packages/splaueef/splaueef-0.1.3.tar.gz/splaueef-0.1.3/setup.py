cef2898fb3a16c41787484767bb48684d01ca2a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:05:52 +0000

    Add package splaueef/0.1.3/splaueef-0.1.3.tar.gz

---
+++ packages/splaueef/splaueef-0.1.3.tar.gz/splaueef-0.1.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+# Налаштування бібліотеки
+setup(
+    name="splaueef",
+    version="0.1.3",
+    packages=find_packages(),
+    install_requires=["aiogram>=3.0", "certifi"],
+    author="Splaueef",
+    author_email="Splaueef@gmail.com",
+    description="Допоміжна бібліотека для роботи з aiogram",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Splaueef/aiohelp.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    entry_points={
+        "console_scripts": [
+            "splaueef-init=splaueef:init_project",
+        ],
+    },
+)
