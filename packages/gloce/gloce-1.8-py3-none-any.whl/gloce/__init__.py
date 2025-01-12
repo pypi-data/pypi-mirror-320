7873a9fa3a14c7df19bea2c8e5acfe98b072c080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:34 +0000

    Add package gloce/1.8/gloce-1.8-py3-none-any.whl

---
+++ packages/gloce/gloce-1.8-py3-none-any.whl/gloce/__init__.py
@@ -0,0 +1,37 @@
+import gloce.func
+import gloce.area
+import gloce.interpolation
+import gloce.resample
+#----func----
+running_mean = gloce.func.running_mean
+nanravel = gloce.func.nanravel
+listdir = gloce.func.listdir
+nanaverage = gloce.func.nanaverage
+arrayinfo = gloce.func.arrayinfo
+exclude_outlier = gloce.func.exclude_outlier
+r2a = gloce.func.r2a
+n2a = gloce.func.n2a
+nans = gloce.nans
+func_to_landpoint = gloce.func.func_to_landpoint
+func_to_ilatilon = gloce.func.func_to_ilatilon
+func_to_latlon = gloce.func.func_to_latlon
+run = gloce.run
+month = gloce.month
+nanaverage = gloce.nanaverage
+w_std = gloce.w_std
+#------------
+
+#----resample----
+downscaling = gloce.resample.downscaling
+upscaling = gloce.resample.upscaling
+#----------------
+
+#----area----
+globalarea = gloce.area.globalarea
+gridarea = gloce.area.gridarea
+#------------
+
+#----interpolation----
+interpolation_map = gloce.interpolation.interpolation_map
+interpolation_time = gloce.interpolation.interpolation_time
+#---------------------
