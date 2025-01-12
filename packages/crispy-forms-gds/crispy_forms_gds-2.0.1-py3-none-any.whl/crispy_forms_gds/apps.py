1625c6e8328c883afe12898ccb9a66211ae03f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:04 +0000

    Add package crispy-forms-gds/2.0.1/crispy_forms_gds-2.0.1-py3-none-any.whl

---
+++ packages/crispy-forms-gds/crispy_forms_gds-2.0.1-py3-none-any.whl/crispy_forms_gds/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CrispyFormsGDSConfig(AppConfig):
+    name = "crispy_forms_gds"
---
+++ packages/crispy-forms-gds/crispy_forms_gds-2.0.1-py3-none-any.whl/crispy_forms_gds/layout/__init__.py
@@ -0,0 +1,36 @@
+from .base import Layout
+from .buttons import Button, Submit
+from .constants import Fixed, Fluid, Size
+from .containers import (
+    Accordion,
+    AccordionSection,
+    ConditionalQuestion,
+    ConditionalRadios,
+    Div,
+    Fieldset,
+    TabPanel,
+    Tabs,
+)
+from .content import HTML, Colour
+from .fields import Field, Hidden
+
+__all__ = [
+    "Accordion",
+    "AccordionSection",
+    "Button",
+    "Colour",
+    "ConditionalQuestion",
+    "ConditionalRadios",
+    "Div",
+    "Field",
+    "Fixed",
+    "Fieldset",
+    "Fluid",
+    "Hidden",
+    "HTML",
+    "Layout",
+    "Size",
+    "Submit",
+    "TabPanel",
+    "Tabs",
+]
