7952b28fb7b2565f0b27e3741bcb3863019c74c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:06:12 +0000

    Add package investing-algorithm-framework/4.0.0/investing_algorithm_framework-4.0.0.tar.gz

---
+++ packages/investing-algorithm-framework/investing_algorithm_framework-4.0.0.tar.gz/investing_algorithm_framework-4.0.0/investing_algorithm_framework/infrastructure/models/order/order.py
@@ -0,0 +1,138 @@
+import logging
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy.orm import relationship
+
+from investing_algorithm_framework.domain import OrderType, \
+    OrderSide, Order, OrderStatus
+from investing_algorithm_framework.infrastructure.database import SQLBaseModel
+from investing_algorithm_framework.infrastructure.models.model_extension \
+    import SQLAlchemyModelExtension
+
+logger = logging.getLogger("investing_algorithm_framework")
+
+
+class SQLOrder(Order, SQLBaseModel, SQLAlchemyModelExtension):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, unique=True)
+    external_id = Column(Integer)
+    target_symbol = Column(String)
+    trading_symbol = Column(String)
+    order_side = Column(String, nullable=False, default=OrderSide.BUY.value)
+    order_type = Column(String, nullable=False, default=OrderType.LIMIT.value)
+    price = Column(Float)
+    amount = Column(Float)
+    filled = Column(Float)
+    remaining = Column(Float)
+    cost = Column(Float)
+    status = Column(String)
+    position_id = Column(Integer, ForeignKey('positions.id'))
+    position = relationship("SQLPosition", back_populates="orders")
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+    trade_closed_at = Column(DateTime, default=None)
+    trade_closed_price = Column(Float, default=None)
+    trade_closed_amount = Column(Float, default=None)
+    net_gain = Column(Float, default=0)
+    order_fee = Column(Float, default=None)
+    order_fee_currency = Column(String)
+    order_fee_rate = Column(Float, default=None)
+    _available_amount = None
+
+    def update(self, data):
+
+        if 'amount' in data and data['amount'] is not None:
+            amount = data.pop('amount')
+            self.amount = amount
+
+        if 'price' in data and data['price'] is not None:
+            price = data.pop('price')
+            self.price = price
+
+        if 'filled' in data and data['filled'] is not None:
+            filled = data.pop('filled')
+            self.filled = filled
+
+        if 'remaining' in data and data['remaining'] is not None:
+            remaining = data.pop('remaining')
+            self.remaining = remaining
+
+        if 'net_gain' in data and data['net_gain'] is not None:
+            net_gain = data.pop('net_gain')
+            self.net_gain = net_gain
+
+        if 'trade_closed_price' in data and \
+                data['trade_closed_price'] is not None:
+            trade_closed_price = data.pop('trade_closed_price')
+            self.trade_closed_price = trade_closed_price
+
+        if 'trade_closed_amount' in data and \
+                data['trade_closed_amount'] is not None:
+            trade_closed_amount = data.pop('trade_closed_amount')
+            self.trade_closed_amount = trade_closed_amount
+
+        if "status" in data and data["status"] is not None:
+            self.status = OrderStatus.from_value(data.pop("status")).value
+
+        super().update(data)
+
+    @staticmethod
+    def from_order(order):
+        return SQLOrder(
+            external_id=order.external_id,
+            amount=order.get_amount(),
+            price=order.price,
+            order_type=order.get_order_type(),
+            order_side=order.get(),
+            status=order.get_status(),
+            target_symbol=order.get_target_symbol(),
+            trading_symbol=order.get_trading_symbol(),
+            created_at=order.get_created_at(),
+            updated_at=order.get_updated_at(),
+            trade_closed_at=order.get_trade_closed_at(),
+            trade_closed_price=order.get_trade_closed_price(),
+            trade_closed_amount=order.get_trade_closed_amount(),
+            net_gain=order.get_net_gain(),
+        )
+
+    @staticmethod
+    def from_ccxt_order(ccxt_order):
+        """
+        Create an Order object from a CCXT order object
+        :param ccxt_order: CCXT order object
+        """
+        status = OrderStatus.from_value(ccxt_order["status"])
+        target_symbol = ccxt_order.get("symbol").split("/")[0]
+        trading_symbol = ccxt_order.get("symbol").split("/")[1]
+        ccxt_fee = ccxt_order.get("fee", None)
+        order_fee = None
+        order_fee_rate = None
+        order_fee_currency = None
+
+        if ccxt_fee is not None:
+            order_fee = ccxt_fee.get("cost", None)
+            order_fee_rate = ccxt_fee.get("rate", None)
+            order_fee_currency = ccxt_fee.get("currency", None)
+
+        return Order(
+            external_id=ccxt_order.get("id", None),
+            target_symbol=target_symbol,
+            trading_symbol=trading_symbol,
+            price=ccxt_order.get("price", None),
+            amount=ccxt_order.get("amount", None),
+            status=status,
+            order_type=ccxt_order.get("type", None),
+            order_side=ccxt_order.get("side", None),
+            filled=ccxt_order.get("filled", None),
+            remaining=ccxt_order.get("remaining", None),
+            cost=ccxt_order.get("cost", None),
+            order_fee=order_fee,
+            order_fee_rate=order_fee_rate,
+            order_fee_currency=order_fee_currency,
+            created_at=ccxt_order.get("datetime", None),
+        )
+
+    def __lt__(self, other):
+        # Define the less-than comparison based on created_at attribute
+        return self.created_at < other.created_at
---
+++ packages/investing-algorithm-framework/investing_algorithm_framework-4.0.0.tar.gz/investing_algorithm_framework-4.0.0/investing_algorithm_framework/services/market_data_source_service/market_data_source_service.py
@@ -0,0 +1,217 @@
+from typing import List
+
+from investing_algorithm_framework.domain import MarketService, \
+    MarketDataSource, OHLCVMarketDataSource, TickerMarketDataSource, \
+    OrderBookMarketDataSource, TimeFrame, OperationalException
+from investing_algorithm_framework.services.market_credential_service \
+    import MarketCredentialService
+
+
+class MarketDataSourceService:
+    """
+    This class is responsible for managing the market data sources.
+
+    It is used by the algorithm to get market data from different sources.
+    The MarketDataSourceService will first check if there is a market data
+    source that matches the symbol, market and time frame provided by the user.
+    If there is, it will use that market data source to get the data. If there
+    is not, it will use the MarketService to get the data.
+    """
+    _market_data_sources: List[MarketDataSource] = []
+
+    def __init__(
+        self,
+        market_service: MarketService,
+        market_credential_service: MarketCredentialService,
+        market_data_sources: List[MarketDataSource] = None,
+    ):
+
+        if market_data_sources is not None:
+            self._market_data_sources: List[MarketDataSource] \
+                = market_data_sources
+
+        self._market_service: MarketService = market_service
+        self._market_credential_service: MarketCredentialService = \
+            market_credential_service
+
+    def get_ticker(self, symbol, market=None):
+        ticker_market_data_source = self.get_ticker_market_data_source(
+            symbol=symbol, market=market
+        )
+
+        if ticker_market_data_source is not None:
+            return ticker_market_data_source.get_data(
+                market_credential_service=self._market_credential_service
+            )
+        return self._market_service.get_ticker(symbol, market)
+
+    def get_order_book(self, symbol, market=None):
+        order_book_market_data_source = self.get_order_book_market_data_source(
+            symbol=symbol, market=market
+        )
+
+        if order_book_market_data_source is not None:
+            return order_book_market_data_source.get_data(
+                market_credential_service=self._market_credential_service
+            )
+
+        return self._market_service.get_order_book(symbol, market)
+
+    def get_ohlcv(
+        self,
+        symbol,
+        from_timestamp,
+        time_frame,
+        market=None,
+        to_timestamp=None
+    ):
+        ohlcv_market_data_source = self.get_ohlcv_market_data_source(
+            symbol=symbol, market=market, time_frame=time_frame
+        )
+
+        if ohlcv_market_data_source is not None:
+            return ohlcv_market_data_source.get_data(
+                from_timestamp=from_timestamp,
+                to_timestamp=to_timestamp,
+                market_credential_service=self._market_credential_service
+            )
+
+        return self._market_service.get_ohlcv(
+            symbol=symbol,
+            time_frame=time_frame,
+            from_timestamp=from_timestamp,
+            market=market,
+            to_timestamp=to_timestamp
+        )
+
+    def get_data(self, identifier):
+
+        for market_data_source in self._market_data_sources:
+            if market_data_source.get_identifier() == identifier:
+                return market_data_source.get_data(
+                    market_credential_service=self._market_credential_service
+                )
+
+        if isinstance(identifier, str):
+            raise OperationalException(
+                f"Market data source with identifier {identifier} not found. "
+                "Please make sure that the market data source is "
+                "registered to the app if you refer to it by "
+                "identifier in your strategy."
+            )
+
+    def get_ticker_market_data_source(self, symbol, market=None):
+
+        if self.market_data_sources is not None:
+            for market_data_source in self._market_data_sources:
+                if isinstance(market_data_source, TickerMarketDataSource):
+
+                    if market is not None:
+                        if market_data_source.market.lower() == market.lower()\
+                                and market_data_source.symbol.lower() \
+                                == symbol.lower():
+                            return market_data_source
+                    else:
+                        if market_data_source.symbol.lower() \
+                                == symbol.lower():
+                            return market_data_source
+
+        return None
+
+    def get_ohlcv_market_data_source(
+        self, symbol, time_frame=None, market=None
+    ):
+        """
+        Function to get the OHLCV market data source for a symbol,
+          time frame and market.
+
+        Parameters:
+            symbol: str - The symbol of the asset
+            time_frame: TimeFrame - The time frame of the data
+            market: str - The market of the asset
+
+        Returns:
+            OHLCVMarketDataSource - The OHLCV market data source for the
+            symbol, time frame and market
+        """
+        if time_frame is not None:
+            time_frame = TimeFrame.from_value(time_frame)
+
+        if self.market_data_sources is not None:
+
+            for market_data_source in self._market_data_sources:
+                if isinstance(market_data_source, OHLCVMarketDataSource):
+
+                    if market is not None and time_frame is not None:
+                        if market_data_source.market.lower() == market.lower()\
+                                and market_data_source.symbol.lower() \
+                                == symbol.lower() and \
+                                time_frame.equals(
+                                    market_data_source.time_frame
+                                ):
+                            return market_data_source
+                    elif market is not None:
+                        if market_data_source.market.lower() == market.lower()\
+                                and market_data_source.symbol.lower() \
+                                == symbol.lower():
+                            return market_data_source
+                    elif time_frame is not None:
+                        if market_data_source.symbol.lower() \
+                                == symbol.lower() and \
+                                time_frame.equals(
+                                    market_data_source.time_frame
+                                ):
+                            return market_data_source
+                    else:
+                        if market_data_source.symbol.lower() \
+                                == symbol.lower():
+                            return market_data_source
+
+        return None
+
+    def get_order_book_market_data_source(self, symbol, market=None):
+
+        if self.market_data_sources is not None:
+            for market_data_source in self._market_data_sources:
+                if isinstance(market_data_source, OrderBookMarketDataSource):
+
+                    if market is not None:
+                        if market_data_source.market.lower() == market.lower()\
+                                and market_data_source.symbol.lower() \
+                                == symbol.lower():
+                            return market_data_source
+                    else:
+                        if market_data_source.symbol.lower() \
+                                == symbol.lower():
+                            return market_data_source
+
+        return None
+
+    @property
+    def market_data_sources(self):
+        return self._market_data_sources
+
+    @market_data_sources.setter
+    def market_data_sources(self, market_data_sources):
+        self._market_data_sources = market_data_sources
+
+    def add(self, market_data_source):
+
+        # Check if the market data source is an instance of MarketDataSource
+        if not isinstance(market_data_source, MarketDataSource):
+            return
+
+        # Check if there is already a market data source with the same
+        # identifier
+        for existing_market_data_source in self._market_data_sources:
+            if existing_market_data_source.get_identifier() == \
+                    market_data_source.get_identifier():
+                return
+
+        self._market_data_sources.append(market_data_source)
+
+    def get_market_data_sources(self):
+        return self._market_data_sources
+
+    def has_ticker_market_data_source(self, symbol, market=None):
+        return self.get_ticker_market_data_source(symbol, market) is not None
