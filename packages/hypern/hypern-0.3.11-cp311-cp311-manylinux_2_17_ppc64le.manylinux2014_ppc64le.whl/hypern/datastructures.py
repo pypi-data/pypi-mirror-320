80ab85adf82f03245c61176f9c3b0af28f513740
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 15:04:21 +0000

    Add package hypern/0.3.11/hypern-0.3.11-cp311-cp311-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl

---
+++ packages/hypern/hypern-0.3.11-cp311-cp311-manylinux_2_17_ppc64le.manylinux2014_ppc64le.whl/hypern/datastructures.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from enum import Enum
+from pydantic import BaseModel, AnyUrl
+
+
+class BaseModelWithConfig(BaseModel):
+    model_config = {"extra": "allow"}
+
+
+class Contact(BaseModelWithConfig):
+    name: Optional[str] = None
+    url: Optional[AnyUrl] = None
+    email: Optional[str] = None
+
+
+class License(BaseModelWithConfig):
+    name: str
+    identifier: Optional[str] = None
+    url: Optional[AnyUrl] = None
+
+
+class Info(BaseModelWithConfig):
+    title: str
+    summary: Optional[str] = None
+    description: Optional[str] = None
+    contact: Optional[Contact] = None
+    license: Optional[License] = None
+    version: str
+
+
+class HTTPMethod(Enum):
+    GET = "GET"
+    POST = "POST"
+    PUT = "PUT"
+    DELETE = "DELETE"
+    PATCH = "PATCH"
+    OPTIONS = "OPTIONS"
+    HEAD = "HEAD"
+    TRACE = "TRACE"
+    CONNECT = "CONNECT"
