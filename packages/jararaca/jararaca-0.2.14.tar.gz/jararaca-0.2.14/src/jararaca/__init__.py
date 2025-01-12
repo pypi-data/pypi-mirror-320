8f397f6b7ed3383236a3cb044b2ea9c1b943e5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:06:04 +0000

    Add package jararaca/0.2.14/jararaca-0.2.14.tar.gz

---
+++ packages/jararaca/jararaca-0.2.14.tar.gz/jararaca-0.2.14/src/jararaca/__init__.py
@@ -0,0 +1,154 @@
+from jararaca.observability.interceptor import ObservabilityInterceptor
+from jararaca.presentation.hooks import (
+    raises_200_on,
+    raises_400_on,
+    raises_401_on,
+    raises_403_on,
+    raises_404_on,
+    raises_422_on,
+    raises_500_on,
+    raises_http_exception_on,
+)
+from jararaca.rpc.http.decorators import Body
+from jararaca.rpc.http.decorators import Delete as HttpDelete
+from jararaca.rpc.http.decorators import Get as HttpGet
+from jararaca.rpc.http.decorators import GlobalHttpErrorHandler, Header, HttpMapping
+from jararaca.rpc.http.decorators import Patch as HttpPatch
+from jararaca.rpc.http.decorators import PathParam
+from jararaca.rpc.http.decorators import Post as HttpPost
+from jararaca.rpc.http.decorators import Put as HttpPut
+from jararaca.rpc.http.decorators import (
+    Query,
+    RequestAttribute,
+    RestClient,
+    RouteHttpErrorHandler,
+)
+
+from .core.providers import ProviderSpec, Token
+from .di import Container
+from .messagebus.decorators import MessageBusController, MessageHandler
+from .messagebus.interceptors.publisher_interceptor import (
+    AIOPikaConnectionFactory,
+    MessageBusPublisherInterceptor,
+)
+from .messagebus.publisher import use_publisher
+from .messagebus.types import Message, MessageOf
+from .messagebus.worker import MessageBusWorker
+from .microservice import Microservice, use_current_container
+from .persistence.base import (
+    T_BASEMODEL,
+    BaseEntity,
+    CriteriaBasedAttributeQueryInjector,
+    CRUDOperations,
+    DateCriteria,
+    DatedEntity,
+    DateOrderedFilter,
+    DateOrderedQueryInjector,
+    Identifiable,
+    IdentifiableEntity,
+    Paginated,
+    PaginatedFilter,
+    QueryInjector,
+    QueryOperations,
+    StringCriteria,
+)
+from .persistence.interceptors.aiosqa_interceptor import (
+    AIOSQAConfig,
+    AIOSqlAlchemySessionInterceptor,
+    use_session,
+)
+from .presentation.decorators import (
+    Delete,
+    Get,
+    Patch,
+    Post,
+    Put,
+    RestController,
+    UseDependency,
+    UseMiddleware,
+)
+from .presentation.http_microservice import HttpMicroservice, HttpMiddleware
+from .presentation.server import create_http_server
+from .presentation.websocket.decorators import WebSocketEndpoint
+from .presentation.websocket.redis import RedisWebSocketConnectionBackend
+from .presentation.websocket.websocket_interceptor import (
+    WebSocketInterceptor,
+    use_ws_manager,
+)
+from .scheduler.decorators import ScheduledAction
+from .tools.app_config.interceptor import AppConfigurationInterceptor
+
+__all__ = [
+    "raises_http_exception_on",
+    "raises_200_on",
+    "raises_422_on",
+    "raises_404_on",
+    "raises_400_on",
+    "raises_401_on",
+    "raises_403_on",
+    "raises_500_on",
+    "HttpMiddleware",
+    "HttpMapping",
+    "RequestAttribute",
+    "Body",
+    "Query",
+    "Header",
+    "PathParam",
+    "RestClient",
+    "HttpPost",
+    "HttpGet",
+    "HttpPatch",
+    "HttpPut",
+    "HttpDelete",
+    "ObservabilityInterceptor",
+    "QueryInjector",
+    "HttpMicroservice",
+    "use_current_container",
+    "T_BASEMODEL",
+    "DatedEntity",
+    "BaseEntity",
+    "use_ws_manager",
+    "WebSocketEndpoint",
+    "CriteriaBasedAttributeQueryInjector",
+    "Identifiable",
+    "IdentifiableEntity",
+    "MessageOf",
+    "Message",
+    "StringCriteria",
+    "DateCriteria",
+    "DateOrderedFilter",
+    "DateOrderedQueryInjector",
+    "Paginated",
+    "PaginatedFilter",
+    "PaginatedQueryInjector",
+    "QueryOperations",
+    "CRUDOperations",
+    "RestController",
+    "MessageBusController",
+    "MessageHandler",
+    "ScheduledAction",
+    "Microservice",
+    "ProviderSpec",
+    "Token",
+    "AIOSqlAlchemySessionInterceptor",
+    "AIOSQAConfig",
+    "create_http_server",
+    "MessageBusWorker",
+    "Container",
+    "WebSocketInterceptor",
+    "use_session",
+    "Post",
+    "Get",
+    "Patch",
+    "Put",
+    "Delete",
+    "use_publisher",
+    "AIOPikaConnectionFactory",
+    "MessageBusPublisherInterceptor",
+    "RedisWebSocketConnectionBackend",
+    "AppConfigurationInterceptor",
+    "UseMiddleware",
+    "UseDependency",
+    "GlobalHttpErrorHandler",
+    "RouteHttpErrorHandler",
+]
