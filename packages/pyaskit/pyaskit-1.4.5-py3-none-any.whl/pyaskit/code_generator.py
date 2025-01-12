7a3a288afaec729dc81b8cbd48b15f8d88b1d1e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 05:05:17 +0000

    Add package pyaskit/1.4.5/pyaskit-1.4.5-py3-none-any.whl

---
+++ packages/pyaskit/pyaskit-1.4.5-py3-none-any.whl/pyaskit/code_generator.py
@@ -0,0 +1,155 @@
+import re
+import sys
+import os
+import importlib
+import tempfile
+import ast
+import importlib.metadata
+from timeout_decorator import timeout, TimeoutError
+from .example import ExampleType
+from .logging_config import setup_logger
+from .path import add_to_sys_path
+from .llm import chat
+
+
+logger = setup_logger(__name__)
+
+TMP_MOD_PATH = tempfile.mktemp(suffix=".py")
+TMP_MOD_DIR = os.path.dirname(TMP_MOD_PATH)
+TMP_MOD_NAME, _ = os.path.splitext(os.path.basename(TMP_MOD_PATH))
+
+
+def list_installed_packages():
+    """Yield the name of each installed package."""
+    installed_distributions = importlib.metadata.distributions()
+    for dist in installed_distributions:
+        yield dist.metadata["Name"]
+
+
+installed_packages = set(list_installed_packages())
+
+
+def extract_python_code(text: str) -> str:
+    # extract ```python...``` from text
+    json_regex = r"```python\n(.*)\n```"
+    json_match = re.search(json_regex, text, re.DOTALL)
+    if json_match:
+        return json_match.group(1)
+    return ""
+
+
+@timeout(60)
+def test_example(func, example: dict) -> bool:
+    input = example["input"]
+    output = example["output"]
+    try:
+        result = func(**input)
+    except Exception:
+        return False
+    logger.debug("input:", input)
+    logger.debug("output:", output)
+    logger.debug("result:", result)
+    return func(**input) == output
+
+
+def test(func, examples: ExampleType) -> bool:
+    try:
+        return all(test_example(func, example) for example in examples)
+    except TimeoutError:
+        return False
+
+
+def validate_python_code(code: str, func_name):
+    # save code in a file
+    with open(TMP_MOD_PATH, "w") as f:
+        f.write(code)
+    with add_to_sys_path(TMP_MOD_DIR):
+        try:
+            packages = get_packages(code)
+            insuficent_packages = set(packages) - installed_packages
+            if insuficent_packages:
+                logger.debug(f"Installing packages: {insuficent_packages}")
+                os.system(f"pip install {' '.join(insuficent_packages)}")
+            module = importlib.import_module(TMP_MOD_NAME)
+            importlib.reload(module)
+        except ImportError as e:
+            logger.debug(f"An error occurred: {e}")
+            return None
+        except Exception as e:
+            logger.debug(f"An error occurred: {e}")
+            return None
+    if not hasattr(module, func_name):
+        print(f"Function {func_name} not found")
+        logger.debug(f"Function {func_name} not found")
+        return None
+    func = getattr(module, func_name)
+    return func
+
+
+def make_messages(skeleton: str):
+    return [
+        {
+            "role": "system",
+            "content": "You are a Python programmer. Your task is to implement the body of the function with the given name and parameters. The function should return the given type.",
+        },
+        #         {
+        #             "role": "user",
+        #             "content": """```python
+        # def add(x, y) -> int:
+        #     # add 'x' and 'y'
+        #     pass
+        # ```""",
+        #         },
+        #         {
+        #             "role": "assistant",
+        #             "content": """```python
+        # def add(x, y) -> int:
+        #     # add 'x' and 'y'
+        #     return x + y
+        # ```""",
+        #         },
+        {
+            "role": "user",
+            "content": skeleton,
+        },
+    ]
+
+
+def implement_body(
+    function_name: str,
+    skeleton: str,
+    test_examples: ExampleType = [],
+):
+    # print("skelton:", skeleton)
+    messages = make_messages(skeleton)
+    # print(messages)
+    test_failed_count = 0
+    for i in range(10):
+        content, completion = chat(messages=messages)
+        code = extract_python_code(content)
+        func = validate_python_code(code, function_name)
+        if func is None:
+            continue
+        ok = test(func, test_examples)
+        if ok:
+            return code, test_failed_count, i
+        else:
+            test_failed_count += 1
+            # print("Test failed")
+            # print("Code:", code)
+    raise ValueError("Failed to generate valid code")
+
+
+def get_packages(source_code):
+    tree = ast.parse(source_code)
+    packages = []
+
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import):
+            for n in node.names:
+                packages.append(n.name)
+        elif isinstance(node, ast.ImportFrom):
+            module = node.module
+            packages.append(module.split(".")[0])
+
+    return packages
---
+++ packages/pyaskit/pyaskit-1.4.5-py3-none-any.whl/pyaskit/function.py
@@ -0,0 +1,155 @@
+from typing import List
+import os
+import importlib
+from . import types as t
+from .template import convert_template, extract_variables
+from .dialog import query, improve
+from .function_name import generate_unique_function_name
+from .code_generator import implement_body
+from .prompt import make_coding_prompt
+from .path import add_to_sys_path
+from .example import ExampleType, check_examples
+from .logging_config import setup_logger
+from .types.converter import convert_type
+
+
+logger = setup_logger(__name__)
+
+# Default path
+module_path = os.path.join(os.getcwd(), "askit")
+
+
+class Response:
+    def __init__(self, answer, answer_type, completion, reason, errors, messages):
+        self.answer = answer
+        self.answer_type = answer_type
+        self.completion = completion
+        self.reason = reason
+        self.errors = errors
+        self.messages = messages
+
+    def improve(self, feedback: str):
+        answer, reason, errors, completion, _ = improve(
+            self.answer, self.answer_type, self.messages, feedback
+        )
+        return Response(
+            answer, self.answer_type, completion, reason, errors, self.messages
+        )
+
+
+class Function:
+    def __init__(
+        self,
+        return_type,
+        param_types,
+        template: str,
+        training_examples: ExampleType = [],
+        order: List[str] = None,
+    ) -> None:
+        return_type = convert_type(return_type)
+        if not isinstance(return_type, t.Type):
+            raise ValueError(
+                "return_type must be a type hint or Type defined in pyaskit.types"
+            )
+        if param_types is not None:
+            for k, v in param_types.items():
+                param_types[k] = convert_type(v)
+        self.return_type = return_type
+        self.param_types = param_types
+        self.template = template
+        self.variables = extract_variables(self.template, order=order)
+
+        check_examples(return_type, self.variables, training_examples)
+        self.training_examples = training_examples
+        self._reason = ""
+        self._errors: List[str] = []
+        self._completion = None
+        self._recompilation_count = 0
+        self._validator = None
+
+    def set_validator(self, validator):
+        self._validator = validator
+
+    def __call__(self, *args, **kwargs):
+        response = self.ask(*args, **kwargs)
+        self._reason = response.reason
+        self._errors = response.errors
+        self._completion = response.completion
+        return response.answer
+
+    def ask(self, *args, **kwargs):
+        converted_template = convert_template(self.template)
+        variableMap = {}
+        self.check_args(args, kwargs, self.variables, variableMap)
+        answer, reason, _errors, completion, messages = query(
+            converted_template,
+            variableMap,
+            self.return_type,
+            self.training_examples,
+            self._validator,
+        )
+        return Response(answer, self.return_type, completion, reason, _errors, messages)
+
+    @property
+    def reason(self):
+        return self._reason
+
+    @property
+    def errors(self):
+        return self._errors
+
+    @property
+    def completion(self):
+        return self._completion
+
+    @property
+    def recompilation_count(self):
+        return self._recompilation_count
+
+    def check_args(self, args, kwargs, variables, variableMap):
+        for var, arg in zip(variables, args):
+            if var in kwargs:
+                raise ValueError(f"got multiple values for argument '{var}'")
+            variableMap[var] = arg
+        for variable in variables:
+            if variable in variableMap:
+                continue
+            if variable in kwargs:
+                variableMap[variable] = kwargs[variable]
+            else:
+                raise ValueError("Variable not found: " + variable)
+
+    def compile(self, test_examples: ExampleType = []):
+        check_examples(self.return_type, self.variables, test_examples)
+        task = convert_template(self.template)
+        function_name = generate_unique_function_name(task)
+        # check if function_name.py exists
+        module_file_path = os.path.join(module_path, function_name + ".py")
+        if not os.path.exists(module_file_path):
+            print(f"Creating file {module_file_path}")
+            prompt = make_coding_prompt(
+                self.return_type,
+                self.param_types,
+                task,
+                function_name,
+                self.variables,
+                self.training_examples,
+            )
+            # print("Prompt:", prompt)
+            code, self._recompilation_count, retry_count = implement_body(
+                function_name, prompt, test_examples
+            )
+            os.makedirs(module_path, exist_ok=True)
+            with open(module_file_path, "w") as f:
+                f.write(
+                    "# Recompilation count: " + str(self._recompilation_count) + "\n"
+                    "# Retry count: " + str(retry_count) + "\n"
+                )
+                f.write(code)
+        else:
+            # logger.setLevel(logging.DEBUG)
+            logger.debug(f"File {module_file_path} already exists")
+        with add_to_sys_path(module_path):
+            module = importlib.import_module(function_name)
+            importlib.reload(module)
+        return getattr(module, function_name)
