b685f5fb5042ab79226628846efcc582ca8ba5c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:05:51 +0000

    Add package vasu/0.0.1/vasu-0.0.1.tar.gz

---
+++ packages/vasu/vasu-0.0.1.tar.gz/vasu-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="vasu",  # Your desired PyPI package name
+    version="0.0.1",  # Initial version
+    author="Ankit Chaubey",
+    author_email="m.ankitchaubey@gmail.com",
+    description="Placeholder for the vasu package",
+    long_description="This is a placeholder package initial release.",
+    long_description_content_type="text/plain",
+    url="https://github.com/ankit-chaubey/vasu",  # Replace with your GitHub URL
+    packages=["vasu"],  # Main package folder
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
