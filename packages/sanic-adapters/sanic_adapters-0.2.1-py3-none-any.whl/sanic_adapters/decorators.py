8c6d64a27039ab1f0f63e3459cb0d1680ed31483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:06:06 +0000

    Add package sanic-adapters/0.2.1/sanic_adapters-0.2.1-py3-none-any.whl

---
+++ packages/sanic-adapters/sanic_adapters-0.2.1-py3-none-any.whl/sanic_adapters/decorators.py
@@ -0,0 +1,26 @@
+from typing import Type
+
+from sanic_adapters.formatters import Formatters
+from sanic_adapters.resources import RESTResource
+from sanic_adapters.routing import (
+    Route,
+    RoutePart,
+    Routing,
+)
+
+
+class ResourceOverride:
+    def __init__(self, path: str):
+        self.path = path
+
+    def __call__(self, cls: Type[RESTResource]):
+        name = Formatters.to_underscore(cls.__name__)
+        Routing.route_parts[cls.__name__] = RoutePart(url_prefix=self.path, name=name)
+        return cls
+
+def route(name: str, path: str, http_method: str = None) -> callable:
+    def route_decorator(handler):
+        new_route = Route(name, path, http_method, handler)
+        Routing.register(new_route)
+        return handler
+    return route_decorator
