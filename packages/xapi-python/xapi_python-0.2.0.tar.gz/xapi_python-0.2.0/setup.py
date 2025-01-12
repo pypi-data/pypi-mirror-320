5afe130c5538bc8a7c9401c9bd69de9316c11420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:01 +0000

    Add package xapi-python/0.2.0/xapi_python-0.2.0.tar.gz

---
+++ packages/xapi-python/xapi_python-0.2.0.tar.gz/xapi_python-0.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='xapi-python',
+    author='Paweł Knioła',
+    author_email='pawel@kniola.pl',
+    description='The xStation5 API Python library',
+    long_description=open('README.md', encoding='utf-8').read(),
+    license='MIT',
+    keywords='python python3 bitcoin trading websocket trading-api forex xapi forex-trading exchange-api forex-data xstation xstation5 xtb xopenhub forex-api xopenhub-api xtb-api xstation-api x-trade-brokers bfbcapital',
+    url='https://github.com/pawelkn/xapi-python',
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+    ],
+    python_requires='>=3.7',
+    version="0.2.0",
+    packages=['xapi'],
+)
