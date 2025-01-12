c1daa468fb37aebe1ca8bd1863eec541471b7b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 04:05:29 +0000

    Add package ramonak/0.2.1/ramonak-0.2.1-py3-none-any.whl

---
+++ packages/ramonak/ramonak-0.2.1-py3-none-any.whl/ramonak/stemmer/base.py
@@ -0,0 +1,35 @@
+import re
+
+
+class Stemmer:
+    def stem_word(self, word):
+        raise NotImplementedError()
+
+    @staticmethod
+    def fix_lang_phenomenons(word: str) -> str:
+        # region dzekannie, tsekannie, soft + jvowel = hard + jvowel
+        vowel_pairs = {
+            "е": "э",
+            "ё": "о",
+            "ю": "у",
+            "я": "а",
+            "і": "ы",
+        }
+
+        for jvowel, vowel in vowel_pairs.items():
+            word = re.sub("дз" + jvowel, "д" + vowel, word)
+            word = re.sub("ц" + jvowel, "т" + vowel, word)
+
+            word = re.sub("дзв" + jvowel, "дв" + vowel, word)
+            word = re.sub("цв" + jvowel, "тв" + vowel, word)
+        # endregion
+
+        return word
+
+    def stem_words(self, words: list[str]) -> list[str]:
+        result = []
+
+        for word in words:
+            result.append(self.stem_word(word))
+
+        return result
