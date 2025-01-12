b7396dd409f05a208f7e1620425439bbbfe0ccc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 05:04:52 +0000

    Add package abcli/9.462.1/abcli-9.462.1.tar.gz

---
+++ packages/abcli/abcli-9.462.1.tar.gz/abcli-9.462.1/abcli/.abcli/plugins/ssm/get.sh
@@ -0,0 +1,45 @@
+#! /usr/bin/env bash
+
+function abcli_ssm_get() {
+    local options=$1
+
+    local secret_name
+    local secret_value
+
+    local path=$(abcli_option "$options" path)
+    if [[ ! -z "$path" ]]; then
+        if [[ ! -f "$path/sample.env" ]]; then
+            abcli_log_warning "@ssm: get: $path/sample.env: file not found."
+            return 1
+        fi
+
+        pushd $path >/dev/null
+        local line
+        local count=0
+        for line in $(dotenv \
+            --file sample.env \
+            list \
+            --format shell); do
+
+            secret_name=$(python3 -c "print('$line'.split('=',1)[0])")
+            abcli_log "🔑 $secret_name"
+
+            secret_value=$(abcli_ssm get $secret_name)
+
+            export $secret_name=$secret_value
+
+            ((count++))
+        done
+        popd >/dev/null
+
+        abcli_log "@ssm: get: $count secret(s): $path/sample.env"
+        return
+    fi
+
+    secret_name=$1
+
+    python3 -m abcli.plugins.ssm \
+        get \
+        --name "$secret_name" \
+        "${@:2}"
+}
