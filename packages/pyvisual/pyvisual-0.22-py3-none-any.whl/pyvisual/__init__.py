995f7908a9f8cb2703021953ec305fc104500c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 05:04:49 +0000

    Add package pyvisual/0.22/pyvisual-0.22-py3-none-any.whl

---
+++ packages/pyvisual/pyvisual-0.22-py3-none-any.whl/pyvisual/__init__.py
@@ -0,0 +1,34 @@
+# Core
+from pyvisual.core.pv_window import PvWindow
+
+# Inputs
+from pyvisual.ui.inputs.pv_button import PvButton
+from pyvisual.ui.inputs.pv_text_input import PvTextInput
+from pyvisual.ui.inputs.pv_checkbox import PvCheckbox
+
+# Outputs
+from pyvisual.ui.outputs.pv_text import PvText
+from pyvisual.ui.outputs.pv_image import PvImage
+
+# Shapes
+from pyvisual.ui.shapes.pv_circle import PvCircle
+from pyvisual.ui.shapes.pv_rectangle import PvRectangle
+from pyvisual.ui.shapes.pv_line import PvLine
+
+# Layouts
+from pyvisual.ui.layouts.pv_scroll import PvScroll
+from pyvisual.ui.layouts.pv_tabs import PvTabs
+from pyvisual.ui.layouts.pv_group import PvGroup
+
+# Experimental
+# from pyvisual.ui.more.basic_menu import BasicMenuBar
+
+from pyvisual.ui.templates.pv_chatbot import PvChatbot
+
+from pyvisual.utils.pv_timer import PvTimer
+
+
+
+
+
+
