d305196a06e291e58484ea0f902b61297fff98dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:45 +0000

    Add package mh-structlog/0.0.31/mh_structlog-0.0.31-py3-none-any.whl

---
+++ packages/mh-structlog/mh_structlog-0.0.31-py3-none-any.whl/mh_structlog/__init__.py
@@ -0,0 +1,38 @@
+import typing as t
+from logging import CRITICAL, DEBUG, ERROR, FATAL, INFO, WARN, WARNING
+
+import structlog
+
+from .config import filter_named_logger, setup
+from .django import StructLogAccessLoggingMiddleware
+from .utils import determine_name_for_logger
+
+
+def getLogger(name: t.Optional[str] = None):  # noqa: ANN201, N802
+    """Return a named logger."""
+    if name is None:
+        name = determine_name_for_logger()
+    return structlog.get_logger(name)
+
+
+def get_logger(name: t.Optional[str] = None):  # noqa: ANN201
+    """Return a named logger."""
+    return getLogger(name)
+
+
+__all__ = [
+    "CRITICAL",
+    "DEBUG",
+    "ERROR",
+    "FATAL",
+    "INFO",
+    "WARN",
+    "WARNING",
+    "StructLogAccessLoggingMiddleware",
+    "filter_named_logger",
+    "getLogger",
+    "getLogger",
+    "get_logger",
+    "get_logger",
+    "setup",
+]
