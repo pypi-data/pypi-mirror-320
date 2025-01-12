4820b656a754f66909d55c7b2f6946661b11804a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 05:04:55 +0000

    Add package sugardata/0.0.1/sugardata-0.0.1.tar.gz

---
+++ packages/sugardata/sugardata-0.0.1.tar.gz/sugardata-0.0.1/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+
+
+with open("Readme.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="sugardata",
+    version="0.0.1",
+    description="Generates synthetic datasets tailored for transformer-based models",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/okanyenigun/sugardata",
+    author="Okan Yenigün",
+    author_email="okanyenigun@gmail.com",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Education',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent'
+    ],
+    install_requires=[
+        'transformers==4.48.0',
+        'pandas==2.2.3',
+        'torch==2.5.1',
+        'ipywidgets==8.1.5',
+    ],
+    python_requires='>=3.7',
+)
