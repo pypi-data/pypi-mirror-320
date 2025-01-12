8c6cd82a036c8b482e39b48a69624008a725cf8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 15:04:25 +0000

    Add package sklcc-ssubmit/0.0.7/sklcc_ssubmit-0.0.7.tar.gz

---
+++ packages/sklcc-ssubmit/sklcc_ssubmit-0.0.7.tar.gz/sklcc_ssubmit-0.0.7/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="sklcc_ssubmit",
+    version="0.0.7",
+    packages=setuptools.find_packages(),
+    install_requires=['bcrypt>=4.0.1','cffi>=1.15.1','cryptography>=40.0.2','paramiko>=3.5.0','pycparser>=2.21','PyNaCl>=1.5.0'],
+    python_requires=">=3.6"
+
+)
