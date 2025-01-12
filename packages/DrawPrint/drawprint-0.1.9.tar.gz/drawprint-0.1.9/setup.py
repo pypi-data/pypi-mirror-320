e1aa927eecd066852d6e41b3f42536063cbddc6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 05:04:46 +0000

    Add package DrawPrint/0.1.9/drawprint-0.1.9.tar.gz

---
+++ packages/DrawPrint/drawprint-0.1.9.tar.gz/drawprint-0.1.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='DrawPrint',
+    version='0.1.9',
+    description='DrawPrint',
+    author='huang yi yi',
+    author_email='363766687@qq.com',
+    packages=find_packages(),
+    py_modules=['DrawPrint'],
+    python_requires='>=3.6',
+    install_requires=[
+        "pyfiglet>=0.8.post1",
+        "pygame>=2.6.0.post1",
+        "gameturtle>=0.280.post1",
+        "sprites>=10.3.0.post1",
+        "pyecharts>=2.0.6.post1",
+    ]
+)
