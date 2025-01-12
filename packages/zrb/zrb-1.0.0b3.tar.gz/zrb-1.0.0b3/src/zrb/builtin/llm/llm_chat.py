e0d814e12bb9804ca537490c4e598a4aa1db16a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:16 +0000

    Add package zrb/1.0.0b3/zrb-1.0.0b3.tar.gz

---
+++ packages/zrb/zrb-1.0.0b3.tar.gz/zrb-1.0.0b3/src/zrb/builtin/llm/llm_chat.py
@@ -0,0 +1,49 @@
+from zrb.builtin.group import llm_group
+from zrb.builtin.llm.tool.cli import run_shell_command
+from zrb.builtin.llm.tool.rag import create_rag_from_directory
+from zrb.builtin.llm.tool.web import open_web_route, query_internet
+from zrb.config import (
+    LLM_ALLOW_ACCESS_SHELL,
+    LLM_ALLOW_ACCESS_WEB,
+    LLM_HISTORY_FILE,
+    LLM_MODEL,
+    LLM_SYSTEM_PROMPT,
+)
+from zrb.input.str_input import StrInput
+from zrb.input.text_input import TextInput
+from zrb.task.llm_task import LLMTask
+
+llm_chat: LLMTask = llm_group.add_task(
+    LLMTask(
+        name="llm-chat",
+        input=[
+            StrInput(
+                "model",
+                description="LLM Model",
+                prompt="LLM Model",
+                default_str=LLM_MODEL,
+            ),
+            TextInput(
+                "system-prompt",
+                description="System prompt",
+                prompt="System prompt",
+                default_str=LLM_SYSTEM_PROMPT,
+            ),
+            TextInput("message", description="User message", prompt="Your message"),
+        ],
+        history_file=LLM_HISTORY_FILE,
+        description="Chat with LLM",
+        model="{ctx.input.model}",
+        system_prompt="{ctx.input['system-prompt']}",
+        message="{ctx.input.message}",
+        retries=0,
+    ),
+    alias="chat",
+)
+
+if LLM_ALLOW_ACCESS_SHELL:
+    llm_chat.add_tool(run_shell_command)
+
+if LLM_ALLOW_ACCESS_WEB:
+    llm_chat.add_tool(open_web_route)
+    llm_chat.add_tool(query_internet)
