c2cd08847c925e932adf77dca0afeb902272701f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:28 +0000

    Add package aiohubspace/0.5.1/aiohubspace-0.5.1.tar.gz

---
+++ packages/aiohubspace/aiohubspace-0.5.1.tar.gz/aiohubspace-0.5.1/CHANGELOG.rst
@@ -0,0 +1,21 @@
+=========
+Changelog
+=========
+
+Version 0.2
+===========
+
+ * Added support for Binary Sensors
+ * Fixed an issue where a dimmer switch could not be dimmed
+
+Version 0.2
+===========
+
+ * Added support for Sensors
+
+Version 0.1
+===========
+
+ * Initial implementation
+ * Rename from hubspace_async to aiohubspace
+ * Utilize the concept of a bridge instead of raw connection
---
+++ packages/aiohubspace/aiohubspace-0.5.1.tar.gz/aiohubspace-0.5.1/src/aiohubspace/v1/models/switch.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass, field
+
+from ..models import features
+from .resource import DeviceInformation, ResourceTypes
+
+
+@dataclass
+class Switch:
+    """Representation of a Hubspace Switch"""
+
+    id: str  # ID used when interacting with Hubspace
+    available: bool
+
+    on: dict[str, features.OnFeature]
+    # Defined at initialization
+    instances: dict = field(default_factory=lambda: dict(), repr=False, init=False)
+    device_information: DeviceInformation = field(default_factory=DeviceInformation)
+
+    type: ResourceTypes = ResourceTypes.FAN
+
+    def __init__(self, functions: list, **kwargs):
+        for key, value in kwargs.items():
+            if key == "instances":
+                continue
+            setattr(self, key, value)
+        instances = {}
+        for function in functions:
+            try:
+                if function["functionInstance"]:
+                    instances[function["functionClass"]] = function["functionInstance"]
+            except KeyError:
+                continue
+        self.instances = instances
+
+    def get_instance(self, elem):
+        """Lookup the instance associated with the elem"""
+        return self.instances.get(elem, None)
+
+
+@dataclass
+class SwitchPut:
+    """States that can be updated for a Switch"""
+
+    on: features.OnFeature | None = None
