36b98d9e3ce6b1d6714985a6d4b439cf17e82213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:05:39 +0000

    Add package qwikswitch-api/0.0.5/qwikswitch_api-0.0.5.tar.gz

---
+++ packages/qwikswitch-api/qwikswitch_api-0.0.5.tar.gz/qwikswitch_api-0.0.5/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+import requests_mock
+
+from qwikswitchapi.entities.apikeys import ApiKeys
+from qwikswitchapi.qsapi import QSApi
+
+
+@pytest.fixture
+def mock_request():
+    with requests_mock.Mocker() as m:
+        yield m
+
+@pytest.fixture()
+def mock_api_keys():
+    return ApiKeys('read_key', 'read_write_key')
+
+@pytest.fixture()
+def api():
+    return QSApi()
