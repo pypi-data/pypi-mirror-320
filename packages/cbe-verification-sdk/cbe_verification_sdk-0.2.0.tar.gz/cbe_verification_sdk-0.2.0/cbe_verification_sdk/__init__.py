5fbbd071980bacd8a325eface996d1fcb4609f02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:06:07 +0000

    Add package cbe-verification-sdk/0.2.0/cbe_verification_sdk-0.2.0.tar.gz

---
+++ packages/cbe-verification-sdk/cbe_verification_sdk-0.2.0.tar.gz/cbe_verification_sdk-0.2.0/cbe_verification_sdk/__init__.py
@@ -0,0 +1,3 @@
+from .client import CBEVerificationClient
+
+__version__ = "0.2.0" 
---
+++ packages/cbe-verification-sdk/cbe_verification_sdk-0.2.0.tar.gz/cbe_verification_sdk-0.2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cbe-verification-sdk",
+    version="0.2.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.25.0",
+        "PyPDF2>=3.0.0"
+    ],
+    author="Naod Yeshiwas",
+    author_email="yegnadevelopers@gmail.com",
+    description="SDK for CBE Transaction Verification API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/NaodYeshiwas/CBE-Verification-API",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+) 
