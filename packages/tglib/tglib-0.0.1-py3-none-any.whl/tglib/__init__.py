353a5b171a5eee3b96451347bd54c508fc2a4e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 15:04:18 +0000

    Add package tglib/0.0.1/tglib-0.0.1-py3-none-any.whl

---
+++ packages/tglib/tglib-0.0.1-py3-none-any.whl/tglib/__init__.py
@@ -0,0 +1,14 @@
+"""
+tglib - A placeholder Python package for Telegram-related functionalities.
+
+This is the initial release of the package and serves as the foundation for future
+development. Additional features and functionality will be added in upcoming releases.
+"""
+
+# Version of the package
+__version__ = "0.0.1"
+
+# Author information
+__author__ = "Ankit Chaubey"
+__email__ = "m.ankitchaubey@gmail.com"
+
