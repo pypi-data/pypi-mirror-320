d3e125c2a758c781e9666eb01b2679cbfd5ced1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 04:04:58 +0000

    Add package multidimio/0.8.4/multidimio-0.8.4.tar.gz

---
+++ packages/multidimio/multidimio-0.8.4.tar.gz/multidimio-0.8.4/src/mdio/segy/blocked_io.py
@@ -0,0 +1,314 @@
+"""Functions for doing blocked I/O from SEG-Y."""
+
+from __future__ import annotations
+
+import multiprocessing as mp
+import os
+from concurrent.futures import ProcessPoolExecutor
+from itertools import repeat
+from typing import TYPE_CHECKING
+
+import numpy as np
+from dask.array import Array
+from dask.array import blockwise
+from dask.array.reductions import _tree_reduce
+from psutil import cpu_count
+from tqdm.auto import tqdm
+from zarr import Blosc
+from zarr import Group
+
+from mdio.core import Grid
+from mdio.core.indexing import ChunkIterator
+from mdio.segy._workers import trace_worker
+from mdio.segy.creation import concat_files
+from mdio.segy.creation import write_to_segy_stack
+
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+    from segy import SegyFactory
+    from segy import SegyFile
+
+try:
+    import zfpy  # Base library
+    from zarr import ZFPY  # Codec
+
+except ImportError:
+    ZFPY = None
+    zfpy = None
+
+default_cpus = cpu_count(logical=True)
+
+
+def to_zarr(
+    segy_file: SegyFile,
+    grid: Grid,
+    data_root: Group,
+    metadata_root: Group,
+    name: str,
+    chunks: tuple[int, ...],
+    lossless: bool,
+    compression_tolerance: float = 0.01,
+    **kwargs,
+) -> dict:
+    """Blocked I/O from SEG-Y to chunked `zarr.core.Array`.
+
+    Args:
+        segy_file: SEG-Y file instance.
+        grid: mdio.Grid instance
+        data_root: Handle for zarr.core.Group we are writing traces
+        metadata_root: Handle for zarr.core.Group we are writing trace headers
+        name: Name of the zarr.Array
+        chunks: Chunk sizes for trace data
+        lossless: Lossless Blosc with zstandard, or ZFP with fixed precision.
+        compression_tolerance: Tolerance ZFP compression, optional. The fixed
+            accuracy mode in ZFP guarantees there won't be any errors larger
+            than this value. The default is 0.01, which gives about 70%
+            reduction in size.
+        **kwargs: Additional keyword arguments passed to zarr.core.Array  # noqa: RST210
+
+    Returns:
+        Global statistics for the SEG-Y as a dictionary.
+
+    Raises:
+        ImportError: if the ZFP isn't installed and user requests lossy.
+    """
+    if lossless is True:
+        trace_compressor = Blosc("zstd")
+        header_compressor = trace_compressor
+    elif ZFPY is not None or zfpy is not None:
+        trace_compressor = ZFPY(
+            mode=zfpy.mode_fixed_accuracy,
+            tolerance=compression_tolerance,
+        )
+        header_compressor = Blosc("zstd")
+    else:
+        raise ImportError(
+            "Lossy compression requires the 'zfpy' library. It is "
+            "not installed in your environment. To proceed please "
+            "install 'zfpy' or install mdio with `--extras lossy`"
+        )
+
+    trace_array = data_root.create_dataset(
+        name=name,
+        shape=grid.shape,
+        compressor=trace_compressor,
+        chunks=chunks,
+        dimension_separator="/",
+        write_empty_chunks=False,
+        **kwargs,
+    )
+
+    # Get header dtype in native order (little-endian 99.9% of the time)
+    header_dtype = segy_file.spec.trace.header.dtype.newbyteorder("=")
+    header_array = metadata_root.create_dataset(
+        name="_".join([name, "trace_headers"]),
+        shape=grid.shape[:-1],  # Same spatial shape as data
+        chunks=chunks[:-1],  # Same spatial chunks as data
+        compressor=header_compressor,
+        dtype=header_dtype,
+        dimension_separator="/",
+        write_empty_chunks=False,
+    )
+
+    # Initialize chunk iterator (returns next chunk slice indices each iteration)
+    chunker = ChunkIterator(trace_array, chunk_samples=False)
+    num_chunks = len(chunker)
+
+    # For Unix async writes with s3fs/fsspec & multiprocessing,
+    # use 'spawn' instead of default 'fork' to avoid deadlocks
+    # on cloud stores. Slower but necessary. Default on Windows.
+    num_cpus = int(os.getenv("MDIO__IMPORT__CPU_COUNT", default_cpus))
+    num_workers = min(num_chunks, num_cpus)
+    context = mp.get_context("spawn")
+    executor = ProcessPoolExecutor(max_workers=num_workers, mp_context=context)
+
+    # Chunksize here is for multiprocessing, not Zarr chunksize.
+    pool_chunksize, extra = divmod(num_chunks, num_workers * 4)
+    pool_chunksize += 1 if extra else pool_chunksize
+
+    tqdm_kw = dict(unit="block", dynamic_ncols=True)
+    with executor:
+        lazy_work = executor.map(
+            trace_worker,  # fn
+            repeat(segy_file),
+            repeat(trace_array),
+            repeat(header_array),
+            repeat(grid),
+            chunker,
+            chunksize=pool_chunksize,
+        )
+
+        lazy_work = tqdm(
+            iterable=lazy_work,
+            total=num_chunks,
+            desc=f"Ingesting SEG-Y in {num_chunks} chunks",
+            **tqdm_kw,
+        )
+
+        # This executes the lazy work.
+        chunk_stats = list(lazy_work)
+
+    # This comes in as n_chunk x 5 columns.
+    # Columns in order: count, sum, sum of squared, min, max.
+    # From here we can compute global mean, std, rms, min, max.
+    # Transposing because we want each statistic as a row to unpack later.
+    # REF: https://math.stackexchange.com/questions/1547141/aggregating-standard-deviation-to-a-summary-point  # noqa: B950
+    # REF: https://www.mathwords.com/r/root_mean_square.htm
+    chunk_stats = [stat for stat in chunk_stats if stat is not None]
+
+    chunk_stats = zip(*chunk_stats)  # noqa: B905
+    glob_count, glob_sum, glob_sum_square, glob_min, glob_max = chunk_stats
+
+    glob_count = np.sum(glob_count)  # Comes in as `uint32`
+    glob_sum = np.sum(glob_sum)  # `float64`
+    glob_sum_square = np.sum(glob_sum_square)  # `float64`
+    glob_min = np.min(glob_min)  # `float32`
+    glob_max = np.max(glob_max)  # `float32`
+
+    glob_mean = glob_sum / glob_count
+    glob_std = np.sqrt(glob_sum_square / glob_count - (glob_sum / glob_count) ** 2)
+    glob_rms = np.sqrt(glob_sum_square / glob_count)
+
+    # We need to write these as float64 because float32 is not JSON serializable
+    # Trace data is originally float32, hence min/max
+    glob_min = glob_min.min().astype("float64")
+    glob_max = glob_max.max().astype("float64")
+
+    stats = {
+        "mean": glob_mean,
+        "std": glob_std,
+        "rms": glob_rms,
+        "min": glob_min,
+        "max": glob_max,
+    }
+
+    return stats
+
+
+def segy_concat(
+    partial_files: NDArray,
+    axis: tuple[int] = None,
+    keepdims: bool = None,
+) -> NDArray:
+    """Aggregate partial SEG-Y blocks on disk, preserving order.
+
+    Used in conjunction with tree reduction. It will take an array
+    of file names, which preserved the adjacency of blocks, and then
+    combines adjacent blocks while flattening for SEG-Y.
+
+    For `axis` and `keepdims` parameters, please see `dask.array.reduce`
+    documentation.
+
+    Args:
+        partial_files: Array containing paths to parts of a SEG-Y row.
+        axis: Which axes to concatenate on.
+        keepdims: Keep the original dimensionality after merging.
+
+    Returns:
+        Concatenated file name array. Dimensions depend on `keepdims`.
+    """
+    concat_shape = partial_files.shape[0]
+    concat_paths = np.full_like(partial_files, fill_value="missing", shape=concat_shape)
+
+    # Fast path if all data in block is missing.
+    if np.all(partial_files == "missing"):
+        return np.expand_dims(concat_paths, axis) if keepdims else concat_paths
+
+    # Flatten and concat section files to a single root file at first axis.
+    for index, section_paths in enumerate(partial_files):
+        section_paths = section_paths.ravel()
+        section_missing = section_paths == "missing"
+
+        if np.all(section_missing):
+            continue
+
+        section_valid_paths = np.extract(~section_missing, section_paths).tolist()
+        section_concat_files = concat_files(section_valid_paths)
+        concat_paths[index] = section_concat_files
+
+    return np.expand_dims(concat_paths, axis) if keepdims else concat_paths
+
+
+def to_segy(
+    samples: Array,
+    headers: Array,
+    live_mask: Array,
+    segy_factory: SegyFactory,
+    file_root: str,
+    axis: tuple[int] | None = None,
+) -> Array:
+    r"""Convert MDIO blocks to SEG-Y parts.
+
+    This uses as a tree reduction algorithm. Blocks are written out
+    in parallel via multiple workers, and then adjacent blocks are
+    tracked and merged on disk via the `segy_concat` function. The
+    adjacent are hierarchically merged, and it preserves order.
+
+    Assume array with shape (4, 3, 2) with chunk sizes (1, 1, 2).
+    The chunk indices for this array would be:
+
+    (0, 0, 0) (0, 1, 0) (0, 2, 0)
+    (1, 0, 0) (1, 1, 0) (1, 2, 0)
+    (2, 0, 0) (2, 1, 0) (2, 2, 0)
+    (3, 0, 0) (3, 1, 0) (3, 2, 0)
+
+    let's rename them to this for convenience:
+
+    a b c
+    d e f
+    g h i
+    j k l
+
+    The tree gets formed this way:
+    a b c d e f g h i
+    \/  | \/  | \/  |
+    ab  c de  f gh  i
+      \/    \/    \/
+     abc   def   ghi
+
+    The module will return file names associated with these
+    concatenated files. Then they can be combined to form the
+    sequence "abcdefghi" which is what we want.
+
+    The above algorithm extrapolates to higher dimensions.
+
+    Args:
+        samples: Sample array.
+        headers: Header array.
+        live_mask: Live mask array.
+        segy_factory: A SEG-Y factory configured to write out with user params.
+        file_root: Root directory to write partial SEG-Y files.
+        axis: Which axes to merge on. Excluding sample axis.
+
+    Returns:
+        Array containing final, flattened SEG-Y blocks.
+    """
+    # Map chunk across all blocks
+    samp_inds = tuple(range(samples.ndim))
+    meta_inds = tuple(range(headers.ndim))
+
+    args = (samples, samp_inds)
+    args += (headers, meta_inds)
+    args += (live_mask, meta_inds)
+
+    # Merge samples axis, append headers, and write block as stack of SEG-Ys.
+    # Note: output is N-1 dimensional (meta_inds) because we merged samples.
+    trace_files = blockwise(
+        write_to_segy_stack,
+        meta_inds,
+        *args,
+        file_root=file_root,
+        segy_factory=segy_factory,
+        concatenate=True,
+    )
+
+    result = _tree_reduce(
+        trace_files,
+        segy_concat,
+        axis,
+        keepdims=False,
+        dtype=trace_files.dtype,
+    )
+
+    return result
---
+++ packages/multidimio/multidimio-0.8.4.tar.gz/multidimio-0.8.4/src/mdio/segy/creation.py
@@ -0,0 +1,210 @@
+"""SEG-Y creation utilities."""
+
+from __future__ import annotations
+
+import logging
+import os
+from os import path
+from shutil import copyfileobj
+from typing import TYPE_CHECKING
+from uuid import uuid4
+
+import numpy as np
+from segy.factory import SegyFactory
+from segy.schema import Endianness
+from segy.schema import SegySpec
+from tqdm.auto import tqdm
+
+from mdio.api.accessor import MDIOReader
+from mdio.segy.compat import mdio_segy_spec
+from mdio.segy.compat import revision_encode
+
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
+
+
+logger = logging.getLogger(__name__)
+
+
+def make_segy_factory(
+    mdio: MDIOReader,
+    spec: SegySpec,
+) -> SegyFactory:
+    """Generate SEG-Y factory from MDIO metadata."""
+    grid = mdio.grid
+    sample_dim = grid.select_dim("sample")
+    sample_interval = sample_dim[1] - sample_dim[0]
+    samples_per_trace = len(sample_dim)
+
+    return SegyFactory(
+        spec=spec,
+        sample_interval=sample_interval * 1000,
+        samples_per_trace=samples_per_trace,
+    )
+
+
+def mdio_spec_to_segy(
+    mdio_path_or_buffer,
+    output_segy_path,
+    access_pattern,
+    output_endian,
+    storage_options,
+    new_chunks,
+    backend,
+):
+    """Create SEG-Y file without any traces given MDIO specification.
+
+    This function opens an MDIO file, gets some relevant information for SEG-Y files,
+    then creates a SEG-Y file with the specification it read from the MDIO file.
+
+    It then returns the `MDIOReader` instance, and the parsed floating point format
+    `sample_format` for further use.
+
+    Function will attempt to read text, and binary headers, and some grid information
+    from the MDIO file. If these don't exist, the process will fail.
+
+    Args:
+        mdio_path_or_buffer: Input path where the MDIO is located.
+        output_segy_path: Path to the output SEG-Y file.
+        access_pattern: This specificies the chunk access pattern.
+            Underlying zarr.Array must exist. Examples: '012', '01'.
+        output_endian: Endianness of the output file.
+        storage_options: Storage options for the cloud storage backend.
+            Default: None (will assume anonymous)
+        new_chunks: Set manual chunksize. For development purposes only.
+        backend: Eager (zarr) or lazy but more flexible 'dask' backend.
+
+    Returns:
+        Initialized MDIOReader for MDIO file and return SegyFactory
+    """
+    mdio = MDIOReader(
+        mdio_path_or_buffer=mdio_path_or_buffer,
+        access_pattern=access_pattern,
+        storage_options=storage_options,
+        return_metadata=True,
+        new_chunks=new_chunks,
+        backend=backend,
+        memory_cache_size=0,  # Making sure disk caching is disabled
+        disk_cache=False,  # Making sure disk caching is disabled
+    )
+
+    mdio_file_version = mdio.root.attrs["api_version"]
+    spec = mdio_segy_spec(mdio_file_version)
+    spec.endianness = Endianness(output_endian)
+    factory = make_segy_factory(mdio, spec=spec)
+
+    text_str = "\n".join(mdio.text_header)
+    text_bytes = factory.create_textual_header(text_str)
+
+    binary_header = revision_encode(mdio.binary_header, mdio_file_version)
+    bin_hdr_bytes = factory.create_binary_header(binary_header)
+
+    with open(output_segy_path, mode="wb") as fp:
+        fp.write(text_bytes)
+        fp.write(bin_hdr_bytes)
+
+    return mdio, factory
+
+
+def write_to_segy_stack(
+    samples: NDArray,
+    headers: NDArray,
+    live: NDArray,
+    file_root: str,
+    segy_factory: SegyFactory,
+) -> NDArray:
+    """Pre-process seismic data for SEG-Y and write partial blocks.
+
+    This function will take numpy arrays for trace samples, headers, and live mask.
+    Then it will do the following:
+    1. Convert sample format to `out_dtype`.
+    2. Byte-swap samples and headers if needed based on `out_byteorder`.
+    3. Iterate inner dimensions and write blocks of traces.
+    3.1. Combine samples and headers to form a SEG-Y trace.
+    3.2. Drop non-live samples.
+    3.3. Write line block to disk.
+    3.4. Save file names for further merges
+    4. Written files will be saved, so further merge methods
+    can combine them to a single flat SEG-Y dataset.
+
+    Args:
+        samples: Array containing the trace samples.
+        headers: Array containing the trace headers.
+        live: Array containing the trace live mask.
+        file_root: Root directory to write partial SEG-Y files.
+        segy_factory: A SEG-Y factory configured to write out with user params.
+
+    Returns:
+        Array containing file names for partial data. None means
+        there were no live traces within the block / line.
+    """
+    # Make output array with string type. We need to know
+    # the length of the string ahead of time.
+    # Last axis can be written as sequential, so we collapse that to 1.
+    mock_path = path.join(file_root, uuid4().hex)
+    paths_dtype = f"U{len(mock_path)}"
+    paths_shape = live.shape[:-1] + (1,)
+    part_segy_paths = np.full(paths_shape, fill_value="missing", dtype=paths_dtype)
+
+    # Fast path to return if no live traces.
+    if np.count_nonzero(live) == 0:
+        return part_segy_paths
+
+    # Iterate on N-1 axes of live mask. Last axis can be written
+    # without worrying about order because it is sequential.
+    for index in np.ndindex(live.shape[:-1]):
+        part_live = live[index]
+        num_live = np.count_nonzero(part_live)
+
+        if num_live == 0:
+            continue
+
+        # Generate unique file name and append to return list.
+        file_path = path.join(file_root, uuid4().hex)
+        part_segy_paths[index] = file_path
+
+        # Create traces in bytes
+        trace_bytes = segy_factory.create_traces(
+            headers=headers[index][part_live],
+            samples=samples[index][part_live],
+        )
+
+        with open(file_path, mode="wb") as fp:
+            fp.write(trace_bytes)
+
+    return part_segy_paths
+
+
+# TODO: Abstract this to support various implementations by
+#  object stores and file systems. Probably fsspec solution.
+def concat_files(paths: list[str], progress=False) -> str:
+    """Concatenate files on disk, sequentially in given order.
+
+    This function takes files on disk, and it combines them by
+    concatenation. Input files are deleted after merge, so disk
+    usage doesn't explode.
+
+    This is only required for disk / on-prem; since object stores
+    have their optimized file concatenation implementations.
+
+    Args:
+        paths: Paths to the blocks of SEG-Y.
+        progress: Enable tqdm progress bar. Default is False.
+
+    Returns:
+        Path to the returned file (first one from input).
+    """
+    first_file = paths.pop(0)
+
+    if progress is True:
+        paths = tqdm(paths, desc="Merging lines")
+
+    with open(first_file, "ab+") as first_fp:
+        for next_file in paths:
+            with open(next_file, "rb") as next_fp:
+                copyfileobj(next_fp, first_fp)
+
+            os.remove(next_file)
+
+    return first_file
