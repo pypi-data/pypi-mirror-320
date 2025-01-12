12caca4ec1899b34ea9095c81920a332edac8be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:03 +0000

    Add package playsoundffmpeg/0.1.0/playsoundffmpeg-0.1.0.tar.gz

---
+++ packages/playsoundffmpeg/playsoundffmpeg-0.1.0.tar.gz/playsoundffmpeg-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='playsoundffmpeg',
+    version='0.1.0',
+    author='yuan',
+    author_email='zhtxiii@163.com',
+    description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=[],
+)
