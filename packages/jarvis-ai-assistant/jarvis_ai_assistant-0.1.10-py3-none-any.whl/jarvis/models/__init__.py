1c02ce600af3f0873d808c6c821db3bfadd281b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 14:06:08 +0000

    Add package jarvis-ai-assistant/0.1.10/jarvis_ai_assistant-0.1.10-py3-none-any.whl

---
+++ packages/jarvis-ai-assistant/jarvis_ai_assistant-0.1.10-py3-none-any.whl/jarvis/models/__init__.py
@@ -0,0 +1,11 @@
+from typing import Dict, List, Optional, Tuple
+from duckduckgo_search import DDGS
+import ollama
+import yaml
+import openai
+
+from ..utils import OutputType, PrettyOutput
+from .base import BaseModel
+from .kimi import KimiModel
+
+__all__ = ['BaseModel', 'KimiModel'] 
