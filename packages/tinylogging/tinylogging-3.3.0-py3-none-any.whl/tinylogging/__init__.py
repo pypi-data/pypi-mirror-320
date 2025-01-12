c6563d2600c4ef4e1a43001e3857539cac510543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 05:04:47 +0000

    Add package tinylogging/3.3.0/tinylogging-3.3.0-py3-none-any.whl

---
+++ packages/tinylogging/tinylogging-3.3.0-py3-none-any.whl/tinylogging/__init__.py
@@ -0,0 +1,35 @@
+from tinylogging.aio import AsyncLogger
+from tinylogging.aio.handlers import (
+    AsyncFileHandler,
+    AsyncStreamHandler,
+    BaseAsyncHandler,
+)
+from tinylogging.formatter import Formatter
+from tinylogging.level import Level
+from tinylogging.record import Record
+from tinylogging.sync import Logger
+from tinylogging.sync.handlers import (
+    BaseHandler,
+    FileHandler,
+    LoggingAdapterHandler,
+    StreamHandler,
+)
+
+__all__ = [
+    "COLOR_MAP",
+    "Record",
+    "Formatter",
+    "BaseHandler",
+    "StreamHandler",
+    "FileHandler",
+    "LoggingAdapterHandler",
+    "Logger",
+    "AsyncLogger",
+    "BaseAsyncHandler",
+    "AsyncStreamHandler",
+    "AsyncFileHandler",
+    "Level",
+]
+
+COLOR_MAP = Formatter().color_map
+"""Deprecated"""
