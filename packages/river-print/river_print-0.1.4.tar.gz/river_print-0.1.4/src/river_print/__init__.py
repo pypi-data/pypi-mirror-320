e5d14c8f6ef2c37a8ad10a28710408b009df8bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-12 03:13:01 +0000

    Add package river-print/0.1.4/river_print-0.1.4.tar.gz

---
+++ packages/river-print/river_print-0.1.4.tar.gz/river_print-0.1.4/src/river_print/__init__.py
@@ -0,0 +1,23 @@
+from .main import (
+    action,
+    error,
+    question,
+    rprint,
+    sep________,
+    separator,
+    set_print_method,
+    success,
+    warning,
+)
+
+__all__ = [
+    "rprint",
+    "action",
+    "question",
+    "success",
+    "warning",
+    "error",
+    "sep________",
+    "separator",
+    "set_print_method",
+]
