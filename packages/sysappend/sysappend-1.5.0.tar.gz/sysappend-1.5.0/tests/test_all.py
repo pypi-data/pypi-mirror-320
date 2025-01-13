b304537a14e264712df6709fc138e2c01f064097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 16:05:31 +0000

    Add package sysappend/1.5.0/sysappend-1.5.0.tar.gz

---
+++ packages/sysappend/sysappend-1.5.0.tar.gz/sysappend-1.5.0/tests/test_all.py
@@ -0,0 +1,25 @@
+if True: import sys; sys.path.append('.') # needed to run this file
+
+
+def test_all():
+    all_paths = sys.path.copy()
+    
+    from sysappend import all
+    all()
+    
+    assert sys.path != all_paths
+    
+
+if __name__ == '__main__':
+    import inspect
+    
+    def run_all_tests():
+        for name, obj in inspect.getmembers(sys.modules[__name__]):
+            if inspect.isfunction(obj) and name.startswith('test_'):
+                print(f"Running {name}...")
+                obj()
+
+    run_all_tests()
+    
+    
+    
