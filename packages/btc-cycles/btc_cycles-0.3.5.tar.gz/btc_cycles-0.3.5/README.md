4b0ec69b666d256210e1279ca4ebb012efc4ad65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:05:32 +0000

    Add package btc-cycles/0.3.5/btc_cycles-0.3.5.tar.gz

---
+++ packages/btc-cycles/btc_cycles-0.3.5.tar.gz/btc_cycles-0.3.5/README.md
@@ -0,0 +1,38 @@
+# btc-cycles
+
+[![Update chart](https://github.com/giocaizzi/btc-cycles/actions/workflows/run.yml/badge.svg)](https://github.com/giocaizzi/btc-cycles/actions/workflows/run.yml)
+![PyPI - Version](https://img.shields.io/pypi/v/btc-cycles?color=blue)
+![PyPI - Python Version](https://img.shields.io/pypi/pyversions/btc-cycles)
+[![Deploy](https://github.com/giocaizzi/btc-cycles/actions/workflows/deployment.yml/badge.svg)](https://github.com/giocaizzi/btc-cycles/actions/workflows/deployment.yml)
+
+______________________________________________________________________
+
+> ❗ This chart is updated everyday at 5 AM UTC.
+
+![Bitcoin](https://github.com/giocaizzi/btc-cycles/blob/main/bitcoin.png)
+
+______________________________________________________________________
+
+## Deprecation warning
+
+> ⚠️ Since v0.3, as _CoinmarketCap v1 API has been deprecated_, **all working sources require an API key** to fetch data. The legacy free *(but broken!)* source `coinmarketcap-free` is however still available, hoping for a workaround is found ([see here](https://github.com/guptarohit/cryptoCMD/issues/86)).
+
+## Install
+
+```bash
+pip install btc-cycles
+```
+
+## Usage
+
+- **Bitcoin price** data is fetched from a set of available sources, such as:
+
+  - [`coinmarketcap`](https://www.coinmarketcap.com), requires **paid** `api_key`.
+  - [`coincompare`](https://www.cryptocompare.com/), requires **free-tier** `api_key`.
+  - [`coinmarketcap-free`](https://www.coinmarketcap.com): free *(but broken!)* legacy source, available hoping for a workaround,  ([see here](https://github.com/guptarohit/cryptoCMD/issues/86))
+
+- **Cycles** are computed considering _past halving dates_ and the _expected future halving date_ fetched from [watchguru](https://watcher.guru/bitcoin-halving)
+
+## Documentation
+
+See this [notebook](https://github.com/giocaizzi/btc-cycles/blob/main/bitcoin.ipynb).
---
+++ packages/btc-cycles/btc_cycles-0.3.5.tar.gz/btc_cycles-0.3.5/btc_cycles/core/halvings.py
@@ -0,0 +1,77 @@
+"""halving module"""
+
+import datetime
+import json
+from pathlib import Path
+
+import pandas as pd
+import requests
+
+URL = "https://api.watcher.guru/bitcoinhalving/predictions"
+
+
+def get_halving_data() -> tuple:
+    """Get next halving data
+
+    Gets the next halving data (date, blocknumber)
+    from the watcher.guru API.
+
+    Returns:
+        tuple: date (datetime), block (int)
+    """
+    response = requests.get(URL).json()
+    date = datetime.datetime.fromtimestamp(response["target"]["predicted_timestamp"])
+    # localize utc
+    date = date.replace(tzinfo=datetime.timezone.utc)
+    block = int(response["target"]["block_number"])
+    return date, block
+
+
+def update_predicted_halving_date(data: pd.DataFrame) -> pd.DataFrame:
+    """Update predicted halving date
+
+    Update the predicted halving date in the halving dataframe
+    by matching the block number.
+
+    Args:
+        data (DataFrame): halving data
+
+    Returns:
+        DataFrame: updated halving data
+    """
+    # TODO: what happends if response is not
+    # updated in time? (like in 2024)
+    date, block = get_halving_data()
+    # update predicted halving date
+    data.loc[data["block"] == block, "Date"] = date
+    return data
+
+
+class Halvings:
+    """Halvings
+
+    Halvings class that contains halving cycles data.
+
+    Attributes:
+        data (pd.DataFrame): halving data
+    """
+
+    def __init__(self):
+        halvings_path = Path(__file__).resolve().parent / "halvings.json"
+        # load json data into DataFrame
+        with open(halvings_path, "r") as f:
+            self.data = pd.DataFrame(json.load(f)).T
+        # convert date to datetime
+        self.data["date"] = pd.to_datetime(self.data["date"])
+        self.data.index.name = "block"
+        # Date with capital D
+        self.data.rename(columns={"date": "Date"}, inplace=True)
+        # reset index
+        self.data.reset_index(inplace=True)
+        self.data["block"] = self.data["block"].astype(int)
+        # update predicted halving date
+        self.data = update_predicted_halving_date(self.data)
+        # cycle length, forced to int
+        self.data["cycle_length"] = (self.data["Date"].diff().dt.days).shift(-1)
+        # cycle id
+        self.data["cycle_id"] = range(1, len(self.data) + 1)
