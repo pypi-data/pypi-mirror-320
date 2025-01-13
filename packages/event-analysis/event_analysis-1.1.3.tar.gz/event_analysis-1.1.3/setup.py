d1f82d93ae5c9790af68cd789963dfe5aa7713c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 06:05:05 +0000

    Add package event-analysis/1.1.3/event_analysis-1.1.3.tar.gz

---
+++ packages/event-analysis/event_analysis-1.1.3.tar.gz/event_analysis-1.1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "event-analysis",
+    version = "1.1.3",
+    description = "This package allows you to run Event Coincidence Analysis and Event Synchronization on your event series on the CPU and Nvidia-GPU",
+    py_modules = [ "EventAnalysis" ],
+    package_dir = { '' : "EventAnalysis" },
+    packages=find_packages(where="EventAnalysis"),
+    classifiers = [
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+    ],
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    install_requires = [ 
+        "numba ",
+        "numpy ",
+        "pandas"
+    ],
+    url = "https://github.com/professorcode1/Event-Analysis",
+    author = "Raghav Kumar",
+    author_email = "raghkum2000@gmail.com"
+
+)
