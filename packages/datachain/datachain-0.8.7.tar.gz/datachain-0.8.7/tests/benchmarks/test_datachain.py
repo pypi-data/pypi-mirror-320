1fcce3aa27ab7158a3a1e7d38e46a550ba69da9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 05:05:47 +0000

    Add package datachain/0.8.7/datachain-0.8.7.tar.gz

---
+++ packages/datachain/datachain-0.8.7.tar.gz/datachain-0.8.7/tests/benchmarks/test_datachain.py
@@ -0,0 +1,17 @@
+from datachain.lib.dc import DataChain
+from datachain.lib.webdataset_laion import process_laion_meta
+
+
+def test_datachain(tmp_dir, test_session, datasets, benchmark):
+    def run_script(uri, **kwargs):
+        DataChain.from_storage(uri, session=test_session, **kwargs).gen(
+            emd=process_laion_meta
+        ).map(
+            stem=lambda file: file.get_file_stem(),
+            params=["emd.file"],
+            output=str,
+        ).save("laion_emb")
+
+    dataset = datasets / "laion-tiny.npz"
+    assert dataset.is_file()
+    benchmark(run_script, dataset.as_uri())
