e8366a5ca878b743fcbdd835434689efa61bcffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 06:05:14 +0000

    Add package bandit/1.8.1/bandit-1.8.1.tar.gz

---
+++ packages/bandit/bandit-1.8.1.tar.gz/bandit-1.8.1/setup.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
+#
+# SPDX-License-Identifier: Apache-2.0
+import setuptools
+
+setuptools.setup(
+    python_requires=">=3.9", setup_requires=["pbr>=2.0.0"], pbr=True
+)
