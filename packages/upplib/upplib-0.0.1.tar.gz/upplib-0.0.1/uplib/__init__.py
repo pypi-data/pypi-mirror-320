162786b4e631e46eb73c6ba8d7bef68d58ffb4ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 14:06:46 +0000

    Add package upplib/0.0.1/upplib-0.0.1.tar.gz

---
+++ packages/upplib/upplib-0.0.1.tar.gz/upplib-0.0.1/uplib/__init__.py
@@ -0,0 +1,29 @@
+# 引入自定义的全部的包
+
+# 基础的，通用的基础包
+from uplib.index import *
+
+# 有关文件操作类的包
+from uplib.file import *
+
+# 有关 图表的 html 代码的包
+from uplib.chart_html import *
+# 有关 图表的 操作类的包
+from uplib.chart import *
+
+# 有关 http 工具类的包
+from uplib.http_util import *
+
+# 有关 数据库操作的包
+from uplib.db import *
+
+# 有关 邮件 操作的包
+from uplib.mail import *
+from uplib.mail_html import *
+# 生成 markdown 的文本
+from uplib.markdown import *
+
+# 临时添加的工具类
+from uplib.temp import *
+# 多线程处理类的数据
+from uplib.multi_thread import *
