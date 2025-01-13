9c74cb18db6f9e555b66f075d98c268664544968
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:29 +0000

    Add package dynamic-library/0.1.1/dynamic_library-0.1.1-py3-none-any.whl

---
+++ packages/dynamic-library/dynamic_library-0.1.1-py3-none-any.whl/dynamic_library/_hook.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2025-present Filipe Laíns <lains@riseup.net>
+# SPDX-FileCopyrightText: 2025 Quansight, LLC
+#
+# SPDX-License-Identifier: MIT
+
+import ctypes
+import os
+
+import dynamic_library
+
+
+if os.environ.get('PYTHON_DYNAMIC_LIBRARY_DISABLE'):
+    import warnings
+
+    warnings.warn('Libraries registered via dynamic_library entrypoints will not be loaded!', RuntimeWarning)
+else:
+    for lib in dynamic_library.get_libraries():
+        ctypes.CDLL(lib, ctypes.RTLD_GLOBAL)
