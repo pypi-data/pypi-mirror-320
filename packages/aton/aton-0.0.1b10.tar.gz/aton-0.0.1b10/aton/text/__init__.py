cb7360652009ddb3d7b84489542f8989b17356da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:05:14 +0000

    Add package aton/0.0.1b10/aton-0.0.1b10.tar.gz

---
+++ packages/aton/aton-0.0.1b10.tar.gz/aton-0.0.1b10/aton/text/__init__.py
@@ -0,0 +1,39 @@
+"""
+# General text operations
+
+This module contains tools for general text operations.
+
+
+# Index
+
+| | |
+| --- | --- |
+| `aton.text.find`    | Search for specific content from a text file |
+| `aton.text.edit`    | Edit specific content from a text file |
+| `aton.text.extract` | Extract data from raw text strings |
+
+
+# Examples
+
+The following example shows how to find a value in a text file, extract it and paste it into another file using the text subpackage:
+
+```python
+from aton import text
+# Get an array with all matches
+alat_lines = text.find.lines('relax.out', 'Lattice parameter =')
+# Extract the numerical value of the last match
+alat = text.extract.number(alat_lines[-1], 'Lattice parameter')
+# Paste it into another file
+text.edit.replace_line('scf.in', 'Lattice parameter =', f'Lattice parameter ='{alat})
+```
+
+Advanced usage such as regular expression matching or
+additional line extraction is detailed in the API documentation.
+
+"""
+
+
+from . import find
+from . import edit
+from . import extract
+
---
+++ packages/aton/aton-0.0.1b10.tar.gz/aton-0.0.1b10/tests/test_spx.py
@@ -0,0 +1,3 @@
+
+calibration = 'calibration_2.02g.csv'
+calibration_ND = '_calibration-ND_2.672g.csv'
