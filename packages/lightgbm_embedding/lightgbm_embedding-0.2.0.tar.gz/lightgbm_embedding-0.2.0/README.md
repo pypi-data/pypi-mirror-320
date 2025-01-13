a2e04d53019e7313564cacb2a0ee6379233ffda6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:12:51 +0000

    Add package lightgbm_embedding/0.2.0/lightgbm_embedding-0.2.0.tar.gz

---
+++ packages/lightgbm_embedding/lightgbm_embedding-0.2.0.tar.gz/lightgbm_embedding-0.2.0/README.md
@@ -0,0 +1,31 @@
+# LightGBM Embeddings
+
+Feature embeddings with LightGBM
+
+## Installation
+
+    pip install lightgbm-embedding
+
+## Examples
+```python
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from lightgbm_embedding import LightgbmEmbedding
+
+df = pd.read_csv(
+    "https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv"
+)
+cols = df.columns[:-1]
+target = df.columns[-1]
+num_classes = df[target].nunique()
+
+X_train, X_test = train_test_split(
+    df, test_size=0.2, stratify=df[target], random_state=42
+)
+
+n_dim = 20
+emb = LightgbmEmbedding(n_dim=n_dim)
+emb.fit(X_train[cols], X_train[target])
+X_train_embed = emb.transform(X_train[cols])
+X_test_embed = emb.transform(X_test[cols])
+```
---
+++ packages/lightgbm_embedding/lightgbm_embedding-0.2.0.tar.gz/lightgbm_embedding-0.2.0/lightgbm_embedding/__init__.py
@@ -0,0 +1,208 @@
+import importlib.metadata
+__version__ = importlib.metadata.version('lightgbm_embedding')
+
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.multiclass import type_of_target
+from sklearn.utils.validation import check_is_fitted
+from lightgbm import LGBMClassifier, LGBMRegressor
+from scipy.special import expit, softmax
+import pandas as pd
+
+__all__ = ["LightgbmEmbedding"]
+
+
+def LightgbmFactory(target_type, n_dim=100, args={}):
+    """LightGBM Factory"""
+
+    if target_type is "binary":
+        model = LGBMClassifier(n_estimators=n_dim, **args)
+    elif target_type is "multiclass":
+        model = LGBMClassifier(n_estimators=n_dim, **args)
+    elif target_type is "continuous":
+        model = LGBMRegressor(n_estimators=n_dim, **args)
+    else:
+        raise ValueError(f"Target type: {target_type} is not supported.")
+
+    return model
+
+
+class LightgbmEmbedding(BaseEstimator, TransformerMixin):
+    """
+    LightGBM Feature Embeddings class. Leaf values for each trees are used for embedding.
+
+
+    Parameters
+    -----------
+    n_dim: int, default = 100
+        Number of dimensions to use. *Note: For multiclass problems dimension will be n_dim*num_classes
+
+    **lgb_kwargs: dict, default = {}
+        LightGBM arguments in dictionary format
+
+
+    Notes
+    ----------
+    Scikit-learn util ``type_of_target`` fails at identifying regression task with array of integers.
+    If that's the case, provide target type in fit method ``fit(X, y, target_type='continuous')``.
+     For scikit-learn pipelines pipeline.fit(X, y, model__target_type='continuous'))
+
+
+    Examples
+    -----------
+    >>> from lightgbm_embedding import LightgbmEmbedding
+    >>> from sklearn.datasets import load_breast_cancer
+    >>> X, y = load_breast_cancer(return_X_y=True)
+
+    >>> embedder = LightgbmEmbedding().fit(X, y)
+    >>> emb.transform(X)
+    """
+
+    def __init__(self, n_dim=100, lgb_kwargs={}):
+        if "n_estimators" in lgb_kwargs:
+            print(
+                f"Ignoring n_estimators in lgb_kwargs because n_dim is provided. n_dim={n_dim}"
+            )
+
+            lgb_kwargs["n_estimators"] = n_dim
+
+        self.n_dim = n_dim
+        self.lgb_kwargs = lgb_kwargs
+        self._is_fit = False
+
+    def fit(self, X, y, target_type=None):
+        """
+        Fit LightGBM Embedder.
+
+        Parameters
+        ----------
+        X : Union[pd.DataFrame, np.ndarray]
+            2-D (n_samples, n_features) feature array.
+
+        y : Union[pd.Series, np.ndarray]
+            1-D target array.
+
+        Returns
+        -------
+        self : object
+            Fitted estimator.
+        """
+        if target_type is not None:
+            self.target_type = target_type
+        else:
+            self.target_type = type_of_target(y)
+
+        self._model = LightgbmFactory(
+            self.target_type, n_dim=self.n_dim, args=self.lgb_kwargs
+        )
+
+        if self.target_type is "multiclass":
+            self.n_dim = self.n_dim * y.nunique()
+            print("Target is multiclass. Setting n_dim = n_dim * num_classes.")
+
+        self._model.fit(X, y)
+        self._booster = self._model._Booster
+
+        # self._tree_dict = self._get_tree_leaf_value_dict()
+        self._leaf_hash_map = self._get_tree_leaf_value_dict_list()
+        self._is_fit = True
+
+        return self
+
+    def _get_leaf_value(self, row: list):
+        """Get leaf values from row of leaf index. Input row shape (n_trees,)"""
+        embed = [
+            self._tree_dict[f"{tree_id}_{leaf_id}"]
+            for tree_id, leaf_id in enumerate(row)
+        ]
+
+        return embed
+
+    def transform(self, X, y=None, n_jobs=1):
+        """
+        Get embeddings from fitted estimator.
+
+        Parameters
+        ----------
+        X : Union[pd.DataFrame, np.ndarray]
+            2-D (n_samples, n_features) feature array.
+
+        y : None
+            Exists for compatibility with pipelines.
+
+        Returns
+        ----------
+        X_embeddings : pd.DataFrame
+            Embeddings dataframe with shape (n_samples, n_dim)
+
+        """
+
+        check_is_fitted(self)
+
+        preds = self._model.predict(X, pred_leaf=True)
+        preds = pd.DataFrame(preds)
+
+        for col in preds.columns:
+            preds[col] = preds[col].map(self._leaf_hash_map[col])
+
+        preds.columns = [f"dim_{i}" for i in range(self.n_dim)]
+
+        if self.target_type == "binary":
+            preds = expit(preds)
+        elif self.target_type == "multiclass":
+            preds = softmax(preds, axis=1)
+
+        return preds
+
+    def _get_tree_leaf_value_dict_list(self):
+        """Returns list with [index:tree_id, values:leaf values]"""
+        tree_df = self._booster.trees_to_dataframe()[
+            ["tree_index", "node_index", "value", "weight", "count"]
+        ]
+
+        # Ignore not terminal nodes to leave out only leaves
+        tree_df["node_index"] = (
+            tree_df["node_index"]
+            .str.replace(r"\d+-L(\d+)", "\\1", regex=True)
+            .replace(r"\d+-S(\d+)", np.nan, regex=True)
+        )
+
+        tree_df = tree_df[tree_df["node_index"].notnull()]
+        tree_df["node_index"] = tree_df["node_index"].apply(int)
+
+        hash_map = []
+
+        for i in range(100):
+            tmp = tree_df[tree_df["tree_index"] == i].reset_index(drop=True)
+            hash_map.append(pd.Series(tmp["value"], index=tmp["node_index"]).to_dict())
+
+        return hash_map
+
+    def _get_tree_leaf_value_dict(self):
+        """Returns leaf values for in dictionary format. Type: List[f"{tree_id}_{leaf_id}", leaf_value]"""
+        tree_df = self._booster.trees_to_dataframe()[
+            ["tree_index", "node_index", "value", "weight", "count"]
+        ]
+
+        # Ignore not terminal nodes to leave out only leaves
+        tree_df["node_index"] = (
+            tree_df["node_index"]
+            .str.replace(r"\d+-L(\d+)", "\\1", regex=True)
+            .replace(r"\d+-S(\d+)", np.nan, regex=True)
+        )
+
+        # Create index with treeid_nodeid
+        tree_df = tree_df[tree_df["node_index"].notnull()]
+        tree_df.index = (
+            tree_df["tree_index"].apply(str) + "_" + tree_df["node_index"].apply(str)
+        )
+
+        # To be able to use to_dict directly
+        tree_df = tree_df["value"]
+        tree_dict = tree_df.to_dict()
+
+        return tree_dict
+
+    def __sklearn_is_fitted__(self):
+        """Sklearn util method for is_fitted validation."""
+        return True if self._is_fit else False
