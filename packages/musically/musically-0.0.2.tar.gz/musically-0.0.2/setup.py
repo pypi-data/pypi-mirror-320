1eb2adfac1bbbdfe708e7bc9f4fab5e2b43fb36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:13:06 +0000

    Add package musically/0.0.2/musically-0.0.2.tar.gz

---
+++ packages/musically/musically-0.0.2.tar.gz/musically-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="musically",
+    version="0.0.2",
+    author="Ankit Chaubey",
+    author_email="m.ankitchaubey@gmail.com",
+    description="A placeholder package for musically, ready for initial development.",
+    long_description="This is the initial release of the musically package, serving as a foundation for further development.",
+    long_description_content_type="text/plain",
+    url="https://github.com/ankit-chaubey/musically",
+    packages=["musically"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
