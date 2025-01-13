9b3c38de7c4e0d1322ee0f8e4c846188d0a2c1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:05:04 +0000

    Add package streamlit-analytics2/0.10.0/streamlit_analytics2-0.10.0.tar.gz

---
+++ packages/streamlit-analytics2/streamlit_analytics2-0.10.0.tar.gz/streamlit_analytics2-0.10.0/src/streamlit_analytics2/__init__.py
@@ -0,0 +1,10 @@
+"""
+Streamlit Analytics 2
+Track & visualize user interactions with your streamlit app.
+"""
+
+from .main import start_tracking, stop_tracking, track  # noqa: F401
+from .state import data  # noqa: F401
+
+__version__ = "0.10.0"
+__name__ = "streamlit_analytics2"
