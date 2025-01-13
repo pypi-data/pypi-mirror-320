66f4872afc0122a847f63dd9aed4875ab46e7734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 16:04:57 +0000

    Add package streamlit-feedback/0.1.4/streamlit-feedback-0.1.4.tar.gz

---
+++ packages/streamlit-feedback/streamlit-feedback-0.1.4.tar.gz/streamlit-feedback-0.1.4/setup.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-feedback",
+    version="0.1.4",
+    author="Jeff Kayne",
+    author_email="jeff.kayne@trubrics.com",
+    description=(
+        "Streamlit component that allows you to collect user feedback in your apps"
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires=">=3.7",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
