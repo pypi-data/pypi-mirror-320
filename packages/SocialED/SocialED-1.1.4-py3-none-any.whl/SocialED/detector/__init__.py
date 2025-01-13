2878b0befdbb87cc891f8819bff1b306a1f74f20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:42 +0000

    Add package SocialED/1.1.4/SocialED-1.1.4-py3-none-any.whl

---
+++ packages/SocialED/SocialED-1.1.4-py3-none-any.whl/SocialED/detector/__init__.py
@@ -0,0 +1,45 @@
+# __init__.py
+
+from .lda import LDA
+from .bilstm import BiLSTM
+from .word2vec import WORD2VEC
+from .glove import GloVe
+from .wmd import WMD
+from .bertmodel import BERT
+from .sbert import SBERT
+from .eventx import EventX
+from .clkd import CLKD
+from .kpgnn import KPGNN
+from .finevent import FinEvent
+from .qsgnn import QSGNN
+from .hcrc import HCRC
+from .etgnn import ETGNN
+from .uclsed import UCLSED
+from .rplmsed import RPLMSED
+from .hisevent import HISEvent
+from .adpsemevent import ADPSEMEvent
+from .hypersed import Hypersed
+# List of all classes to be exported
+
+
+__all__ = [
+    "LDA",
+    "BiLSTM", # test 20s
+    "WORD2VEC",
+    "GloVe",# test 1min
+    "WMD", # test 10s
+    "BERT",# test 10s
+    "SBERT",# test 10s
+    "EventX",# test 10s
+    "CLKD",#
+    "KPGNN",
+    "FinEvent",
+    "QSGNN",
+    "HCRC",
+    "ETGNN",
+    "UCLSED",
+    "RPLMSED",
+    "HISEvent", #test160s
+    "ADPSEMEvent", #test160s
+    "Hypersed"
+]
---
+++ packages/SocialED/SocialED-1.1.4-py3-none-any.whl/SocialED/utils/__init__.py
@@ -0,0 +1,4 @@
+from .utility import *
+from .dataprocess import *
+from .loss import *
+
