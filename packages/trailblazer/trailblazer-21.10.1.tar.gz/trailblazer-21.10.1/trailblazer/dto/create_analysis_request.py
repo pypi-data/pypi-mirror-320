437774f8ab7f5d8a6ef99eb0cb23f78c7fba6ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:36 +0000

    Add package trailblazer/21.10.1/trailblazer-21.10.1.tar.gz

---
+++ packages/trailblazer/trailblazer-21.10.1.tar.gz/trailblazer-21.10.1/trailblazer/dto/create_analysis_request.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+from trailblazer.constants import TrailblazerPriority, TrailblazerTypes, WorkflowManager
+
+
+class CreateAnalysisRequest(BaseModel):
+    case_id: str
+    email: str | None = None
+    config_path: str
+    out_dir: str
+    order_id: int | None = None
+    priority: TrailblazerPriority
+    workflow: str | None = None
+    ticket: str | None = None
+    type: TrailblazerTypes
+    workflow_manager: WorkflowManager | None = None
+    tower_workflow_id: str | None = None
+    is_hidden: bool | None = None
