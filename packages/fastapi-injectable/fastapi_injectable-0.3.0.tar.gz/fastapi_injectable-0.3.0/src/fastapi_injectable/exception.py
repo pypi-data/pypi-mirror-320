30b45a6b882529ce79b76756eeb0793cfd7d2d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:12:26 +0000

    Add package fastapi-injectable/0.3.0/fastapi_injectable-0.3.0.tar.gz

---
+++ packages/fastapi-injectable/fastapi_injectable-0.3.0.tar.gz/fastapi_injectable-0.3.0/src/fastapi_injectable/exception.py
@@ -0,0 +1,6 @@
+class DependencyResolveError(Exception):
+    """Custom error for dependency resolve issues."""
+
+
+class DependencyCleanupError(Exception):
+    """Custom error for dependency cleanup issues."""
