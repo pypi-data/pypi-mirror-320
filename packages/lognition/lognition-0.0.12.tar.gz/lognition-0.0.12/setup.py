1873fe37e62f980b4eee4fc83b5b01dc01f1d96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:04:46 +0000

    Add package lognition/0.0.12/lognition-0.0.12.tar.gz

---
+++ packages/lognition/lognition-0.0.12.tar.gz/lognition-0.0.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os import path
+working_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(working_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+setup(
+    name="lognition",
+    version="0.0.12",
+    author="Abtin Turing",
+    description="Log everything in style.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "coloredlogs",
+        "verboselogs",
+        "yaspin"
+    ]
+)
