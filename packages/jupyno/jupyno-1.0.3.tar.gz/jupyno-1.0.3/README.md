2938693558cb8eb79b06abb1fa1c2d27cddd754e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 06:05:03 +0000

    Add package jupyno/1.0.3/jupyno-1.0.3.tar.gz

---
+++ packages/jupyno/jupyno-1.0.3.tar.gz/jupyno-1.0.3/README.md
@@ -0,0 +1,3 @@
+# Jupyno
+Module permettant d'écrire et de téléverser des croquis Arduino directement depuis un notebook Jupyter.
+Nécessite le programme arduino-cli (https://github.com/arduino/arduino-cli) présent dans le même dossier.
---
+++ packages/jupyno/jupyno-1.0.3.tar.gz/jupyno-1.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jupyno',
+    version='1.0.3',
+    packages=find_packages(),
+    description='Utiliser Arduino dans un notebook Jupyter',
+    author='Guillaume Froehlicher',
+    author_email='guillaume.froehlicher@ac-rennes.fr',
+    license='MIT',
+)
