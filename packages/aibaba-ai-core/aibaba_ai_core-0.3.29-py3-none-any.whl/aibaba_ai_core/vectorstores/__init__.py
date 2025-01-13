71eeadfd90b755c86489a38a978041f28faa1a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:12:22 +0000

    Add package aibaba-ai-core/0.3.29/aibaba_ai_core-0.3.29-py3-none-any.whl

---
+++ packages/aibaba-ai-core/aibaba_ai_core-0.3.29-py3-none-any.whl/aibaba_ai_core/vectorstores/__init__.py
@@ -0,0 +1,9 @@
+from aibaba-ai-core.vectorstores.base import VST, VectorStore, VectorStoreRetriever
+from aibaba-ai-core.vectorstores.in_memory import InMemoryVectorStore
+
+__all__ = [
+    "VectorStore",
+    "VST",
+    "VectorStoreRetriever",
+    "InMemoryVectorStore",
+]
