7e536119d661b0d8d40b72bc728f994a2748c3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:04:49 +0000

    Add package freeact/0.2.3/freeact-0.2.3.tar.gz

---
+++ packages/freeact/freeact-0.2.3.tar.gz/freeact-0.2.3/freeact/model/__init__.py
@@ -0,0 +1,4 @@
+from freeact.model.base import CodeActModel, CodeActModelResponse, CodeActModelTurn
+from freeact.model.claude.model import Claude, ClaudeModelName
+from freeact.model.gemini.model.chat import Gemini, GeminiResponse
+from freeact.model.gemini.model.live import GeminiLive, GeminiModelName
