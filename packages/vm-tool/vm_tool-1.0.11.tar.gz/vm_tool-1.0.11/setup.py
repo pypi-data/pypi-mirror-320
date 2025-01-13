71c7c3b7b0ca81c1ea02a2fc7bcfb0cf52ca1bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:12:34 +0000

    Add package vm-tool/1.0.11/vm_tool-1.0.11.tar.gz

---
+++ packages/vm-tool/vm_tool-1.0.11.tar.gz/vm_tool-1.0.11/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+# Read the contents of README.md
+readme_path = os.path.join(os.path.dirname(__file__), 'Readme.md')
+if os.path.exists(readme_path):
+    with open(readme_path, encoding='utf-8') as f:
+        long_description = f.read()
+else:
+    long_description = ''
+
+setup(
+    name='vm_tool',
+    version='1.0.11',  # This will be updated by bump2version
+    packages=find_packages(),
+    description='A Comprehensive Tool for Setting Up Virtual Machines Using Ansible.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'ansible',
+        'ansible-runner',
+        'paramiko'
+    ],
+    entry_points={
+        'console_scripts': [
+            'vm_tool=vm_tool.cli:main',
+        ],
+    },
+    license='MIT',
+    include_package_data=True,
+)
