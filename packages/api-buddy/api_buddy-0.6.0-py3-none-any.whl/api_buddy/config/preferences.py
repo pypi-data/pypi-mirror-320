4f4d39ed83828b0f751d0ca110cd26d882cc7c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:05:14 +0000

    Add package api-buddy/0.6.0/api_buddy-0.6.0-py3-none-any.whl

---
+++ packages/api-buddy/api_buddy-0.6.0-py3-none-any.whl/api_buddy/config/preferences.py
@@ -0,0 +1,132 @@
+import yaml
+from os import path
+from copy import deepcopy
+from typing import Any
+
+from ..utils.exceptions import PrefsException
+from ..utils.http import unpack_query_params
+from ..utils.typing import Preferences
+from ..validation.preferences import (
+    DEFAULT_PREFS,
+    NESTED_DEFAULT_PREFS,
+    validate_preferences,
+)
+
+EXAMPLE_OAUTH2_PREFS = {
+    'client_id': 'your_client_id',
+    'client_secret': 'your_client_secret',
+    'scopes': ['one_scope', 'another_scope'],
+}
+EXAMPLE_PREFS = {
+    "api_url": "https://www.freetestapi.com/",
+}
+
+
+def _remove_defaults(prefs: Preferences) -> Preferences:
+    """Remove defaults if they haven't been changed"""
+    filtered_prefs = deepcopy(prefs)
+    for key, default_val in DEFAULT_PREFS.items():
+        if key in NESTED_DEFAULT_PREFS:
+            continue
+        if filtered_prefs[key] == default_val:                 # type: ignore
+            del filtered_prefs[key]                            # type: ignore
+    for nested_name, nested_defaults in NESTED_DEFAULT_PREFS.items():
+        nested_prefs = filtered_prefs[nested_name]             # type: ignore
+        for nested_key, default_nested_val in nested_defaults.items():  # type: ignore # noqa
+            if nested_prefs[nested_key] == default_nested_val:
+                del filtered_prefs[nested_name][nested_key]    # type: ignore
+        if filtered_prefs[nested_name] == {}:  # type: ignore[literal-required]
+            del filtered_prefs[nested_name]    # type: ignore[misc]
+    return filtered_prefs
+
+
+def _convert_types(prefs: Preferences) -> Preferences:
+    """Convert any types that are changed in validation for saving"""
+    converted_prefs = deepcopy(prefs)
+    auth_prefs = converted_prefs.get('oauth2')
+    if auth_prefs:
+        auth_params = auth_prefs.get('authorize_params')
+        if auth_params:
+            converted_prefs['oauth2']['authorize_params'] = (
+                unpack_query_params(auth_params)  # type: ignore
+            )
+    return converted_prefs
+
+
+def _extract_yaml_from_file(file_name: str) -> Any:
+    """Load contents of yaml file
+
+    Retuns:
+        - None if file doesn't exist
+        - The python-native data if it does
+
+    Raises:
+        PrefsException if:
+            - file contents are not valid yaml
+            - user preferences are None
+    """
+    if not path.isfile(file_name):
+        return None
+    with open(file_name, 'r') as prefs_file:
+        try:
+            user_prefs = yaml.load(prefs_file, Loader=yaml.Loader)
+        except yaml.YAMLError:
+            raise PrefsException(
+                title='There was a problem reading the file',
+                message=(
+                    'Please make sure it\'s valid yaml: '
+                    'http://www.yaml.org/start.html'
+                ),
+            )
+    if user_prefs is None:
+        example_yaml = yaml.dump(EXAMPLE_PREFS, Dumper=yaml.Dumper)
+        raise PrefsException(
+            title='It looks like your file is empty',
+            message=(
+                'Make sure you have something in there\n'
+                f'For example:\n\n{example_yaml}'
+            )
+        )
+    return user_prefs
+
+
+def save_api_url(url: str, prefs: Preferences, file_name: str) -> None:
+    prefs['api_url'] = url
+    save_prefs(prefs, file_name)
+
+
+def load_prefs(
+            file_name: str,
+        ) -> Preferences:
+    """Load preferences from a yaml file
+
+    Notes:
+        - Expands ~
+        - Creates a preferences file if it doesn't exist
+        - Merges with defaults
+    """
+    expanded_file_name = path.expanduser(file_name)
+    raw_prefs = _extract_yaml_from_file(expanded_file_name)
+    if raw_prefs is None:
+        prefs = validate_preferences(EXAMPLE_PREFS)
+        save_prefs(prefs, expanded_file_name)
+    else:
+        prefs = validate_preferences(raw_prefs)
+    return prefs
+
+
+def save_prefs(
+            preferences: Preferences,
+            file_name: str,
+        ) -> None:
+    """Save preferences as a yaml file
+
+    Notes:
+        - Expands ~
+        - Ignores defaults if they haven't changed
+    """
+    expanded_file_name = path.expanduser(file_name)
+    minimal_prefs = _remove_defaults(preferences)
+    converted_prefs = _convert_types(minimal_prefs)
+    with open(expanded_file_name, 'w') as prefs_file:
+        yaml.dump(converted_prefs, prefs_file, Dumper=yaml.Dumper)
---
+++ packages/api-buddy/api_buddy-0.6.0-py3-none-any.whl/api_buddy/validation/options.py
@@ -0,0 +1,164 @@
+from json import loads, JSONDecodeError
+from copy import deepcopy
+from colorama import Fore, Style
+from typing import Any, cast, List
+from urllib.parse import urlparse
+from ..utils.typing import Options, RawOptions
+from ..utils.exceptions import APIBuddyException
+from ..utils.http import HTTP_METHODS, GET, pack_query_params
+
+USE = "use"
+NON_HTTP_CMDS = {USE}
+
+
+def _more_than_one_method_selected(opts: RawOptions) -> bool:
+    return sum(
+        cast(bool, opts[method]) for method in HTTP_METHODS
+    ) > 1
+
+
+def _validate_method(opts: RawOptions) -> RawOptions:
+    """Converts named bools to str enum
+
+    Implicitly removed the old docopt booleans from opts
+    Defaults to 'get'
+    """
+    if _more_than_one_method_selected(opts):
+        raise APIBuddyException(
+            title='These HTTP methods are borked',
+            message=(
+                'It appears you selected more than one HTTP method...\n'
+                'How did you even do this?'
+            )
+        )
+    opts['<method>'] = None
+    for method in HTTP_METHODS:
+        using_this_method = opts[method]
+        del opts[method]
+        if using_this_method is True:
+            opts['<method>'] = method
+    return opts
+
+
+def _validate_use(opts: RawOptions) -> None:
+    url = opts['<api_url>']
+    if url is None:
+        raise APIBuddyException(
+            title='What API do you want to use?',
+            message=(
+                'You just need to tell me the url '
+                'and I\'ll save it in '
+                '{Fore.MAGENTA}{PREFS_FILE}{Style.RESET_ALL}'
+            )
+        )
+    url_parts = urlparse(cast(str, url))
+    if not url_parts.scheme:
+        raise APIBuddyException(
+            title='Something\'s off about that url',
+            message=(
+                f'{Fore.MAGENTA}{url}{Style.RESET_ALL} is not a valid url, '
+                'try something more like '
+                f'{Fore.BLUE}{Style.BRIGHT}'
+                'https://www.freetestapi.com{Style.RESET_ALL}'
+            )
+        )
+
+
+def _validate_non_http_cmd(opts: RawOptions) -> RawOptions:
+    opts['<cmd>'] = None
+    for cmd in NON_HTTP_CMDS:
+        using_this_cmd = opts[cmd]
+        del opts[cmd]
+        if using_this_cmd is True:
+            opts['<cmd>'] = cmd
+    if opts['<cmd>'] == USE:
+        _validate_use(opts)
+    return opts
+
+
+def _validate_endpoint(endpoint: str) -> str:
+    url_parts = urlparse(endpoint)
+    if url_parts.scheme:
+        message = 'You don\'t need to supply the full url, just the path.'
+        path = url_parts.path
+        if path:
+            message += (
+                f'\nDid you mean {Fore.BLUE}{Style.BRIGHT}{url_parts.path}'
+                f'{Style.RESET_ALL}?'
+            )
+        raise APIBuddyException(
+            title='Check your endpoint, dude',
+            message=message,
+        )
+    return endpoint
+
+
+def _validate_data(data: str) -> Any:
+    try:
+        return loads(data)
+    except JSONDecodeError:
+        raise APIBuddyException(
+            title='Your request body data are wack',
+            message=(
+                'Please use valid json for: '
+                f'{Fore.MAGENTA}{data}{Style.RESET_ALL}'
+            ),
+        )
+
+
+def _validate_params_and_data(opts: RawOptions) -> RawOptions:
+    """Validate query params and request body data
+
+    Because of the cli argument signature, docopt always puts <data> at the end
+    the <params> if it's given, and None in opts['<data>'] no matter what. It
+    can't ever know the difference between the last param and the optional data
+    arg, so we have to check the last param and see if it's data.
+    """
+    params = cast(List[str], opts['<params>'])
+    data = None
+    if len(params) > 0:
+        maybe_data = params[-1]
+        maybe_params = params[:-1]
+        try:
+            data = _validate_data(maybe_data)
+        except APIBuddyException as data_err:
+            try:  # maybe it's a param?
+                pack_query_params([maybe_data])
+            except APIBuddyException:
+                if opts['<method>'] != GET:  # should never be data for GET
+                    raise data_err  # it's not a param, its bad data
+        else:  # maybe_data is data
+            params = maybe_params
+    if data is not None and opts['<method>'] == GET:
+        raise APIBuddyException(
+            title=(
+                'You can\'t use request body data with '
+                f'{Fore.MAGENTA}GET{Style.RESET_ALL}'
+            ),
+            message=(
+                'Did you mean to use '
+                f'{Fore.MAGENTA}POST{Style.RESET_ALL}?'
+            ),
+        )
+    opts['<params>'] = pack_query_params(params)  # type: ignore
+    opts['<data>'] = data
+    return opts
+
+
+def _validate_help(opts: RawOptions) -> RawOptions:
+    opts['--help'] = opts['help']
+    del opts['help']
+    return opts
+
+
+def validate_options(opts: RawOptions) -> Options:
+    """Convert types and validate"""
+    valid_opts = deepcopy(opts)
+    valid_opts = _validate_non_http_cmd(valid_opts)
+    valid_opts['<endpoint>'] = _validate_endpoint(
+        cast(str, valid_opts['<endpoint>'])
+    )
+    _validate_method(valid_opts)
+    _validate_params_and_data(valid_opts)
+    _validate_help(valid_opts)
+    return cast(Options, valid_opts)
