60abca9f7bcb4b80e8feefc66d5fd124409e545f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:12:48 +0000

    Add package exam-library-rqiza/0.1/exam_library_rqiza-0.1.tar.gz

---
+++ packages/exam-library-rqiza/exam_library_rqiza-0.1.tar.gz/exam_library_rqiza-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="exam_library_rqiza",
+    version="0.1",
+    packages=find_packages(),
+    description="A library to get code snippets for exams",
+    author="rqiza",
+    author_email="your.email@example.com",
+    url="https://github.com/yourusername/exam_library",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
