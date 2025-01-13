3355873d87526b0907694e5f9221ead4a6f55f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:56 +0000

    Add package datamart-yhdr/0.1.8/datamart_yhdr-0.1.8-py3-none-any.whl

---
+++ packages/datamart-yhdr/datamart_yhdr-0.1.8-py3-none-any.whl/datamart_yhdr/settings.py
@@ -0,0 +1,12 @@
+from datamart_yhdr import const
+
+const.TOKEN_CREATE = "/base/token/create"
+const.QUERY = "/query/query"
+const.COMMAND = "/message/message/submit"
+const.BATCH = "/message/message/submit/batch"
+const.WORKSPACE = "YHDR-DM"
+const.HALO_URL = "http://192.168.100.100:82"
+const.HALO_URL_TRADER = "http://10.255.0.120:82"
+const.HALO_URL_BACKUP = "http://192.168.101.120:82"
+const.HALO_URL_UAT = "http://192.168.10.175:82"
+const.HALO_URL_DEV = "http://192.168.100.108:82"
