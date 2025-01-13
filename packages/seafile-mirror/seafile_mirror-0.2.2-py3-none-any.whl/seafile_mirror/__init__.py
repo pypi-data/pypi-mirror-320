4a0b2d081051dbf0345872a6669d993eed67481b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 05:06:17 +0000

    Add package seafile-mirror/0.2.2/seafile_mirror-0.2.2-py3-none-any.whl

---
+++ packages/seafile-mirror/seafile_mirror-0.2.2-py3-none-any.whl/seafile_mirror/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2023 Max Mehl <https://mehl.mx>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+"""Global init file"""
+
+from importlib.metadata import version
+
+__version__ = version("seafile-mirror")
