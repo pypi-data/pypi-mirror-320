5224bfd1cd103bd091808dcd40b2c0cd160e73e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:28 +0000

    Add package rajesh-check-this-out/0.1.0/rajesh_check_this_out-0.1.0.tar.gz

---
+++ packages/rajesh-check-this-out/rajesh_check_this_out-0.1.0.tar.gz/rajesh_check_this_out-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rajesh_check_this_out",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Not gay",
+    author_email="notgay@example.com",
+    description="A message to gay people",
+    long_description="A big message to gay people",
+    keywords="life, gay, rainbow",
+    url="https://github.com/sachiin-acharya/custom_package.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
