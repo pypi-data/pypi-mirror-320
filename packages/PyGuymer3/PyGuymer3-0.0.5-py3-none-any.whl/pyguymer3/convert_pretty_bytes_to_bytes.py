e531b90a3096e36b88cb96ba6900b164a793c48c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:12:58 +0000

    Add package PyGuymer3/0.0.5/PyGuymer3-0.0.5-py3-none-any.whl

---
+++ packages/PyGuymer3/PyGuymer3-0.0.5-py3-none-any.whl/pyguymer3/convert_pretty_bytes_to_bytes.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+# Define function ...
+def convert_pretty_bytes_to_bytes(
+    string,
+    /,
+):
+    """Convert a pretty string of a bytes with some units to a value of bytes
+
+    This function reads in a string of a human-readable value of bytes with some
+    units and returns the same value as a float.
+
+    Parameters
+    ----------
+    string : str
+        the human-readable value of bytes with some units
+
+    Returns
+    -------
+    size : float
+        the value of bytes
+
+    Notes
+    -----
+    Copyright 2017 Thomas Guymer [1]_
+
+    References
+    ----------
+    .. [1] PyGuymer3, https://github.com/Guymer/PyGuymer3
+
+    Examples
+    --------
+    >>> pyguymer3.convert_pretty_bytes_to_bytes("1.5 KiB")
+    1536.0
+
+    """
+
+    # Import standard modules ...
+    import re
+
+    # Extract digits (with decimal point) and letters separately ...
+    size = float(re.sub(r"[a-zA-Z]", "", string).strip())                       # [?]
+    units = re.sub(r"[0-9\.]", "", string).strip()
+
+    # Check what the units are and return it scaled ...
+    match units:
+        case "B":
+            return size
+        case "KB" | "KiB":
+            return 1024.0 * size
+        case "MB" | "MiB":
+            return 1024.0 * 1024.0 * size
+        case "GB" | "GiB":
+            return 1024.0 * 1024.0 * 1024.0 * size
+        case "TB" | "TiB":
+            return 1024.0 * 1024.0 * 1024.0 * 1024.0 * size
+        case "PB" | "PiB":
+            return 1024.0 * 1024.0 * 1024.0 * 1024.0 * 1024.0 * size
+        case _:
+            # Crash ...
+            raise ValueError(f"\"units\" is an unexpected value ({repr(units)})") from None
---
+++ packages/PyGuymer3/PyGuymer3-0.0.5-py3-none-any.whl/pyguymer3/openstreetmap/tile.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+
+# Define function ...
+def tile(
+    xtile,
+    ytile,
+    zoom,
+    sess,
+    /,
+    *,
+       chunksize = 1048576,
+         cookies = None,
+           debug = __debug__,
+    exiftoolPath = None,
+    gifsiclePath = None,
+         headers = None,
+    jpegtranPath = None,
+     optipngPath = None,
+         timeout = 60.0,
+          verify = True,
+):
+    """Fetch an OpenStreetMap tile
+
+    This function reads in a tile number and then checks to see if the tile
+    already exists in the local cache. It will load up the PNG image if it
+    exists or download the OpenStreetMap tile (and save it as both a NPY array
+    and a PNG image) if it is missing.
+
+    Parameters
+    ----------
+    xtile : int
+        the tile x location
+    ytile : int
+        the tile y location
+    zoom : int
+        the OpenStreetMap zoom level
+    sess : requests.Session
+        the session for any requests calls
+    chunksize : int, optional
+        the size of the chunks of any files which are read in (in bytes)
+    cookies : dict, optional
+        extra cookies for any requests calls
+    debug : bool, optional
+        print debug messages
+    exiftoolPath : str, optional
+        the path to the "exiftool" binary (if not provided then Python will attempt to
+        find the binary itself)
+    gifsiclePath : str, optional
+        the path to the "gifsicle" binary (if not provided then Python will attempt to
+        find the binary itself)
+    headers : dict, optional
+        extra headers for any requests calls
+    jpegtranPath : str, optional
+        the path to the "jpegtran" binary (if not provided then Python will attempt to
+        find the binary itself)
+    optipngPath : str, optional
+        the path to the "optipng" binary (if not provided then Python will attempt to
+        find the binary itself)
+    timeout : float, optional
+        the timeout for any requests/subprocess calls (in seconds)
+    verify : bool, optional
+        verify the server's certificates for any requests calls
+
+    Returns
+    -------
+    image : PIL.Image
+        the OpenStreetMap tile
+
+    Notes
+    -----
+    Copyright 2017 Thomas Guymer [1]_
+
+    References
+    ----------
+    .. [1] PyGuymer3, https://github.com/Guymer/PyGuymer3
+    """
+
+    # Import standard modules ...
+    import os
+    import time
+
+    # Import special modules ...
+    try:
+        import numpy
+    except:
+        raise Exception("\"numpy\" is not installed; run \"pip install --user numpy\"") from None
+    try:
+        import PIL
+        import PIL.Image
+        PIL.Image.MAX_IMAGE_PIXELS = 1024 * 1024 * 1024                         # [px]
+    except:
+        raise Exception("\"PIL\" is not installed; run \"pip install --user Pillow\"") from None
+
+    # Import sub-functions ...
+    from ..download_file import download_file
+    from ..image import optimize_image
+
+    # Check inputs ...
+    if not 0 <= xtile < pow(2, zoom):
+        raise Exception(f"\"xtile\" is not in the required range ({xtile:d})") from None
+    if not 0 <= ytile < pow(2, zoom):
+        raise Exception(f"\"ytile\" is not in the required range ({ytile:d})") from None
+    if not 0 <= zoom <= 19:
+        raise Exception(f"\"zoom\" is not in the required range ({zoom:d})") from None
+
+    # Populate default values ...
+    if cookies is None:
+        cookies = {}
+    if headers is None:
+        headers = {}
+
+    # **************************************************************************
+
+    # Deduce tile names ...
+    npy = os.path.expanduser(f"~/.local/share/cartopy_cache/OSM/{xtile:d}_{ytile:d}_{zoom:d}.npy")
+    png = os.path.expanduser(f"~/.local/share/openstreetmap/tiles/{zoom:d}/{xtile:d}/{ytile:d}.png")
+
+    # **************************************************************************
+
+    # Check if the tile is missing ...
+    if not os.path.exists(png):
+        # Deduce tile URL ...
+        url = f"https://tile.openstreetmap.org/{zoom:d}/{xtile:d}/{ytile:d}.png"
+
+        if debug:
+            print(f"INFO: Downloading \"{url}\" to \"{png}\" ...")
+
+        # Download tile ...
+        resp = download_file(
+            sess,
+            url,
+            png,
+            cookies = cookies,
+            headers = headers,
+            timeout = timeout,
+             verify = verify,
+        )
+
+        # Check if the download failed ...
+        if resp is False:
+            # Return answer ...
+            return None
+
+        # Optimize tile ...
+        optimize_image(
+            png,
+               chunksize = chunksize,
+                   debug = debug,
+            exiftoolPath = exiftoolPath,
+            gifsiclePath = gifsiclePath,
+            jpegtranPath = jpegtranPath,
+             optipngPath = optipngPath,
+                    pool = None,
+                   strip = True,
+                 timeout = timeout,
+        )
+
+        # Sleep ...
+        time.sleep(1.0)
+
+    # **************************************************************************
+
+    # Open image as RGB (even if it is paletted) ...
+    with PIL.Image.open(png) as imageObj:
+        image = imageObj.convert("RGB")
+
+    # **************************************************************************
+
+    # Check if the tile is missing ...
+    if not os.path.exists(npy):
+        if debug:
+            print(f"INFO: Making \"{npy}\" from \"{png}\" ...")
+
+        # Save tile ...
+        numpy.save(npy, image, allow_pickle = False)
+
+    # **************************************************************************
+
+    # Return answer ...
+    return image
