e51feacd7c10c6b539d7e045805a5e798e3ea770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 14:06:03 +0000

    Add package spectral-derivatives/0.1/spectral_derivatives-0.1-py3-none-any.whl

---
+++ packages/spectral-derivatives/spectral_derivatives-0.1-py3-none-any.whl/specderiv/tests/test_specderiv.py
@@ -0,0 +1,15 @@
+
+# To be run by CI with `coverage` and `pytest`, with results uploaded to coveralls and badge displayed in the readme
+
+# A test that the MSE of derivatives up to some high power is suitably low
+
+# A test of the endpoints code
+
+# A test of the middle polynomial-finding code (maybe need to split out?)
+
+# A test for even derivatives vs odd derivatives
+
+# A test for multidimensional derivatives
+
+# A test for non-periodic derivatives vs periodic and equispaced
+
