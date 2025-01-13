ca08a92b6fc19458f0b258c4892ceec32281dc24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 14:06:38 +0000

    Add package ssh-python/1.1.0rc1/ssh_python-1.1.0rc1-cp39-cp39-win_amd64.whl

---
+++ packages/ssh-python/ssh_python-1.1.0rc1-cp39-cp39-win_amd64.whl/ssh/_version.py
@@ -0,0 +1,10 @@
+
+import json
+
+version_json = '''
+{"date": "2025-01-13T01:07:26.009426", "dirty": false, "error": null, "full-revisionid": "5d09370e9c61f129c0afbaa851050ed118b54384", "version": "1.1.0rc1"}'''  # END VERSION_JSON
+
+
+def get_versions():
+    return json.loads(version_json)
+
