e62e28f1b76a7b871e1682e8819d82540399567c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:19 +0000

    Add package cmip_ref_core/0.1.4/cmip_ref_core-0.1.4.tar.gz

---
+++ packages/cmip_ref_core/cmip_ref_core-0.1.4.tar.gz/cmip_ref_core-0.1.4/tests/unit/test_datasets.py
@@ -0,0 +1,68 @@
+import pytest
+
+from cmip_ref_core.datasets import DatasetCollection, MetricDataset, SourceDatasetType
+
+
+@pytest.fixture
+def dataset_collection(cmip6_data_catalog) -> DatasetCollection:
+    return DatasetCollection(
+        cmip6_data_catalog,
+        "instance_id",
+    )
+
+
+@pytest.fixture
+def metric_dataset(dataset_collection) -> MetricDataset:
+    return MetricDataset({SourceDatasetType.CMIP6: dataset_collection})
+
+
+class TestMetricDataset:
+    def test_get_item(self, metric_dataset):
+        assert metric_dataset["cmip6"] == metric_dataset._collection[SourceDatasetType.CMIP6]
+        assert metric_dataset[SourceDatasetType.CMIP6] == metric_dataset._collection[SourceDatasetType.CMIP6]
+
+    def test_get_item_missing(self, metric_dataset):
+        with pytest.raises(KeyError):
+            metric_dataset["cmip7"]
+
+    def test_python_hash(self, metric_dataset, cmip6_data_catalog, data_regression):
+        dataset_hash = hash(metric_dataset)
+
+        # The python hash is different to the hash of the dataset
+        assert hash(metric_dataset.hash) == dataset_hash
+        assert isinstance(dataset_hash, int)
+
+        assert dataset_hash == hash(
+            MetricDataset({SourceDatasetType.CMIP6: DatasetCollection(cmip6_data_catalog, "instance_id")})
+        )
+        assert dataset_hash != hash(
+            MetricDataset(
+                {
+                    SourceDatasetType.CMIP6: DatasetCollection(
+                        cmip6_data_catalog[cmip6_data_catalog.variable_id == "tas"], "instance_id"
+                    )
+                }
+            )
+        )
+        data_regression.check(metric_dataset.hash, basename="metric_dataset_hash")
+
+
+class TestDatasetCollection:
+    def test_get_item(self, dataset_collection):
+        expected = dataset_collection.datasets.instance_id
+        assert dataset_collection["instance_id"].equals(expected)
+
+    def test_get_attr(self, dataset_collection):
+        expected = dataset_collection.datasets.instance_id
+        assert dataset_collection.instance_id.equals(expected)
+
+    def test_hash(self, dataset_collection, cmip6_data_catalog, data_regression):
+        dataset_hash = hash(dataset_collection)
+        assert isinstance(dataset_hash, int)
+
+        assert dataset_hash == hash(DatasetCollection(cmip6_data_catalog, "instance_id"))
+        assert dataset_hash != hash(
+            DatasetCollection(cmip6_data_catalog[cmip6_data_catalog.variable_id == "tas"], "instance_id")
+        )
+
+        data_regression.check(dataset_hash, basename="dataset_collection_hash")
