59b4556a280ed405fa181ef6f7a74c3a6855e123
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 14:06:23 +0000

    Add package weba/0.2.27/weba-0.2.27.tar.gz

---
+++ packages/weba/weba-0.2.27.tar.gz/weba-0.2.27/weba/__init__.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from .component import (
+    Component,
+    no_tag_context,
+)
+from .component_tag import component_tag
+from .context import Context, ContextMixin
+from .errors import (
+    ComponentAfterRenderError,
+    ComponentAsyncError,
+    ComponentSrcFileNotFoundError,
+    ComponentSrcRequiredError,
+    ComponentSrcRootTagNotFoundError,
+    ComponentSrcTypeError,
+    ComponentTagNotFoundError,
+    ComponentTypeError,
+)
+from .tag import Tag, current_tag_context
+from .ui import ui
+
+tag = component_tag
+
+__all__ = [
+    "Component",
+    "ComponentAfterRenderError",
+    "ComponentAsyncError",
+    "ComponentSrcFileNotFoundError",
+    "ComponentSrcRequiredError",
+    "ComponentSrcRootTagNotFoundError",
+    "ComponentSrcTypeError",
+    "ComponentTagNotFoundError",
+    "ComponentTypeError",
+    "Context",
+    "ContextMixin",
+    "Tag",
+    "component_tag",
+    "current_tag_context",
+    "no_tag_context",
+    "tag",
+    "ui",
+]
