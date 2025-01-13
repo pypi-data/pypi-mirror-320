6b65372c95d759b17727c5f62d6a92134d243540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 14:06:15 +0000

    Add package furiosa-native-runtime/0.10.5/furiosa_native_runtime-0.10.5-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/furiosa-native-runtime/furiosa_native_runtime-0.10.5-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/furiosa/native_runtime/errors.pyi
@@ -0,0 +1,19 @@
+from furiosa.native_runtime import (
+    InvalidInput,
+    QueueWaitTimeout,
+    SessionClosed,
+    SessionTerminated,
+    TensorNameNotFound,
+)
+from furiosa.native_runtime import DeviceBusy
+from furiosa.native_runtime import FuriosaRuntimeError as NativeException
+
+__all__ = [
+    "NativeException",
+    "SessionClosed",
+    "SessionTerminated",
+    "DeviceBusy",
+    "InvalidInput",
+    "QueueWaitTimeout",
+    "TensorNameNotFound",
+]
