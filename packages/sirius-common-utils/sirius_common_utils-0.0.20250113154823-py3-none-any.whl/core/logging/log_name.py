1033f75afd9345ae61683a549e07ea97d539cdf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:42 +0000

    Add package sirius-common-utils/0.0.20250113154823/sirius_common_utils-0.0.20250113154823-py3-none-any.whl

---
+++ packages/sirius-common-utils/sirius_common_utils-0.0.20250113154823-py3-none-any.whl/core/logging/log_name.py
@@ -0,0 +1,7 @@
+
+LOG_COMMON_CORE = 'app.core'
+LOG_COMMON_CORE_WEB_INTERCEPTOR = 'app.core.web.interceptor'
+
+LOG_COMMON_CORE_MQTT = 'app.mqtt'
+LOG_COMMON_CORE_FETCHER = 'app.fetcher'
+LOG_COMMON_CORE_GPIO = 'app.gpio'
