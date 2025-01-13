3afef5d22903f2b92e07e7f1faf84955a9dad5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:12:34 +0000

    Add package dokkey/1.0.1/dokkey-1.0.1.tar.gz

---
+++ packages/dokkey/dokkey-1.0.1.tar.gz/dokkey-1.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+
+module = Extension(
+    "dokkey",
+    sources=["main.c"],
+    libraries=["user32", "kernel32"],  # Windows libraries
+)
+
+setup(
+    name="dokkey",
+    version="1.0.1",
+    description="A Python module for detecting keyboard events",
+    author="Peter Bohus",
+    author_email="v2020.bohus.peter@gmail.com",
+    ext_modules=[module],
+)
