28df7dd16e84db66d174b5b5ddcc580af7018a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:57 +0000

    Add package elfi-python-sdk/1.0.2/elfi_python_sdk-1.0.2-py3-none-any.whl

---
+++ packages/elfi-python-sdk/elfi_python_sdk-1.0.2-py3-none-any.whl/clients/configs.py
@@ -0,0 +1,9 @@
+GAS_LIMIT = {
+    'placeIncreaseOrderGasFeeLimit': 4000000,
+    'placeDecreaseOrderGasFeeLimit': 4000000,
+    'positionUpdateMarginGasFeeLimit': 2000000,
+    'positionUpdateLeverageGasFeeLimit': 2500000,
+    'withdrawGasFeeLimit': 1500000,
+    'claimRewardsGasFeeLimit': 1000000,
+}
+
