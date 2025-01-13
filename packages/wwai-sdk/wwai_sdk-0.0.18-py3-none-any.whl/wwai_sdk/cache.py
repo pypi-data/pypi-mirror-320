c12a61aa1d490223a8d281ccf771917796c2a18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:55 +0000

    Add package wwai-sdk/0.0.18/wwai_sdk-0.0.18-py3-none-any.whl

---
+++ packages/wwai-sdk/wwai_sdk-0.0.18-py3-none-any.whl/wwai_sdk/cache.py
@@ -0,0 +1,67 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+class WwaiSdkCache:
+    def __init__(self,
+                 cache_type="local",
+                 redis_host=None,
+                 redis_port=6379,
+                 redis_password=None,
+                 redis_db=0
+                 ):
+        self.cache_type = cache_type
+        self.cache_prefix = "wwai-sdk-cache"
+        if cache_type == "redis":
+            try:
+                import redis
+            except ImportError:
+                raise ImportError("Please install redis with `pip install redis`")
+
+            try:
+                password = os.getenv("REDIS_PASSWORD", "") if os.getenv("REDIS_PASSWORD", "") != "" else redis_password
+                if password is not None and (password.lower() == "none" or password.lower() == "null"):
+                    password = None
+                self.redis = redis.StrictRedis(
+                    host=os.getenv("REDIS_HOST", redis_host),
+                    port=int(os.getenv("REDIS_PORT", redis_port)),
+                    db=int(os.getenv("REDIS_DB", redis_db)),
+                    password=password,
+                    decode_responses=True,
+                    socket_timeout=10,
+                    socket_connect_timeout=10,
+                    socket_keepalive=True,
+                )
+            except Exception as e:
+                logger.error(f"redis 连接失败，现在使用本地缓存: {e}")
+                os.environ["WWAI_CACHE_TYPE"] = "local"
+                self.cache_type = "local"
+        if not self.cache or self.cache_type == "local":
+            try:
+                from diskcache import Cache
+                import tempfile
+            except ImportError:
+                raise ImportError("Please install diskcache with `pip install diskcache`")
+            cache_dir = f"{tempfile.gettempdir()}/wwai-sdk-cache"
+            self.cache = Cache(cache_dir)
+            print(f"Cache dir: {cache_dir}")
+
+    def get(self, key):
+        if self.cache_type == "redis":
+            return self.redis.get(f"{self.cache_prefix}:{key}")
+        else:
+            return self.cache.get(f"{self.cache_prefix}:{key}")
+
+    def set(self, key, value, ttl=None):
+        if self.cache_type == "redis":
+            return self.redis.set(f"{self.cache_prefix}:{key}", value, ex=ttl)
+        else:
+            return self.cache.set(f"{self.cache_prefix}:{key}", value, expire=ttl)
+
+    def delete(self, key):
+        if self.cache_type == "redis":
+            return self.redis.delete(f"{self.cache_prefix}:{key}")
+        else:
+            return self.cache.delete(f"{self.cache_prefix}:{key}")
