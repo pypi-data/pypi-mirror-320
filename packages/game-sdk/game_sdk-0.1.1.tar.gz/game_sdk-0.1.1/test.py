5a2e3577d49ca28fca51f28678760d5082cc47ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:19 +0000

    Add package game-sdk/0.1.1/game_sdk-0.1.1.tar.gz

---
+++ packages/game-sdk/game_sdk-0.1.1.tar.gz/game_sdk-0.1.1/test.py
@@ -0,0 +1,4 @@
+from game_sdk.hosted_game.agent import Agent
+from game_sdk.hosted_game.functions.telegram import TelegramClient
+from game_sdk.hosted_game.functions.farcaster import FarcasterClient
+from game_sdk.hosted_game.functions.discord import DiscordClient
