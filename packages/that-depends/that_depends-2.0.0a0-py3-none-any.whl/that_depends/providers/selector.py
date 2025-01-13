0ea4ab781bea90476c1788622e359a9842bc92d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:22 +0000

    Add package that-depends/2.0.0a0/that_depends-2.0.0a0-py3-none-any.whl

---
+++ packages/that-depends/that_depends-2.0.0a0-py3-none-any.whl/that_depends/providers/selector.py
@@ -0,0 +1,35 @@
+import typing
+
+from that_depends.providers.base import AbstractProvider
+
+
+T_co = typing.TypeVar("T_co", covariant=True)
+
+
+class Selector(AbstractProvider[T_co]):
+    __slots__ = "_override", "_providers", "_selector"
+
+    def __init__(self, selector: typing.Callable[[], str], **providers: AbstractProvider[T_co]) -> None:
+        super().__init__()
+        self._selector: typing.Final = selector
+        self._providers: typing.Final = providers
+
+    async def async_resolve(self) -> T_co:
+        if self._override:
+            return typing.cast(T_co, self._override)
+
+        selected_key: typing.Final = self._selector()
+        if selected_key not in self._providers:
+            msg = f"No provider matches {selected_key}"
+            raise RuntimeError(msg)
+        return await self._providers[selected_key].async_resolve()
+
+    def sync_resolve(self) -> T_co:
+        if self._override:
+            return typing.cast(T_co, self._override)
+
+        selected_key: typing.Final = self._selector()
+        if selected_key not in self._providers:
+            msg = f"No provider matches {selected_key}"
+            raise RuntimeError(msg)
+        return self._providers[selected_key].sync_resolve()
