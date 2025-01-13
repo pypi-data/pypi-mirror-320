9100c8603eb3b4e2639025009c0ccb5a1467019e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 14:06:09 +0000

    Add package blue-sandbox/5.118.1/blue_sandbox-5.118.1.tar.gz

---
+++ packages/blue-sandbox/blue_sandbox-5.118.1.tar.gz/blue_sandbox-5.118.1/blue_sandbox/list.py
@@ -0,0 +1,23 @@
+list_of_experiments = {
+    "sagesemseg": {
+        "ICON": "🌀",
+        "title": "A SemSeg (Semantic Segmenter) trained and deployed on AWS Sagemaker.",
+        "url": "https://github.com/kamangir/blue-sandbox/blob/main/blue_sandbox/sagesemseg/README.md",
+        "marquee": "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
+        "status": "🔥",
+    },
+    "`@damages`": {
+        "ICON": "🌐",
+        "title": "Satellite imagery damage assessment workflow",
+        "url": "https://github.com/kamangir/blue-sandbox/blob/main/blue_sandbox/microsoft_building_damage_assessment/README.md",
+        "marquee": "https://github.com/kamangir/assets/raw/main/blue-sandbox/Maui-Hawaii-fires-Aug-23-ingest-2025-01-10-qqJqhm.png?raw=true",
+        "status": "⏸️",
+    },
+    "template": {
+        "ICON": "",
+        "title": "",
+        "url": "",
+        "marquee": "https://github.com/kamangir/assets/raw/main/blue-plugin/marquee.png?raw=true",
+        "status": "🎰",
+    },
+}
