a66cbe14961c0b3e55218499a92e3838e7fba006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:05:07 +0000

    Add package tmeasure/1.0/tmeasure-1.0.tar.gz

---
+++ packages/tmeasure/tmeasure-1.0.tar.gz/tmeasure-1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="tmeasure",
+    url="https://github.com/tboulet/PythonTimeMeasure", 
+    author="Timothé Boulet",
+    author_email="timothe.boulet0@gmail.com",
+    
+    packages=find_namespace_packages(),
+
+    version="1.0",
+    license="MIT",
+    description="Time measure tool for python",
+)
