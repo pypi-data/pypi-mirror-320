9a393af641176b09ac6c10b908fae723eae98825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:05:39 +0000

    Add package ortho-editor/0.1.3/ortho_editor-0.1.3.tar.gz

---
+++ packages/ortho-editor/ortho_editor-0.1.3.tar.gz/ortho_editor-0.1.3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ortho-editor",
+    version="0.1.3",
+    packages=find_packages(include=['.']),
+    package_dir={
+        "": ".",
+    },
+    include_package_data=True,
+    package_data={
+        "": ["fonts/*.ttf", "fonts/*.otf"],  # Specify font file extensions
+    },
+    data_files=[
+        ('fonts', ["fonts/MaterialIcons-Regular.ttf"]),
+    ],
+    install_requires=[
+        "black",
+        "flake8",
+        "PyQt5",
+    ],
+    entry_points={
+        "console_scripts": [
+            "ortho-editor=main:main",
+        ],
+    },
+    author="yaacov",
+    author_email="kobi.zamir@gmail.com",
+    description="ORTHO mask editor tool",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yaacov/ortho-editor",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
