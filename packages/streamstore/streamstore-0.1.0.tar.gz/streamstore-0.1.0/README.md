cfee8ba9211cfabd36f51931889f6498a3f15d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 16:05:02 +0000

    Add package streamstore/0.1.0/streamstore-0.1.0.tar.gz

---
+++ packages/streamstore/streamstore-0.1.0.tar.gz/streamstore-0.1.0/README.md
@@ -0,0 +1,20 @@
+# streamstore
+
+`streamstore` is the Python package that provides an async client for interacting with the [gRPC API](https://s2.dev/docs/interface/grpc) of [S2, the Stream Store](https://s2.dev/).
+
+## Installation
+
+You can install the package from the [Python Package Index](https://pypi.org/) using the package manager of your choice. E.g., with `pip`:
+
+```bash
+pip install streamstore
+```
+
+If you are managing the dependencies of your project through [uv](https://docs.astral.sh/uv/concepts/projects/dependencies/#managing-dependencies):
+```bash
+uv add streamstore
+```
+
+## Requirements
+
+Python >= 3.11
---
+++ packages/streamstore/streamstore-0.1.0.tar.gz/streamstore-0.1.0/docs/source/conf.py
@@ -0,0 +1,40 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+# import os
+# import sys
+from datetime import date
+
+# sys.path.insert(0, os.path.abspath("../../src/"))
+
+project = "streamstore"
+copyright = f"{date.today().year}, Bandar Systems Inc"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autodoc.typehints",
+    "sphinx.ext.intersphinx",
+    "enum_tools.autoenum",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
