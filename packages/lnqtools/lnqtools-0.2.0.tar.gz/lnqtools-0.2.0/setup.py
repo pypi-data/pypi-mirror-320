1b793030308c0cf2661ca4b96f8da19d87b09cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:22 +0000

    Add package lnqtools/0.2.0/lnqtools-0.2.0.tar.gz

---
+++ packages/lnqtools/lnqtools-0.2.0.tar.gz/lnqtools-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lnqtools',
+    version='0.2.0',
+    packages=find_packages(),
+    description='A simple package for adding and multiplying numbers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='LyNgocQue',
+    author_email='your_email@example.com',
+    url='https://github.com/yourusername/your_project', 
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
