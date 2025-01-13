2957641019315ba3a74fc6c13e724c6584aa8f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 14:05:55 +0000

    Add package misaki/0.2.3/misaki-0.2.3.tar.gz

---
+++ packages/misaki/misaki-0.2.3.tar.gz/misaki-0.2.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='misaki',
+    version='0.2.3',
+    packages=find_packages(),
+    package_data={
+        'misaki': ['data/*.json'],
+    },
+    install_requires=[
+        'num2words',
+        'regex',
+        'spacy',
+        'spacy-curated-transformers',
+    ],
+    python_requires='>=3.7',
+    author='hexgrad',
+    author_email='hello@hexgrad.com',
+    description='G2P engine for TTS',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hexgrad/misaki',
+    license='Apache 2.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+)
