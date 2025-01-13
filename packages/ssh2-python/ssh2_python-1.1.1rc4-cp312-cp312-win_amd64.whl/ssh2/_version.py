c4bb1eb31d0d0d8f867971fe4b7026cc0ef75d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 06:05:19 +0000

    Add package ssh2-python/1.1.1rc4/ssh2_python-1.1.1rc4-cp312-cp312-win_amd64.whl

---
+++ packages/ssh2-python/ssh2_python-1.1.1rc4-cp312-cp312-win_amd64.whl/ssh2/_version.py
@@ -0,0 +1,10 @@
+
+import json
+
+version_json = '''
+{"date": "2025-01-12T22:15:53.659782", "dirty": false, "error": null, "full-revisionid": "4b771590477282107016d3ddeea769b62785f935", "version": "1.1.1rc4"}'''  # END VERSION_JSON
+
+
+def get_versions():
+    return json.loads(version_json)
+
