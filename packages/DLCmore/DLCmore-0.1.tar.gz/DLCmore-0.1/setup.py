2bd40d0bb750e9d6e5e8d78a7c714cb4f641ec06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 03:12:41 +0000

    Add package DLCmore/0.1/DLCmore-0.1.tar.gz

---
+++ packages/DLCmore/DLCmore-0.1.tar.gz/DLCmore-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name='DLCmore',
+    version='0.1',
+    packages=find_packages(),
+    description='一个便于python写代码的一个扩展',
+    author='叶承恩',
+    author_email='qwp234526@qq.com',
+    url='https://github.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
