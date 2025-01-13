b7178dab2795e664d10d91c626364385a3f4e045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:31 +0000

    Add package openapi-python-generator/1.2.0/openapi_python_generator-1.2.0.tar.gz

---
+++ packages/openapi-python-generator/openapi_python_generator-1.2.0.tar.gz/openapi_python_generator-1.2.0/src/openapi_python_generator/models.py
@@ -0,0 +1,79 @@
+from typing import List
+from typing import Optional
+
+from openapi_pydantic.v3.v3_0 import Operation, PathItem, Schema
+from pydantic import BaseModel
+
+
+class LibraryConfig(BaseModel):
+    name: str
+    library_name: str
+    template_name: str
+    include_async: bool
+    include_sync: bool
+
+
+class TypeConversion(BaseModel):
+    original_type: str
+    converted_type: str
+    import_types: Optional[List[str]] = None
+
+
+class OpReturnType(BaseModel):
+    type: Optional[TypeConversion] = None
+    status_code: int
+    complex_type: bool = False
+    list_type: Optional[str] = None
+
+
+class ServiceOperation(BaseModel):
+    params: str
+    operation_id: str
+    query_params: List[str]
+    header_params: List[str]
+    return_type: OpReturnType
+    operation: Operation
+    pathItem: PathItem
+    content: str
+    async_client: Optional[bool] = False
+    tag: Optional[str] = None
+    path_name: str
+    body_param: Optional[str] = None
+    method: str
+    use_orjson: bool = False
+
+
+class Property(BaseModel):
+    name: str
+    type: TypeConversion
+    required: bool
+    default: Optional[str]
+    import_type: Optional[List[str]] = None
+
+
+class Model(BaseModel):
+    file_name: str
+    content: str
+    openapi_object: Schema
+    properties: List[Property] = []
+
+
+class Service(BaseModel):
+    file_name: str
+    operations: List[ServiceOperation]
+    content: str
+    async_client: Optional[bool] = False
+    library_import: str
+    use_orjson: bool = False
+
+
+class APIConfig(BaseModel):
+    file_name: str
+    base_url: str
+    content: str
+
+
+class ConversionResult(BaseModel):
+    models: List[Model]
+    services: List[Service]
+    api_config: APIConfig
