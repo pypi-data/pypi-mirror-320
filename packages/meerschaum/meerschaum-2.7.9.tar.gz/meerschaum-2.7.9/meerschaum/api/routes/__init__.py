997142693c66100f899a3aa11d88089c6dd6f1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 06:05:13 +0000

    Add package meerschaum/2.7.9/meerschaum-2.7.9.tar.gz

---
+++ packages/meerschaum/meerschaum-2.7.9.tar.gz/meerschaum-2.7.9/meerschaum/api/routes/__init__.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+Import all routes from other modules in package
+"""
+
+from meerschaum.api.routes._login import *
+from meerschaum.api.routes._actions import *
+from meerschaum.api.routes._jobs import *
+from meerschaum.api.routes._connectors import *
+from meerschaum.api.routes._index import *
+from meerschaum.api.routes._misc import *
+from meerschaum.api.routes._pipes import *
+from meerschaum.api.routes._plugins import *
+from meerschaum.api.routes._users import *
+from meerschaum.api.routes._version import *
+from meerschaum.api.routes._webterm import *
