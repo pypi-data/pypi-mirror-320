c0f285f3b7b9d6c8bb7066ff4632448794bd749e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 05:06:13 +0000

    Add package mecher-amt-api-client/0.0.3/mecher_amt_api_client-0.0.3.tar.gz

---
+++ packages/mecher-amt-api-client/mecher_amt_api_client-0.0.3.tar.gz/mecher_amt_api_client-0.0.3/mecher_amt_api_client/client/endpoints/auth.py
@@ -0,0 +1,18 @@
+import allure
+
+from mecher_amt_api_client.amt_api_common import AMT_ApiCommon
+from mecher_amt_api_client.models.amt_user import AMT_User_Auth
+
+
+class Auth_Api(AMT_ApiCommon):
+    def __init__(self):
+        super().__init__()
+        self.auth_url = self.url + "/auth"
+
+    def login(self, user: AMT_User_Auth):
+        uri = self.auth_url + "/login"
+        with allure.step('AMT API: Login'):
+            response = self.send_request(method='post',
+                                         body=user.dict(),
+                                         uri=uri)
+            return response
