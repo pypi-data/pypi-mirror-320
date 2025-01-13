a7531b1a01b3b8490bb38585fe1e8902d68f7cec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 15:05:44 +0000

    Add package stimulsoft-data-adapters/2025.1.3/stimulsoft_data_adapters-2025.1.3-py3-none-any.whl

---
+++ packages/stimulsoft-data-adapters/stimulsoft_data_adapters-2025.1.3-py3-none-any.whl/stimulsoft_data_adapters/classes/StiFunctions.py
@@ -0,0 +1,20 @@
+import re
+from enum import Enum
+
+
+class StiFunctions:
+
+### String
+
+    def getJavaScriptValue(value) -> str:
+        if value == None: return 'null'
+        if isinstance(value, Enum): return 'null' if value.value == None else value.value
+        if type(value) == list: return str(value)
+        if type(value) == str: return f"'{value}'"
+        return str(value).lower()
+
+    def isNullOrEmpty(value) -> bool:
+        return len(value or '') == 0
+
+    def isJavaScriptFunctionName(value) -> bool:
+        return re.search("^[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*$", value)
