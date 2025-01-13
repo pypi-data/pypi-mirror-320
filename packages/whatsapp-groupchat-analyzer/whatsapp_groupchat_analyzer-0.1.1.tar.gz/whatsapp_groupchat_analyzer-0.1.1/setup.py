271a572d8fef8be9181063e746142e687cdf1c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 06:05:42 +0000

    Add package whatsapp-groupchat-analyzer/0.1.1/whatsapp_groupchat_analyzer-0.1.1.tar.gz

---
+++ packages/whatsapp-groupchat-analyzer/whatsapp_groupchat_analyzer-0.1.1.tar.gz/whatsapp_groupchat_analyzer-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='whatsapp-groupchat-analyzer',
+    version='0.1.1',  # Increment the version number
+    author="Gaurav Meena",
+    author_email="gauravmeena0708@gmail.com",
+    description="A Python package for analyzing WhatsApp group chats.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/gauravmeena0708/whatsapp-analyzer",  # Your project URL
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here, e.g.,
+        # "matplotlib>=3.0",
+        # "seaborn>=0.9",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # Choose a license
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",  # Minimum Python version
+)
