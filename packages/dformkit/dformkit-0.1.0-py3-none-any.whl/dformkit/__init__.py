92ff3a0bbebbbd0777560231d6f6d0edec63d882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 05:05:06 +0000

    Add package dformkit/0.1.0/dformkit-0.1.0-py3-none-any.whl

---
+++ packages/dformkit/dformkit-0.1.0-py3-none-any.whl/dformkit/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.0"
+from .generate_form import generate_DformKit, generate_form_code
+from .template_creator import TemplateCreator
+from .check_forms_import import FormsFileChecker
+from .view_url_manager import ViewUrlManager
