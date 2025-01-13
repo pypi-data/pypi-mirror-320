9c196002037d03cbecea8c5666a676c1ab22b2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-13 04:05:29 +0000

    Add package dokkey/1.0.5/dokkey-1.0.5.tar.gz

---
+++ packages/dokkey/dokkey-1.0.5.tar.gz/dokkey-1.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension
+
+with open("README.md", "r") as fh: 
+    long_description = fh.read()
+    
+module = Extension(
+    "dokkey",
+    sources=["main.c"],
+    libraries=["user32", "kernel32"],  # Windows libraries
+)
+
+setup(
+    name="dokkey",
+    version="1.0.5",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    author="Peter Bohus",
+    author_email="v2020.bohus.peter@gmail.com",
+    license="Apache-2.0",
+    ext_modules=[module],
+)
