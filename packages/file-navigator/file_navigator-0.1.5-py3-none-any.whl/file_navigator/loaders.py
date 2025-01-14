f106124af17a315292ebd7daef9a784e0a2f2f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 06:04:38 +0000

    Add package file-navigator/0.1.5/file_navigator-0.1.5-py3-none-any.whl

---
+++ packages/file-navigator/file_navigator-0.1.5-py3-none-any.whl/file_navigator/loaders.py
@@ -0,0 +1,16 @@
+"""
+Pandas Loader - Impementation of Base Loader class
+"""
+import pandas as pd
+from .abc_loader import BaseLoader
+
+PDLoader = BaseLoader({pd.read_csv: ['.csv', '.txt'],
+                       pd.read_excel: ['.xlsx', '.xls'],
+                       pd.read_feather: '.feather',
+                       pd.read_hdf:['.h5', '.hdf5'],
+                       pd.read_json: '.json',
+                       pd.read_parquet:'.gzip',
+                       pd.read_pickle: '.pkl',
+                       pd.read_sas: '.sas7bdat',
+                       pd.read_stata: '.dta',
+                       pd.read_html: '.html'})
