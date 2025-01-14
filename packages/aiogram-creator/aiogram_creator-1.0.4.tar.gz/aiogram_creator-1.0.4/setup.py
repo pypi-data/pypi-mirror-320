cce8b720eb85852d321ee66799928b45eb006412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:04:57 +0000

    Add package aiogram-creator/1.0.4/aiogram_creator-1.0.4.tar.gz

---
+++ packages/aiogram-creator/aiogram_creator-1.0.4.tar.gz/aiogram_creator-1.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiogram-creator',
+    version='1.0.4',
+    packages=find_packages(),
+    package_data={'': ['aiogram_creator/template/**/*']},
+    entry_points={
+        'console_scripts': [
+            'aiogram-creator = aiogram_creator.main:main',
+        ],
+    },
+    author='What-XD',
+    description='Template for generating aiogram bots',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/What-XD/aiogram-creator',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
