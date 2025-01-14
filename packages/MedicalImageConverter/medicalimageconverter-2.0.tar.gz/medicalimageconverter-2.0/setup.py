e2374a294a7461275ccb61b96eaf61071c66d332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 05:05:42 +0000

    Add package MedicalImageConverter/2.0/medicalimageconverter-2.0.tar.gz

---
+++ packages/MedicalImageConverter/medicalimageconverter-2.0.tar.gz/medicalimageconverter-2.0/setup.py
@@ -0,0 +1,26 @@
+__author__ = 'Caleb OConnor'
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='MedicalImageConverter',
+    author='Caleb OConnor',
+    author_email='csoconnor@mdanderson.org',
+    version='2.00',
+    description='Reads in medical images and structures them into 3D arrays with associated ROI/POIs if they exist.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['MedicalImageConverter'],
+    include_package_data=True,
+    url='https://github.com/caleb-oconnor/MedicalImageConverter',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+    ],
+    install_requires=required,
+)
