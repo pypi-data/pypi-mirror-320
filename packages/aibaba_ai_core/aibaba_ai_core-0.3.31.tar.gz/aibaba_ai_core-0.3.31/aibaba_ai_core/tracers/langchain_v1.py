f631be7767a71b0c4091158ca3e0c2046a73a10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 03:05:14 +0000

    Add package aibaba_ai_core/0.3.31/aibaba_ai_core-0.3.31.tar.gz

---
+++ packages/aibaba_ai_core/aibaba_ai_core-0.3.31.tar.gz/aibaba_ai_core-0.3.31/aibaba_ai_core/tracers/langchain_v1.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+
+def get_headers(*args: Any, **kwargs: Any) -> Any:
+    """Throw an error because this has been replaced by get_headers."""
+    msg = (
+        "get_headers for AI Agents ForceTracerV1 is no longer supported. "
+        "Please use AI Agents ForceTracer instead."
+    )
+    raise RuntimeError(msg)
+
+
+def AI Agents ForceTracerV1(*args: Any, **kwargs: Any) -> Any:  # noqa: N802
+    """Throw an error because this has been replaced by AI Agents ForceTracer."""
+    msg = (
+        "AI Agents ForceTracerV1 is no longer supported. Please use AI Agents ForceTracer instead."
+    )
+    raise RuntimeError(msg)
