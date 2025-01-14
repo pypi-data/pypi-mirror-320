b245ebccf2ca9f470b0e36b9f896b497f45ee6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:14 +0000

    Add package findus/0.9.10/findus-0.9.10.tar.gz

---
+++ packages/findus/findus-0.9.10.tar.gz/findus-0.9.10/findus/helper/upload.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# Copyright (C) 2024 Dr. Matthias Kesenheimer - All Rights Reserved.
+# This file is based on TAoFI-FaultLib which is released under the GPL3 license.
+# Go to https://github.com/raelize/TAoFI-FaultLib/LICENSE for full license details.
+#
+# You should have received a copy of the GPL3 license with this file.
+# If not, please write to: m.kesenheimer@gmx.net.
+
+import argparse
+import subprocess
+import os
+import sys
+
+def upload(file:str, port:str):
+    ret = subprocess.check_output(["ampy", "-p", port, "ls"])
+    filename = os.path.basename(file)
+    if filename.encode() in ret:
+        print(f"[+] Deleting {filename}...")
+        subprocess.call(["ampy", "-p", port, "rm", filename])
+        print("[+] Done.")
+
+    # upload the new script
+    print(f"[+] Uploading {file}...")
+    subprocess.call(["ampy", "-p", port, "put", file])
+    print("[+] Done.")
+
+def reset(port:str):
+    # resetting
+    print("[+] Resetting Raspberry Pi Pico...")
+    subprocess.call(["ampy", "-p", port, "reset"])
+    print("[+] Done.")
+
+def main(argv=sys.argv):
+    parser = argparse.ArgumentParser(
+        description="Upload a micro python script to the Raspberry Pi Pico."
+    )
+    parser.add_argument("--port", help="/dev/tty* of the Raspberry Pi Pico", required=False, default='/dev/ttyACM1')
+    parser.add_argument("--delete-all", help="Delete all files from the Raspberry Pi Pico", required=False, action='store_true')
+    parser.add_argument("--delete", help="Delete the file from the Raspberry Pi Pico", required=False, default=None)
+    parser.add_argument("--file", help="File to upload to the Raspberry Pi Pico", required=False, default=None)
+    parser.add_argument("--files", nargs='+', help="Files to upload to the Raspberry Pi Pico", required=False, default=None)
+    args = parser.parse_args()
+
+    ret = subprocess.check_output(["ampy", "-p", args.port, "ls"])
+    if args.delete_all:
+        print("[+] Deleting all files...")
+        for filename in ret.decode().split():
+            subprocess.call(["ampy", "-p", args.port, "rm", filename[1:]])
+        print("[+] Done.")
+
+    if args.delete:
+        filename = os.path.basename(args.delete)
+        if filename.encode() in ret:
+            print(f"[+] Deleting {filename}...")
+            subprocess.call(["ampy", "-p", args.port, "rm", filename])
+            print("[+] Done.")
+
+    if args.file is not None:
+        upload(args.files, args.port)
+        reset(args.port)
+
+    if args.files is not None:
+        for f in args.files:
+            upload(f, args.port)
+        reset(args.port)
+
+    # what is on the Raspberry Pi Pico?
+    print("[+] Content of the Raspberry Pi Pico:")
+    subprocess.call(["ampy", "-p", args.port, "ls"])
+
+if __name__ == "__main__":
+    main()
