ecb517f4e72567ae1e229bc6aad4bc8cf4721e2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 14:06:44 +0000

    Add package ipcserver/0.2.5/ipcserver-0.2.5-py3-none-any.whl

---
+++ packages/ipcserver/ipcserver-0.2.5-py3-none-any.whl/ipcserver/core/__init__.py
@@ -0,0 +1,6 @@
+from .app import IpcServer
+from .config import IpcConfig
+from .request import IpcRequest
+from .response import IpcResponse
+from .router import APIRouter
+from .client import IpcClient, IpcSyncClient
