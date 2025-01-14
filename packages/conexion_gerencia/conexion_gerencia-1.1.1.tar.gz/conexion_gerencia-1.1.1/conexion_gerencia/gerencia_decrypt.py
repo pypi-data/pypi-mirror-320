eadf97b2b6d50a9f41b0ec39d13a2c293035db3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 14:05:59 +0000

    Add package conexion_gerencia/1.1.1/conexion_gerencia-1.1.1.tar.gz

---
+++ packages/conexion_gerencia/conexion_gerencia-1.1.1.tar.gz/conexion_gerencia-1.1.1/conexion_gerencia/gerencia_decrypt.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+from pkg.decrypt import DecryptService
+from pkg.get_database import GetDatabaseService
+
+from pkg.models import DatabaseConnectionGerencia
+
+class GerenciaDecrypt(BaseModel):
+    nb_agente_comercial: str = None
+    get_database_service: GetDatabaseService = None
+    decrypt_service: DecryptService = None
+    base_url: str = None
+    key: str = None
+
+    def model_post_init(self, __context):
+        self.get_database_service = GetDatabaseService(base_url=self.base_url)
+        self.decrypt_service = DecryptService(key=self.key)
+
+    def do(self) -> DatabaseConnectionGerencia:
+        response_data = self.get_database_service.get_database(nb_agente_comercial=self.nb_agente_comercial)
+        return self.decrypt_service.decrypt(data=response_data)
---
+++ packages/conexion_gerencia/conexion_gerencia-1.1.1.tar.gz/conexion_gerencia-1.1.1/conexion_gerencia/pkg/decrypt.py
@@ -0,0 +1,32 @@
+import json
+import base64
+
+from pydantic import BaseModel
+
+from Crypto.Cipher import AES
+
+from pkg.models import DatabaseConnectionGerencia, ResponseData
+
+class DecryptService(BaseModel):
+    key: str
+
+    def decrypt(self, data: ResponseData) -> DatabaseConnectionGerencia:
+        try:
+            ciphertext = base64.b64decode(data.content)
+            iv = data.iv.encode()
+
+            cipher = AES.new(self.key.encode(), AES.MODE_CBC, iv)
+            decrypted = cipher.decrypt(ciphertext)
+
+            unpadding = decrypted[-1]
+            decrypted = decrypted[:-unpadding].decode()
+
+            database_connection = json.loads(decrypted)
+            return DatabaseConnectionGerencia(
+                host=database_connection["host"],
+                port=database_connection["port"],
+                bd=database_connection["bd"]
+            )
+
+        except (base64.binascii.Error, KeyError, ValueError, json.JSONDecodeError) as e:
+            raise Exception(f"Decryption failed: {e}")
