f1e05dd9ba92f850d1d814b7417bbc53b6221f94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 14:06:43 +0000

    Add package py-trees-js/0.6.6/py_trees_js-0.6.6.tar.gz

---
+++ packages/py-trees-js/py_trees_js-0.6.6.tar.gz/py_trees_js-0.6.6/py_trees_js/__init__.py
@@ -0,0 +1,18 @@
+#
+# License: BSD
+#   https://github.com/splintered-reality/py_trees_js/raw/devel/LICENSE
+#
+##############################################################################
+# Documentation
+##############################################################################
+
+"""PyTrees javascript libraries and development/demo qt-js hybrid viewer."""
+
+##############################################################################
+# Imports
+##############################################################################
+
+# fmt: off
+from . import resources  # usort:skip
+from . import viewer  # usort:skip
+# fmt: on
---
+++ packages/py-trees-js/py_trees_js-0.6.6.tar.gz/py_trees_js-0.6.6/py_trees_js/viewer/__init__.py
@@ -0,0 +1,21 @@
+#
+# License: BSD
+#   https://github.com/splintered-reality/py_trees_js/raw/devel/LICENSE
+#
+##############################################################################
+# Documentation
+##############################################################################
+
+"""A qt-js hybrid viewer demonstrating js library integration."""
+
+##############################################################################
+# Imports
+##############################################################################
+
+# fmt: off
+from . import console  # usort:skip
+from . import main_window  # usort:skip
+from . import trees  # usort:skip
+from . import viewer  # usort:skip
+from . import web_view  # usort:skip
+# fmt: on
