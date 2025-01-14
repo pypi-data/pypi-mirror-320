caa59be1fa247159114c2fa4e9031b741db3f7db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:20 +0000

    Add package wildcard-core/0.1.0/wildcard_core-0.1.0.tar.gz

---
+++ packages/wildcard-core/wildcard_core-0.1.0.tar.gz/wildcard_core-0.1.0/wildcard_core/tool_search/__init__.py
@@ -0,0 +1,5 @@
+# Import main classes or functions from modules
+from .ToolSearchClient import ToolSearchClient
+
+# Define what is available when using `from wildcard_core.tool_search import *`
+__all__ = ['ToolSearchClient']
