905d097a34f72165e9caa422769932377e738f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 03:05:13 +0000

    Add package surrealdb-rpc/0.1.1/surrealdb_rpc-0.1.1.tar.gz

---
+++ packages/surrealdb-rpc/surrealdb_rpc-0.1.1.tar.gz/surrealdb_rpc-0.1.1/surrealdb_rpc/data_model/__init__.py
@@ -0,0 +1,34 @@
+from .ext_types import UUID, DateTime, Decimal, Duration
+from .thing import (
+    ArrayRecordId,
+    EscapedString,
+    ExtTypes,
+    NumericRecordId,
+    ObjectRecordId,
+    RecordId,
+    String,
+    SurrealTypes,
+    Table,
+    TextRecordId,
+    Thing,
+    pack_record_id,
+)
+
+__all__ = [
+    "ArrayRecordId",
+    "DateTime",
+    "Decimal",
+    "Duration",
+    "EscapedString",
+    "ExtTypes",
+    "NumericRecordId",
+    "ObjectRecordId",
+    "RecordId",
+    "String",
+    "SurrealTypes",
+    "Table",
+    "TextRecordId",
+    "Thing",
+    "UUID",
+    "pack_record_id",
+]
