ff866dd78df7ab0b7ed3ade8aa158eab5c351aa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 14:06:19 +0000

    Add package adsbxcot/7.0.0/adsbxcot-7.0.0.tar.gz

---
+++ packages/adsbxcot/adsbxcot-7.0.0.tar.gz/adsbxcot-7.0.0/adsbxcot/classes.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright Sensors & Signals LLC https://www.snstac.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""ADSBXCOT Class Definitions."""
+
+import asyncio
+
+from configparser import SectionProxy
+from typing import Union, Optional
+
+import aiohttp
+
+import pytak
+import aircot
+import adsbxcot
+
+
+class ADSBXWorker(pytak.QueueWorker):
+    """Reads ADS-B Aggregator Data, renders to COT, and puts on Queue."""
+
+    def __init__(self, queue: asyncio.Queue, config: SectionProxy) -> None:
+        super().__init__(queue, config)
+        self.known_craft_db: Union[dict, None] = None
+        self.session: Union[aiohttp.ClientSession, None] = None
+        self.altitudes: dict = {}
+
+    async def handle_data(self, data: list) -> None:
+        """Marshal ADS-B data into CoT, and put it onto a TX queue."""
+        if not isinstance(data, list):
+            self._logger.warning("Invalid aircraft data, should be a Python list.")
+            return None
+
+        if not data:
+            self._logger.warning("Empty aircraft list")
+            return None
+
+        lod = len(data)
+        i = 1
+        for craft in data:
+            i += 1
+            icao = await self.process_craft(craft)
+            self._logger.debug("Handling %s/%s ICAO: %s", i, lod, icao)
+
+    async def process_craft(self, craft: dict) -> Optional[str]:
+        """Process individual aircraft data."""
+        if not isinstance(craft, dict):
+            self._logger.warning("Aircraft list item was not a Python `dict`.")
+            return None
+
+        icao: str = craft.get("hex", craft.get("icao", ""))
+        if icao:
+            icao = icao.strip().upper()
+        else:
+            self._logger.warning("No ICAO in craft data: %s", craft)
+            return None
+
+        if "~" in icao:
+            if not self.config.getboolean("INCLUDE_TISB"):
+                return None
+        else:
+            if self.config.getboolean("TISB_ONLY"):
+                return None
+
+        known_craft: dict = aircot.get_known_craft(self.known_craft_db, icao, "HEX")
+
+        if (
+            self.known_craft_db
+            and not known_craft
+            and not self.config.getboolean("INCLUDE_ALL_CRAFT")
+        ):
+            self._logger.debug("Not including unknown craft: %s", icao)
+            return None
+
+        ref_alts = self.calc_altitude(craft)
+        craft.update(ref_alts)
+
+        if not craft:
+            self._logger.debug("No altitude data for craft: %s", icao)
+            return None
+
+        event: Optional[bytes] = adsbxcot.adsbx_to_cot(
+            craft, config=self.config, known_craft=known_craft
+        )
+
+        if not event:
+            self._logger.debug("Empty CoT for craft: %s", icao)
+            return None
+
+        await self.put_queue(event)
+        return icao
+
+    def calc_altitude(self, craft: dict) -> dict:
+        """Calculate altitude based on barometric and geometric altitude."""
+        alt_baro = craft.get("alt_baro", "")
+        alt_geom = craft.get("alt_geom", "")
+
+        if not alt_baro:
+            return {}
+
+        if alt_baro == "ground":
+            return {}
+
+        alt_baro = float(alt_baro)
+        if alt_geom:
+            self.altitudes["alt_geom"] = float(alt_geom)
+            self.altitudes["alt_baro"] = alt_baro
+        elif "alt_baro" in self.altitudes and "alt_geom" in self.altitudes:
+            ref_alt_baro = float(self.altitudes["alt_baro"])
+            alt_baro_offset = alt_baro - ref_alt_baro
+            return {
+                "x_alt_baro_offset": alt_baro_offset,
+                "x_alt_geom": ref_alt_baro + alt_baro_offset,
+            }
+
+        return {}
+
+    async def get_feed(self, url: str) -> None:
+        """
+        ADS-B Aggregator API Client wrapper.
+        Connects to API and passes messages to `self.handle_message()`.
+        """
+        if self.session is None or self.session.closed:
+            self._logger.error("Session is closed, cannot proceed.")
+            return
+
+        api_key: str = self.config.get("API_KEY")
+
+        # Support for either direct ADSBX API, or RapidAPI:
+        if "rapidapi" in url.lower():
+            headers = {
+                "x-rapidapi-key": api_key,
+                "x-rapidapi-host": self.config.get(
+                    "RAPIDAPI_HOST", adsbxcot.DEFAULT_RAPIDAPI_HOST
+                ),
+            }
+        else:
+            headers = {"api-auth": api_key}
+
+        async with self.session.get(url=url, headers=headers) as resp:
+            if resp.status != 200:
+                response_content = await resp.text()
+                self._logger.warning("Received HTTP Status %s for %s", resp.status, url)
+                self._logger.warning(response_content)
+                return
+
+            json_resp = await resp.json()
+            if json_resp is None:
+                self._logger.warning("No JSON response from %s", url)
+                return
+
+            data = json_resp.get("ac")
+            if data is None:
+                self._logger.warning("No 'ac' key in JSON response from %s", url)
+                return
+
+            self._logger.info("Retrieved %s aircraft messages.", str(len(data) or "No"))
+            await self.handle_data(data)
+
+    async def run(self, _=-1) -> None:
+        """Runs this Thread, Reads from Pollers."""
+        self._logger.info("Running %s", self.__class__)
+
+        url: Optional[str] = self.config.get("FEED_URL")
+        if not url:
+            self._logger.error("FEED_URL not set in config, cannot proceed.")
+            raise ValueError("FEED_URL not set in config, cannot proceed.")
+
+        poll_interval: Union[int, str, None] = self.config.get("POLL_INTERVAL")
+        if poll_interval == "" or poll_interval is None:
+            self._logger.info(
+                "POLL_INTERVAL not set, using default of %s seconds.",
+                adsbxcot.DEFAULT_POLL_INTERVAL,
+            )
+            poll_interval = adsbxcot.DEFAULT_POLL_INTERVAL
+
+        known_craft = self.config.get("KNOWN_CRAFT")
+        if known_craft:
+            self._logger.info("Using KNOWN_CRAFT: %s", known_craft)
+            self.known_craft_db = aircot.read_known_craft(known_craft)
+
+        async with aiohttp.ClientSession() as self.session:
+            while self.session.closed is False:
+                self._logger.info(
+                    "%s polling every %ss: %s", self.__class__, poll_interval, url
+                )
+                await self.get_feed(url)
+                await asyncio.sleep(int(poll_interval))
---
+++ packages/adsbxcot/adsbxcot-7.0.0.tar.gz/adsbxcot-7.0.0/adsbxcot/functions.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright Sensors & Signals LLC https://www.snstac.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""ADSBXCOT Functions."""
+
+import xml.etree.ElementTree as ET
+
+from configparser import SectionProxy
+from typing import Union, Set, Optional
+
+import pytak
+import aircot
+import adsbxcot  # pylint: disable=cyclic-import
+
+
+def create_tasks(config: SectionProxy, clitool: pytak.CLITool) -> Set[pytak.Worker,]:
+    """
+    Creates specific coroutine task set for this application.
+
+    Parameters
+    ----------
+    config : `ConfigParser`
+        Configuration options & values.
+    clitool : `pytak.CLITool`
+        A PyTAK Worker class instance.
+
+    Returns
+    -------
+    `set`
+        Set of PyTAK Worker classes for this application.
+    """
+    return set([adsbxcot.ADSBXWorker(clitool.tx_queue, config)])
+
+
+def adsbx_to_cot_xml(  # NOQA pylint: disable=too-many-locals,too-many-branches,too-many-statements
+    craft: dict, config: Optional[dict] = None, known_craft: Optional[dict] = None
+) -> Optional[ET.Element]:
+    """
+    Serializes ADS-B Aggregator aircraft objects as Cursor on Target.
+
+    Parameters
+    ----------
+    craft : `dict`
+        Key/Value data struct of decoded ADS-B aircraft data.
+    config : `configparser.ConfigParser`
+        Configuration options and values.
+        Uses config options: UID_KEY, COT_STALE, COT_HOST_ID
+
+    Returns
+    -------
+    `xml.etree.ElementTree.Element`
+        Cursor on Target XML ElementTree object.
+    """
+    known_craft = known_craft or {}
+    config = config or {}
+
+    lat = craft.get("lat")
+    lon = craft.get("lon")
+
+    if lat is None or lon is None:
+        return None
+
+    remarks_fields = []
+
+    uid_key = config.get("UID_KEY", "ICAO")
+    cot_stale = int(config.get("COT_STALE", pytak.DEFAULT_COT_STALE))
+    cot_host_id = config.get("COT_HOST_ID", pytak.DEFAULT_HOST_ID)
+
+    aircotx = ET.Element("_aircot_")
+    aircotx.set("cot_host_id", cot_host_id)
+
+    icao_hex = craft.get("hex", craft.get("icao", ""))
+    reg = craft.get("r", "")
+    flight = craft.get("flight", "")
+    cat = craft.get("category")
+    squawk = craft.get("squawk")
+    craft_type = craft.get("t", "")
+
+    if reg:
+        reg = reg.strip().upper()
+        remarks_fields.append(reg)
+        aircotx.set("reg", reg)
+
+    if flight:
+        flight = flight.strip().upper()
+        remarks_fields.append(flight)
+        aircotx.set("flight", flight)
+
+    if squawk:
+        squawk = squawk.strip().upper()
+        remarks_fields.append(f"Squawk:{squawk}")
+        aircotx.set("squawk", squawk)
+
+    if icao_hex:
+        icao_hex = icao_hex.strip().upper()
+        remarks_fields.append(icao_hex)
+        aircotx.set("icao", icao_hex)
+
+    if cat:
+        cat = cat.strip().upper()
+        remarks_fields.append(f"Cat:{cat}")
+        aircotx.set("cat", cat)
+
+    if craft_type:
+        craft_type = craft_type.strip().upper()
+        remarks_fields.append(f"Type:{craft_type}")
+        aircotx.set("type", craft_type)
+
+    if "alt_geom" in craft:
+        remarks_fields.append(f"Alt:{craft.get('alt_geom')}")
+
+    aircotx.set("alt_geom", str(craft.get("alt_geom")))
+    aircotx.set("x_alt_geom", str(craft.get("x_alt_geom")))
+    aircotx.set("alt_baro", str(craft.get("alt_baro")))
+    aircotx.set("x_alt_baro_offset", str(craft.get("x_alt_baro_offset")))
+
+    if "REG" in uid_key and reg:
+        cot_uid = f"REG-{reg}"
+    elif "ICAO" in uid_key and icao_hex:
+        cot_uid = f"ICAO-{icao_hex}"
+    if "FLIGHT" in uid_key and flight:
+        cot_uid = f"FLIGHT-{flight}"
+    elif icao_hex:
+        cot_uid = f"ICAO-{icao_hex}"
+    elif flight:
+        cot_uid = f"FLIGHT-{flight}"
+    else:
+        return None
+
+    if flight:
+        callsign = flight
+    elif reg:
+        callsign = reg
+    else:
+        callsign = icao_hex
+
+    _, callsign = aircot.set_name_callsign(
+        icao_hex, reg, craft_type, flight, known_craft
+    )
+    cat = aircot.set_category(cat, known_craft)
+    cot_type = aircot.set_cot_type(icao_hex, cat, flight, known_craft)
+
+    contact = ET.Element("contact")
+    contact.set("callsign", str(callsign))
+
+    track = ET.Element("track")
+    track.set("course", str(craft.get("track", "9999999.0")))
+
+    track.set("speed", aircot.functions.get_speed(craft.get("gs")))
+
+    detail = ET.Element("detail")
+    detail.set("uid", cot_uid)
+    detail.append(contact)
+    detail.append(track)
+    detail.append(aircotx)
+
+    icon = known_craft.get("ICON")
+    if icon:
+        usericon = ET.Element("usericon")
+        usericon.set("iconsetpath", icon)
+        detail.append(usericon)
+
+    remarks = ET.Element("remarks")
+    remarks_fields.append(f"{cot_host_id}")
+    _remarks = " ".join(list(filter(None, remarks_fields)))
+    remarks.text = _remarks
+    detail.append(remarks)
+
+    cot_d = {
+        "lat": str(lat),
+        "lon": str(lon),
+        "ce": str(craft.get("nac_p", "9999999.0")),
+        "le": str(craft.get("nac_v", "9999999.0")),
+        "hae": aircot.functions.get_hae(craft.get("alt_geom", craft.get("alt_geom_x"))),
+        "uid": cot_uid,
+        "cot_type": cot_type,
+        "stale": cot_stale,
+    }
+    cot = pytak.gen_cot_xml(**cot_d)
+
+    _detail = cot.findall("detail")[0]
+    flowtags = _detail.findall("_flow-tags_")
+    detail.extend(flowtags)
+    cot.remove(_detail)
+    cot.append(detail)
+
+    return cot
+
+
+def adsbx_to_cot(
+    craft: dict, config: Optional[dict] = None, known_craft: Optional[dict] = None
+) -> Optional[bytes]:
+    """Wrapper that returns COT as an XML string."""
+    cot: Union[ET.Element, None] = adsbx_to_cot_xml(craft, config, known_craft)
+    return (
+        b"\n".join([pytak.DEFAULT_XML_DECLARATION, ET.tostring(cot)]) if cot else None
+    )
