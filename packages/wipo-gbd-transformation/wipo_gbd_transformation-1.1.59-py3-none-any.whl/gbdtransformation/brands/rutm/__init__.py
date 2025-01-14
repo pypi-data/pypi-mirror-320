479a79612809cbb9c71b78cede51f6479151d146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:52 +0000

    Add package wipo-gbd-transformation/1.1.59/wipo_gbd_transformation-1.1.59-py3-none-any.whl

---
+++ packages/wipo-gbd-transformation/wipo_gbd_transformation-1.1.59-py3-none-any.whl/gbdtransformation/brands/rutm/__init__.py
@@ -0,0 +1,11 @@
+# instruction to render the output to JSON format
+render = 'JSON'
+source = 'national'
+
+
+# 201302148
+# 95711885
+# 141807
+appnum_mask = [ '\\d{4}(\\d{6})',
+                '\\d{2}(\\d{6})',
+                '(\\d*)' ]
