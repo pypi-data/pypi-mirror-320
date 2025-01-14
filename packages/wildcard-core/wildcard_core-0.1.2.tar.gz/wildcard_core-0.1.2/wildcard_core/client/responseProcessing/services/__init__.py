4a207462ba1a806872cddbc3f2b1edc1a953beee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:35 +0000

    Add package wildcard-core/0.1.2/wildcard_core-0.1.2.tar.gz

---
+++ packages/wildcard-core/wildcard_core-0.1.2.tar.gz/wildcard_core-0.1.2/wildcard_core/client/responseProcessing/services/__init__.py
@@ -0,0 +1,9 @@
+from .GmailResponseProcessor import GmailResponseProcessor
+from .AirtableResponseProcessor import AirtableResponseProcessor
+from .ShopifyResponseProcessor import ShopifyResponseProcessor
+
+__all__ = [
+    'GmailResponseProcessor',
+    'AirtableResponseProcessor',
+    'ShopifyResponseProcessor'
+]
---
+++ packages/wildcard-core/wildcard_core-0.1.2.tar.gz/wildcard_core-0.1.2/wildcard_core/tool_registry/tools/__init__.py
@@ -0,0 +1,9 @@
+# Import main classes or functions from modules
+from .rest_api.RestAPIHandler import RestAPIHandler
+from .swagger.SwaggerHandler import SwaggerHandler
+from .swagger.services.gmailTool import GmailTool
+from .swagger.services.airtableTool import AirtableTool
+from .graphql.services.shopify.shopifyTool import ShopifyTool
+
+# Define what is available when using `from wildcard_core.tool_registry import *`
+__all__ = ['RestAPIHandler', 'SwaggerHandler', 'GmailTool', 'AirtableTool', 'ShopifyTool']
