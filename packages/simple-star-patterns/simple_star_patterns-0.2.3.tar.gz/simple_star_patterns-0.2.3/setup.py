65d57e052128bd308756aef86e1c9e9938ecd686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:04:50 +0000

    Add package simple-star-patterns/0.2.3/simple_star_patterns-0.2.3.tar.gz

---
+++ packages/simple-star-patterns/simple_star_patterns-0.2.3.tar.gz/simple_star_patterns-0.2.3/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="simple_star_patterns",
+    version="0.2.3",
+    author="Yash Agrawal",
+    author_email="yashmtr1744@gmail.com",
+    description="A simple library for printing star patterns",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["simple_star_patterns"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
