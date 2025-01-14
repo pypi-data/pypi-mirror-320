0c551d1aab26c177a259d833423d1f91bca50f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 03:04:46 +0000

    Add package burphttprequest/0.1.0/burphttprequest-0.1.0.tar.gz

---
+++ packages/burphttprequest/burphttprequest-0.1.0.tar.gz/burphttprequest-0.1.0/test/main.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+# 添加父目录到Python路径
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from burphttp import burphttp
+
+def main():
+    # 从文件读取请求
+    with open('test/proxy.http', 'r') as f:
+        request_content = f.read()
+    
+    # 创建解析器实例
+    bq = burphttp()
+    bq.parse_request(request_content) # 解析请求从字符串
+    # bq.parse_request_from_file("test/proxy.http") # 解析请求从文件
+    bq.set_cookie("session=abc123; user=test; token=xyz789")# 设置cookie示例
+    bq.set_proxy("http://127.0.0.1:7890") # 设置代理
+    bq.save_response_body("test/response.json") #save response body to file
+    bq.fixEncoding() #remove Accept-Encoding: gzip, deflate, br
+    bq.send_request() #send request
+
+    
+    # 输出响应信息 
+    print("full response:")
+    print(bq.response)
+    print("\nheaders:")
+    print(bq.response_headers)
+    print("\nbody:")
+    print(bq.response_body)
+
+if __name__ == "__main__":
+    main()
