53aefa4be86d8958feaa9a5247a6b3f774c1a2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 14:06:20 +0000

    Add package ipcserver/0.2.4/ipcserver-0.2.4.tar.gz

---
+++ packages/ipcserver/ipcserver-0.2.4.tar.gz/ipcserver-0.2.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+setup(
+    name="ipcserver",
+    version="0.2.4",
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    description="A fastapi-like but a sock server",
+    install_requires=[
+        "colorama>=0.4.6",
+        "msgpack>=1.0.8",
+        "pytest-asyncio",
+        "pytest"
+    ],
+    author="class-undefined",
+    author_email="luyukai@tsinghua.edu.cn",
+    python_requires='>=3.6',
+)
