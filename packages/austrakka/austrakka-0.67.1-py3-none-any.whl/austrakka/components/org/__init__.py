38e341fc99858be263969d6fb73df20bbed0979c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 14:07:07 +0000

    Add package austrakka/0.67.1/austrakka-0.67.1-py3-none-any.whl

---
+++ packages/austrakka/austrakka-0.67.1-py3-none-any.whl/austrakka/components/org/__init__.py
@@ -0,0 +1,62 @@
+import click
+
+from austrakka.utils.output import table_format_option
+from austrakka.utils.cmd_filter import hide_admin_cmds
+from austrakka.utils.options import opt_name
+from austrakka.utils.options import opt_abbrev
+from austrakka.utils.options import opt_is_active
+from austrakka.utils.options import opt_country
+from austrakka.utils.options import opt_state
+from austrakka.utils.subcommands.privilege import privilege_subcommands
+from .funcs import list_orgs
+from .funcs import add_org
+from .funcs import update_org
+
+
+@click.group()
+@click.pass_context
+def org(ctx):
+    '''Commands related to organisations'''
+    ctx.context = ctx.parent.context
+
+org.add_command(privilege_subcommands('Organisation'))
+
+@org.command('list')
+@table_format_option()
+def org_list(out_format: str):
+    '''List organisations in AusTrakka'''
+    list_orgs(out_format)
+
+
+@org.command('add', hidden=hide_admin_cmds())
+@opt_name(help="Organisation Name")
+@opt_abbrev(help="Organisation Abbreviation")
+@opt_state(required=False)
+@opt_country(required=False)
+@opt_is_active()
+def org_add(
+    name: str,
+    abbrev: str,
+    country: str,
+    state: str,
+    is_active: bool,
+):
+    '''Add organisations in AusTrakka'''
+    add_org(name, abbrev, country, state, is_active)
+
+
+@org.command('update', hidden=hide_admin_cmds())
+@click.argument('org-abbrev', type=str)
+@opt_name(help="Organisation Name", required=False)
+@opt_state(required=False)
+@opt_country(required=False)
+@opt_is_active(is_update=True)
+def org_update(
+        org_abbrev: str,
+        name: str,
+        country: str,
+        state: str,
+        is_active: bool,
+):
+    '''Update organisations in AusTrakka'''
+    update_org(org_abbrev, name, country, state, is_active)
---
+++ packages/austrakka/austrakka-0.67.1-py3-none-any.whl/austrakka/components/project/__init__.py
@@ -0,0 +1,120 @@
+# pylint: disable=expression-not-assigned
+import click
+from austrakka.utils.output import table_format_option
+from austrakka.utils.output import object_format_option
+from austrakka.utils.cmd_filter import hide_admin_cmds
+from austrakka.utils.options import opt_abbrev, \
+    opt_is_active, \
+    opt_type, \
+    opt_view_type
+from austrakka.utils.options import opt_name
+from austrakka.utils.options import opt_dashboard_name
+from austrakka.utils.options import opt_description
+from austrakka.utils.options import opt_organisation
+from .funcs import list_projects, \
+    add_project, \
+    update_project, \
+    set_dashboard, \
+    get_dashboard, \
+    show_project_settings, \
+    set_project_type
+
+from .dataset import dataset
+from .field import field
+from .provision import provision
+from .metadata import metadata
+
+
+@click.group()
+@click.pass_context
+def project(ctx):
+    """Commands related to projects"""
+    ctx.context = ctx.parent.context
+
+
+project.add_command(field)
+project.add_command(provision)
+project.add_command(metadata)
+project.add_command(dataset)
+
+
+@project.command('add', hidden=hide_admin_cmds())
+@opt_abbrev(help="Project Abbreviation")
+@opt_name(help="Project name")
+@opt_description(required=False)
+@opt_organisation(help="Requesting organisation abbreviation", required=False)
+@opt_dashboard_name(required=False)
+def project_add(
+        abbrev: str,
+        name: str,
+        description: str,
+        org: str,
+        dashboard_name: str):
+    '''
+    Add a new project to AusTrakka.
+    '''
+    add_project(abbrev, name, description, org, dashboard_name)
+
+
+@project.command('update', hidden=hide_admin_cmds())
+@click.argument('project-abbrev', type=str)
+@opt_abbrev(help="New project abbreviation", required=False)
+@opt_name(help="New project name", required=False)
+@opt_description(help="New project description", required=False)
+@opt_is_active(help="Set project active status", is_update=True, required=False)
+@opt_organisation(help="New requesting organisation abbreviation", required=False)
+@opt_dashboard_name(help="New dashboard", required=False)
+def project_update(
+        project_abbrev: str,
+        abbrev: str,
+        name: str,
+        description: str,
+        is_active: bool,
+        org: str,
+        dashboard_name: str):
+    '''
+    Update an existing project in AusTrakka.
+    '''
+    update_project(project_abbrev, abbrev, name, description, is_active, org, dashboard_name)
+
+
+@project.command('set-dashboard', hidden=hide_admin_cmds())
+@opt_name(help="name of a known dashboard")
+@click.argument('project-abbrev', type=str)
+def dashboard_set(project_abbrev: str, name: str):
+    '''
+    Assign a dashboard to a project.
+    '''
+    set_dashboard(project_abbrev, name)
+
+
+@project.command('get-dashboard', hidden=hide_admin_cmds())
+@click.argument('project-abbrev', type=str)
+@object_format_option()
+def dashboard_get(project_abbrev: str, out_format: str):
+    '''
+    Get the name of the dashboard currently assigned to a project.
+    '''
+    get_dashboard(project_abbrev, out_format)
+
+
+@project.command('list')
+@opt_view_type()
+@table_format_option()
+def projects_list(view_type: str,out_format: str):
+    '''List projects in AusTrakka'''
+    list_projects(view_type, out_format)
+
+@project.command('settings')
+@click.argument('project-abbrev', type=str)
+@object_format_option()
+def project_settings(project_abbrev: str, out_format: str):
+    '''Show project settings'''
+    show_project_settings(project_abbrev, out_format)
+    
+@project.command('set-type')
+@click.argument('project-abbrev', type=str)
+@opt_type()
+def project_set_type(project_abbrev: str, project_type: str):
+    '''Set a type for a project'''
+    set_project_type(project_abbrev, project_type)
