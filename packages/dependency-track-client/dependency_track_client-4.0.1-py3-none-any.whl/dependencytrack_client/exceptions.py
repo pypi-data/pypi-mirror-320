783c93e03243cc1207968b5b682be28125be42fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:04:49 +0000

    Add package dependency-track-client/4.0.1/dependency_track_client-4.0.1-py3-none-any.whl

---
+++ packages/dependency-track-client/dependency_track_client-4.0.1-py3-none-any.whl/dependencytrack_client/exceptions.py
@@ -0,0 +1,40 @@
+# Copyright 2020 Alvin Chen sonoma001@gmail.com
+# SPDX-License-Identifier: GPL-2.0+
+
+
+import json
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+
+    pass
+
+
+class AuthenticationError(Error):
+    """Authentication error"""
+
+    def __init__(self, url):
+        self.url = url
+        self.message = (
+            f"An error occurred during authentication against {self.url}\n"
+            f"Check your API Token and try again"
+        )
+
+
+class AuthorizationError(Error):
+    """Authorization error"""
+
+    def __init__(self, description, response):
+        self.message = (
+            f"{description}\n{json.loads(response.text)['message']} ({response.status_code})"
+        )
+
+
+class DependencyTrackApiError(Error):
+    """Error during a DependencyTrack GET request"""
+
+    def __init__(self, description, response):
+        self.message = (
+            f"{description}: '{json.loads(response.text)['message']}' ({response.status_code})"
+        )
