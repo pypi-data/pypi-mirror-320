f75dee708ad9be69b73ab68d7ff6e5acb332ef38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 03:05:32 +0000

    Add package hera-sim/4.3.1/hera_sim-4.3.1-py3-none-any.whl

---
+++ packages/hera-sim/hera_sim-4.3.1-py3-none-any.whl/hera_sim-4.3.1.data/scripts/hera-sim-vis.py
@@ -0,0 +1,18 @@
+#!python
+
+"""
+Command-line interface for simulating visibilities with ``hera_sim``.
+
+This script may be used to run a visibility simulation from a configuration file and
+write the result to disk.
+"""
+
+from hera_cli_utils import parse_args, run_with_profiling
+
+from hera_sim.visibilities.cli import run_vis_sim, vis_cli_argparser
+
+if __name__ == "__main__":
+    parser = vis_cli_argparser()
+    args = parse_args(parser)
+
+    run_with_profiling(run_vis_sim, args, args)
