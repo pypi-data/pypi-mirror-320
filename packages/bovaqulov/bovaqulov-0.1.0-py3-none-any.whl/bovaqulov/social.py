cd7a821bcbf6b217e14a0e89f236f14f753e153e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:42 +0000

    Add package bovaqulov/0.1.0/bovaqulov-0.1.0-py3-none-any.whl

---
+++ packages/bovaqulov/bovaqulov-0.1.0-py3-none-any.whl/bovaqulov/social.py
@@ -0,0 +1,10 @@
+def telegram():
+    return  "My Telegram link: https://t.me/bovaqulov"
+
+
+def instagram():
+    return  "My Instagram link: https://www.instagram.com/bovaqulov"
+
+def youtube():
+    return  "My YouTube link: www.youtube.com/@Bovaqulov"
+
