7069af92576b1f5fe8cd203178f4ec711e270208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:57 +0000

    Add package lynx-e2e-appium/0.0.2/lynx_e2e_appium-0.0.2.tar.gz

---
+++ packages/lynx-e2e-appium/lynx_e2e_appium-0.0.2.tar.gz/lynx_e2e_appium-0.0.2/lynx_e2e/_impl/appium/device.py
@@ -0,0 +1,10 @@
+# Copyright 2024 The Lynx Authors. All rights reserved.
+# Licensed under the Apache License Version 2.0 that can be found in the
+# LICENSE file in the root directory of this source tree.
+
+from ..device.device import Device as BaseDevice
+
+class AndroidDevice(BaseDevice):
+    
+    def set_appium_driver(self, driver):
+        self._driver.set_appium_driver(driver)
