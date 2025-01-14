b5d181cd59419996b4ee285b6e71d1014a6301c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:05:24 +0000

    Add package collectivemind/1.0.0/collectivemind-1.0.0-py3-none-any.whl

---
+++ packages/collectivemind/collectivemind-1.0.0-py3-none-any.whl/collectivemind/__init__.py
@@ -0,0 +1,8 @@
+from warnings import warn
+
+warn(
+	"You have installed an incorrect version of the `collectivemind` package from the public python package index (PyPI). " \
+	"Please install the appropriate package from your private distribution channel instead."
+)
+
+__all__ = ()
