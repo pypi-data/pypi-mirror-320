cd6e898ea18bc20bc54e08f8be0bd241be58d5c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:42 +0000

    Add package crep/2025.1.1/crep-2025.1.1.tar.gz

---
+++ packages/crep/crep-2025.1.1.tar.gz/crep-2025.1.1/test/test_table.py
@@ -0,0 +1,17 @@
+
+from crep.table import DataFrameContinuous
+
+
+def test_constructor(get_examples):
+    df_left, df_right = get_examples
+    df = DataFrameContinuous(
+        df_left,
+        discrete_index=["id"],
+        continuous_index=["t1", "t2"])
+
+    assert hasattr(df, "discrete_index")
+    assert hasattr(df, "continuous_index")
+    assert hasattr(df, "admissible")
+
+    # print(df**2)
+    # assert hasattr(df**2, "discrete_index")
