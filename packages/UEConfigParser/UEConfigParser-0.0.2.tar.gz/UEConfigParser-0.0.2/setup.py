7cb1d0cac88024b866e4702b5ceb0268c99a036d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 05:04:57 +0000

    Add package UEConfigParser/0.0.2/UEConfigParser-0.0.2.tar.gz

---
+++ packages/UEConfigParser/UEConfigParser-0.0.2.tar.gz/UEConfigParser-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UEConfigParser',
+    version='0.0.2',
+    description='ConfigParser created for read/edit/write Unreal Engine Config files',
+    author='WooJin Kim',
+    author_email='woojinian@gmail.com',
+    url='https://github.com/xwoojin/UEConfigParser',
+    install_requires=['configparser'],
+    packages=find_packages(exclude=[]),
+    keywords=['ueconfig', 'xwoojin', 'config', 'parser', 'config parser', 'configparser', 'unreal engine', 'unreal config', 'unreal parser'],
+    python_requires=">=2.7, <4",
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
