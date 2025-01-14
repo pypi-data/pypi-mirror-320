6f6a5c282103a711ffe687e7a4edee6280addbcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:05:38 +0000

    Add package blipper/0.1.0/blipper-0.1.0-py3-none-any.whl

---
+++ packages/blipper/blipper-0.1.0-py3-none-any.whl/blipper.py
@@ -0,0 +1,13 @@
+import requests
+
+
+class Blipper:
+
+    def __init__(
+        self,
+        blipper_api_key: str
+    ):
+        self.blipper_api_key = blipper_api_key
+
+    def test(self):
+        ...
