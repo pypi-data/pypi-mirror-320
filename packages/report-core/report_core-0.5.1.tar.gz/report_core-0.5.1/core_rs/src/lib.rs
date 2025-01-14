b6c4d9dc0b7a1fb25d6e3b52362b6f2de03d46e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:04:49 +0000

    Add package report-core/0.5.1/report_core-0.5.1.tar.gz

---
+++ packages/report-core/report_core-0.5.1.tar.gz/report_core-0.5.1/core_rs/src/lib.rs
@@ -0,0 +1,17 @@
+pub mod datatype;
+pub mod funcs;
+pub mod structs;
+pub mod traits;
+pub mod utils;
+
+/// Функция для получения версии.
+pub fn version() -> &'static str {
+    env!("CARGO_PKG_VERSION")
+}
+
+const MAX_COL: u16 = 16_384;
+const MAX_ROW: u32 = 1_048_576;
+
+// Стандартные размеры как в Excel
+const DEFAULT_COLUMN_WIDTH: f64 = 8.43; // ~ 64 пикселя
+const DEFAULT_ROW_HEIGHT: f64 = 15.0; // ~ 20 пикселей
