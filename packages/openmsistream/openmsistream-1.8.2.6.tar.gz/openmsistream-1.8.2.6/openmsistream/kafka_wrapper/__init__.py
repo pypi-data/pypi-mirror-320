1efc69bde2a2915f7b0e22e5ab2b24c47dfc0a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 06:04:28 +0000

    Add package openmsistream/1.8.2.6/openmsistream-1.8.2.6.tar.gz

---
+++ packages/openmsistream/openmsistream-1.8.2.6.tar.gz/openmsistream-1.8.2.6/openmsistream/kafka_wrapper/__init__.py
@@ -0,0 +1,22 @@
+"""
+The OpenMSIStream wrapper around standard Kafka in confluent_kafka.
+Also includes wrapping around needed KafkaCrypto objects.
+"""
+
+from .openmsistream_producer import OpenMSIStreamProducer
+from .openmsistream_consumer import OpenMSIStreamConsumer
+from .producer_group import ProducerGroup
+from .consumer_group import ConsumerGroup
+from .consumer_and_producer_group import ConsumerAndProducerGroup
+from .openmsistream_kafka_crypto import OpenMSIStreamKafkaCrypto
+from .producible import Producible
+
+__all__ = [
+    "OpenMSIStreamProducer",
+    "OpenMSIStreamConsumer",
+    "ProducerGroup",
+    "ConsumerGroup",
+    "ConsumerAndProducerGroup",
+    "OpenMSIStreamKafkaCrypto",
+    "Producible",
+]
