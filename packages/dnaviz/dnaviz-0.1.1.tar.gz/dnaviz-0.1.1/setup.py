22436d7b5e330a445f00ad1e17189fe3ba5df28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:11 +0000

    Add package dnaviz/0.1.1/dnaviz-0.1.1.tar.gz

---
+++ packages/dnaviz/dnaviz-0.1.1.tar.gz/dnaviz-0.1.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dnaviz",
+    version="0.1.0",
+    author="SemiQuant",
+    author_email="Jason.Limberis@ucsf.edu",
+    description="Interactive DNA/RNA sequence visualization tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "pygame>=2.5.0",
+        "click>=8.0.0",
+        "rich>=10.0.0",
+    ],
+    entry_points={
+        "console_scripts": [
+            "dnaviz=dnaviz.cli:main",
+        ],
+    },
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "black>=22.0",
+            "isort>=5.0",
+            "flake8>=4.0",
+        ],
+    },
+) 
