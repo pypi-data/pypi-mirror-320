32e6522fdf880669f2fb9c7aa5ad908ac1ffe8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:33 +0000

    Add package ldt-identity-client/1.0.0/ldt_identity_client-1.0.0-py3-none-any.whl

---
+++ packages/ldt-identity-client/ldt_identity_client-1.0.0-py3-none-any.whl/ldt_identity_client/api/__init__.py
@@ -0,0 +1,10 @@
+# flake8: noqa
+
+# import apis into api package
+from ldt_identity_client.api.invitations_api import InvitationsApi
+from ldt_identity_client.api.organizations_api import OrganizationsApi
+from ldt_identity_client.api.service_accounts_api import ServiceAccountsApi
+from ldt_identity_client.api.session_api import SessionApi
+from ldt_identity_client.api.user_roles_api import UserRolesApi
+from ldt_identity_client.api.users_api import UsersApi
+
