063445e878cbe5aec75f6bc5a54bb43c2e3f7c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 07:05:03 +0000

    Add package tgram/1.12.3/tgram-1.12.3.tar.gz

---
+++ packages/tgram/tgram-1.12.3.tar.gz/tgram-1.12.3/tgram/__init__.py
@@ -0,0 +1,18 @@
+__all__ = [
+    "types",
+    "TgBot",
+    "handlers",
+    "filters",
+    "compose",
+    "StopPropagation",
+    "utils",
+    "storage",
+]
+__version__ = "1.12.3"
+
+from . import filters, utils, storage, handlers, types
+
+from .client import TgBot
+from .sync import compose
+
+from .errors import StopPropagation
