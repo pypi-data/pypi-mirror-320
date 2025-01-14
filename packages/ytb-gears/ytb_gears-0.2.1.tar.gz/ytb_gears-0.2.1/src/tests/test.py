b820218db5d492a0d9bb585b4589084f334c72ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:32 +0000

    Add package ytb-gears/0.2.1/ytb_gears-0.2.1.tar.gz

---
+++ packages/ytb-gears/ytb_gears-0.2.1.tar.gz/ytb_gears-0.2.1/src/tests/test.py
@@ -0,0 +1,36 @@
+import logging
+
+import tqdm
+
+import ytb_gears
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    client_secret_file = "client_secret.json"
+    video_path = "test.mp4"
+
+    if ytb_gears.today_already_uploaded(client_secret_file):
+        return
+
+    print(f"即将上传视频: {video_path}")
+
+    with tqdm.tqdm(total=100) as pbar:
+
+        def update_progress(progress: float):
+            pbar.update(int(progress * 100) - pbar.n)
+
+        ytb_gears.upload(
+            video_path,
+            client_secret_file,
+            "YouTube Gears Test Video",
+            "Uploaded by YouTube Gears",
+            "ytb-gears",
+            "22",
+            "unlisted",
+            progress_callback=update_progress,
+        )
+
+
+main()
