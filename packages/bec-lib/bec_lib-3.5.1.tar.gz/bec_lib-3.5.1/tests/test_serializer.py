afcbc73d6d847388bade16fe90de3ec140cafe7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:25 +0000

    Add package bec-lib/3.5.1/bec_lib-3.5.1.tar.gz

---
+++ packages/bec-lib/bec_lib-3.5.1.tar.gz/bec_lib-3.5.1/tests/test_serializer.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pytest
+
+from bec_lib.endpoints import MessageEndpoints
+from bec_lib.messages import DeviceMessage
+from bec_lib.serialization import json_ext, msgpack
+
+
+@pytest.fixture(params=[json_ext, msgpack])
+def serializer(request):
+    yield request.param
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {"a": 1, "b": 2},
+        "hello",
+        1,
+        1.0,
+        [1, 2, 3],
+        np.array([1, 2, 3]),
+        {1, 2, 3},
+        {
+            "hroz": {
+                "hroz": {"value": 0, "timestamp": 1708336264.5731058},
+                "hroz_setpoint": {"value": 0, "timestamp": 1708336264.573121},
+            }
+        },
+        MessageEndpoints.progress("test"),
+        DeviceMessage,
+        float,
+    ],
+)
+def test_serialize(serializer, data):
+    res = serializer.loads(serializer.dumps(data)) == data
+    assert all(res) if isinstance(data, np.ndarray) else res
