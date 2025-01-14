c43cdd9ca6e56be7aa9b465eca2f94015dc662b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 06:04:36 +0000

    Add package ikigai/0.0.4/ikigai-0.0.4.tar.gz

---
+++ packages/ikigai/ikigai-0.0.4.tar.gz/ikigai-0.0.4/src/ikigai/components/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2024-present Harsh Parekh <harsh@ikigailabs.io>
+#
+# SPDX-License-Identifier: MIT
+
+from ikigai.components.app import App, AppBuilder
+from ikigai.components.dataset import Dataset, DatasetBuilder
+
+__all__ = ["App", "AppBuilder", "Dataset", "DatasetBuilder"]
