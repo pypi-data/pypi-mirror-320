beed51d3ac12ec92be1dc2f435a2cb894c5d4762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 14:06:14 +0000

    Add package calculator-ONLY-FOR-TESTING/0.1/calculator_ONLY_FOR_TESTING-0.1-py3-none-any.whl

---
+++ packages/calculator-ONLY-FOR-TESTING/calculator_ONLY_FOR_TESTING-0.1-py3-none-any.whl/NR_GUI/__init__.py
@@ -0,0 +1,12 @@
+def add_number(num1, num2):
+    return (num1 + num2)
+
+def subtract_number(num1, num2):
+    return (num1 - num2)
+
+def multi_number(num1, num2):
+    return (num1 * num2)
+
+def dev_number(num1, num2):
+    return (num1 / num2)
+
