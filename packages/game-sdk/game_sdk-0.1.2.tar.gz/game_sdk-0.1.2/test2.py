a5adf461a90043a7cfea62b4bbfd7da13eef8b11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:14 +0000

    Add package game-sdk/0.1.2/game_sdk-0.1.2.tar.gz

---
+++ packages/game-sdk/game_sdk-0.1.2.tar.gz/game_sdk-0.1.2/test2.py
@@ -0,0 +1,5 @@
+from game_sdk.game.agent import Agent, WorkerConfig
+from game_sdk.game.custom_types import Function, Argument
+from game_sdk.game.custom_types import FunctionResult, FunctionResultStatus
+from game_sdk.game.worker import Worker
+
