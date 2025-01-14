5c005f67b44e172de684e366e4685a08625841d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:05:47 +0000

    Add package maisaedu-utilities-prefect/1.2.5/maisaedu-utilities-prefect-1.2.5.tar.gz

---
+++ packages/maisaedu-utilities-prefect/maisaedu-utilities-prefect-1.2.5.tar.gz/maisaedu-utilities-prefect-1.2.5/maisaedu_utilities_prefect/utils.py
@@ -0,0 +1,33 @@
+import os
+from prefect.futures import wait
+
+
+def read_file(path: str):
+    with open(path, "rb") as f:
+        return f.read()
+
+
+def write_file(path: str, content: bytes):
+    folder = "/".join(path.split("/")[:-1])
+    os.makedirs(folder, exist_ok=True)
+
+    with open(path, "wb") as f:
+        f.write(content)
+
+
+def build_prefect_logger():
+    try:
+        import prefect
+
+        logger = prefect.context.logger
+    except Exception as e:
+        from prefect import get_run_logger
+
+        logger = get_run_logger()
+    return logger
+
+def resolve_future_tasks(future_tasks):
+    wait(future_tasks)
+
+    for task in future_tasks:
+        task.result()
