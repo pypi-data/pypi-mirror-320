04c4a477b963bae7ca44cde56b8b183fdb12cd17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:04:50 +0000

    Add package report-core/0.5.1/report_core-0.5.1-cp312-cp312-win32.whl

---
+++ packages/report-core/report_core-0.5.1-cp312-cp312-win32.whl/report_core/__init__.py
@@ -0,0 +1,20 @@
+from ._report_core import (
+    __version__,
+    Service,
+    Book,
+    Sheet,
+    column_number_to_letter,
+    get_letter_coordinate,
+    version,
+)
+
+
+__all__ = (
+    "__version__",
+    "Service",
+    "Book",
+    "Sheet",
+    "column_number_to_letter",
+    "get_letter_coordinate",
+    "version",
+)
