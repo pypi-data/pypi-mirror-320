b89dfdbbaba15d4f8c33b053ec38187427ca2f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 03:05:16 +0000

    Add package oip-tracking-client/0.0.12/oip-tracking-client-0.0.12.tar.gz

---
+++ packages/oip-tracking-client/oip-tracking-client-0.0.12.tar.gz/oip-tracking-client-0.0.12/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name="oip-tracking-client",
+    version="0.0.12",
+    author="Rachid Belmeskine",
+    author_email="rachid.belmeskine@gmail.com",
+    description="This is the API client of Open Innovation Platform - Tracking",
+    long_description=open("README.public.md").read(),
+    long_description_content_type="text/markdown",
+    # url='https://github.com/...',
+    python_requires=">=3.7",
+    packages=[
+        "oip_tracking_client",
+        "oip_tracking_client.enums",
+        "oip_tracking_client.monitors",
+    ],
+    install_requires=[
+        "requests",
+        "pandas",
+        "typing",
+        "mlflow==2.11.3",
+        "numpy",
+        "Pillow",
+        "matplotlib",
+        "plotly",
+        "psutil",
+    ],
+    extras_require={"amd": ["pyamdgpuinfo"], "nvidia": ["pynvml"]},
+    license="PRIVATE LICENSE",
+)
