e7b57202ecb7239e1ceeaca7fe9009c7204b4729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:14 +0000

    Add package zenoh-ros-type/0.2.3/zenoh_ros_type-0.2.3.tar.gz

---
+++ packages/zenoh-ros-type/zenoh_ros_type-0.2.3.tar.gz/zenoh_ros_type-0.2.3/zenoh_ros_type/tier4_autoware_msgs/tier4_control_msgs.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from pycdr2 import IdlStruct,Enum
+from pycdr2.types import uint8
+
+@dataclass
+class GateMode(IdlStruct, typename="GateMode"):
+    class DATA(Enum):
+        AUTO = 0
+        EXTERNAL = 1
+    data: uint8
