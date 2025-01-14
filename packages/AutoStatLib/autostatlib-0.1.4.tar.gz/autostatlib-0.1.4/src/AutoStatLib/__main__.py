5b8e714e327ebe75e005e6990bf7e9e5ec83cf37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 03:04:56 +0000

    Add package AutoStatLib/0.1.4/autostatlib-0.1.4.tar.gz

---
+++ packages/AutoStatLib/autostatlib-0.1.4.tar.gz/autostatlib-0.1.4/src/AutoStatLib/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from .AutoStatLib import StatisticalAnalysis
+from ._version import __version__
+
+if __name__ == '__main__':
+    print('\nThis package works as an imported module only\n')
