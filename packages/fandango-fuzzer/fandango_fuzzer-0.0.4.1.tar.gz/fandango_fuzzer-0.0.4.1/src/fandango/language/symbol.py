5048b4778fccdf3f38d80ddddedc60603d0282d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:50 +0000

    Add package fandango-fuzzer/0.0.4.1/fandango_fuzzer-0.0.4.1.tar.gz

---
+++ packages/fandango-fuzzer/fandango_fuzzer-0.0.4.1.tar.gz/fandango_fuzzer-0.0.4.1/src/fandango/language/symbol.py
@@ -0,0 +1,103 @@
+import abc
+import enum
+
+
+class SymbolType(enum.Enum):
+    TERMINAL = "Terminal"
+    NON_TERMINAL = "NonTerminal"
+    IMPLICIT = "Implicit"
+
+
+class Symbol(abc.ABC):
+    def __init__(self, symbol: str | bytes, type_: SymbolType):
+        self.symbol = symbol
+        self.type = type_
+
+    def check(self, word: str) -> bool:
+        return False
+
+    def check_all(self, word: str) -> bool:
+        return False
+
+    @property
+    def is_terminal(self):
+        return self.type == SymbolType.TERMINAL
+
+    @property
+    def is_non_terminal(self):
+        return self.type == SymbolType.NON_TERMINAL
+
+    @property
+    def is_implicit(self):
+        return self.type == SymbolType.IMPLICIT
+
+    @abc.abstractmethod
+    def __hash__(self):
+        return NotImplemented
+
+
+class NonTerminal(Symbol):
+    def __init__(self, symbol: str):
+        super().__init__(symbol, SymbolType.NON_TERMINAL)
+
+    def __repr__(self):
+        return self.symbol
+
+    def __eq__(self, other):
+        return isinstance(other, NonTerminal) and self.symbol == other.symbol
+
+    def __hash__(self):
+        return hash((self.symbol, self.type))
+
+
+class Terminal(Symbol):
+    def __init__(self, symbol: str):
+        super().__init__(symbol, SymbolType.TERMINAL)
+
+    def __len__(self):
+        return len(self.symbol)
+
+    @staticmethod
+    def clean(symbol: str) -> str | bytes:
+        if len(symbol) >= 2:
+            if symbol[0] == symbol[-1] == "'" or symbol[0] == symbol[-1] == '"':
+                return eval(symbol)
+            elif len(symbol) >= 3:
+                if symbol[0] == "b" and (
+                    symbol[1] == symbol[-1] == "'" or symbol[1] == symbol[-1] == '"'
+                ):
+                    return eval(symbol)
+        return symbol
+
+    @staticmethod
+    def from_symbol(symbol: str) -> "Terminal":
+        return Terminal(Terminal.clean(symbol))
+
+    def check(self, word: str) -> bool:
+        return word.startswith(self.symbol)
+
+    def check_all(self, word: str) -> bool:
+        return word == self.symbol
+
+    def __repr__(self):
+        return f'"{self.symbol}"'
+
+    def __eq__(self, other):
+        return isinstance(other, Terminal) and self.symbol == other.symbol
+
+    def __str__(self):
+        return self.symbol
+
+    def __hash__(self):
+        return hash((self.symbol, self.type))
+
+
+class Implicit(Symbol):
+    def __init__(self, symbol: str):
+        super().__init__(symbol, SymbolType.IMPLICIT)
+
+    def __hash__(self):
+        return hash((self.symbol, self.type))
+
+    def __eq__(self, other):
+        return isinstance(other, Implicit) and self.symbol == other.symbol
---
+++ packages/fandango-fuzzer/fandango_fuzzer-0.0.4.1.tar.gz/fandango_fuzzer-0.0.4.1/tests/test_constraints.py
@@ -0,0 +1,388 @@
+import unittest
+
+
+from fandango.language.symbol import NonTerminal, Terminal
+from fandango.language.tree import DerivationTree
+from fandango.language.parse import parse
+
+
+class ConstraintTest(unittest.TestCase):
+    GRAMMAR = """
+<start> ::= <ab>;
+<ab> ::= 
+      "a" <ab> 
+    | <ab> "b"
+    | ""
+    ;
+"""
+
+    def get_constraint(self, constraint):
+        _, constraints = parse(self.GRAMMAR + constraint)
+        self.assertEqual(1, len(constraints))
+        return constraints[0]
+
+    def test_explicit_fitness(self):
+        constraint = self.get_constraint("len(str(<start>));")
+        example = DerivationTree(
+            NonTerminal("<start>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"),
+                            [
+                                DerivationTree(
+                                    NonTerminal("<ab>"),
+                                    [DerivationTree(Terminal(""))],
+                                ),
+                                DerivationTree(Terminal("b")),
+                            ],
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                )
+            ],
+        )
+        self.assertEqual(1, constraint.fitness(example).fitness())
+        example = DerivationTree(
+            NonTerminal("<start>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                )
+            ],
+        )
+        self.assertEqual(1, constraint.fitness(example).fitness())
+
+    def test_expression_constraint(self):
+        constraint = self.get_constraint("'a' not in str(<ab>);")
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        counter_example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(Terminal("a")),
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertFalse(constraint.check(counter_example))
+
+    def test_comparison_constraint(self):
+        constraint = self.get_constraint("|<ab>| > 2;")
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        counter_example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(NonTerminal("<ab>"), [DerivationTree(Terminal(""))]),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertFalse(constraint.check(counter_example))
+
+    def test_conjunction_constraint(self):
+        constraint = self.get_constraint("'a' not in str(<ab>) and |<ab>| > 2;")
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        counter_example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(Terminal("a")),
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertFalse(constraint.check(counter_example))
+        counter_example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(NonTerminal("<ab>"), [DerivationTree(Terminal(""))]),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertFalse(constraint.check(counter_example))
+
+    def test_disjunction_constraint(self):
+        constraint = self.get_constraint("'a' not in str(<ab>) or |<ab>| > 2;")
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(Terminal("a")),
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(NonTerminal("<ab>"), [DerivationTree(Terminal(""))]),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        counter_example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(Terminal("a")),
+                DerivationTree(NonTerminal("<ab>"), [DerivationTree(Terminal(""))]),
+            ],
+        )
+        self.assertFalse(constraint.check(counter_example))
+
+    def test_implication_constraint(self):
+        constraint = self.get_constraint("|<ab>| > 2 -> 'a' not in str(<ab>);")
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        counter_example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(Terminal("a")),
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertFalse(constraint.check(counter_example))
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(NonTerminal("<ab>"), [DerivationTree(Terminal(""))]),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(Terminal("a")),
+                DerivationTree(NonTerminal("<ab>"), [DerivationTree(Terminal(""))]),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+
+    def test_forall_constraint(self):
+        constraint = self.get_constraint("forall <x> in <ab>: 'a' not in str(<x>);")
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+        counter_example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(Terminal("a")),
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertFalse(constraint.check(counter_example))
+
+    def test_hash(self):
+        tree_1 = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(Terminal("a")),
+                DerivationTree(NonTerminal("<ab>"), [DerivationTree(Terminal(""))]),
+            ],
+        )
+        tree_2 = DerivationTree(NonTerminal("<ab>"), [DerivationTree(Terminal(""))])
+        self.assertNotEqual(tree_1, tree_2)
+
+    def test_exists_constraint(self):
+        constraint = self.get_constraint("exists <x> in <ab>: 'a' == str(<x>);")
+        counter_example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                        DerivationTree(Terminal("b")),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertFalse(constraint.check(counter_example))
+        example = DerivationTree(
+            NonTerminal("<ab>"),
+            [
+                DerivationTree(
+                    NonTerminal("<ab>"),
+                    [
+                        DerivationTree(Terminal("a")),
+                        DerivationTree(
+                            NonTerminal("<ab>"), [DerivationTree(Terminal(""))]
+                        ),
+                    ],
+                ),
+                DerivationTree(Terminal("b")),
+            ],
+        )
+        self.assertTrue(constraint.check(example))
+
+    def test_complex_constraint(self):
+        grammar = """
+<start> ::= <number>;
+<number> ::= <digit> | <digit><number>;
+<digit> ::= "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0";
+"""
+        constraint = """
+int(<number>) % 2 == 0;
+int(<number>) > 10000;
+int(<number>) < 100000;
+"""
+
+        _, constraints = parse(grammar + constraint)
+        self.assertEqual(3, len(constraints))
+
+        def get_tree(x):
+            return DerivationTree(
+                NonTerminal("<number>"), [DerivationTree(Terminal(str(x)))]
+            )
+
+        examples = [
+            (get_tree(20002), True, True, True),
+            (get_tree(20001), False, True, True),
+            (get_tree(2), True, False, True),
+            (get_tree(1), False, False, True),
+            (get_tree(200002), True, True, False),
+            (get_tree(200001), False, True, False),
+        ]
+
+        for tree, sat_even, sat_greater, sat_less in examples:
+            for sat, constraint in zip((sat_even, sat_greater, sat_less), constraints):
+                fitness = constraint.fitness(tree)
+                self.assertEqual(sat, fitness.success)
+                self.assertEqual(1 if sat else 0, fitness.solved)
+                if not sat:
+                    self.assertEqual(1, len(fitness.failing_trees))
+                    self.assertEqual(tree, fitness.failing_trees[0].tree)
