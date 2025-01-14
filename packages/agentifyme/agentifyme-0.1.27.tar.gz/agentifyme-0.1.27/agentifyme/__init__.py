aefe5e94c78571f9127d1abcff75c2afcdca9ebf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:13 +0000

    Add package agentifyme/0.1.27/agentifyme-0.1.27.tar.gz

---
+++ packages/agentifyme/agentifyme-0.1.27.tar.gz/agentifyme-0.1.27/agentifyme/__init__.py
@@ -0,0 +1,7 @@
+from agentifyme.config import AgentifyMeConfig
+from agentifyme.logger import get_logger
+from agentifyme.tasks import task
+from agentifyme.workflows import workflow
+
+__version__ = "0.1.27"
+__all__ = ["get_logger", "AgentifyMeConfig", "task", "workflow"]
