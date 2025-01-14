ff5c291f09fc83bb229e6fb546d038af6d4f56ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:07:01 +0000

    Add package findme-hackbit.org/1.0.3/findme_hackbit_org-1.0.3.tar.gz

---
+++ packages/findme-hackbit.org/findme_hackbit_org-1.0.3.tar.gz/findme_hackbit_org-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="findme-hackbit.org", 
+    version="1.0.3",
+    author="0xSaikat",
+    author_email="saikat@hackbit.org",
+    description="A tool to search for usernames across various platforms.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/0xSaikat/findme",  
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "colorama>=0.4.4",
+        "requests>=2.25.1",
+        "jsonschema>=4.0.0",
+        "termcolor>=1.1.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "findme=findme:main",  
+        ],
+    },
+)
