5a01f462af6568af037a6a3fb70809a399dc3376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:15 +0000

    Add package pymsgbus/0.1.0/pymsgbus-0.1.0-py3-none-any.whl

---
+++ packages/pymsgbus/pymsgbus-0.1.0-py3-none-any.whl/pymsgbus/__init__.py
@@ -0,0 +1,7 @@
+from fast_depends import Depends as Depends
+from pymsgbus.events import Event as Event
+from pymsgbus.events import Consumer as Consumer
+from pymsgbus.events import Producer as Producer
+from pymsgbus.messages import Message as Message
+from pymsgbus.messages import Publisher as Publisher
+from pymsgbus.messages import Subscriber as Subscriber
