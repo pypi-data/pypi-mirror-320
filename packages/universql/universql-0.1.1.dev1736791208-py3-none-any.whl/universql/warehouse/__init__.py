84e06b9e5675fb1dcf61d09e8e9497e65b0a6b64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 05:05:15 +0000

    Add package universql/0.1.1.dev1736791208/universql-0.1.1.dev1736791208-py3-none-any.whl

---
+++ packages/universql/universql-0.1.1.dev1736791208-py3-none-any.whl/universql/warehouse/__init__.py
@@ -0,0 +1,72 @@
+import typing
+from abc import ABC, abstractmethod
+from typing import List
+
+import pyiceberg.table
+import sqlglot
+from pyiceberg.catalog import Catalog
+from snowflake.connector.options import pyarrow
+
+
+Locations = typing.Dict[sqlglot.exp.Table, sqlglot.exp.Expression | None]
+Tables = typing.Dict[sqlglot.exp.Table, pyiceberg.table.Table | None]
+
+
+class ICatalog(ABC):
+    def __init__(self, context,
+                 session_id: str,
+                 credentials: dict,
+                 compute: dict,
+                 iceberg_catalog: Catalog,
+                 base_catalog: "universql.warehouse.snowflake.SnowflakeCatalog" = None):
+        self.context = context
+        self.session_id = session_id
+        self.credentials = credentials
+        self.compute = compute
+        self.iceberg_catalog = iceberg_catalog
+        self.base_catalog = base_catalog
+
+    def create(self, ast: sqlglot.exp.Table, table: pyarrow.Table):
+        pass
+
+    @abstractmethod
+    def get_table_paths(self, tables: List[sqlglot.exp.Table]) -> Tables:
+        pass
+
+    @abstractmethod
+    def register_locations(self, tables: Locations):
+        pass
+
+    @abstractmethod
+    def executor(self) -> "Executor":
+        pass
+
+
+T = typing.TypeVar('T', bound=ICatalog)
+
+
+class Executor(ABC, typing.Generic[T]):
+
+    def __init__(self, catalog: T):
+        self.catalog = catalog
+
+    @abstractmethod
+    def execute(self, ast: sqlglot.exp.Expression, locations: Tables) -> \
+            typing.Optional[Locations]:
+        pass
+
+    @abstractmethod
+    def execute_raw(self, raw_query: str) -> None:
+        pass
+
+    @abstractmethod
+    def get_as_table(self) -> pyarrow.Table:
+        pass
+
+    @abstractmethod
+    def get_query_log(self, total_duration) -> str:
+        pass
+
+    @abstractmethod
+    def close(self):
+        pass
