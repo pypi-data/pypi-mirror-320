ce2406b2c39a9ae2f543e7e40f71a6dcaf5e1e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:05:18 +0000

    Add package aiogram-creator/1.0.3/aiogram_creator-1.0.3-py3-none-any.whl

---
+++ packages/aiogram-creator/aiogram_creator-1.0.3-py3-none-any.whl/aiogram_creator/main.py
@@ -0,0 +1,24 @@
+import argparse
+import shutil
+import os
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('command', choices=['create'])
+    # parser.add_argument('--token', required=False, help='Telegram token')
+
+    args = parser.parse_args()
+
+    if args.command == 'create':
+        create_object()
+
+def create_object():
+    print(f'Creating...')
+    shutil.move('./template', os.getcwd())
+    print('Done')
+
+
+if __name__ == '__main__':
+    main()
