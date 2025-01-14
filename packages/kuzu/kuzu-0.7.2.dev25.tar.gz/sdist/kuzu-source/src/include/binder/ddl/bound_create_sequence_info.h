bb8f3060fddd1ee849c243bf86c0ba2e217b2a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:25 +0000

    Add package kuzu/0.7.2.dev25/kuzu-0.7.2.dev25.tar.gz

---
+++ packages/kuzu/kuzu-0.7.2.dev25.tar.gz/sdist/kuzu-source/src/include/binder/ddl/bound_create_sequence_info.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+
+#include "common/copy_constructors.h"
+#include "common/enums/conflict_action.h"
+
+namespace kuzu {
+namespace binder {
+
+struct BoundCreateSequenceInfo {
+    std::string sequenceName;
+    int64_t startWith;
+    int64_t increment;
+    int64_t minValue;
+    int64_t maxValue;
+    bool cycle;
+    common::ConflictAction onConflict;
+    bool hasParent = false;
+
+    BoundCreateSequenceInfo(std::string sequenceName, int64_t startWith, int64_t increment,
+        int64_t minValue, int64_t maxValue, bool cycle, common::ConflictAction onConflict)
+        : sequenceName{std::move(sequenceName)}, startWith{startWith}, increment{increment},
+          minValue{minValue}, maxValue{maxValue}, cycle{cycle}, onConflict{onConflict} {}
+    EXPLICIT_COPY_DEFAULT_MOVE(BoundCreateSequenceInfo);
+
+private:
+    BoundCreateSequenceInfo(const BoundCreateSequenceInfo& other)
+        : sequenceName{other.sequenceName}, startWith{other.startWith}, increment{other.increment},
+          minValue{other.minValue}, maxValue{other.maxValue}, cycle{other.cycle},
+          onConflict{other.onConflict}, hasParent{other.hasParent} {}
+};
+
+} // namespace binder
+} // namespace kuzu
