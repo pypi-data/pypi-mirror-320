0971ab9e032da503a0a145c53447360b2898fb23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:06:13 +0000

    Add package cadquery-ocp/7.8.1.0/cadquery_ocp-7.8.1.0-cp311-cp311-win_amd64.whl

---
+++ packages/cadquery-ocp/cadquery_ocp-7.8.1.0-cp311-cp311-win_amd64.whl/OCP/__init__.py
@@ -0,0 +1,31 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_9_1():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'cadquery_ocp.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_9_1()
+del _delvewheel_patch_1_9_1
+# end delvewheel patch
+
+def _vtkmodules():
+    import os
+    import sys
+
+    if sys.version_info[0] == 3 and sys.version_info[1] == 13:
+        libs_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), os.pardir, "cadquery_vtk.libs")
+        )
+    else:
+        libs_dir = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), os.pardir, "vtk.libs")
+        )
+    os.add_dll_directory(libs_dir)
+
+
+_vtkmodules()
+del _vtkmodules
+
+from OCP.OCP import *
+from OCP.OCP import __version__
