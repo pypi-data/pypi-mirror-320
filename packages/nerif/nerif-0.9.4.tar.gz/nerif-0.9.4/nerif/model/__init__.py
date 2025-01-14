fe53a263809768eea82d5d5d6d864d15963d97e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 03:05:29 +0000

    Add package nerif/0.9.4/nerif-0.9.4.tar.gz

---
+++ packages/nerif/nerif-0.9.4.tar.gz/nerif-0.9.4/nerif/model/__init__.py
@@ -0,0 +1,11 @@
+from .audio_model import AudioModel, SpeechModel
+from .model import LogitsChatModel, SimpleChatModel, SimpleEmbeddingModel, VisionModel
+
+__all__ = [
+    "LogitsChatModel",
+    "SimpleChatModel",
+    "SimpleEmbeddingModel",
+    "VisionModel",
+    "AudioModel",
+    "SpeechModel",
+]
