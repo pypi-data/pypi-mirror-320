bac43f1dd403fcb1142efdab21cee74a068da19a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 05:05:18 +0000

    Add package rocket-tayebk/1.0.0/rocket_tayebk-1.0.0.tar.gz

---
+++ packages/rocket-tayebk/rocket_tayebk-1.0.0.tar.gz/rocket_tayebk-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rocket_tayebk",
+    version="1.0.0",
+    author="Kahia Tayeb",
+    author_email="t.kahia@esi-sba.dz",
+    packages=find_packages(),
+    url="http://pypi.python.org/pypi/rocket/",
+    license="MIT",
+    description="An awesome package that does something",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+)
