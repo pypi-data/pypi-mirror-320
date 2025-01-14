1631e5ca684eea73906b06a3d4c707e200991be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 06:04:40 +0000

    Add package tecton-gen-ai/0.0.2b5/tecton_gen_ai-0.0.2b5-py3-none-any.whl

---
+++ packages/tecton-gen-ai/tecton_gen_ai-0.0.2b5-py3-none-any.whl/tecton_gen_ai/constants.py
@@ -0,0 +1,7 @@
+_TECTON_CHECKPOINT_ATTR = "__tecton_checkpoint__"
+_TECTON_MOCK_OBJECT_ATTR = "_tecton_mock_object_attr"
+_DEV_MODE_AGENT_KEY = "dev_mode_agent"
+_INPUT_JSON_FIELD = "input_json"
+_OUTPUT_JSON_FIELD = "output_json"
+UUID_ATTR = "__uuid__"
+DEFAULT_TIMEOUT = "5s"
