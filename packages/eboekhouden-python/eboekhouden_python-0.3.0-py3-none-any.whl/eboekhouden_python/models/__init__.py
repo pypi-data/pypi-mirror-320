d3e360c63f516b224a4830caf59d326058845036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:05:19 +0000

    Add package eboekhouden-python/0.3.0/eboekhouden_python-0.3.0-py3-none-any.whl

---
+++ packages/eboekhouden-python/eboekhouden_python-0.3.0-py3-none-any.whl/eboekhouden_python/models/__init__.py
@@ -0,0 +1,15 @@
+"""Models package."""
+from .mutatie_filter import MutatieFilter
+from .mutatie_regel import MutatieRegel
+from .mutatie import Mutatie
+from .relatie_filter import RelatieFilter
+from .relatie import Relatie
+
+
+__all__ = [
+    "MutatieFilter",
+    "MutatieRegel",
+    "Mutatie",
+    "RelatieFilter",
+    "Relatie",
+]
