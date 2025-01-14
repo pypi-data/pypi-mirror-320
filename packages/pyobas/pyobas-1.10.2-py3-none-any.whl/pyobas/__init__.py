4214e610bd2dfa7b999ed5d78762d31a657e098a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 16:06:50 +0000

    Add package pyobas/1.10.2/pyobas-1.10.2-py3-none-any.whl

---
+++ packages/pyobas/pyobas-1.10.2-py3-none-any.whl/pyobas/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+__version__ = "1.10.2"
+
+from pyobas._version import (  # noqa: F401
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __title__,
+)
+from pyobas.client import OpenBAS  # noqa: F401
+from pyobas.contracts import *  # noqa: F401,F403,F405
+from pyobas.exceptions import *  # noqa: F401,F403,F405
+
+__all__ = [
+    "__author__",
+    "__copyright__",
+    "__email__",
+    "__license__",
+    "__title__",
+    "__version__",
+    "OpenBAS",
+]
+__all__.extend(exceptions.__all__)  # noqa: F405
