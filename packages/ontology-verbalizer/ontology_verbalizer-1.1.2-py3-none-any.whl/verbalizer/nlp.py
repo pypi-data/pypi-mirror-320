0781255603247a5181ae58d1a6663013d2e53ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 14:06:13 +0000

    Add package ontology-verbalizer/1.1.2/ontology_verbalizer-1.1.2-py3-none-any.whl

---
+++ packages/ontology-verbalizer/ontology_verbalizer-1.1.2-py3-none-any.whl/verbalizer/nlp.py
@@ -0,0 +1,174 @@
+import logging
+from abc import ABC, abstractmethod
+from typing import Optional
+
+try:
+    from openai import OpenAI
+
+    logging.getLogger("openai").setLevel(logging.ERROR)
+    logging.getLogger("httpx").setLevel(logging.ERROR)
+except ModuleNotFoundError as err:
+    OpenAI = None
+
+
+class ParaphraseLanguageModel(ABC):
+
+    @abstractmethod
+    def pseudo_to_text(self, pseudo_text: str, extra: str = None) -> str:
+        """
+        Given a pseudo text or controlled natural language, return a rephrased version of that same text.
+        :param pseudo_text: The CNL set of statements,
+        :param extra: Additional context to include as part of the prompt.
+        :return: Paraphrased text.
+        """
+        return pseudo_text
+
+    @property
+    def cost(self) -> float:
+        """
+        The usage cost so far of the model.
+        """
+        return 0.0
+
+    @property
+    def name(self) -> str:
+        """
+        The name of the model used.
+        """
+        return 'Unknown'
+
+
+def get_messages(pseudo_text: str, extra_context: Optional[str] = None):
+    """
+    Get messages (system, assistant, user)
+    """
+    extra = extra_context if extra_context else ""
+    return [
+        {
+            "role": "system",
+            "content": f"You are an extremely specific data expert capable of converting pseudo English sentences "
+                       f"into a meaningful and casual paragraph without losing information. Avoid repeating "
+                       f"information. Spell out everything don't be lazy! {extra}"
+        },
+        {
+            "role": "user", "content":
+            """
+            x relation 1 y
+            z relation 2 x
+            m relation 3 n
+            """
+        },
+        {
+            "role": "assistant", "content":
+            """
+            X has this relation 1 with Y, Z shares a relation 2 with X.
+            Moreover, m has relation 3 with n.
+            """
+        },
+        {"role": "user", "content": "X is same as something that intersection of something that something that has at "
+                                    "least 3 N and M."},
+        {"role": "assistant", "content": "X is the same as M which has at least three N"},
+        {"role": "user", "content":
+            """
+            X is a type of at least has Y relation some a M.
+            X is a type of at least has Y relation some a N.
+            X is a type of only has Y relation any of (a M and a N)
+            """
+         },
+        {"role": "assistant", "content": "X has Y relation with M and N and nothing else."},
+        {"role": "user", "content": pseudo_text},
+    ]
+
+
+class ChatGptModelParaphrase(ParaphraseLanguageModel):
+    """
+    OpenAI wrapper implementation.
+    """
+
+    models = {
+        "gpt-4o": {
+            "input": 0.005,
+            "output": 0.015
+        },
+        "gpt-4-0125-preview": {
+            "input": 0.01,
+            "output": 0.03
+        },
+        "gpt-4-1106-preview": {
+            "input": 0.01,
+            "output": 0.03
+        },
+        "gpt-4-1106-vision-preview": {
+            "input": 0.01,
+            "output": 0.03
+        },
+        "gpt-4": {
+            "input": 0.03,
+            "output": 0.06
+        },
+        "gpt-4-0613": {
+            "input": 0.03,
+            "output": 0.06
+        },
+        "gpt-4-32k": {
+            "input": 0.06,
+            "output": 0.12
+        },
+        "gpt-3.5-turbo-0125": {
+            "input": 0.0005,
+            "output": 0.0015
+        },
+        "gpt-3.5-turbo-instruct": {
+            "input": 0.0015,
+            "output": 0.0020
+        },
+        "gpt-3.5-turbo-16k-0613": {
+            "input": 0.0030,
+            "output": 0.0040
+        },
+        "gpt-3.5-turbo-1106": {
+            "input": 0.0010,
+            "output": 0.0020
+        },
+        "gpt-3.5-turbo-0613": {
+            "input": 0.0015,
+            "output": 0.0020
+        },
+        "gpt-3.5-turbo-0301": {
+            "input": 0.0015,
+            "output": 0.0020
+        }
+    }
+
+    def __init__(self, api_key: str, model: str = 'gpt-3.5-turbo-0613', temperature=0.5):
+        if not OpenAI:
+            raise ModuleNotFoundError("OpenAI is not installed. Please install it with `pip install openai`")
+
+        self.model = model
+        self.temperature = temperature
+        self.client = OpenAI(api_key=api_key)
+        self._in_token_usage = 0
+        self._out_token_usage = 0
+
+    def pseudo_to_text(self, pseudo_text: str, extra: str = None) -> str:
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=get_messages(pseudo_text, extra),
+            temperature=self.temperature
+        )
+        self._in_token_usage += response.usage.prompt_tokens
+        self._out_token_usage += response.usage.completion_tokens
+        return response.choices[0].message.content
+
+    @property
+    def cost(self) -> float:
+        model_pricing = self.models.get(self.model) or {'input': 0.0, 'output': 0.0}
+
+        in_tokens = self._in_token_usage / 1000
+        out_tokens = self._out_token_usage / 1000
+
+        return in_tokens * model_pricing['input'] + out_tokens * model_pricing["output"]
+
+    @property
+    def name(self) -> str:
+        return self.model
---
+++ packages/ontology-verbalizer/ontology_verbalizer-1.1.2-py3-none-any.whl/verbalizer/vocabulary.py
@@ -0,0 +1,227 @@
+import re
+import logging
+from rdflib import URIRef, Graph
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+
+
+class Vocabulary:
+    """
+    Vocabulary class, used as a URI to term lookup.
+    """
+
+    IGNORE_VALUE = object()
+
+    RELATIONSHIP_QUERY = """
+    SELECT DISTINCT ?p ?relation
+    WHERE {
+        ?o1 ?p ?o2 .
+        OPTIONAL {
+            ?p rdfs:label ?pLabel
+        }
+        BIND (
+          COALESCE(
+            ?pLabel,
+            ?p
+          ) AS ?relation
+        )
+    }
+    """
+
+    OBJECTS_QUERY = """
+    SELECT DISTINCT ?o1 ?o1Label
+    WHERE {
+        {
+            SELECT ?o1 ?o1Label WHERE {
+                ?o1 ?p ?o2 .
+                ?o1 rdfs:label ?o1Label
+            }
+        }
+        UNION
+        {
+            SELECT ?o1 ?o1Label WHERE {
+                ?o1 a owl:Class .
+                OPTIONAL {
+                    ?o1 rdfs:label ?optionalLabel
+                }
+                BIND (
+                  COALESCE(
+                    ?optionalLabel,
+                    ?o1
+                  ) AS ?o1Label
+                )
+            }
+        }    
+    }
+    """
+
+    def __init__(self, graph: Graph, ignore: set[str] = None, guard: set[str] = None, rephrased: dict[str, str] = None):
+        """
+        :param graph: The ontology.
+        :param ignore: URIs to ignore.
+        :param guard: URIs to keep in the fragment even if they are in the ignore list.
+        :param rephrased: URIs to rephrase/rename
+        """
+        logger.info('Initializing vocabulary')
+        self.graph = graph
+        self.relationship_labels = self._get_ontology_relationship_labels()
+        self.object_labels = self._get_ontology_object_labels()
+        self._load_imports()
+        self.rephrased = rephrased or dict()
+        self._ignore_list = ignore or {}
+        self._guard_list = guard or {
+            'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
+            'http://www.w3.org/2000/01/rdf-schema#label',
+            'http://www.w3.org/2002/07/owl#onDatatype'
+        }
+
+    def should_ignore(self, uri: str | URIRef) -> bool:
+        """
+        Check if URI should be ignored or not.
+        """
+        if isinstance(uri, URIRef):
+            uri = uri.toPython()
+        return uri in self._ignore_list
+
+    def should_keep(self, uri: str | URIRef) -> bool:
+        """
+        Check if URI should be kept in the fragment or not.
+        """
+        if isinstance(uri, URIRef):
+            uri = uri.toPython()
+        return uri in self._guard_list
+
+    def get_relationship_label(self, val, default=None) -> str:
+        """
+        Get relationship label.
+        :param val: The URI to lookup with.
+        :param default: Default value if not found.
+        :return: string.
+        """
+        return self._util_lookup(self.relationship_labels, val) or default
+
+    def get_class_label(self, val, default=None) -> str:
+        """
+        Get class label.
+        :param val: The URI to lookup with.
+        :param default: Default value if not found.
+        :return: string.
+        """
+        return self._util_lookup(self.object_labels, val) or default
+
+    def _get_ontology_relationship_labels(self) -> dict[str, str]:
+        """
+        Returns a IRI (URI) to label dictionary.
+        """
+        results = self.graph.query(self.RELATIONSHIP_QUERY)
+        ontology_relations = {}
+
+        for result in tqdm(results, desc='Loading Ontology Relationship Labels'):
+            iri, label = result
+            iri_str = iri.toPython()
+            label_str = label.toPython()
+
+            # if the label is also the URI then try to parse it.
+            if label_str.startswith('http'):
+                label_str = self._from_uri_to_text(label_str)
+
+            label_str = re.sub('[^0-9a-zA-Z]+', ' ', label_str)
+
+            ontology_relations[iri_str] = label_str
+
+        return ontology_relations
+
+    def _get_ontology_object_labels(self) -> dict[str, str]:
+        """
+        Returns a IRI (URI) to label dictionary.
+        """
+        results = self.graph.query(self.OBJECTS_QUERY)
+        object_labels = {}
+        for result in tqdm(results, desc='Loading Ontology Object Labels'):
+            iri, label = result
+            iri_str = iri.toPython()
+            label_str = label.toPython()
+
+            if iri_str == label_str and not label_str.startswith('http'):
+                # skip BNodes
+                continue
+
+            # if the label is also the URI then try to parse it.
+            if label_str.startswith('http'):
+                label_str = self._from_uri_to_text(label_str)
+            # Convert label to lower case snake case and remove spaces.
+            label_str = self._camel_to_snake(label_str)
+            label_str = re.sub('[^0-9a-zA-Z]+', ' ', label_str)
+
+            object_labels[iri_str] = label_str
+
+        return object_labels
+
+    def _util_lookup(self, dictionary, val):
+        """
+        Helper function used to perform lookup in a given dictionary using a key.
+
+        If the value is found, it is returned.
+        If the value is in the ignore list, a `IGNORE_VALUE` object is returned.
+        If a rephrase configuration is set of the key, it is returned.
+        If not found, text extraction is attempt from the key URI.
+
+        :param dictionary: The dictionary to look in.
+        :param val: The key to lookup with.
+        :return: The value.
+        """
+        if isinstance(val, URIRef):
+            val = val.toPython()
+
+        if not isinstance(val, str):
+            return None
+
+        if val in self._ignore_list:
+            return self.__class__.IGNORE_VALUE
+
+        # try get from overrides first
+        if result := self.rephrased.get(val):
+            return result
+
+        result = dictionary.get(val)
+        if result:
+            return result
+
+        # result is none, try to verbalize from URI
+        return self._from_uri_to_text(val)
+
+    def _from_uri_to_text(self, uri: str):
+        """
+        Attempt to extract phrases from the URI by taking the suffix and converting it into snake-case.
+        """
+        if '#' in uri:
+            text = uri.split('#')[1]
+        else:
+            text = uri.split('/')[-1]
+
+        # convert underscore or camel case notation into regular text
+        text = self._camel_to_snake(text)
+        return text.replace('_', ' ')
+
+    @staticmethod
+    def _camel_to_snake(name):
+        """
+        Convert camelCase into snake_case.
+        e.g. MyPhrase -> my_phrase
+        """
+        name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+        return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
+    def _load_imports(self):
+        """
+        Loads concepts from imports. This is done by creating a new instance of Vocabulary, using the imported graph.
+        """
+        owl_imports = {o[0].toPython() for o in self.graph.query("SELECT DISTINCT ?o WHERE { ?s owl:imports ?o }")}
+        for owl_import in owl_imports:
+            logging.info(f'LOADING IMPORT: {owl_import}')
+            graph = Graph()
+            graph.parse(owl_import, format='xml')
+            sub_vocab = self.__class__(graph)
+            self.object_labels.update(sub_vocab.object_labels)
+            self.relationship_labels.update(sub_vocab.relationship_labels)
