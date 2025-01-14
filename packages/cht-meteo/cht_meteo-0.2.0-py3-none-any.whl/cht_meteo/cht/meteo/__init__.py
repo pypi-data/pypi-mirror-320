01ab13769e75f3691f169230e336dcf9116d285f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 03:04:48 +0000

    Add package cht-meteo/0.2.0/cht_meteo-0.2.0-py3-none-any.whl

---
+++ packages/cht-meteo/cht_meteo-0.2.0-py3-none-any.whl/cht_meteo/cht/meteo/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+This is the old meteo toolkit that was originally in CoastalHazardsToolkit
+Created on Sun Apr 25 10:58:08 2021
+
+@author: ormondt
+"""
+from .database import MeteoDatabase
