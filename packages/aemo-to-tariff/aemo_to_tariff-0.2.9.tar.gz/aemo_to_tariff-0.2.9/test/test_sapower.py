8f774a8fb796613f3f6ecbfb5052cc0b5084eb4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:40 +0000

    Add package aemo-to-tariff/0.2.9/aemo_to_tariff-0.2.9.tar.gz

---
+++ packages/aemo-to-tariff/aemo_to_tariff-0.2.9.tar.gz/aemo_to_tariff-0.2.9/test/test_sapower.py
@@ -0,0 +1,8 @@
+import unittest
+from zoneinfo import ZoneInfo
+from aemo_to_tariff.sapower import *
+
+class TestSAPower(unittest.TestCase):
+    def test_some_sapower_functionality(self):
+        # Add test cases for SAPower module here
+        pass
