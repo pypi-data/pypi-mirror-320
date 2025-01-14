9d583ae2d32da79dbb43f2c541c224e9ee4e48b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:41 +0000

    Add package neurostats-API/0.0.15/neurostats_API-0.0.15-py3-none-any.whl

---
+++ packages/neurostats-API/neurostats_API-0.0.15-py3-none-any.whl/neurostats_API/__init__.py
@@ -0,0 +1,13 @@
+__version__='0.0.15'
+
+from .fetchers import (
+    BalanceSheetFetcher,
+    CashFlowFetcher,
+    FinanceOverviewFetcher,
+    FinanceReportFetcher,
+    InstitutionFetcher,
+    MarginTradingFetcher,
+    MonthRevenueFetcher,
+    TechFetcher,
+    ProfitLoseFetcher
+)
