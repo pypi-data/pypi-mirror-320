ee61dd1f17791ef5a9f7887208320e14f65694df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 04:05:02 +0000

    Add package bytesloop-requests/2.32.4/bytesloop_requests-2.32.4-py3-none-any.whl

---
+++ packages/bytesloop-requests/bytesloop_requests-2.32.4-py3-none-any.whl/requests/__version__.py
@@ -0,0 +1,14 @@
+# .-. .-. .-. . . .-. .-. .-. .-.
+# |(  |-  |.| | | |-  `-.  |  `-.
+# ' ' `-' `-`.`-' `-' `-'  '  `-'
+
+__title__ = "bytesloop-requests"
+__description__ = "Python HTTP for Humans."
+__url__ = "https://requests.readthedocs.io"
+__version__ = "2.32.4"
+__build__ = 0x023203
+__author__ = "bytesloop"
+__author_email__ = "iwanclian@gmail.com"
+__license__ = "Apache-2.0"
+__copyright__ = "Copyright Kenneth Reitz"
+__cake__ = "\u2728 \U0001f370 \u2728"
