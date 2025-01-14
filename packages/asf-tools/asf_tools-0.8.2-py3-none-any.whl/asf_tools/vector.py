d86800be9c9b66faf0a9a7d9b4b6d1bc292e8b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 06:04:32 +0000

    Add package asf-tools/0.8.2/asf_tools-0.8.2-py3-none-any.whl

---
+++ packages/asf-tools/asf_tools-0.8.2-py3-none-any.whl/asf_tools/vector.py
@@ -0,0 +1,28 @@
+from collections.abc import Iterator
+from pathlib import Path
+
+from osgeo import ogr
+
+
+ogr.UseExceptions()
+
+
+def get_features(vector_path: str | Path) -> list[ogr.Feature]:
+    ds = ogr.Open(str(vector_path))
+    layer = ds.GetLayer()
+    return [feature for feature in layer]
+
+
+def get_property_values_for_intersecting_features(geometry: ogr.Geometry, features: Iterator) -> bool:
+    for feature in features:
+        if feature.GetGeometryRef().Intersects(geometry):
+            return True
+    return False
+
+
+def intersecting_feature_properties(geometry: ogr.Geometry, features: Iterator, feature_property: str) -> list[str]:
+    property_values = []
+    for feature in features:
+        if feature.GetGeometryRef().Intersects(geometry):
+            property_values.append(feature.GetField(feature_property))
+    return property_values
