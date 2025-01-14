e82508ff61c1bd4cbfbccf065eb68ca8b70a48d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-14 15:06:26 +0000

    Add package sincpro-framework/2.1.1/sincpro_framework-2.1.1-py3-none-any.whl

---
+++ packages/sincpro-framework/sincpro_framework-2.1.1-py3-none-any.whl/sincpro_framework/sincpro_abstractions.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from typing import Type
+
+from pydantic import BaseModel
+
+
+class DataTransferObject(BaseModel):
+    """
+    Abstraction that represent a object that will travel through to any layer
+    """
+
+    model_config = {"arbitrary_types_allowed": True}
+
+
+class Bus(ABC):
+    @abstractmethod
+    def execute(
+        self, dto: DataTransferObject | Type[DataTransferObject]
+    ) -> DataTransferObject | Type[DataTransferObject]:
+        """
+        :param dto:
+        :return:
+        """
+
+
+class Feature(ABC):
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def execute(
+        self, dto: DataTransferObject | Type[DataTransferObject]
+    ) -> DataTransferObject | Type[DataTransferObject]:
+        """
+        :param dto: Any command or event
+        :return:
+        """
+
+
+class ApplicationService(ABC):
+    feature_bus: Bus
+
+    def __init__(self, feature_bus: Bus, *args, **kwargs):
+        self.feature_bus = feature_bus
+
+    @abstractmethod
+    def execute(
+        self, dto: DataTransferObject | Type[DataTransferObject]
+    ) -> DataTransferObject | Type[DataTransferObject]:
+        """
+        Main difference between Feature and ApplicationService can execute features internally
+        :param dto: Any command or event
+        :return: Any response
+        """
