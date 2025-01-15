f0944ca8441a452c8d18f1c6caa20bf9463cb8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 05:06:17 +0000

    Add package pz-rail-yaw/1.0.0/pz_rail_yaw-1.0.0-py3-none-any.whl

---
+++ packages/pz-rail-yaw/pz_rail_yaw-1.0.0-py3-none-any.whl/rail/pipelines/estimation/plot_output.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#
+# This scripts plots the redshift estimate produced by the example pipeline.
+# Automatically run by run_pipeline.sh
+#
+
+# pylint: skip-file
+import pickle
+import os
+
+with open(os.path.join("data", "output_summarize.pkl"), "rb") as f:
+    ncc = pickle.load(f)
+    ncc.to_files(os.path.join("data", "output"))
+ax = ncc.plot(indicate_zero=True)
+ax.set_xlabel("Redshift")
+ax.figure.savefig(os.path.join("data", "checkplot.png"))
