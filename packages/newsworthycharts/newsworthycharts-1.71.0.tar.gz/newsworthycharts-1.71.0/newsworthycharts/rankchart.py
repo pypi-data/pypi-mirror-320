f1d247623f36faca930decb4f4331b8d9aa8c064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 04:05:15 +0000

    Add package newsworthycharts/1.71.0/newsworthycharts-1.71.0.tar.gz

---
+++ packages/newsworthycharts/newsworthycharts-1.71.0.tar.gz/newsworthycharts-1.71.0/newsworthycharts/rankchart.py
@@ -0,0 +1,35 @@
+"""
+A chart showing ranking over time (like ”most popular baby names”)
+"""
+from .serialchart import SerialChart
+
+
+class BumpChart(SerialChart):
+    """Plot a rank chart
+
+        Data should be a list of iterables of (rank, date string) tuples, eg:
+    `[ [("2010-01-01", 2), ("2011-01-01", 3)] ]`, combined with a list of
+    labels in the same order
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(BumpChart, self).__init__(*args, **kwargs)
+        self.line_width = 0.5
+        self.label_placement = 'line'
+        self.type = "line"
+        self.decimals = 0
+        self.revert_value_axis = True
+        self.ymin = 1
+        self.allow_broken_y_axis = False
+        self.grid = False
+        self.line_marker = "o-"
+
+    def _get_line_colors(self, i, *args):
+        if not self.data:
+            # Don't waste time
+            return None
+        if self.highlight and self.highlight in self.labels and i == self.labels.index(self.highlight):
+            return self._nwc_style["strong_color"]
+        elif self.colors and i < len(self.colors):
+            return self.colors[i]
+        return self._nwc_style["neutral_color"]
