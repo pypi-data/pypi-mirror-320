3caff997a90c567ace5b5ee0c971aeadc0e36ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 08:06:48 +0000

    Add package topk-sdk/0.0.1/topk_sdk-0.0.1.tar.gz

---
+++ packages/topk-sdk/topk_sdk-0.0.1.tar.gz/topk_sdk-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="topk-sdk",
+    version="0.0.1",
+    author="topk",
+    author_email="admin@topk.io",
+    description="topk",
+    long_description="topk",
+    long_description_content_type="text/plain",
+    url="https://topk.io",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=["topk_sdk"],
+    python_requires=">=3.6",
+)
