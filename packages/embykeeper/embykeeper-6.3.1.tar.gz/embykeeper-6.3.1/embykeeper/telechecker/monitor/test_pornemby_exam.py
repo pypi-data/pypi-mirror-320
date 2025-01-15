17976f310e878b7e40b4df2ce904ee2fc740b9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 04:05:24 +0000

    Add package embykeeper/6.3.1/embykeeper-6.3.1.tar.gz

---
+++ packages/embykeeper/embykeeper-6.3.1.tar.gz/embykeeper-6.3.1/embykeeper/telechecker/monitor/test_pornemby_exam.py
@@ -0,0 +1,10 @@
+from .pornemby_answer import _PornembyAnswerAnswerMonitor
+
+__ignore__ = True
+
+
+class TestPornembyExamMonitor(_PornembyAnswerAnswerMonitor):
+    name = "Pornemby 科举 测试"
+    chat_name = "api_group"
+    chat_user = "embykeeper_test_bot"
+    chat_allow_outgoing = True
