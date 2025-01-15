864122630fd737bf1cd4685a88ad2620ed370cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 07:05:30 +0000

    Add package unstructured-ingest/0.3.13/unstructured_ingest-0.3.13-py3-none-any.whl

---
+++ packages/unstructured-ingest/unstructured_ingest-0.3.13-py3-none-any.whl/test/integration/connectors/utils/constants.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+SOURCE_TAG = "source"
+DESTINATION_TAG = "destination"
+
+env_setup_path = Path(__file__).parents[1] / "env_setup"
+expected_results_path = Path(__file__).parents[1] / "expected_results"
