7d37fdbe9013b01ad28bf91bdea3ef17989f2d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 03:06:32 +0000

    Add package dreamsboard/0.3.1/dreamsboard-0.3.1-py3-none-any.whl

---
+++ packages/dreamsboard/dreamsboard-0.3.1-py3-none-any.whl/dreamsboard/dreams/task_step_md/prompts.py
@@ -0,0 +1,13 @@
+
+TASK_MD_TEMPLATE = """
+# {start_task_context} 
+
+
+{context_placeholder}
+ 
+"""
+
+
+TASK_STEP_MD_TEMPLATE = """
+{task_step_name} [{task_step_level}]({task_step_id}), {task_step_question_answer}
+"""
