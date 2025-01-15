f35c716e2ddf5105976071594dc350e20a3ada45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 04:05:14 +0000

    Add package smarthome-meterportal/1.0.2/smarthome_meterportal-1.0.2-py3-none-any.whl

---
+++ packages/smarthome-meterportal/smarthome_meterportal-1.0.2-py3-none-any.whl/smarthome_meterportal/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from smarthome_meterportal.api.home_assistant_api import HomeAssistantApi
+from smarthome_meterportal.api.homey_api import HomeyApi
+
