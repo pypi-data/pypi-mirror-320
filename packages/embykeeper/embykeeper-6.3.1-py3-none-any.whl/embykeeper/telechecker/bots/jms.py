acc24b7ec61fa2722328542ce7db1f6386dd03f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 04:05:22 +0000

    Add package embykeeper/6.3.1/embykeeper-6.3.1-py3-none-any.whl

---
+++ packages/embykeeper/embykeeper-6.3.1-py3-none-any.whl/embykeeper/telechecker/bots/jms.py
@@ -0,0 +1,47 @@
+import aiohttp
+from aiohttp_socks import ProxyConnector, ProxyType
+from pyrogram.types import Message
+from pyrogram.raw.functions.messages import AcceptUrlAuth
+from pyrogram.raw.types import UrlAuthResultAccepted
+from faker import Faker
+
+from ._base import BotCheckin
+
+__ignore__ = True
+
+
+class JMSCheckin(BotCheckin):
+    name = "卷毛鼠"
+    bot_username = "jmsembybot"
+    bot_checked_keywords = "请明天再来签到"
+
+    async def message_handler(self, client, message: Message):
+        if message.reply_markup:
+            keys = [k for r in message.reply_markup.inline_keyboard for k in r]
+            for k in keys:
+                if "点我签到" in k.text:
+                    r: UrlAuthResultAccepted = await self.client.invoke(
+                        AcceptUrlAuth(
+                            peer=await self.client.resolve_peer(message.chat.id),
+                            msg_id=message.id,
+                            button_id=k.login_url.button_id,
+                            url=k.login_url.url,
+                        )
+                    )
+                    url = r.url
+                    if self.proxy:
+                        connector = ProxyConnector(
+                            proxy_type=ProxyType[self.proxy["scheme"].upper()],
+                            host=self.proxy["hostname"],
+                            port=self.proxy["port"],
+                            username=self.proxy.get("username", None),
+                            password=self.proxy.get("password", None),
+                        )
+                    else:
+                        connector = aiohttp.TCPConnector()
+                    for _ in range(1, 3):
+                        async with aiohttp.ClientSession(connector=connector) as session:
+                            async with session.get(url, headers={"User-Agent": Faker().safari()}) as resp:
+                                if resp.status == 200:
+                                    return
+        return await super().message_handler(client, message)
---
+++ packages/embykeeper/embykeeper-6.3.1-py3-none-any.whl/embykeeper/telechecker/bots/tdck.py
@@ -0,0 +1,8 @@
+from ._templ_a import TemplateACheckin
+
+__ignore__ = True
+
+
+class TdckCheckin(TemplateACheckin):
+    name = "起点站"
+    bot_username = "StartTdckBot"
