3696c2b8d58a77eda9d210bd4b2ce0e4f1c18596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 03:06:33 +0000

    Add package aspose-words/25.1.0/aspose_words-25.1.0-py3-none-manylinux1_x86_64.whl

---
+++ packages/aspose-words/aspose_words-25.1.0-py3-none-manylinux1_x86_64.whl/aspose/words/lowcode/reporting/__init__.pyi
@@ -0,0 +1,52 @@
+﻿import aspose.words
+import aspose.pydrawing
+import datetime
+import decimal
+import io
+import uuid
+from typing import Iterable, List
+
+class ReportBuilderOptions:
+    """Represents options for the LINQ Reporting Engine functionality."""
+    
+    def __init__(self):
+        ...
+    
+    @property
+    def options(self) -> aspose.words.reporting.ReportBuildOptions:
+        """Gets or sets a set of flags controlling behavior of this :class:`aspose.words.reporting.ReportingEngine` instance
+        while building a report."""
+        ...
+    
+    @options.setter
+    def options(self, value: aspose.words.reporting.ReportBuildOptions):
+        ...
+    
+    @property
+    def missing_member_message(self) -> str:
+        """Gets or sets a string value printed instead of a template expression that represents a plain reference to
+        a missing member of an object. The default value is an empty string.
+        
+        The property should be used in conjunction with the :attr:`aspose.words.reporting.ReportBuildOptions.ALLOW_MISSING_MEMBERS`
+        option. Otherwise, an exception is thrown when a missing member of an object is encountered.
+        
+        The property affects only printing of a template expression representing a plain reference to a missing
+        object member. For example, printing of a binary operator, one of which operands references a missing
+        object member, is not affected.
+        
+        The value of this property cannot be set to null."""
+        ...
+    
+    @missing_member_message.setter
+    def missing_member_message(self, value: str):
+        ...
+    
+    @property
+    def known_types(self) -> aspose.words.reporting.KnownTypeSet:
+        """Gets an unordered set (i.e. a collection of unique items) containing  objects
+        which fully or partially qualified names can be used within report templates processed by this engine
+        instance to invoke the corresponding types' static members, perform type casts, etc."""
+        ...
+    
+    ...
+
---
+++ packages/aspose-words/aspose_words-25.1.0-py3-none-manylinux1_x86_64.whl/aspose/words/lowcode/splitting/__init__.pyi
@@ -0,0 +1,48 @@
+﻿import aspose.words
+import aspose.pydrawing
+import datetime
+import decimal
+import io
+import uuid
+from typing import Iterable, List
+from enum import Enum
+
+class SplitOptions:
+    """Specifies options how the document is split into parts."""
+    
+    def __init__(self):
+        ...
+    
+    @property
+    def split_criteria(self) -> aspose.words.lowcode.splitting.SplitCriteria:
+        """Specifies the criteria for splitting the document into parts."""
+        ...
+    
+    @split_criteria.setter
+    def split_criteria(self, value: aspose.words.lowcode.splitting.SplitCriteria):
+        ...
+    
+    @property
+    def split_style(self) -> str:
+        """Specifies the paragraph style for splitting the document into parts when :attr:`SplitCriteria.STYLE` is used."""
+        ...
+    
+    @split_style.setter
+    def split_style(self, value: str):
+        ...
+    
+    ...
+
+class SplitCriteria(Enum):
+    """Specifies how the document is split into parts."""
+    
+    """Specifies that the document is split into pages."""
+    PAGE: int
+    
+    """Specifies that the document is split into parts at a section break of any type."""
+    SECTION_BREAK: int
+    
+    """Specifies that the document is split into parts at a paragraph formatted using the style specified in :attr:`SplitOptions.split_style`."""
+    STYLE: int
+    
+
