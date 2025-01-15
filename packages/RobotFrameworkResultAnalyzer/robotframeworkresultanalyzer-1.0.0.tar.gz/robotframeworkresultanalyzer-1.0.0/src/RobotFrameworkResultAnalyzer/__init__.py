2aec44ca935d0f7a351f77dc251990fd3f78b769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 06:05:32 +0000

    Add package RobotFrameworkResultAnalyzer/1.0.0/robotframeworkresultanalyzer-1.0.0.tar.gz

---
+++ packages/RobotFrameworkResultAnalyzer/robotframeworkresultanalyzer-1.0.0.tar.gz/robotframeworkresultanalyzer-1.0.0/src/RobotFrameworkResultAnalyzer/__init__.py
@@ -0,0 +1,9 @@
+# pylint: disable=invalid-name
+"""
+RobotFrameworkResultAnalyzer module.
+This module provides the RobotFrameworkResultAnalyzer class, which is imported from the listener module.
+The class is included in the __all__ list to specify the public interface of the module.
+"""
+from .listener import RobotFrameworkResultAnalyzer
+
+__all__ = ["RobotFrameworkResultAnalyzer"]
