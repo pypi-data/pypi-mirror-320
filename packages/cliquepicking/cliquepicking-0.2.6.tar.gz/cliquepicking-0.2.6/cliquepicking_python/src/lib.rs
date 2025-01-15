b39cf4f2646b07ee48b08860ef22690f47a28e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 07:05:52 +0000

    Add package cliquepicking/0.2.6/cliquepicking-0.2.6.tar.gz

---
+++ packages/cliquepicking/cliquepicking-0.2.6.tar.gz/cliquepicking-0.2.6/cliquepicking_python/src/lib.rs
@@ -0,0 +1,80 @@
+use pyo3::prelude::*;
+
+use cliquepicking_rs::count::count_cpdag;
+use cliquepicking_rs::enumerate::list_cpdag;
+use cliquepicking_rs::enumerate::list_cpdag_orders;
+use cliquepicking_rs::partially_directed_graph::PartiallyDirectedGraph;
+use cliquepicking_rs::sample::sample_cpdag;
+use cliquepicking_rs::sample::sample_cpdag_orders;
+
+use num_bigint::BigUint;
+
+/// A Python module for counting and sampling Markov equivalent DAGs.
+#[pymodule]
+fn cliquepicking(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(mec_size, m)?)?;
+    m.add_function(wrap_pyfunction!(mec_sample_dags, m)?)?;
+    m.add_function(wrap_pyfunction!(mec_sample_orders, m)?)?;
+    m.add_function(wrap_pyfunction!(mec_list_dags, m)?)?;
+    m.add_function(wrap_pyfunction!(mec_list_orders, m)?)?;
+    Ok(())
+}
+
+/// Compute the number of DAGs in the Markov equivalence represented by CPDAG cpdag.
+#[pyfunction]
+fn mec_size(cpdag: Vec<(usize, usize)>) -> PyResult<BigUint> {
+    let mx = max_element(&cpdag);
+    let g = PartiallyDirectedGraph::from_edge_list(cpdag, mx + 1);
+    let res = count_cpdag(&g);
+    Ok(res)
+}
+
+/// Sample k DAGs uniformly from the Markov equivalence class represented by CPDAG cpdag.
+#[pyfunction]
+fn mec_sample_dags(cpdag: Vec<(usize, usize)>, k: usize) -> PyResult<Vec<Vec<(usize, usize)>>> {
+    let mx = max_element(&cpdag);
+    let g = PartiallyDirectedGraph::from_edge_list(cpdag, mx + 1);
+    let samples = sample_cpdag(&g, k)
+        .into_iter()
+        .map(|sample| sample.to_edge_list())
+        .collect();
+    Ok(samples)
+}
+
+/// Sample k DAGs (represented by a topological order) uniformly from the Markov equivalence class represented by CPDAG cpdag.
+#[pyfunction]
+fn mec_sample_orders(cpdag: Vec<(usize, usize)>, k: usize) -> PyResult<Vec<Vec<usize>>> {
+    let mx = max_element(&cpdag);
+    let g = PartiallyDirectedGraph::from_edge_list(cpdag, mx + 1);
+    Ok(sample_cpdag_orders(&g, k))
+}
+
+/// List all DAGs from the Markov equivalence class represented by CPDAG cpdag.
+#[pyfunction]
+fn mec_list_dags(cpdag: Vec<(usize, usize)>) -> PyResult<Vec<Vec<(usize, usize)>>> {
+    let mx = max_element(&cpdag);
+    let g = PartiallyDirectedGraph::from_edge_list(cpdag, mx + 1);
+    let samples = list_cpdag(&g)
+        .into_iter()
+        .map(|sample| sample.to_edge_list())
+        .collect();
+    Ok(samples)
+}
+
+/// List all DAGs (represented by a topological orderfrom the Markov equivalence class represented by CPDAG cpdag.
+#[pyfunction]
+fn mec_list_orders(cpdag: Vec<(usize, usize)>) -> PyResult<Vec<Vec<usize>>> {
+    let mx = max_element(&cpdag);
+    let g = PartiallyDirectedGraph::from_edge_list(cpdag, mx + 1);
+    Ok(list_cpdag_orders(&g))
+}
+
+// small helper
+fn max_element(tuple_list: &[(usize, usize)]) -> usize {
+    let mut mx = 0;
+    for &(u, v) in tuple_list.iter() {
+        mx = std::cmp::max(u, mx);
+        mx = std::cmp::max(v, mx);
+    }
+    mx
+}
