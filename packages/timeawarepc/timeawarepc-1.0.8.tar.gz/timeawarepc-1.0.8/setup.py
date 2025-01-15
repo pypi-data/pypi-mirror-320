2d97acfc8bcdc6d1473d8db3af07920e092ebba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 06:05:32 +0000

    Add package timeawarepc/1.0.8/timeawarepc-1.0.8.tar.gz

---
+++ packages/timeawarepc/timeawarepc-1.0.8.tar.gz/timeawarepc-1.0.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md','r') as fh:
+    README = fh.read()
+
+VERSION = "1.0.8"
+
+setuptools.setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name = 'timeawarepc',
+    version = VERSION,
+    author = '',
+    description = 'Time-Aware PC Python Package',
+    long_description= README,
+    long_description_content_type = 'text/markdown',
+    install_requires=['numpy','pandas','rpy2','networkx','scipy'],
+    url='https://github.com/shlizee/TimeAwarePC',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
