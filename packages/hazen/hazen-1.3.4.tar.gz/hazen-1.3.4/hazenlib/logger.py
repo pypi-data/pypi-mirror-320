158474c4120f1117492566962df00f39f0dc86c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 04:05:10 +0000

    Add package hazen/1.3.4/hazen-1.3.4.tar.gz

---
+++ packages/hazen/hazen-1.3.4.tar.gz/hazen-1.3.4/hazenlib/logger.py
@@ -0,0 +1,35 @@
+import sys
+import logging
+import colorlog
+
+
+def configure_logger():
+    """Configure logger for the standard out (command line) stream and save logs to file"""
+    # make log formatters
+    stream_formatter = colorlog.ColoredFormatter(
+        "%(log_color)s%(asctime)-15s %(levelname).1s "
+        "[%(filename)s:%(funcName)s:%(lineno)d] %(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
+
+    file_formatter = logging.Formatter(
+        "%(asctime)-15s %(levelname).1s [%(filename)s:%(funcName)s:%(lineno)d]"
+        " %(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
+
+    log_file = "Hazen_logger.log"
+    stream_handler = logging.StreamHandler(sys.stdout)
+    file_handler = logging.FileHandler(log_file)
+
+    # set formatters
+    stream_handler.setFormatter(stream_formatter)
+    file_handler.setFormatter(file_formatter)
+
+    # add handlers
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+
+
+logger = logging.getLogger(__name__)
+configure_logger()
