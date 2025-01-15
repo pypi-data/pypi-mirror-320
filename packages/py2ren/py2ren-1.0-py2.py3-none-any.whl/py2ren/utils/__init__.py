0bf72ca93c02a15d651edae86be2214fe9e256a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 06:05:23 +0000

    Add package py2ren/1.0/py2ren-1.0-py2.py3-none-any.whl

---
+++ packages/py2ren/py2ren-1.0-py2.py3-none-any.whl/py2ren/utils/__init__.py
@@ -0,0 +1,27 @@
+from . import filepath, iter
+
+
+def rowchars(text, char, start=0):
+    count = 0
+    for ch in text[start:]:
+        if ch == char:
+            count += 1
+        elif count != 0:
+            break
+    return -1 if count == 0 else count
+
+
+def strip_indexes(source, indexes):
+    removed = 0
+    for index in indexes:
+        del source[index - removed]
+        removed += 1
+
+def trymap(func, value, default=None, allow=None):
+    if allow is None:
+        allow = Exception
+
+    try:
+        return func(value)
+    except allow:
+        return default
