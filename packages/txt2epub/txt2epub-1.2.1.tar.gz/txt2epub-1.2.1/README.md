0386f22c22c210ea63d7ab2bfce91f2ef67c303e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 05:05:39 +0000

    Add package txt2epub/1.2.1/txt2epub-1.2.1.tar.gz

---
+++ packages/txt2epub/txt2epub-1.2.1.tar.gz/txt2epub-1.2.1/README.md
@@ -0,0 +1,55 @@
+# TXT2EPUB
+
+[中文说明](README.zh.md)
+
+A simple tool for converting TXT books into ePub.
+
+![image](https://github.com/k4yt3x/txt2epub/assets/21986859/b342f068-28ff-4789-a261-1f82830f76a5)
+
+## Installation
+
+You'll first need to have Python3 and Pip installed. If you're using Windows, then the default Python installer will come with Pip. If you're using Linux, you may need to install an extra package like `python3-pip`. The exact package name depends on your distro.
+
+Then, execute the following command to install TXT2EPUB.
+
+```shell
+pip install txt2epub
+```
+
+## Usage
+
+You may convert a file from the command line:
+
+```shell
+txt2epub convert -i <input> -o <output> -t <title> -a <author> -l <language>
+```
+
+...or using the GUI:
+
+```shell
+txt2epub gui
+```
+
+## Chapter Detection
+
+This program detects the book chapters and chapter titles following the standard TXT book format:
+
+- Chapters are separated by three new lines (i.e., `\n\n\n`)
+- The first line in a new chapter is the chapter's title.
+
+For example, in the text below, there are two chapters with titles "Chapter 1" and "Chapter 2."
+
+```txt
+Chapter 1
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at sapien ante.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+
+
+Chapter 2
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at sapien ante.
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae.
+```
---
+++ packages/txt2epub/txt2epub-1.2.1.tar.gz/txt2epub-1.2.1/txt2epub/txt2epub_gui.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pathlib
+import traceback
+
+import langdetect
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QDragEnterEvent, QDropEvent, QKeySequence, QShortcut
+from PyQt6.QtWidgets import (
+    QApplication,
+    QFileDialog,
+    QFormLayout,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QMessageBox,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
+from .txt2epub import Txt2Epub
+
+
+class Txt2EpubGUI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.title = "TXT2EPUB"
+        self.initUI()
+        self.file_path = None
+
+        # Enable dragging and dropping onto the GUI
+        self.setAcceptDrops(True)
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(100, 100, 400, 250)
+
+        QShortcut(QKeySequence("Ctrl+Q"), self).activated.connect(
+            QApplication.instance().quit
+        )
+
+        layout = QVBoxLayout()
+
+        form_layout = QFormLayout()
+
+        self.title_input = QLineEdit(self)
+        self.title_input.setPlaceholderText("Enter book title")
+        form_layout.addRow(QLabel("Title:", self), self.title_input)
+
+        self.language_input = QLineEdit(self)
+        self.language_input.setPlaceholderText("Enter book language")
+        form_layout.addRow(QLabel("Language:", self), self.language_input)
+
+        self.author_input = QLineEdit(self)
+        self.author_input.setPlaceholderText("Enter author name")
+        form_layout.addRow(QLabel("Author:", self), self.author_input)
+
+        layout.addLayout(form_layout)
+
+        self.label = QLabel(
+            "Drop a file here or select a file using the button below", self
+        )
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.label)
+
+        select_file_button = QPushButton("Select File", self)
+        select_file_button.setToolTip("Select a text file to convert")
+        select_file_button.clicked.connect(self.select_file)
+        layout.addWidget(select_file_button)
+
+        generate_epub_button = QPushButton("Generate ePub", self)
+        generate_epub_button.setToolTip("Generate ePub from the selected text file")
+        generate_epub_button.clicked.connect(self.generate_epub)
+        layout.addWidget(generate_epub_button)
+
+        central_widget = QWidget(self)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def select_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "Select a text file", "", "Text Files (*.txt);;All Files (*)"
+        )
+        self.on_select(file_path)
+
+    def on_select(self, file: str):
+        file_path = pathlib.Path(file)
+        if file is not None and file_path.is_file():
+            self.file_path = pathlib.Path(file)
+            self.title_input.setText(self.file_path.stem)
+            with self.file_path.open("r", encoding="utf-8") as txt_file:
+                text = txt_file.read()
+                try:
+                    self.language_input.setText(langdetect.detect(text))
+                except langdetect.lang_detect_exception.LangDetectException:
+                    self.language_input.setText("en")
+            self.label.setText(f"Selected file: {self.file_path.name}")
+
+    def generate_epub(self):
+        if self.file_path:
+            if self.file_path.with_suffix(".epub").is_file() is True:
+                reply = QMessageBox.question(
+                    self,
+                    "Overwrite?",
+                    "The output file already exists. Overwrite?",
+                    QMessageBox.StandardButton.Yes,
+                    QMessageBox.StandardButton.No,
+                )
+
+                if reply == QMessageBox.StandardButton.No:
+                    return
+
+            try:
+                Txt2Epub.create_epub(
+                    input_file=pathlib.Path(self.file_path),
+                    book_title=self.title_input.text() or "Default Title",
+                    book_author=self.author_input.text() or "Unknown Author",
+                    book_language=self.language_input.text() or "en",
+                )
+                self.label.setText(f"ePub generated for: {self.file_path.name}")
+                QMessageBox.information(
+                    self,
+                    "Success",
+                    f"ePub generated at: {self.file_path.with_suffix('.epub')}",
+                )
+
+            except Exception as error:
+                traceback.print_exc()
+                QMessageBox.critical(
+                    self,
+                    "Error",
+                    f"Error generating ePub: {error}",
+                )
+        else:
+            self.label.setText(
+                "Drop a file here or select a file using the button below"
+            )
+
+    # New functions to handle drag and drop
+    def dragEnterEvent(self, event: QDragEnterEvent):
+        if event.mimeData().hasUrls():
+            event.acceptProposedAction()
+
+    def dropEvent(self, event: QDropEvent):
+        url = event.mimeData().urls()[0].toLocalFile()
+        self.on_select(url)
