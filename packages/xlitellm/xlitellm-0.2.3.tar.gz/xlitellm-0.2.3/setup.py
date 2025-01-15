4fd4a7933d9a065ad4427347ce75b4466174081c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 05:05:51 +0000

    Add package xlitellm/0.2.3/xlitellm-0.2.3.tar.gz

---
+++ packages/xlitellm/xlitellm-0.2.3.tar.gz/xlitellm-0.2.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="xlitellm",
+    version="0.2.3",
+    description="A client library for making requests to various LLMs through a unified interface",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Sam Meng",
+    packages=find_packages(),
+    install_requires=[
+        "litellm==1.58.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
