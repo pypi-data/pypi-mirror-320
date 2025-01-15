6515f6a3db8204da218f52141f94b70725f3219c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 03:06:31 +0000

    Add package aspose-words/25.1.0/aspose_words-25.1.0-py3-none-macosx_11_0_arm64.whl

---
+++ packages/aspose-words/aspose_words-25.1.0-py3-none-macosx_11_0_arm64.whl/aspose/words/xattr/__init__.pyi
@@ -0,0 +1,287 @@
+﻿import aspose.words
+import aspose.pydrawing
+import datetime
+import decimal
+import io
+import uuid
+from typing import Iterable, List
+from enum import Enum
+
+class DCD:
+    
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    ...
+
+class GCD:
+    
+    @overload
+    def __init__(self):
+        ...
+    
+    @overload
+    def __init__(self, d: aspose.words.xattr.xCL):
+        ...
+    
+    @overload
+    def __init__(self, d: aspose.words.xattr.xCL, v: str, c: str):
+        ...
+    
+    def n(self) -> aspose.words.xattr.xCL:
+        ...
+    
+    ...
+
+class N5:
+    
+    @overload
+    def __init__(self):
+        ...
+    
+    @overload
+    def __init__(self, d: str):
+        ...
+    
+    ...
+
+class O5:
+    
+    @overload
+    def __init__(self):
+        ...
+    
+    @overload
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    def v(self, d: str) -> None:
+        ...
+    
+    ...
+
+class QCD:
+    
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    ...
+
+class SCD:
+    
+    def __init__(self):
+        ...
+    
+    ...
+
+class b5:
+    
+    @overload
+    def __init__(self, d: str):
+        ...
+    
+    @overload
+    def __init__(self, d: str, v: str):
+        ...
+    
+    @overload
+    def __init__(self, d: str, v: str, c: str):
+        ...
+    
+    def v(self) -> List[str]:
+        ...
+    
+    def c(self, d: List[str]) -> None:
+        ...
+    
+    ...
+
+class e5:
+    
+    @overload
+    def __init__(self):
+        ...
+    
+    @overload
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    def v(self, d: str) -> None:
+        ...
+    
+    ...
+
+class fCD:
+    
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    ...
+
+class g5:
+    
+    @overload
+    def __init__(self, d: bool):
+        ...
+    
+    @overload
+    def __init__(self, d: str):
+        ...
+    
+    @property
+    def D(self) -> bool:
+        ...
+    
+    @property
+    def V(self) -> str:
+        ...
+    
+    ...
+
+class hCD:
+    
+    def __init__(self, d: str, v: bool, c: bool):
+        ...
+    
+    def t(self) -> str:
+        ...
+    
+    ...
+
+class iCD:
+    
+    def __init__(self):
+        ...
+    
+    ...
+
+class kCD:
+    
+    def __init__(self):
+        ...
+    
+    ...
+
+class m5:
+    
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    def v(self, d: str) -> None:
+        ...
+    
+    ...
+
+class n5:
+    
+    @overload
+    def __init__(self):
+        ...
+    
+    @overload
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    def v(self, d: str) -> None:
+        ...
+    
+    ...
+
+class oCD:
+    
+    def __init__(self):
+        ...
+    
+    ...
+
+class pCL:
+    
+    def __init__(self, d: bool):
+        ...
+    
+    def c(self) -> bool:
+        ...
+    
+    ...
+
+class sCD:
+    
+    def __init__(self):
+        ...
+    
+    ...
+
+class t5:
+    
+    def __init__(self):
+        ...
+    
+    ...
+
+class u5:
+    
+    @overload
+    def __init__(self):
+        ...
+    
+    @overload
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    def v(self, d: str) -> None:
+        ...
+    
+    ...
+
+class wCL:
+    
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    ...
+
+class yCL:
+    
+    @overload
+    def __init__(self):
+        ...
+    
+    @overload
+    def __init__(self, d: str):
+        ...
+    
+    def c(self) -> str:
+        ...
+    
+    ...
+
+class xCL(Enum):
+    
+
