bd749263a8e79ebb1bd6afa8995311e6bd78fc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 04:05:48 +0000

    Add package pycaption/2.2.16/pycaption-2.2.16-py3-none-any.whl

---
+++ packages/pycaption/pycaption-2.2.16-py3-none-any.whl/pycaption/transcript.py
@@ -0,0 +1,35 @@
+import os
+
+try:
+    import nltk.data
+except ModuleNotFoundError:
+    nltk = None
+
+from pycaption.base import BaseWriter, CaptionNode
+
+
+class TranscriptWriter(BaseWriter):
+    def __init__(self, *args, **kw):
+        if not nltk:
+            raise ModuleNotFoundError('Missing Dependency You must install nltk ')
+        self.nltk = nltk.data.load(f'file:{os.path.dirname(__file__)}'
+                                   '/english.pickle')
+
+    def write(self, captions):
+        transcripts = []
+
+        for lang in captions.get_languages():
+            lang_transcript = ''
+
+            for caption in captions.get_captions(lang):
+                lang_transcript = self._strip_text(caption.nodes,
+                                                   lang_transcript)
+
+            lang_transcript = '\n'.join(self.nltk.tokenize(lang_transcript))
+            transcripts.append(lang_transcript)
+
+        return '\n'.join(transcripts)
+
+    def _strip_text(self, elements, lang_transcript):
+        return ' '.join([lang_transcript] + [el.content for el in elements if
+                                             el.type_ == CaptionNode.TEXT])
