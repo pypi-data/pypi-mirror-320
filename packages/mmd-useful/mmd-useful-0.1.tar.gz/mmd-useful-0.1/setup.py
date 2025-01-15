e303c9a76c1b3a79ad1a064738466d6e8e4ab1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 06:05:41 +0000

    Add package mmd-useful/0.1/mmd-useful-0.1.tar.gz

---
+++ packages/mmd-useful/mmd-useful-0.1.tar.gz/mmd-useful-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1"  # PEP-440
+
+NAME = "mmd-useful"
+
+INSTALL_REQUIRES = []
+
+setup(
+    name=NAME,
+    version=VERSION,
+    packages=find_packages(),
+    INSTALL_REQUIRES=[]
+)
