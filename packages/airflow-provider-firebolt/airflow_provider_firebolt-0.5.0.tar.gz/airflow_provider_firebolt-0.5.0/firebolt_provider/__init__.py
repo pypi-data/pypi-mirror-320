266673b0ff4964a0009e127bcd6b1432a456b910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 03:06:02 +0000

    Add package airflow-provider-firebolt/0.5.0/airflow_provider_firebolt-0.5.0.tar.gz

---
+++ packages/airflow-provider-firebolt/airflow_provider_firebolt-0.5.0.tar.gz/airflow_provider_firebolt-0.5.0/firebolt_provider/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "0.5.0"
+
+from typing import Any, Dict
+
+
+def get_provider_info() -> Dict[str, Any]:
+    return {
+        "package-name": "airflow-provider-firebolt",
+        "name": "Firebolt Provider",
+        "description": "A Firebolt provider for Apache Airflow.",
+        "hook-class-names": ["firebolt_provider.hooks.firebolt.FireboltHook"],
+        "extra-links": ["firebolt_provider.operators.firebolt.RegistryLink"],
+    }
