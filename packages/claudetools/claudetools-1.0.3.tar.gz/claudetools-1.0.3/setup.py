6859bcea48ef8f30ab5550b4193e30de4617602f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 04:05:29 +0000

    Add package claudetools/1.0.3/claudetools-1.0.3.tar.gz

---
+++ packages/claudetools/claudetools-1.0.3.tar.gz/claudetools-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="claudetools",
+    version="1.0.3",
+    author="Vatsal J. Saglani",
+    author_email="saglanivatsal@gmail.com",
+    description=
+    "Claudetools is a Python library that enables function calling with the Claude 3 family of language models from Anthropic.",
+    long_description=open("Readme.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/vatsalsaglani/claudetools",
+    packages=find_packages(),
+    install_requires=["httpx>=0.25.0", "anthropic>=0.31.0", "pydantic>=2.4.2"],
+    python_requires=">=3.7")
