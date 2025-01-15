73da72655d37913e494dc6ce0019d91617f3231b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 07:05:31 +0000

    Add package python-can-coe/0.1.2/python_can_coe-0.1.2-py3-none-any.whl

---
+++ packages/python-can-coe/python_can_coe-0.1.2-py3-none-any.whl/coe/__init__.py
@@ -0,0 +1,11 @@
+from .coe import CoE, CoEType, CoEVersion
+from .message import Message
+
+__all__ = [
+    "CoE",
+    "CoEType",
+    "CoEVersion",
+    "Message"
+]
+
+from .__version__ import __version__
