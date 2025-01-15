45361b0575b43a0d3c669d3ead476a4e3e60a5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 05:06:07 +0000

    Add package gnt-monitoring/0.2.0/gnt_monitoring-0.2.0.tar.gz

---
+++ packages/gnt-monitoring/gnt_monitoring-0.2.0.tar.gz/gnt_monitoring-0.2.0/gnt_monitoring/sentry.py
@@ -0,0 +1,22 @@
+from logging import getLogger
+
+_logger = getLogger(__name__)
+
+
+class Sentry():
+    """
+    Sentry remote logging class
+    :param str dsn: Sentry dsn
+    :param str env: Sentry env, default: dev
+    :returns: None
+    """
+
+    def __init__(self, dsn: str, env: str = "dev") -> None:
+        """
+        """
+        try:
+            import sentry_sdk
+            _logger.debug("Initializing sentry")
+            sentry_sdk.init(dsn=dsn, environment=env)
+        except ImportError:
+            pass
