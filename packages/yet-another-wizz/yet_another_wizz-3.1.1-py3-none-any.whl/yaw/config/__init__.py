75f0c116ae0a35ab580df75131e0153cf54d0450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 05:06:34 +0000

    Add package yet-another-wizz/3.1.1/yet_another_wizz-3.1.1-py3-none-any.whl

---
+++ packages/yet-another-wizz/yet_another_wizz-3.1.1-py3-none-any.whl/yaw/config/__init__.py
@@ -0,0 +1,14 @@
+"""
+Implements the configuration class with all parameters needed for correlation
+function measurements.
+"""
+
+from yaw.config.base import Parameter
+from yaw.config.classes import BinningConfig, Configuration, ScalesConfig
+
+__all__ = [
+    "BinningConfig",
+    "Configuration",
+    "Parameter",
+    "ScalesConfig",
+]
---
+++ packages/yet-another-wizz/yet_another_wizz-3.1.1-py3-none-any.whl/yaw/examples/__init__.py
@@ -0,0 +1,49 @@
+"""
+This module provides some precomputed example data, which are loaded when
+importing the module.
+
+>>> from yaw import examples  # reads the data sets from disk
+>>> examples.w_sp
+CorrFunc(counts=dd|dr|rd|rr, auto=False, binning=30 bins @ (0.100...1.200], num_patches=64)
+"""
+
+from pathlib import Path
+
+from yaw.correlation.corrfunc import CorrFunc
+
+__all__ = [
+    "w_sp",
+    "w_ss",
+    "w_pp",
+    "normalised_counts",
+    "patched_count",
+    "patched_sum_weights",
+]
+
+
+_path = Path(__file__).parent
+
+w_sp = CorrFunc.from_file(_path / "cross.hdf")
+"""Example data from a crosscorrelation measurement
+(:obj:`~yaw.CorrFunc` instance)."""
+
+w_ss = CorrFunc.from_file(_path / "auto_reference.hdf")
+"""Example data from a reference sample autocorrelation measurement
+(:obj:`~yaw.CorrFunc` instance)."""
+
+w_pp = CorrFunc.from_file(_path / "auto_unknown.hdf")
+"""Example data from a unknown sample autocorrelation measurement
+(:obj:`~yaw.CorrFunc` instance)."""
+
+
+normalised_counts = w_sp.dd
+"""Example data for patch-wise, normalised pair counts
+(:obj:`~yaw.correlation.paircounts.NormalisedCounts` instance, from :obj:`w_sp.dd`)"""
+
+patched_count = normalised_counts.counts
+"""Example data for patch-wise pair counts
+(:obj:`~yaw.correlation.paircounts.PatchedCount` instance, from :obj:`w_sp.dd.count`)"""
+
+patched_sum_weights = normalised_counts.sum_weights
+"""Example data for patch-wise sum of object weights
+(:obj:`~yaw.correlation.paircounts.PatchedSumWeights` instance, from :obj:`w_sp.dd.sum_weights`)"""
