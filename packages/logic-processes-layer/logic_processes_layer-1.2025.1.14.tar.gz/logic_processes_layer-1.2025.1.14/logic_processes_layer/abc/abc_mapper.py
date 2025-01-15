75f2fbafa19a74f3ee6f4a00fdfed27dbe8d1ce3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 07:05:49 +0000

    Add package logic-processes-layer/1.2025.1.14/logic_processes_layer-1.2025.1.14.tar.gz

---
+++ packages/logic-processes-layer/logic_processes_layer-1.2025.1.14.tar.gz/logic_processes_layer-1.2025.1.14/logic_processes_layer/abc/abc_mapper.py
@@ -0,0 +1,18 @@
+import dataclasses
+import typing
+from abc import ABC, abstractmethod
+
+if typing.TYPE_CHECKING:
+    from logic_processes_layer.structures import AttrsData
+
+
+@dataclasses.dataclass
+class AbstractMapper(ABC):
+    start_attrs: "AttrsData"
+
+    def __call__(self, prev_results) -> "AttrsData":
+        return self.build_attrs_strategy(prev_results)
+
+    @abstractmethod
+    def build_attrs_strategy(self, prev_results) -> "AttrsData":
+        ...
