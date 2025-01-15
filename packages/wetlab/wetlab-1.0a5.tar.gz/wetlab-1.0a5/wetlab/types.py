2eeb65a135ad06247fa9fd9acb75b2b9f14e2f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 04:05:20 +0000

    Add package wetlab/1.0a5/wetlab-1.0a5.tar.gz

---
+++ packages/wetlab/wetlab-1.0a5.tar.gz/wetlab-1.0a5/wetlab/types.py
@@ -0,0 +1,24 @@
+from typing import Literal
+
+GeneticPerturbationSystem = Literal[
+    "CRISPR-Cas9",
+    "CRISPRi",
+    "CRISPRa",
+    "shRNA",
+    "siRNA",
+    "transgene",
+    "transient-transfection",
+]
+
+BiologicType = Literal[
+    "protein",
+    "peptide",
+    "antibody",
+    "enzyme",
+    "growth-factor",
+    "cytokine",
+    "hormone",
+    "vaccine",
+    "oligonucleotide",
+    "co-culture",
+]
