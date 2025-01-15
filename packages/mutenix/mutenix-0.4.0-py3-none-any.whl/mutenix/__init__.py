6f55a5dcfe45a6c83e568ba496fe06e1eabbdff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 08:06:57 +0000

    Add package mutenix/0.4.0/mutenix-0.4.0-py3-none-any.whl

---
+++ packages/mutenix/mutenix-0.4.0-py3-none-any.whl/mutenix/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from mutenix.__main__ import runmain
+
+__all__ = ["runmain"]
---
+++ packages/mutenix/mutenix-0.4.0-py3-none-any.whl/mutenix/package.py
@@ -0,0 +1,6 @@
+# This file is for packaging with pyInstaller
+from __future__ import annotations
+
+from mutenix.__main__ import runmain
+
+runmain()
