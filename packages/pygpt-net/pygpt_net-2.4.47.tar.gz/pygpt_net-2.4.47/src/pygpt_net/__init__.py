8cc9d69660276cb2473af951ba531c35429215fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-01-15 05:06:20 +0000

    Add package pygpt-net/2.4.47/pygpt_net-2.4.47.tar.gz

---
+++ packages/pygpt-net/pygpt_net-2.4.47.tar.gz/pygpt_net-2.4.47/src/pygpt_net/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2025.01.13 21:00:00                  #
+# ================================================== #
+
+__author__ = "Marcin Szczygliński"
+__copyright__ = "Copyright 2025, Marcin Szczygliński"
+__credits__ = ["Marcin Szczygliński"]
+__license__ = "MIT"
+__version__ = "2.4.47"
+__build__ = "2025.01.14"
+__maintainer__ = "Marcin Szczygliński"
+__github__ = "https://github.com/szczyglis-dev/py-gpt"
+__website__ = "https://pygpt.net"
+__pypi__ = "https://pypi.org/project/pygpt-net"
+__snap__ = "https://snapcraft.io/pygpt"
+__donate__ = "https://pygpt.net/#donate"
+__documentation__ = "https://pygpt.readthedocs.io"
+__discord__ = "https://pygpt.net/discord"
+__email__ = "info@pygpt.net"
